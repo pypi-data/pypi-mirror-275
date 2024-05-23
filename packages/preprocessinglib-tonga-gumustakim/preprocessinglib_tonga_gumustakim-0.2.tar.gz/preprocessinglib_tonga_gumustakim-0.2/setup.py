2affe3b5cf337b8c1b4c328a25f8e83428ba9a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 14:03:29 +0000

    Add package preprocessinglib-tonga-gumustakim/0.2/preprocessinglib_tonga_gumustakim-0.2.tar.gz

---
+++ packages/preprocessinglib-tonga-gumustakim/preprocessinglib_tonga_gumustakim-0.2.tar.gz/preprocessinglib_tonga_gumustakim-0.2/setup.py
@@ -0,0 +1,27 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='preprocessinglib_tonga_gumustakim',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'scikit-learn',
+        'nltk'
+    ],
+    author='Zehra Tonga-Ayse Serra Gumustakim',
+    author_email='tongafatmazehra@gmail.com-ayseserra.gumustakim@stu.fsm.edu.tr ',
+    description='A comprehensive data preprocessing library for Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Fzehzeh/mypreprocessinglib , https://github.com/ayserragm/mypreprocessinglib',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
