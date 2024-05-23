5f17f3bec0bd1b00156c021d463776338c21c9b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 15:02:45 +0000

    Add package testwizard.commands-powerswitch/3.8.0/testwizard.commands-powerswitch-3.8.0.tar.gz

---
+++ packages/testwizard.commands-powerswitch/testwizard.commands-powerswitch-3.8.0.tar.gz/testwizard.commands-powerswitch-3.8.0/setup.py
@@ -0,0 +1,80 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testwizard.commands-powerswitch",
+    version="3.8.0",
+    author="Resillion - Belgium",
+    author_email="testwizard-support@resillion.com",
+    description="Testwizard Powerswitch commands",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.resillion.com/testwizard/",
+    packages=['testwizard.commands_powerswitch'],
+    install_requires=[
+        'testwizard.commands-core==3.8.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.3",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
