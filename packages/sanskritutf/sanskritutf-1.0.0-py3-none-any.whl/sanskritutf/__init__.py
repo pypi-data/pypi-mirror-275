bdc49fe07876801984deaa2297eafe1a9cece8e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 15:02:48 +0000

    Add package sanskritutf/1.0.0/sanskritutf-1.0.0-py3-none-any.whl

---
+++ packages/sanskritutf/sanskritutf-1.0.0-py3-none-any.whl/sanskritutf/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# sanskritutf/__init__.py
+
+__all__ = ['SansUtfDict', 'Transliterate', 'UtfListDecoder']
+
+from sanskritutf.sanskritutf import SansUtfDict, Transliterate, UtfListDecoder
