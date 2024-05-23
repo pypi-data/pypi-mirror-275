f4b78fcf31b4c912271aa35f0d38140b68c54c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 14:02:45 +0000

    Add package genoodww/0.1.0/genoodww-0.1.0.tar.gz

---
+++ packages/genoodww/genoodww-0.1.0.tar.gz/genoodww-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='genoodww',
+version='0.1.0',
+author='Fred',
+author_email='your.email@example.com',
+description='A short description of your package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
