b8793800552d01cdbf2758d7cd9f59138bb74aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 14:02:53 +0000

    Add package lalsuite/7.22.1.dev20240523/lalsuite-7.22.1.dev20240523-cp312-cp312-macosx_11_0_x86_64.whl

---
+++ packages/lalsuite/lalsuite-7.22.1.dev20240523-cp312-cp312-macosx_11_0_x86_64.whl/lalapps/git_version.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+# git_version.py - vcs information module
+#
+# Copyright (C) 2010 Nickolas Fotopoulos
+# Copyright (C) 2012-2013 Adam Mercer
+# Copyright (C) 2016 Leo Singer
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with with program; see the file COPYING. If not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301 USA
+
+id = "c9a8ef690dbc1fed401ca724e085c9bf283ac079"
+date = "2024-05-21 01:43:10 +0000"
+branch = "None"
+tag = "None"
+if tag == "None":
+    tag = None
+author = "Karl Wette <karl.wette@ligo.org>"
+builder = "Raffi Enficiaud <raffi.enficiaud@ligo.org>"
+committer = "Karl Wette <karl.wette@ligo.org>"
+status = "CLEAN: All modifications committed"
+version = id
+verbose_msg = """Branch: None
+Tag: None
+Id: c9a8ef690dbc1fed401ca724e085c9bf283ac079
+
+Builder: Raffi Enficiaud <raffi.enficiaud@ligo.org>
+Repository status: CLEAN: All modifications committed"""
+
+import warnings
+
+class VersionMismatchError(ValueError):
+    pass
+
+def check_match(foreign_id, onmismatch="raise"):
+    """
+    If foreign_id != id, perform an action specified by the onmismatch
+    kwarg. This can be useful for validating input files.
+
+    onmismatch actions:
+      "raise": raise a VersionMismatchError, stating both versions involved
+      "warn": emit a warning, stating both versions involved
+    """
+    if onmismatch not in ("raise", "warn"):
+        raise ValueError(onmismatch + " is an unrecognized value of onmismatch")
+    if foreign_id == "c9a8ef690dbc1fed401ca724e085c9bf283ac079":
+        return
+    msg = "Program id (c9a8ef690dbc1fed401ca724e085c9bf283ac079) does not match given id (%s)." % foreign_id
+    if onmismatch == "raise":
+        raise VersionMismatchError(msg)
+
+    # in the backtrace, show calling code
+    warnings.warn(msg, UserWarning)
