bd6cabd778bbb24e56c31e7ac20eaec998181f79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 14:03:08 +0000

    Add package ragtime/0.0.37/ragtime-0.0.37-py3-none-any.whl

---
+++ packages/ragtime/ragtime-0.0.37-py3-none-any.whl/ragtime/text_generators/__init__.py
@@ -0,0 +1,5 @@
+from ragtime.base.text_generator import *
+
+from ragtime.text_generators.answer_generator import AnsGenerator
+from ragtime.text_generators.eval_generator import EvalGenerator
+from ragtime.text_generators.fact_generator import FactGenerator
