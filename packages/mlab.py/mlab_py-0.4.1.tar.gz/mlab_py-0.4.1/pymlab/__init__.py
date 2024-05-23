379880d36f0c8b9eb5c700a2079057e58b781ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 14:03:28 +0000

    Add package mlab.py/0.4.1/mlab_py-0.4.1.tar.gz

---
+++ packages/mlab.py/mlab_py-0.4.1.tar.gz/mlab_py-0.4.1/pymlab/__init__.py
@@ -0,0 +1,15 @@
+from .m_train import (
+    train,
+    TrainResults,
+)
+from .m_test import (
+    test,
+    TestResults,
+)
+
+__all__ = [
+    "train",
+    "TrainResults",
+    "test",
+    "TestResults",
+]
