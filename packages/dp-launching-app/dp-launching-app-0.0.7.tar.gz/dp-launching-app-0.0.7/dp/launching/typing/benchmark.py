845b4c6a4b8e93805095c078a017b3eb1e14ce33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 14:03:23 +0000

    Add package dp-launching-app/0.0.7/dp-launching-app-0.0.7.tar.gz

---
+++ packages/dp-launching-app/dp-launching-app-0.0.7.tar.gz/dp-launching-app-0.0.7/dp/launching/typing/benchmark.py
@@ -0,0 +1,27 @@
+from typing import Any, Dict, List
+
+__all__ = ["BenchmarkTags"]
+
+
+class BenchmarkTags(list):
+    def get_value(self) -> List:
+        if hasattr(self, "value"):
+            return self.value
+        return []
+
+    @classmethod
+    def __modify_schema__(cls, field_schema: Dict[str, Any]) -> None:
+        field_schema.update(format="benchmark_tags", scope="executor")
+
+    @classmethod
+    def __get_validators__(cls) -> Any:  # type: ignore
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, value: Any) -> "BenchmarkTags":
+        if isinstance(value, list):
+            f = BenchmarkTags(value)
+            f.value = value
+            return f
+        else:
+            raise Exception(f"BenchmarkTags Wrong type: {type(value)}")
