479b45db2d9b5b8f517cb790332e600b11e9de9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 14:03:08 +0000

    Add package ragtime/0.0.37/ragtime-0.0.37.tar.gz

---
+++ packages/ragtime/ragtime-0.0.37.tar.gz/ragtime-0.0.37/src/ragtime/prompters/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from ragtime.base.prompter import *
+
+from ragtime.prompters.answer.base_fr import ( PptrAnsBase )
+from ragtime.prompters.eval.base_fr import ( PptrEvalFR )
+from ragtime.prompters.fact.base_fr import ( PptrFactsFR )
+from ragtime.prompters.answer.with_retriever import ( PptrAnsWithRetrieverFR )
+
+table:dict = {
+    "PptrAnsBase": (lambda : PptrAnsBase()),
+    "PptrFactsFR": (lambda: PptrFactsFR()),
+    "PptrEvalFR": (lambda: PptrEvalFR()),
+    "PptrAnsWithRetrieverFR": (lambda: PptrAnsWithRetrieverFR()),
+}
