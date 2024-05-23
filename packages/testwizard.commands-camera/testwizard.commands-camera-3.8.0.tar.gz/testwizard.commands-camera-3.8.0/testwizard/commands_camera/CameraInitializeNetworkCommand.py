36127e7850f6c1c5587b505a9d17441e866079ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 15:02:45 +0000

    Add package testwizard.commands-camera/3.8.0/testwizard.commands-camera-3.8.0.tar.gz

---
+++ packages/testwizard.commands-camera/testwizard.commands-camera-3.8.0.tar.gz/testwizard.commands-camera-3.8.0/testwizard/commands_camera/CameraInitializeNetworkCommand.py
@@ -0,0 +1,17 @@
+import sys
+import json
+
+from testwizard.commands_core import CommandBase
+from testwizard.commands_core import OkErrorCodeAndMessageResult
+
+
+class CameraInitializeNetworkCommand(CommandBase):
+    def __init__(self, testObject):
+        CommandBase.__init__(self, testObject, "Camera.InitializeNetwork")
+
+    def execute(self):
+        requestObj = []
+
+        result = self.executeCommand(requestObj)
+
+        return OkErrorCodeAndMessageResult(result, "Camera.InitializeNetwork was successful", "Camera.InitializeNetwork failed")
