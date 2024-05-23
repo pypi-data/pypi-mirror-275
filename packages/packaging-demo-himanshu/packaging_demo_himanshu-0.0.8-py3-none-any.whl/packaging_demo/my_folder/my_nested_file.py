dc50a37edacb4bfbf2e4fa966d33c761a34927b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 14:03:14 +0000

    Add package packaging-demo-himanshu/0.0.8/packaging_demo_himanshu-0.0.8-py3-none-any.whl

---
+++ packages/packaging-demo-himanshu/packaging_demo_himanshu-0.0.8-py3-none-any.whl/packaging_demo/my_folder/my_nested_file.py
@@ -0,0 +1,11 @@
+"""Module to demonstrate packaging."""
+
+import sys
+
+sys.path.insert(0, "/home/himansys/repos")
+
+from packaging_demo.my_other_file import CONSTANT as CONSTANT2
+
+CONSTANT = "Hello!"
+
+print(CONSTANT2)
