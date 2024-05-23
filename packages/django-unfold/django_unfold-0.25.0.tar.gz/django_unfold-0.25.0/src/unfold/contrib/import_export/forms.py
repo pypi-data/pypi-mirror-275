6836e2d8f1c9435f78dd81e4a5ff67d9ef09ce78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 15:02:43 +0000

    Add package django-unfold/0.25.0/django_unfold-0.25.0.tar.gz

---
+++ packages/django-unfold/django_unfold-0.25.0.tar.gz/django_unfold-0.25.0/src/unfold/contrib/import_export/forms.py
@@ -0,0 +1,21 @@
+from import_export.forms import ExportForm as BaseExportForm
+from import_export.forms import ImportForm as BaseImportForm
+from unfold.widgets import (
+    SELECT_CLASSES,
+    UnfoldAdminFileFieldWidget,
+)
+
+
+class ImportForm(BaseImportForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["resource"].widget.attrs["class"] = " ".join(SELECT_CLASSES)
+        self.fields["import_file"].widget = UnfoldAdminFileFieldWidget()
+        self.fields["format"].widget.attrs["class"] = " ".join(SELECT_CLASSES)
+
+
+class ExportForm(BaseExportForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["format"].widget.attrs["class"] = " ".join(SELECT_CLASSES)
