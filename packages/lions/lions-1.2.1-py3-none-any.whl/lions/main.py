e7a7a579b90f429b01a5d03f1eb7f572ffe751a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 15:02:48 +0000

    Add package lions/1.2.1/lions-1.2.1-py3-none-any.whl

---
+++ packages/lions/lions-1.2.1-py3-none-any.whl/lions/main.py
@@ -0,0 +1,91 @@
+"""
+Module Name: main.py
+Author: Diogo Ferreira (ItsNotSoftware)
+Date: May 8, 2024
+
+Description:
+    This module is the main entry point for the LIONS compiler. It is responsible for
+    parsing the command line arguments, reading the message files, and generating the
+    corresponding C++ message files.
+
+License:
+    Copyright (c) 2024 Diogo Ferreira. All rights reserved.
+    This code is licensed under the MIT License.
+"""
+
+import sys
+import colorama
+from lions.cpp_gen.cpp_generator import CppGenerator
+from lions.yaml_parser import YamlParser
+from colorama import Fore, Style
+from lions.js_gen.js_generator import JsGenerator
+from lions.errors import InvalidTargetLanguageError
+
+
+def print_title():
+    """Function to print title of the program in ASCII art"""
+
+    print(Fore.GREEN)
+
+    print(
+        """
+██╗     ██╗ ██████╗ ███╗   ██╗███████╗
+██║     ██║██╔═══██╗████╗  ██║██╔════╝
+██║     ██║██║   ██║██╔██╗ ██║███████╗
+██║     ██║██║   ██║██║╚██╗██║╚════██║
+███████╗██║╚██████╔╝██║ ╚████║███████║
+╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝
+"""
+    )
+    print(Style.RESET_ALL + "\nInitiating compilation of messages...\n")
+
+
+def print_success_message():
+    """Function to print success message after all messages are compiled"""
+
+    success_message = "All messages compiled successfully!"
+    print(Fore.GREEN + "+" + "-" * (len(success_message) + 2) + "+" + Style.RESET_ALL)
+    print(Fore.GREEN + f"| {success_message} |" + Style.RESET_ALL)
+    print(Fore.GREEN + "+" + "-" * (len(success_message) + 2) + "+" + Style.RESET_ALL)
+
+
+def main():
+    colorama.init(autoreset=True)
+
+    # Check if the number of arguments is correct
+    if len(sys.argv) != 4:
+        print("Usage: lions <msg_files_dir> <output_dir> <target_language>")
+
+        # targert language help
+        print(
+            """
+    Target Language:
+        cpp - C++
+        js - JavaScript
+        """
+        )
+
+        sys.exit(1)
+
+    msg_files_dir = sys.argv[1]
+    output_dir = sys.argv[2]
+
+    print_title()
+
+    # Initialize the parser and generator objects
+    parser = YamlParser(msg_files_dir)
+    code_generator = None
+
+    # Check the target language and initialize the corresponding code generator
+    if sys.argv[3] == "c++" or sys.argv[3] == "cpp":
+        code_generator = CppGenerator(output_dir)
+    elif sys.argv[3] == "js":
+        code_generator = JsGenerator(output_dir)
+    else:
+        raise InvalidTargetLanguageError(sys.argv[3])
+
+    # Parse the message files and generate the corresponding C++ files
+    for filename, msgs in parser.parse_file():
+        code_generator.generate_msg_files(filename, msgs)
+
+    print_success_message()
