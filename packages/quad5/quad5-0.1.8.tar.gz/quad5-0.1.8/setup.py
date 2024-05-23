c31d4a803b36366d5f45a04d299805d9c8989dfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 15:02:43 +0000

    Add package quad5/0.1.8/quad5-0.1.8.tar.gz

---
+++ packages/quad5/quad5-0.1.8.tar.gz/quad5-0.1.8/setup.py
@@ -0,0 +1,28 @@
+import codecs
+import os
+
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="quad5",
+    version="0.1.8",
+    description="""Quadratic Approximation custom step sampler for PYMC.""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Carsten Jørgensen",
+    packages=find_packages(include=["quad5"]),
+    keywords=["Bayesian Statistics", "Probabalistic Programming Language", "Python"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["PYMC==5.15.0"],
+    license="MIT",
+    url="https://github.com/carsten-j/quad5",
+)
