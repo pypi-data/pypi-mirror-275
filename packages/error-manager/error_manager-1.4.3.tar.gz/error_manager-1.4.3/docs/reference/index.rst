8cca6330a77be3adb8b655de269de23ae240773f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 15:02:44 +0000

    Add package error-manager/1.4.3/error_manager-1.4.3.tar.gz

---
+++ packages/error-manager/error_manager-1.4.3.tar.gz/error_manager-1.4.3/docs/reference/index.rst
@@ -0,0 +1,8 @@
+Reference
+=========
+
+.. toctree::
+    :glob:
+
+    listerrors*
+    base*
---
+++ packages/error-manager/error_manager-1.4.3.tar.gz/error_manager-1.4.3/src/errors/settings.py
@@ -0,0 +1,5 @@
+"""
+Module to define default settings, keys and messages for error-manager package.
+"""
+
+EXC_ERROR_NOT_OF_ERROR_CODE_TYPE = "Provided error not of type ErrorCode"
