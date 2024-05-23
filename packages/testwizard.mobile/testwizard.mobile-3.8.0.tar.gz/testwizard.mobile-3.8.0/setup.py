d91e462329812fd782f6818342667d6b139ad0cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 15:02:46 +0000

    Add package testwizard.mobile/3.8.0/testwizard.mobile-3.8.0.tar.gz

---
+++ packages/testwizard.mobile/testwizard.mobile-3.8.0.tar.gz/testwizard.mobile-3.8.0/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testwizard.mobile",
+    version="3.8.0",
+    author="Resillion - Belgium",
+    author_email="testwizard-support@resillion.com",
+    description="Testwizard for Mobile testobjects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.resillion.com/testwizard/",
+    packages=['testwizard.mobile'],
+    install_requires=[
+        'testwizard.test==3.8.0',
+        'testwizard.testobjects-core==3.8.0',
+        'testwizard.commands-audio==3.8.0',
+        'testwizard.commands-mobile==3.8.0',
+        'testwizard.commands-video==3.8.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.3",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
