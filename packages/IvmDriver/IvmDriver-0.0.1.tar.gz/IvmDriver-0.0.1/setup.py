0dbc98f6fdf073e20686346413f07cde0c59f4f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 15:02:43 +0000

    Add package IvmDriver/0.0.1/IvmDriver-0.0.1.tar.gz

---
+++ packages/IvmDriver/IvmDriver-0.0.1.tar.gz/IvmDriver-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('./README.md', 'r') as file :
+    long_description = file.read()
+AUTHOR_USER_NAME = 'HarishKumarSedu'
+AUTHOR_EMAIL = 'harishkumarsedu@gmail.com'
+REPO_NAME = 'matrix'
+setup(
+    name=f'IvmDriver',
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    version='0.0.1',
+    py_modules=['*'],
+    # description=[ 'text/markdown','text/x-rst'],
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    long_description_content_type=['text/plain', 'text/x-rst', 'text/markdown'],
+    long_description=long_description,
+    packages=find_packages(),
+    package_data={'': ['*.json','*.py']},
+    include_package_data=True,
+
+    
+    
+)
