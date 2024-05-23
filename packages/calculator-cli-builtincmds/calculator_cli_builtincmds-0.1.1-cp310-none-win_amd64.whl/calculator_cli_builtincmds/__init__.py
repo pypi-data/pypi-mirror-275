22760b61cf5d8fece3bd45980f76da4511b00215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 14:03:35 +0000

    Add package calculator-cli-builtincmds/0.1.1/calculator_cli_builtincmds-0.1.1-cp310-none-win_amd64.whl

---
+++ packages/calculator-cli-builtincmds/calculator_cli_builtincmds-0.1.1-cp310-none-win_amd64.whl/calculator_cli_builtincmds/__init__.py
@@ -0,0 +1,7 @@
+# ruff: noqa
+from .calculator_cli_builtincmds import *
+
+
+__doc__ = calculator_cli_builtincmds.__doc__
+if hasattr(calculator_cli_builtincmds, "__all__"):
+    __all__ = calculator_cli_builtincmds.__all__
