f51faba6b697d1836d2ccfbea8bbe6c6c3ba769b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 14:02:59 +0000

    Add package mteb/1.11.13/mteb-1.11.13.tar.gz

---
+++ packages/mteb/mteb-1.11.13.tar.gz/mteb-1.11.13/scripts/data/scala_classification/create_multiling_data.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from huggingface_hub import create_repo
+
+from datasets import load_dataset
+
+repo_id = "mteb/multilingual-scala-classification"
+create_repo(repo_id, repo_type="dataset")
+
+languages = {
+    "Danish": "da",
+    "Norwegian_b": "nb",
+    "Norwegian_n": "nn",
+    "Swedish": "sv",
+}
+
+for lang in languages.keys():
+    raw_ds = load_dataset(f"mteb/scala_{languages[lang]}_classification")
+    raw_ds.push_to_hub(repo_id=repo_id, config_name=lang)
