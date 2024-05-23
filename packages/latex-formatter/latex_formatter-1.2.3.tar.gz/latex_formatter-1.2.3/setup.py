7e13bd6cd01724744c8b243f1b9384d99a3742f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 14:02:51 +0000

    Add package latex-formatter/1.2.3/latex_formatter-1.2.3.tar.gz

---
+++ packages/latex-formatter/latex_formatter-1.2.3.tar.gz/latex_formatter-1.2.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="latex_formatter",
+    version="1.2.3",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "ujson >= 5.8.0",
+        # 你的依赖包列表，例如：
+        # "matplotlib >= 2.2.0"
+    ],
+)
