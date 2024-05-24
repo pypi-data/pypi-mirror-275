4c9e6f20a9ef19c7ae15f86b0fd93d4d451fe372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 15:03:47 +0000

    Add package haskellian/0.3.24/haskellian-0.3.24.tar.gz

---
+++ packages/haskellian/haskellian-0.3.24.tar.gz/haskellian-0.3.24/src/haskellian/thunk/thunk.py
@@ -0,0 +1,38 @@
+from haskellian import DEBUG_IMPORTS
+if DEBUG_IMPORTS:
+  print('Import:', __name__)
+from dataclasses import dataclass
+from typing_extensions import TypeVar, Callable, Generic
+from haskellian import Monad
+
+A = TypeVar('A')
+B = TypeVar('B')
+
+@dataclass
+class Thunk(Monad[A], Generic[A]):
+
+  supplier: Callable[[], A]
+
+  def fmap(self, f: Callable[[A], B]) -> 'Thunk[B]':
+    return Thunk(lambda: f(self.supplier()))
+  
+  def bind(self, f: Callable[[A], 'Thunk[B]']) -> 'Thunk[B]':
+    def _supplier():
+      value = self()
+      thunk = f(value)
+      value2 = thunk()
+      return value2
+    return Thunk(_supplier)
+  
+  def ap(self, f: 'Thunk[Callable[[A], B]]') -> 'Thunk[B]':
+    return super().ap(f) # type: ignore
+
+  def __or__(self, f: Callable[[A], B]) -> 'Thunk[B]':
+    return self.fmap(f)
+  
+  def __call__(self) -> A:
+    return self.supplier()
+  
+  @classmethod
+  def of(cls, value: A) -> 'Thunk[A]':
+    return cls(lambda: value)
