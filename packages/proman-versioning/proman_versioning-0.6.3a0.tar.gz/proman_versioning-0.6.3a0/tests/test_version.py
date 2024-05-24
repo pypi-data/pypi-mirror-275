09634c5e587390a93f15a477cc322f321712365d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 14:03:17 +0000

    Add package proman-versioning/0.6.3a0/proman_versioning-0.6.3a0.tar.gz

---
+++ packages/proman-versioning/proman_versioning-0.6.3a0.tar.gz/proman_versioning-0.6.3a0/tests/test_version.py
@@ -0,0 +1,7 @@
+"""Provide tests for project version."""
+from versioning import __version__
+
+
+def test_version() -> None:
+    """Test project metadata version."""
+    assert __version__ == '0.6.3a0'
