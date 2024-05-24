50251aaec9a13aa9755a215b456925bb962338f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 04:03:09 +0000

    Add package better-hydroponics/0.1.2/better_hydroponics-0.1.2.tar.gz

---
+++ packages/better-hydroponics/better_hydroponics-0.1.2.tar.gz/better_hydroponics-0.1.2/README.md
@@ -0,0 +1,139 @@
+![Project Logo](assets/Banner.webp)
+
+![Coverage Status](assets/coverage-badge.svg)
+
+<h1 align="center">
+Project Hydroponic simulation
+</h1>
+
+<br>
+
+This python package aims to provide some basic tools to simulate hydroponic plant growing.🌱 Hydroponics is a farming method where plants grow without soil and instead use a solution containing the required mineral nutriments.👨‍🌾 This technique is already widely used because it is more efficient in water usage and is in development for farming on mars.🪐🚀
+
+In particular, this package allows you to simulate the concentration of minerals and the pH of hydroponic solutions, determine the pH and check the solubility among others.
+See the "Basic Usage" section below and "example.ipynb" document for further examples and explainantions of the package.
+
+Also see the Githubwebpages associated with this project to see the full documentation: [hydroponics](https://nilstheeten.github.io/Project_ppchem/index.html).
+
+Learn more about hydropics with the following links: 
+ - [Wikipedia](https://en.wikipedia.org/wiki/Hydroponics#:~:text=Hydroponics%20is%20a%20type%20of,solutions%20in%20an%20artificial%20environment)
+ - [USDA](https://www.nal.usda.gov/farms-and-agricultural-production-systems/hydroponics)
+ - [NASA](https://www.nasa.gov/science-research/nasa-plant-researchers-explore-question-of-deep-space-food-crops/)
+
+<br>
+
+## 0. Usage 🔥
+
+```python
+import hydroponics as hp
+
+plant_name = "Tomato"
+required_nutriments = {"Na+": 0.1, "NO3(-)": 0.5, "K+": 0.2} #minerals needed by the plant
+solution = {"Na+": 0.2, "NO3(-)": 0.6, "K+": 0.3} #composition of the solution {"ion_name":conc. [g/L]}
+analysed_ions = ["Na+", "NO3(-)", "K+"] #ions for which the conc. will be analysed
+volume = 2 #L
+growth_time = 20 #days
+forbidden_ions = ["Cl-"] #ions you don't want in the solution
+
+# Generate an awsome simulation report (PDF)
+hp.generate_report(plant_name, required_nutriments, growth_time, analysed_ions, solution, volume, forbidden_ions = forbidden_ions)
+
+```
+
+This usage example shows how to quickly use the package's main functionality: `generate_report` that will provide a lot of essential informations of the simulation. 📝
+
+<br>
+
+## 1. Installation 👩‍💻
+
+ > Everything is just one pip install away - philippe schwaller
+<br>
+
+The following *pip command* can be used to install the python package. The package was made with python 3.10
+```
+pip install better-hydroponics==0.1.2
+```
+  
+Be aware, the package uses the following dependencies:
+ * Numpy (1.26.4)
+ * Pandas (2.2.2)
+ * matplotlib (3.8.4)
+ * sympy (1.12)
+ * openpyxl (3.1.2)
+
+Please install the dependencies with their according `pip install` commands in the right environment.
+
+<br>
+
+## 2. Basic Usage  
+The package uses python dictionaries to store the concentration of the different salts/ion in the solution. By default concentrations are given in [g/L]. However different functions exist to convert g/L to mol/L and inversly.
+
+Start by importing the package:
+
+```python
+import hydroponics as hp
+```
+
+To determine the quantity of each salt to add to the solution to obain the desired concentration of each ion one can use the `make_solution` function. This function checks the solubility of the salt. 🧑‍🔬
+
+```python
+ion_composition = {"K+":0.1, "Cl-": 0.3, "H2PO4(-)":0.5, "Ca(2+)":0.4} #desired concentrations in [g/L]
+volume = 10 #L
+forbidden_ions = ["Li+", "SO4(2-)"]
+
+salts_to_add = hp.make_solution(ion_composition, volume, forbidden_ions)
+print(salts_to_add)
+
+```
+
+One can visualise the evolution of the concentration of the ions as the plant grows with the `plot_graph` function.
+
+```python
+#Define the variables
+solution = {"Na+": 0.2, "NO3(-)": 0.6, "K+": 0.3} #composition of the solution {"ion_name":conc. [g/L]}
+required_nutriments = {"Na+": 0.1, "NO3(-)": 0.5, "K+": 0.2} #minerals needed by the plant
+growth_time = 20 #days
+volume = 5 #L
+
+#plot a figure that shows the concentration of ions as a function of time.
+#The figure is directly downloaded in the current directory.
+hp.plot_graph(solution, "ion", required_nutriments, growth_time, volume)
+
+```
+
+One can determine the pH of the solution given the concentration  of the ions with the `pH_approximation` function:
+
+```python
+solution = {"K+":0.1, "Cl-": 0.3, "H2PO4(-)":0.5, "Ca(2+)":0.4}
+temperature = 25 #°C
+pH = hp.pH_approximation(solution, temperature)
+print(pH)
+```
+<br>
+
+See the "tutorial.ipynb" notebook for more complete examples.
+
+Or visit [hydroponics](https://nilstheeten.github.io/Project_ppchem/index.html) to see the full documentation of the package.
+
+<br>
+
+## 3. Datasets  
+
+This package uses data from the PRIF17 ([link](https://www.agroscope.admin.ch/agroscope/en/home/topics/plant-production/field-crops/Pflanzenernaehrung/grud.html)) and the wikipedia page about solubility ([wiki](https://en.wikipedia.org/wiki/Solubility_table)).
+
+Accuracy of the results of the package are not guaranteed.
+
+Some solutions and plant compositions are predefined (bell pepper 🫑, cucumber 🥒 and eggplant 🍆), please define a new dict or complete the excel file to use other solutions and new plants.
+
+
+<br>
+
+## 4. Licence and References  
+This package is under MIT license.
+
+Please click on the MIT sign for further information
+
+
+
+
+
---
+++ packages/better-hydroponics/better_hydroponics-0.1.2.tar.gz/better_hydroponics-0.1.2/src/hydroponics/Basic_functions.py
@@ -0,0 +1,515 @@
+"""
+This file contains the most basic functions used in the project.
+In particular, the file contains functions to:
+- Import user data from an Excel file
+- Calculate the molar mass of a salt
+- Get the solubility product constant (Ksp) and solubility product (Q) of a salt
+- Convert salt concentrations to ion concentrations
+- Make a solution given the ion concentration wanted
+"""
+
+#Imports
+import re
+import os
+import csv
+import pandas as pd
+from sympy import symbols, Eq, solve, solveset, S, Interval 
+
+
+# ----- Import User Data from Excel file -------------
+
+def load_from_data(file_name):
+    # Get the directory of the current file
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    # Construct the absolute path to the CSV file
+    path = os.path.join(current_dir, '\\data', file_name)
+    return path
+
+def import_solution_data(solution_name: str) -> dict:
+    """
+    Import user data from the Excel file "UserData.xlsx" and returns it as a dictionary.
+
+    Args:
+        solution_name (str): Name of the column in the Excel file.
+
+    Returns:
+        dict: Dictionary containing the concentration of the ions [g/L] in the solution.
+    """
+    # Read the Excel file
+    file_name = load_from_data("UserData.xlsx")
+    df = pd.read_excel(file_name, sheet_name="My solution", skiprows=1)
+    # Create a dictionary using zip and dictionary comprehension
+    try:
+        ion_solution_dict = dict(zip(df['Ions'], df[solution_name]))
+    except KeyError:
+        raise ValueError(f"Solution {solution_name} not found in the Excel file. Please check the name of the column.")
+    return ion_solution_dict
+
+def predefined_solutions(plant:str, concentration:str="g" ) -> dict:
+    """
+    Import user data from the Excel file "UserData.xlsx" and returns it as a dictionary.
+
+    Args:
+        plant (str): Name of the plant. Can either be 'Eggplant', 'Tomato' or 'Cucumber'.
+        concentration (str): Unit of concentration. Can either be 'g' or 'mol'.
+
+    Returns:
+        dict: Dictionary containing the concentration of the ions [g or mol/L] in the solution.
+    """
+    if concentration not in ['g','mol']:
+        raise ValueError("Concentration must be 'g' or 'mol'.")
+    available_plants = ['Eggplant', 'Tomato', 'Cucumber']
+    if plant not in available_plants:
+        raise ValueError(f"Plant {plant} not found. Available plants are {available_plants}.")
+    else:
+        # Read the Excel file
+        file_name = load_from_data("UserData.xlsx")
+        df = pd.read_excel(file_name, sheet_name="Optimal Solution", skiprows=0)
+        # Create a dictionary using zip and dictionary comprehension
+        ion_solution_dict = dict(zip(df['Formula'], df[str('c ['+concentration+'/L] '+plant)]))
+        return ion_solution_dict
+
+def import_plant_data(plant_name:str) -> dict:
+    """
+    Import user data from sheet "My Plant" from the Excel file "UserData.xlsx" and returns it as a dictionary.
+
+    Args:
+        plant (str): name of the column where the data is located
+        
+    Returns:
+        dict: dictionary containing the needs of the plant for each ion [g] for a full growth cycle
+    """
+    # Read the Excel file
+    file_name = load_from_data("UserData.xlsx")
+    df = pd.read_excel(file_name, sheet_name="My plant", skiprows=1)
+    # Create a dictionary using zip and dictionary comprehension
+    plant_dict = dict(zip(df['Ions'], df[plant_name]))
+    return plant_dict
+
+
+
+# ----- Molar mass calculator ------------------------
+
+def get_atom_mass(atom_name: str) -> float:
+    """
+    Gets the atomic mass of the atom from a CVS file
+
+    Args:
+        atom_name (str): Name of the atom, i.e. 'H', 'O', 'C', etc.
+
+    Returns:
+        float: atomic mass of the atom in g/mol
+    """
+    # Define the name of the CSV file
+    data_file = load_from_data("molar_mass.csv") #Check path to CSV file
+    with open(data_file, mode='r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if row['Atom'] == atom_name:
+                return float(row['Molar mass [g/mol]'])
+
+def get_molar_mass(salt: str) -> float:
+    """
+    Calculate the molar mass of a salt in g/mol.
+    
+    Args:
+        salt (str): The chemical formula of the salt.
+            Handles chemical formulas of the following types:
+            - "Ca(NO3)2"
+            - "Ca(2+)(NO3-)2"
+            - "Ca2(NO3)2"
+            - "Ca(NO3)2(2+)"
+            Please do not use spaces in the formula and avoid putting brackets inside brackets.
+
+    Returns:
+        float: The molar mass of the salt in g/mol.
+    """
+    #Check for special characters
+    def contains_special_characters(input_string:str):
+        # Define a regular expression pattern to match special characters
+        pattern = re.compile(r'[!@#$%^&*_=\[\]{};\'\\:"|,.<>\/?]')
+        # Use search method to find if any special characters exist in the input string
+        if pattern.search(input_string):
+            return True
+        else:
+            return False
+
+    def molar(molecule:str):
+        #Check for special characters
+        if contains_special_characters(molecule):
+            raise ValueError("Invalid input. The formula of the molcule can only contain letters,numbers, parentheses and '+' or '-' signs.")
+        
+        current_count = ""
+        current_element = ""
+        i=0
+        molar_mass = 0
+        #Test for charges and numbers
+        while molecule[-1] == "+" or molecule[-1] == "-":
+            molecule = molecule[:-1]
+            if len(molecule) == 0:
+                break
+        if molecule.isnumeric():
+            return 0   
+    
+        while i < len(molecule):
+            if i<len(molecule)-3:
+                if molecule[i].isupper() and molecule[i+1].islower() and molecule[i+2].isnumeric() and molecule[i+3].isnumeric():
+                    current_element = molecule[i]+molecule[i+1]
+                    current_count = molecule[i+2]+molecule[i+3]
+                    i+=4
+                    molar_mass += get_atom_mass(current_element)*int(current_count)
+                    continue
+                
+            if i<len(molecule)-2:
+                if molecule[i].isupper() and molecule[i+1].islower() and molecule[i+2].isnumeric():
+                    current_element = molecule[i]+molecule[i+1]
+                    current_count = molecule[i+2]
+                    i+=3
+                    molar_mass += get_atom_mass(current_element)*int(current_count)
+                    continue
+                    
+                if molecule[i].isupper() and molecule[i+1].islower() and molecule[i+2].isalpha():
+                    current_element = molecule[i]+molecule[i+1]
+                    current_count = 1
+                    i+=2
+                    molar_mass += get_atom_mass(current_element)*int(current_count)
+                    continue
+                
+                if molecule[i].isupper() and molecule[i+1].isnumeric() and molecule[i+2].isnumeric():
+                    current_element = molecule[i]
+                    current_count = molecule[i+1]+molecule[i+2]
+                    i+=2
+                    molar_mass += get_atom_mass(current_element)*int(current_count)
+                    continue
+            if i<len(molecule)-1:
+                if molecule[i].isupper() and molecule[i+1].isnumeric():
+                    current_element = molecule[i]
+                    current_count = molecule[i+1]
+                    i+=2
+                    molar_mass += get_atom_mass(current_element)*int(current_count)
+                    continue
+                elif molecule[i].isupper() and molecule[i+1].isupper():
+                    current_element = molecule[i]
+                    current_count = 1
+                    i+=1
+                    molar_mass += get_atom_mass(current_element)*int(current_count)
+                    continue
+            
+            if i == len(molecule)-2:
+                if molecule[i].isupper() and molecule[i+1].islower():
+                    current_element = molecule[i]+molecule[i+1]
+                    current_count = 1
+                    i+=2
+                    molar_mass += get_atom_mass(current_element)*int(current_count)
+                    continue
+                if molecule[i].isupper() and molecule[i+1].isupper():
+                    current_element = molecule[i]
+                    current_count = 1
+                    i+=1
+                    molar_mass += get_atom_mass(current_element)*int(current_count)
+                    continue
+                if molecule[i].isupper() and molecule[i+1].isnumeric():
+                    current_element = molecule[i]
+                    current_count = molecule[i+1]
+                    i+=2
+                    molar_mass += get_atom_mass(current_element)*int(current_count)
+                    continue  
+            if i == len(molecule)-1:
+                current_element = molecule[i]
+                current_count = 1
+                i+=1
+                molar_mass += get_atom_mass(current_element)*int(current_count)
+                continue
+            if molar_mass >=100000:
+                break
+
+        return molar_mass
+    
+    #Take care of brakets
+    M = 0    
+    while "(" in salt:
+        left = salt.index("(")
+        right = salt.index(")")
+        substing = salt[left+1:right]
+
+        if right != len(salt)-1:
+            if salt[right+1].isnumeric():
+                M += int(salt[right+1])*molar(substing)
+                salt = salt[:left] + salt[right+2:]
+            else:
+                M += molar(substing)
+                salt = salt[:left] + salt[right+1:]
+        else:
+            M += molar(substing)
+            salt = salt[:left] + salt[right+1:]
+            
+    total_molar_mass = M + molar(salt)
+    return total_molar_mass
+
+
+
+#------ Basic Solubility functions -----------------------------
+#Dictionaries used to handle salts with multiple ions, extend dictionaries if needed
+salt2nbIons = {
+    "Ca(NO3)2": [1,2],
+    "MgSO4": [1,1],
+    "KNO3": [1,1],
+    "KH2PO4": [1,1],
+    "H3BO3": [1],
+    "MnCl2": [1,2],
+    "ZnSO4": [1,1],
+    "CuSO4": [1,1],
+    "(NH4)6Mo7O24": [6,1],
+    "C10H12FeN2NaO8": [1,1,1],
+    "Fe(III)EDTANa": [1,1,1], #
+    "(NH4)H2PO4" : [1,1],
+    "NaCl": [1,1],
+    "NaOH": [1,1],
+    "Na2CO3": [2,1],
+    "Na2SO4": [2,1],
+    "NaHCO3": [1,1],
+    "Na2S": [2,1],
+    "KCl": [1,1],
+    "KOH": [1,1],
+    "K2SO4": [2,1],
+    "K2CO3": [2,1],
+    "KHCO3": [1,1],
+    "CaCl2": [1,2],
+    "Ca(OH)2": [1,2],
+    "CaCO3": [1,1],
+    "MgCl2": [1,2],
+    "Mg(OH)2": [1,2],
+    "MgCO3": [1,1],
+    "FeSO4": [1,1],
+    "FeCl3": [1,3],
+    "AlCl3": [1,3],
+    "KBr" : [1,1],
+    "Mg(NO3)2": [1,2],
+    "Zn(NO3)2": [1,2],
+    "Cu(NO3)2": [1,2]
+
+    
+}
+dict_salts_trad = {
+    "Ca(NO3)2": {"Ca(2+)": 1, "NO3(-)": 2},
+    "MgSO4": {"Mg(2+)": 1, "SO4(2-)": 1},
+    "KNO3": {"K+": 1, "NO3(-)": 1},
+    "KH2PO4": {"K+": 1, "H2PO4(-)": 1},
+    "H3BO3": {"B": 1},
+    "MnCl2": {"Mn(2+)": 1, "Cl-": 2},
+    "ZnSO4": {"Zn(2+)": 1, "SO4(2-)": 1},
+    "CuSO4": {"Cu(2+)": 1, "SO4(2-)": 1},
+    "(NH4)6Mo7O24": {"NH(4+)" : 6, "Mo": 1},
+    "C10H12FeN2NaO8": {"Fe(3+)": 1, "Na+": 1, "EDTA": 1},
+    "Fe(III)EDTANa": {"Fe(3+)": 1, "Na+": 1, "EDTA": 1},
+    "(NH4)H2PO4" : {"NH4+": 1, "H2PO4(-)": 1},
+    "NaCl": {"Na+": 1, "Cl-": 1},
+    "NaOH": {"Na+": 1, "OH-": 1},
+    "Na2CO3": {"Na+": 2, "CO3": 1},
+    "Na2SO4": {"Na+": 2, "SO4(2-)": 1},
+    "NaHCO3": {"Na+": 1, "HCO3(-)": 1},
+    "Na2S": {"Na+": 2, "S(2-)": 1},
+    "KCl": {"K+": 1, "Cl-": 1},
+    "KOH": {"K+": 1, "OH-": 1},
+    "K2SO4": {"K+": 2, "SO4(2-)": 1},
+    "K2CO3": {"K+": 2, "CO3(2-)": 1},
+    "KHCO3": {"K+": 1, "HCO3(-)": 1},
+    "CaCl2": {"Ca(2+)": 1, "Cl-": 2},
+    "Ca(OH)2": {"Ca(2+)": 1, "OH-": 2},
+    "CaCO3": {"Ca(2+)": 1, "CO3(2-)": 1},
+    "MgCl2": {"Mg(2+)": 1, "Cl-": 2},
+    "Mg(OH)2": {"Mg(2+)": 1, "OH-": 2},
+    "MgCO3": {"Mg(2+)": 1, "CO3(2-)": 1},
+    "FeSO4": {"Fe(2+)": 1, "SO4(2-)": 1},
+    "FeCl3": {"Fe(3+)": 1, "Cl-": 3},
+    "AlCl3": {"Al(3+)": 1, "Cl-": 3},
+    "KBr" : {"K+": 1, "Br-": 1},
+    "Mg(NO3)2": {"Mg(2+)": 1, "NO3(-)": 2},
+    "Zn(NO3)2": {"Zn(2+)": 1, "NO3(-)": 2},
+    "Cu(NO3)2": {"Cu(2+)": 1, "NO3(-)": 2}
+
+    
+}
+
+
+# Get the solubility constant Ksp of a salt
+def get_Ksp(salt_name: str) -> float:
+    """
+    Access the solubility product constant (Ksp) of a salt based on its name. 
+        Uses a salt2nbIons dictionary to find the number of ions in a salt.
+
+    Args:
+        salt_name (str): The formula of the salt. Use standard notation for the formula,
+                        e.g. "Ca(NO3)2", "H2O", etc.
+
+    Returns:
+        float: The solubility product constant (Ksp) of the salt.
+    """
+    solubility_file = load_from_data("Solubility_data.csv")
+    df_solubility = pd.read_csv(solubility_file)
+    
+    row = df_solubility.loc[df_solubility["Formula"] == salt_name].values.tolist() # [[name, formula, value]]
+    Ksp = 0
+    if row:
+        solubility = float(row[0][2]) *10 #go from g/100mL to g/L
+        mol_sol = solubility/get_molar_mass(salt_name) #mol/L
+        
+        #Ksp = n^n * m^m * mol_sol^(n+m) for salt of type nXmY
+        ions = salt2nbIons[salt_name]
+        if len(ions) <=1:
+            Ksp = mol_sol
+        else: 
+            n = ions[0]
+            m = ions[1]
+            Ksp = (n**n)*(m**m)*(mol_sol**(n+m))
+    return Ksp 
+
+# Get the solubility product Q of a salt
+def get_Q_solubility(salt_name: str, ions_in_solution: dict) -> float:
+    '''
+    Returns the solubility product of a salt
+    
+    Args:
+        salt_name (str): name of the salt
+        ions_in_solution (dict): dictionary with keys as ions and values as the concentration in mol/L
+    
+    Returns:
+        float: solubility product Q
+    '''
+    Q = 1
+    i=0
+    for ion in dict_salts_trad[salt_name]:
+        if ion in ions_in_solution:
+            Q *= ions_in_solution[ion]**salt2nbIons[salt_name][i]
+            i+=1
+            
+    return Q
+
+
+
+
+#-----------Basic ion-salt functions----------------------------
+
+# Convert salt concentrations to ion concentrations
+def salt2ions(solution: dict, volume: float = 1, unit: str = "g") -> dict:
+    """
+    Convert salts to ions based on their concentrations and volume. Concentrations are in g/L by default 
+        but can also be given in mol/L by specifying 'unit' as 'mol'.
+        A 'dict_salts_trad' dictionary is used to convert salt names to ions.
+        Make sure the salts are in the 'dict_salts_trad' dictionary.
+
+    Args:
+        solution (dict): A dictionary containing salt names as keys and concentrations as values.
+        volume (float, optional): Volume of the solution. Defaults to 1.
+        unit (str, optional): Unit of concentration. Defaults to "g". Options are "g" or "mol".
+
+    Returns:
+        dict: A dictionary containing ions and their concentrations [g/L] in the solution (volume =1), their quantity [g] if volume > 1
+
+    Raises:
+        ValueError: If the volume is non-positive.
+    """
+    if volume <= 0:
+        raise ValueError("Volume must be positive.")
+    if unit == "mol":
+        ions = {}
+        for salt in solution:
+            if salt in dict_salts_trad:
+                for ion in dict_salts_trad[salt].keys():
+                    if ion in ions:
+                        ions[ion] += dict_salts_trad[salt][ion] * solution[salt]
+                    else:
+                        ions[ion] = dict_salts_trad[salt][ion] * solution[salt]
+            else:
+                raise ValueError(f"Salt {salt} not found in the 'dict_salts_trad' dictionary.")
+            
+        ions_in_solution = {n: volume * ions[n] for n in ions.keys()}    
+        return ions_in_solution
+    
+    elif unit == "g":
+        salts = {salt: mass/get_molar_mass(salt) for salt, mass in solution.items()}
+        ions = {}
+        for salt in salts:
+            if salt in dict_salts_trad:
+                for ion in dict_salts_trad[salt].keys():
+                    if ion in ions:
+                        ions[ion] += dict_salts_trad[salt][ion] * solution[salt]
+                    else:
+                        ions[ion] = dict_salts_trad[salt][ion] * solution[salt]
+            else:
+                raise ValueError(f"Salt {salt} not found in the 'dict_salts_trad' dictionary.")
+        ions_in_solution = {ion: float(get_molar_mass(ion)) * float(volume) * ions[ion] for ion in ions.keys()}
+        return ions_in_solution
+    else:
+        raise ValueError("'Unit' must be 'g' or 'mol'.")
+
+#Make a solution given the ion concentration wanted
+def make_solution (ions_in_solution :dict, forbidden_ions:list, volume:float, )->dict:
+    '''
+    Returns how much of each salt [g] to add to a solution of certain volume [L]
+    to obtain a certain concentration of ions [g/L]
+    
+    Args:
+        ions_in_solution (dict): dictionary with keys as ions and values as the concentration in g/L.
+        forbidden_ions (list): list of ions that cannot be in the solution.
+        volume (float): volume of the final solution in L.
+    
+    Returns:
+        dict: dictionary with keys as salt names and values as the amount of salt needed in g.
+    '''
+    
+    if set(ions_in_solution.keys()).intersection(forbidden_ions) != set() :
+        problems = set(ions_in_solution.keys()).intersection(forbidden_ions)
+        raise ValueError(f"Forbidden ions are required in the solution: {problems}")
+    
+    molar_ions = {ion: ions_in_solution[ion]/get_molar_mass(ion) for ion in ions_in_solution}
+    # Define variables
+    possible_salts = []
+    for salt in dict_salts_trad.keys():
+        for ion in dict_salts_trad[salt]:
+            if (ion in molar_ions) and (salt not in possible_salts) and (len(set(dict_salts_trad[salt].keys()).intersection(forbidden_ions)) == 0):
+                possible_salts.append(salt)
+    
+    #Check for soluble salts
+    for salt in possible_salts:
+        if get_Ksp(salt) <= get_Q_solubility(salt, molar_ions):
+            possible_salts.remove(salt)
+    
+    #NB variables == NB equations
+    final_salts = []
+    for ion in molar_ions.keys():
+        for salt in possible_salts:
+            if ion in dict_salts_trad[salt] and salt not in final_salts:
+                final_salts.append(salt)
+                break
+                
+    variables = symbols(final_salts)
+        
+    #Set up equations
+    equations = []
+    for ion in molar_ions.keys():
+        lhs = 0
+        rhs = molar_ions[ion] * volume
+        for salt in final_salts:
+            if ion in dict_salts_trad[salt]:
+                lhs += dict_salts_trad[salt][ion] * symbols(salt)
+        equation = Eq(lhs, rhs)
+        equations.append(equation)
+    
+    #Solve equations
+    try :
+        positive_solutions = solveset(equations, variables, domain=Interval(0, 1000000))
+    except:
+        positive_solutions = set()
+    if positive_solutions == set():
+        solution = solve(equations, variables)
+        if solution == []:
+            raise ValueError("No solution found. Please check the ions in the solution and the forbidden ions.")
+        else:
+            mass_salts = {salt: solution[symbols(salt)]*get_molar_mass(salt) for salt in final_salts}
+            return mass_salts  
+    else:
+        mass_salts = {salt: positive_solutions[symbols(salt)]*get_molar_mass(salt) for salt in final_salts}
+        return mass_salts
+        
+        
