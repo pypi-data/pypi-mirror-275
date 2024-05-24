68a59f888bd72fb1ef0c61bd923ce6cd9d82f870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 04:03:00 +0000

    Add package conjugate-map/0.2.0/conjugate_map-0.2.0-py3-none-any.whl

---
+++ packages/conjugate-map/conjugate_map-0.2.0-py3-none-any.whl/conjugate_map/__init__.py
@@ -0,0 +1,8 @@
+from importlib import metadata
+
+from conjugate_map.conjCalcFunctions import calc_mlat_rings
+from conjugate_map.conjCalcFunctions import conjcalc
+from conjugate_map.conjCalcFunctions import findconj
+
+# Set version
+__version__ = metadata.version('conjugate_map')
