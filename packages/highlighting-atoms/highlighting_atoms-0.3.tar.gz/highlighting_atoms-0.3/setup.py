fa43dd4d4c9828596cb20cd071bda8479623294b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 04:03:14 +0000

    Add package highlighting-atoms/0.3/highlighting_atoms-0.3.tar.gz

---
+++ packages/highlighting-atoms/highlighting_atoms-0.3.tar.gz/highlighting_atoms-0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='highlighting_atoms',
+    version='0.3',
+    description='Highlighting atoms function',
+    license='GNU',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Pablo Rodríguez Belenguer',
+    author_email='parodbe@gmail.com',
+    url='https://github.com/phi-grib/highlighting_atoms',
+    download_url='https://github.com/phi-grib/highlighting_atoms',
+    packages=find_packages())
