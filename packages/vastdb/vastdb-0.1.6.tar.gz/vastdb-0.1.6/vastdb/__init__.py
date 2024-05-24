a383be0c5d6045881c7a15f59acf88a224774ce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:41 +0000

    Add package vastdb/0.1.6/vastdb-0.1.6.tar.gz

---
+++ packages/vastdb/vastdb-0.1.6.tar.gz/vastdb-0.1.6/vastdb/__init__.py
@@ -0,0 +1,11 @@
+"""VAST Database Python SDK."""
+
+import functools
+
+from . import session
+
+
+# A helper function, useful as a short-hand for Session c-tor: `session = vastdb.connect(...)`
+@functools.wraps(session.Session)
+def connect(*args, **kwargs):  # noqa: D103
+    return session.Session(*args, **kwargs)
