9ed5079ddee408a5003155e10412c6bbf3042458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 04:03:02 +0000

    Add package pyodmongo/0.10.0/pyodmongo-0.10.0-py3-none-any.whl

---
+++ packages/pyodmongo/pyodmongo-0.10.0-py3-none-any.whl/pyodmongo/__init__.py
@@ -0,0 +1,7 @@
+from .async_engine.engine import AsyncDbEngine
+from .engine.engine import DbEngine
+from .models.db_model import DbModel, MainBaseModel
+from .models.id_model import Id
+from .models.paginate import ResponsePaginate
+from .models.responses import SaveResponse, DeleteResponse
+from .models.fields import Field
---
+++ packages/pyodmongo/pyodmongo-0.10.0-py3-none-any.whl/pyodmongo/version.py
@@ -0,0 +1,17 @@
+import platform
+import sys
+import pymongo
+import motor
+
+
+VERSION = "0.10.0"
+
+info = {
+    "PyODMongo version": VERSION,
+    "Pymongo version": pymongo.__version__,
+    "Motor version": motor._version.version,
+    "Python version": sys.version,
+    "Platform": platform.platform(),
+}
+response = "\n".join([f"{key}: {value}" for key, value in info.items()])
+print(response)
