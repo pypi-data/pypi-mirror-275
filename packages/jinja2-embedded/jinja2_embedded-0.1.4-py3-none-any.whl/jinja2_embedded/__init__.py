88e78dc4f7c8b3f226a5f93666adf19615b75e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 04:03:10 +0000

    Add package jinja2-embedded/0.1.4/jinja2_embedded-0.1.4-py3-none-any.whl

---
+++ packages/jinja2-embedded/jinja2_embedded-0.1.4-py3-none-any.whl/jinja2_embedded/__init__.py
@@ -0,0 +1,15 @@
+from .loaders import EmbeddedPackageLoader
+
+__all__ = [
+    'EmbeddedPackageLoader',
+]
+
+__version__ = '0.1.4'
+
+__title__ = 'jinja2-embedded'
+__description__ = 'Jinja2 template loader for embedded Python runtimes'
+
+__author__ = 'Gorden Platz'
+__email__ = '36087062+GPla@users.noreply.github.com'
+
+__copyright__ = 'Copyright (c) 2024 Gorden Platz'
