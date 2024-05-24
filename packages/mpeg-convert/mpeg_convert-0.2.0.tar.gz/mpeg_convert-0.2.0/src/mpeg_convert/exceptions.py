59acb530d027eade3a1b7cf932a37e3135b10258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 14:03:21 +0000

    Add package mpeg-convert/0.2.0/mpeg_convert-0.2.0.tar.gz

---
+++ packages/mpeg-convert/mpeg_convert-0.2.0.tar.gz/mpeg_convert-0.2.0/src/mpeg_convert/exceptions.py
@@ -0,0 +1,76 @@
+import re
+import functools
+
+from typing import Callable, TypeVar
+from typing import Tuple, Type, Union
+
+# (For type-checkers) To spell out an exception type that could be catched in the try-except block
+ExceptionType = Type[BaseException]
+CatchableExceptions = Union[Tuple[ExceptionType, ...], ExceptionType]
+
+
+class ArgumentsError(Exception):
+    """Represents an error during arguments parsing. 
+    
+    Shall be thrown when an error has been encountered in the command-line arguments
+    """
+
+    def __init__(
+        self,
+        message: str,
+        code: int = 1
+    ) -> None:
+        """Initializes an ArgumentsError instance"""
+        super().__init__(message)
+        self.arguments = message
+        self.exit_code = code
+        return
+    
+class ForceExit(Exception):
+    """Represents a force exit of the program due to some reason. 
+    
+    This exception shall be thrown if the program encounters an error and could no
+    longer continue execution, but can still exit gracefully with a message and exit
+    code
+    """
+    
+    def __init__(
+        self,
+        reason: str,
+        code: int = 1
+    ) -> None:
+        """Initializes a ForceExit instance"""
+        super().__init__(reason)
+        self.exit_code = code
+        self.reason = reason
+        return
+
+
+def exception_name(exception: BaseException) -> str:
+    """Returns a clean and human-readable version of the exception by converting the
+    camel-case naming convention to regular space-separated words. Mostly for debug
+    pretty printing purposes
+    """
+    return re.sub(r"(?<!^)([A-Z][a-z]+)", r"_\1", type(exception).__name__).lower()
+
+
+def catch(exceptions: CatchableExceptions, message: str, code: int = 1):
+    """Run a function with the try and except block, catching any exceptions specified
+    in the exceptions parameter. 
+    
+    If any exceptions are caught, throw a ForceExit exception with the reason specified 
+    in the message parameter, and the exit code specified by the code parameter (defaulted
+    to 1)
+    """
+    T = TypeVar("T")
+    
+    def decorator(func: Callable[..., T]):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs) -> T:
+            try:
+                ret = func(*args, **kwargs)
+            except exceptions:
+                raise ForceExit(message, code=code)
+            return ret
+        return wrapper
+    return decorator
---
+++ packages/mpeg-convert/mpeg_convert-0.2.0.tar.gz/mpeg_convert-0.2.0/src/mpeg_convert/utils.py
@@ -0,0 +1,137 @@
+import os
+import sys
+import yaml
+import platform
+import subprocess
+
+from yaml import YAMLError
+from rich.console import Console
+from typing import Any, Dict, List, Tuple
+from ffmpeg import FFmpeg, FFmpegError
+
+from .exceptions import ForceExit, catch
+
+__version__ = "v0.2.0"
+
+# Using the console class from rich to better integrate with its
+# progress bar (e.g. printing on top of the progress bar)
+console = Console(highlight=False)
+
+ROOT_PATH = "~/.local/share/mpeg-convert/"
+MODULE_PATH = os.path.dirname(__file__) + "/"
+
+
+class NamedPreset:
+    """Represents a named conversion preset"""
+    name: str = ""
+    options: str = ""
+
+
+class UnnamedPreset:
+    """Represents an unnamed conversion preset used automatically"""
+    from_type: List[str] = []
+    to_type: List[str] = []
+    options: str = ""
+
+
+def get_python_version() -> str:
+    """Returns basic Python information in a string"""
+    return f"{platform.python_implementation()} {platform.python_version()}"
+
+
+def get_platform_version() -> str:
+    """Returns basic OS information in a string"""
+    return f"{platform.platform(True, True)} {platform.machine()}"
+
+
+def check_interactivity() -> None:
+    """Checks whether the console is a tty, and print a warning if not"""
+    if not (hasattr(sys.stdout, "isatty") or sys.stdout.isatty()):
+        console.print(" • warning: mpeg-convert is not being run in a tty")
+        console.print(" • warning: some features may not work correctly")
+
+
+def open_file(filepath: str) -> None:
+    """Opens a file with the default application on different platforms. Taken
+    from https://stackoverflow.com/questions/434597/open-document-with-default-
+    os-application-in-python-both-in-windows-and-mac-os#435669"""
+    if platform.system() == 'Darwin':       # macOS
+        subprocess.call(('open', filepath))
+    elif platform.system() == 'Windows':    # Windows
+        os.startfile(filepath) # type: ignore
+    else:                                   # Linux variants
+        subprocess.call(('xdg-open', filepath))
+
+
+def expand_paths(path: str) -> str:
+    """Expand relative paths or paths with tilde (~) and dot (.) to absolute paths"""
+    return os.path.normpath(
+        os.path.join(
+            os.environ["PWD"],
+            os.path.expanduser(path)
+    ))
+
+
+@catch((YAMLError, KeyError, OSError), "an error occurred when reading presets")
+def load_config() -> Tuple[List[NamedPreset], List[UnnamedPreset]]:
+    """Loads user-defined config from a yaml file into a list"""
+    with open(expand_paths(ROOT_PATH + "config.yml"), "r") as f:
+        config = yaml.safe_load(f)
+        named = []
+        unnamed = []
+        if "named" in config:
+            for item in config["named"]:
+                temp = NamedPreset()
+                temp.name = item["name"]
+                temp.options = item["options"]
+                named.append(temp)
+        if "unnamed" in config:
+            for item in config["unnamed"]:
+                temp = UnnamedPreset()
+                temp.from_type = item["from-type"]
+                temp.to_type = item["to-type"]
+                temp.options = item["options"]
+                unnamed.append(temp)
+        return (named, unnamed)
+
+
+@catch(FileNotFoundError, "ffmpeg is not installed")
+def check_ffmpeg() -> None:
+    """Checks whether FFmpeg is installed and on the system path"""
+    try:
+        ffprobe_instance = FFmpeg(executable="ffprobe").option("h")
+        ffmpeg_instance = FFmpeg(executable="ffmpeg").option("h")
+        ffprobe_instance.execute()
+        ffmpeg_instance.execute()
+    except FFmpegError:
+        raise ForceExit("ffmpeg is not installed or is corrupted", code=1)
+    return
+
+
+def readable_size(path: str, decimal_points=2) -> str:
+    """Calculates the size of a particular file on disk and returns the
+    size in a human-readable fashion
+    """
+    if not os.path.exists(path):
+        return "0 bytes"
+    size: float = os.path.getsize(path)
+    for i in ["bytes", "kb", "mb", "gb", "tb", "pb"]:
+        if size < 1024.0:
+            return f"{size:.{decimal_points}f} {i}"
+        size /= 1024.0
+    return f"{size:.{decimal_points}f} pb"
+
+
+@catch(OSError, "an error occurred when initializing files and directories")
+def initialize(arguments: Dict[str, Any]) -> None:
+    """Checks terminal integrity and enables debug logging if applicable"""
+    check_interactivity()
+    check_ffmpeg()
+    if not os.path.exists(expand_paths(ROOT_PATH)):
+        os.makedirs(expand_paths(ROOT_PATH))
+    if not os.path.exists(expand_paths(ROOT_PATH + "config.yml")):
+        with open(expand_paths(MODULE_PATH + "assets/default.yml")) as f:
+            default = f.read()
+        with open(expand_paths(ROOT_PATH + "config.yml"), "w") as f:
+            f.write(default)
+    return
