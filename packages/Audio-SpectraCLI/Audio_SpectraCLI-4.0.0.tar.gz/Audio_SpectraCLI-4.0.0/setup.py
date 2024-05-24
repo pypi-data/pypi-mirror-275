3169ec0b1f7dda2b3c3f7df959832d1d0a163071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:12 +0000

    Add package Audio-SpectraCLI/4.0.0/Audio_SpectraCLI-4.0.0.tar.gz

---
+++ packages/Audio-SpectraCLI/Audio_SpectraCLI-4.0.0.tar.gz/Audio_SpectraCLI-4.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Audio_SpectraCLI",
+    version="4.0.0",
+    author="Aditya Seth",
+    long_description=open("Readme.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "sounddevice",
+        "tabulate",
+        "setuptools",
+        "twine",
+        "wheel",
+        "pyqt5",
+        "scipy",
+        "pyaudio",
+    ],
+    license="Apache License 2.0",
+    url="https://github.com/AdityaSeth777/Audio-SpectraCLI",
+)
