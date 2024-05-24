bf3bb42e8f064eece0c7c428d28d1d76ab2e571d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 04:03:11 +0000

    Add package shipyard-smartsheets/0.1.1/shipyard_smartsheets-0.1.1-py3-none-any.whl

---
+++ packages/shipyard-smartsheets/shipyard_smartsheets-0.1.1-py3-none-any.whl/shipyard_smartsheets/cli/authtest.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+import requests
+from shipyard_templates import ShipyardLogger
+
+logger = ShipyardLogger.get_logger()
+
+
+def connect(token: str):
+    url = "https://api.smartsheet.com/2.0/users/me"
+    headers = {"Authorization": f"Bearer {token}", "Accept": "application/json"}
+    response = requests.get(url, headers=headers)
+    if response.ok:
+        return 0
+
+    logger.error("Error in connecting to Smartsheet")
+    logger.error(response.text)
+    return 1
+
+
+def main():
+    sys.exit(connect(token=os.getenv("SMARTSHEET_ACCESS_TOKEN")))
+
+
+if __name__ == "__main__":
+    main()
