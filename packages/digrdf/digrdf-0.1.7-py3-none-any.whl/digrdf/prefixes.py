94f91cacf009393f21239fcdd74117214c058eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 14:03:28 +0000

    Add package digrdf/0.1.7/digrdf-0.1.7-py3-none-any.whl

---
+++ packages/digrdf/digrdf-0.1.7-py3-none-any.whl/digrdf/prefixes.py
@@ -0,0 +1,17 @@
+prefix_map = {
+    "http://www.w3.org/2004/02/skos/core#": "skos",
+    "http://www.w3.org/ns/sosa/": "sosa",
+    "http://www.w3.org/1999/02/22-rdf-syntax-ns#": "rdf",
+    "http://www.w3.org/2000/01/rdf-schema#": "rdfs",
+    "http://www.w3.org/2001/XMLSchema#": "xsd",
+    "http://purl.org/dc/terms/": "dcterms",
+    "http://purl.org/dc/elements/1.1/": "dc",
+    "http://schema.org/": "sdo",
+    "https://schema.org/": "sdo",
+    "http://www.ontoweb.org/ontology/1#": "ow",
+    "http://www.w3.org/2006/time#": "time",
+    "http://www.opengis.net/ont/geosparql#": "geo",
+    "http://www.w3.org/ns/dcat#": "dcat",
+    "http://www.w3.org/2002/07/owl#": "owl",
+    "http://purl.org/ontology/bibo/": "bibo"
+}
