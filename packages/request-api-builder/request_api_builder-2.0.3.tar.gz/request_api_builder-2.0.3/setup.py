139a6b433ade25322fc52addd3a065e92356dcde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 16:04:59 +0000

    Add package request-api-builder/2.0.3/request_api_builder-2.0.3.tar.gz

---
+++ packages/request-api-builder/request_api_builder-2.0.3.tar.gz/request_api_builder-2.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+__version__ = "2.0.3"
+
+setup(
+    name="request-api-builder",
+    version=__version__,
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+)
