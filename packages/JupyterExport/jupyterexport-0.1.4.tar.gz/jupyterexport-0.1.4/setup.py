787b9e68713ca3f81840ae120f37c0d8ed72b551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 15:04:09 +0000

    Add package JupyterExport/0.1.4/jupyterexport-0.1.4.tar.gz

---
+++ packages/JupyterExport/jupyterexport-0.1.4.tar.gz/jupyterexport-0.1.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+from JupyterExport import __version__ as version
+
+setup(
+    name="JupyterExport",
+    version="0.1.4",
+    author="Alan",
+    author_email="alananalyst00@gmail.com",
+    url='https://github.com/Alan-Analyst/JupyterExport',
+    description="Convert .ipynb files to .pdf/.html using a simple graphical interface.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'JupyterExport': ['assets/*.ico', 'assets/*.json'],  # Include all PNG and TXT files in the assets directory
+    },
+    python_requires='>=3.6',
+    install_requires=[
+        # List your project dependencies here
+        'customtkinter',
+        'nbformat',
+        'nbconvert'
+    ],
+    entry_points={
+        'console_scripts': [
+            'JupyterExport=JupyterExport.main:main',
+        ],
+    },
+)
