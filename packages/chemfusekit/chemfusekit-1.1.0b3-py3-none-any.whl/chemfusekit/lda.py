0e745ba81f08c98d465595c89991bb331a7cbb7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:40 +0000

    Add package chemfusekit/1.1.0b3/chemfusekit-1.1.0b3-py3-none-any.whl

---
+++ packages/chemfusekit/chemfusekit-1.1.0b3-py3-none-any.whl/chemfusekit/lda.py
@@ -0,0 +1,83 @@
+'''Linear Discriminant Analysis module'''
+from typing import Optional
+
+import numpy as np
+import pandas as pd
+
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LD
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+
+from chemfusekit.lldf import LLDFModel
+from chemfusekit.__utils import graph_output, run_split_test
+
+class LDASettings:
+    '''Holds the settings for the LDA object.'''
+    def __init__(self, components: int = 3, output: bool = False, test_split: bool = False):
+        if components <= 2:
+            raise ValueError("Invalid component number: must be a > 1 integer.")
+        if test_split is True and output is False:
+            raise Warning(
+                "You selected test_split but it won't run because you disabled the output."
+            )
+        self.components = components
+        self.output = output
+        self.test_split = test_split
+
+class LDA:
+    '''Class to store the data, methods and artifacts for Linear Discriminant Analysis'''
+    def __init__(self, lldf_model: LLDFModel, settings: LDASettings):
+        self.settings = settings
+        self.x_data = lldf_model.x_data
+        self.x_train = lldf_model.x_train
+        self.y = lldf_model.y
+        self.model: Optional[LD] = None
+
+    def lda(self):
+        '''Performs Linear Discriminant Analysis'''
+
+        lda = LD(n_components=self.settings.components) # N-1 where N are the classes
+        scores_lda = lda.fit(self.x_data, self.y).transform(self.x_data)
+
+        if self.settings.output:
+            print(f"LDA scores:\n{scores_lda}")
+            print(lda.priors_)
+            print(lda.means_)
+            print(lda.coef_[0:self.settings.components,:])
+            pred=lda.predict(self.x_data)
+            print(np.unique(pred, return_counts=True))
+
+            print(confusion_matrix(pred, self.y))
+            print(classification_report(self.y, pred, digits=2))
+
+        lv_cols = [f'LV{i+1}' for i in range(self.settings.components)]
+        scores = pd.DataFrame(data = scores_lda, columns = lv_cols) # latent variables
+        scores.index = self.x_data.index
+        y_dataframe = pd.DataFrame(self.y, columns=['Substance'])
+
+        scores = pd.concat([scores, y_dataframe], axis = 1)
+
+        # Store the traiend model
+        self.model = lda
+
+        # Show graphs if required by the user
+        if self.settings.output:
+            graph_output(scores, self.model, "Linear Discriminant Analysis")
+
+            # Run split tests if required by the user
+            if self.settings.test_split:
+                run_split_test(
+                    (scores.drop('Substance', axis=1).values),
+                    self.y,
+                    LD(n_components=self.settings.components)
+                )
+
+    def predict(self, x_data: pd.DataFrame):
+        '''Performs LDA prediction once the model is trained.'''
+        if x_data is None:
+            raise TypeError("X data for LDA prediction must be non-empty.")
+        if self.model is None:
+            raise RuntimeError("The LDA model is not trained yet!")
+
+        y_pred = self.model.predict(x_data)
+        return y_pred
---
+++ packages/chemfusekit/chemfusekit-1.1.0b3-py3-none-any.whl/chemfusekit/lr.py
@@ -0,0 +1,109 @@
+'''Logistic Regression Module'''
+from typing import Optional
+
+import numpy as np
+import pandas as pd
+
+from sklearn.linear_model import LogisticRegression
+
+from chemfusekit.__utils import run_split_test, print_confusion_matrix
+
+class LRSettings:
+    '''Holds the settings for the LR object.'''
+    def __init__(self, algorithm: str = 'liblinear', output: bool = False,
+                 test_split: bool = False):
+        if algorithm not in [
+            'lbfgs',
+            'liblinear',
+            'newton-cg',
+            'newton-cholesky',
+            'sag',
+            'saga'
+        ]:
+            raise ValueError(f"{algorithm}: this algorithm does not exist.")
+        if test_split is True and output is False:
+            raise Warning(
+                "You selected test_split but it won't run because you disabled the output."
+            )
+        self.algorithm = algorithm
+        self.output = output
+        self.test_split = test_split
+
+class LR:
+    '''Class to store the data, methods and artifacts for Logistic Regression'''
+    def __init__(self, settings: LRSettings, array_scores: np.ndarray, y: np.ndarray):
+        self.settings = settings
+        self.array_scores = array_scores
+        self.y = y
+        self.model: Optional[LogisticRegression] = None
+
+    def lr(self):
+        '''Performs Logistic Regression'''
+
+        # Let's build our model on the training set
+        model = LogisticRegression(
+            solver=self.settings.algorithm,
+            random_state=0,
+            class_weight='balanced',
+            max_iter=10000
+        ).fit(self.array_scores, self.y)
+
+        if self.settings.output:
+            #we can see the classes the model used
+            print(model.classes_)
+            # See the intercept of the model
+            print(model.intercept_)
+            # See the coefficients of the model - that can be easily interpreted
+            # (correlating or not with y)
+            print(model.coef_)
+
+        '''
+        Evaluate the model: each sample has a probability of belonging to Positive
+        or Negative outcome. Class 0 is Negative, class 1 is Positive.  If the value
+        of the first column (probability of being Negative) is higher than 0.5, we
+        have a Negative sample. Otherwise, it will be Positive
+        '''
+
+        probabilities = model.predict_proba(self.array_scores)
+        predictions = model.predict(self.array_scores)
+
+        # This tells us the accuracy of our model in calibration
+        scores = model.score(self.array_scores, self.y)
+
+        # Save the trained model
+        self.model = model
+
+        if self.settings.output:
+            print(probabilities)
+            print(predictions)
+            print(scores)
+
+            print_confusion_matrix(
+                self.y,
+                predictions,
+                "Confusion Matrix based on whole data set",
+            )
+
+        if self.settings.test_split and self.settings.output:
+            split_model = LogisticRegression(
+                solver='lbfgs',
+                random_state=0,
+                class_weight='balanced',
+                max_iter=10000
+            )
+
+            run_split_test(self.array_scores, self.y, split_model, extended=True)
+
+    def predict(self, x_sample: pd.DataFrame):
+        '''Performs LR prediction once the model is trained.'''
+        if self.model is None:
+            raise RuntimeError("The LR model is not trained yet!")
+
+        prediction = self.model.predict(x_sample)
+        probabilities = self.model.predict_proba(x_sample)
+
+        if self.settings.output:
+            print(prediction)
+            print(probabilities)
+
+        return prediction
