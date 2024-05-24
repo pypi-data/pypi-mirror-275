56c5b5da3f47105b2d0b4c31deb760bb8b3d01e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:56 +0000

    Add package laser-mind-client/0.23.0/laser_mind_client-0.23.0.tar.gz

---
+++ packages/laser-mind-client/laser_mind_client-0.23.0.tar.gz/laser_mind_client-0.23.0/tests/test_solve_qubo_matrix_async.py
@@ -0,0 +1,26 @@
+import numpy
+from laser_mind_client_meta import MessageKeys
+from laser_mind_client import LaserMind
+
+# Create a mock QUBO problem
+quboProblemData = numpy.random.randint(-1, 2, (10,10))
+
+# Symmetrize our matrix
+quboProblemData = (quboProblemData + quboProblemData.T) // 2
+
+# Connect to the LightSolver Cloud
+lsClient = LaserMind()
+
+# Request a solution to the QUBO problem and get the request token for future retrieval.
+# This call does not block operations until the problem is solved.
+requestToken = lsClient.solve_qubo(matrixData = quboProblemData, timeout=1, waitForSolution=False)
+
+# You can run other code here that is not dependant on the request, while the server processes your request.
+
+# Retrieve the solution using the get_solution_sync method.
+# This blocks operations until the solution is acquired.
+res = lsClient.get_solution_sync(requestToken)
+
+assert MessageKeys.SOLUTION in res, "Test FAILED, response is not in expected format"
+
+print(f"Test PASSED, response is: \n{res}")
