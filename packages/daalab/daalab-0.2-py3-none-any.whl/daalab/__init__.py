60611a9040d55bfb61f32cbfde14ecb4714241e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:58 +0000

    Add package daalab/0.2/daalab-0.2-py3-none-any.whl

---
+++ packages/daalab/daalab-0.2-py3-none-any.whl/daalab/__init__.py
@@ -0,0 +1,10 @@
+from .codes import print01, print_fk, print_huffman, print_queen1, print_queen2
+
+zerone = print01
+fracknap = print_fk
+huffman = print_huffman
+queen1 = print_queen1
+queen2 = print_queen2
+
+
+
