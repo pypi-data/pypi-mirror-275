fd5cee2fe4465752f2f4e23ab275ef797a8e989f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 07:03:28 +0000

    Add package picopy/1.0.5/picopy-1.0.5.tar.gz

---
+++ packages/picopy/picopy-1.0.5.tar.gz/picopy-1.0.5/README.md
@@ -0,0 +1,16 @@
+# Convert an Image To a Watercolor Image
+
+
+## Installation 
+
+    pip install picopy
+
+
+## How to use 
+Add your image address in below command : 
+
+    from picopy import picopy 
+
+    picopy.convert_image_to_watercolor("test.jpg") 
+
+Your watercolored image will be saved in your file folder .
---
+++ packages/picopy/picopy-1.0.5.tar.gz/picopy-1.0.5/setup.py
@@ -0,0 +1,22 @@
+# we can use a package with its setup.py file
+
+# this setuptools package help us to install other package 
+from setuptools import setup
+
+
+def pre_install():
+    f = open("README.md" , "r")
+    text = f.read()
+    return text
+
+setup(name="picopy" ,
+      version="1.0.5", 
+      author="kiki js" , 
+      description="convert an image to a watercolor image 🎨", 
+      long_description=pre_install() , # its like readme
+      requires= [] ,
+      author_email="k.jhnshid@gmail.com",
+      packages=["picopy"]
+      )
+
+
