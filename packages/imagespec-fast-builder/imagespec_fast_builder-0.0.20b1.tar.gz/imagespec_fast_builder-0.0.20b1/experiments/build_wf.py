890f75ebbac0d7b62ebe3902652294bd509b700c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 04:03:09 +0000

    Add package imagespec-fast-builder/0.0.20b1/imagespec_fast_builder-0.0.20b1.tar.gz

---
+++ packages/imagespec-fast-builder/imagespec_fast_builder-0.0.20b1.tar.gz/imagespec_fast_builder-0.0.20b1/experiments/build_wf.py
@@ -0,0 +1,22 @@
+from flytekit import ImageSpec, task, workflow
+
+image = ImageSpec(
+    builder="fast-builder",
+    # builder="envd",
+    packages=["numpy"],
+    registry="localhost:30000",
+    env={"ABC": "CDE"},
+)
+
+
+@task(container_image=image)
+def my_task() -> int:
+    import numpy as np
+
+    X = np.asarray([1, 2, 3])
+    return int(X[1])
+
+
+@workflow
+def wf() -> int:
+    return my_task()
