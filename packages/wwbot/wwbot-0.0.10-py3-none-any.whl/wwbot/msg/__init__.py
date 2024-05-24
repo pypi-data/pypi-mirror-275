6e63cb5439641b79c1de290bcc2d3541ada75c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:35 +0000

    Add package wwbot/0.0.10/wwbot-0.0.10-py3-none-any.whl

---
+++ packages/wwbot/wwbot-0.0.10-py3-none-any.whl/wwbot/msg/__init__.py
@@ -0,0 +1,77 @@
+# -*- encoding: utf-8
+
+import time, random
+from ..lib import Logger
+from abc import abstractmethod
+from xml.etree.cElementTree import Element
+
+class Message:
+
+    logger:Logger = Logger('Message')
+
+    # message type 
+    type:str = 'message'
+    # handler key 
+    key:str = 'message'
+
+    def __init__(self, to_username:str, from_username:str, agent_id:str, create_time:int=None, msg_id:int=None, safe:bool=False, enable_id_trans:bool=False, enable_duplicate_check:bool=False, duplicate_check_interval:int=1800) -> None:
+        # the id of the receiver of the message 
+        self.to_username:str = to_username
+        # the id of the sender of the message 
+        self.from_username:str = from_username
+        # the agent id of the message
+        # also known as the application id 
+        self.agent_id:str = agent_id
+        # timestamp for the message 
+        self.create_time:int = create_time or int(time.time())
+        # message id 
+        self.message_id:int = msg_id or self.create_time * 1000 + random.randint(1000, 9999)
+        # whether the message can be shared when it's sent by send_to 
+        self.safe:bool = safe
+        # whether to enable the id translation
+        self.enable_id_trans:bool = enable_id_trans
+        # whether to check the message duplication 
+        self.enable_duplicate_check:bool = enable_duplicate_check
+        # the interval of message duplication checking, in second
+        self.duplicate_check_interval:int = duplicate_check_interval
+    
+    @abstractmethod
+    def to_xml(self) -> str:
+        '''Represent the message in XML format'''
+        pass
+
+    @abstractmethod
+    def to_json(self) -> str:
+        '''Represent the message in JSON format'''
+        pass
+
+    @classmethod
+    @abstractmethod
+    def from_xml(cls, xml_tree:Element):
+        '''Parse different types of messages from XML object'''
+        pass 
+
+from .event import event_msg_from_xml
+from .text_msg import TextMessage
+from .link_msg import LinkMessage
+from .file_msg import FileMessage
+from .image_msg import ImageMessage
+from .voice_msg import VoiceMessage
+from .video_msg import VideoMessage
+from .news_msg import News, NewsMessage
+from .location_msg import LocationMessage
+from .markdown_msg import MarkdownMessage
+from .textcard_msg import TextCardMessage
+from .mpnews_msg import MPNews, MPNewsMessage
+
+def msg_from_xml(xml_tree:Element) -> Message:
+    '''Parse message from xml tree'''
+    msg_type:str = xml_tree.find('MsgType').text
+    if msg_type == 'event': return event_msg_from_xml(xml_tree)
+    if msg_type == 'text': return TextMessage.from_xml(xml_tree)
+    if msg_type == 'image': return ImageMessage.from_xml(xml_tree)
+    if msg_type == 'voice': return VoiceMessage.from_xml(xml_tree)
+    if msg_type == 'video': return VideoMessage.from_xml(xml_tree)
+    if msg_type == 'link': return LinkMessage.from_xml(xml_tree)
+    if msg_type == 'location': return LocationMessage.from_xml(xml_tree)
+    Message.logger.warn('Unregistered message type')
---
+++ packages/wwbot/wwbot-0.0.10-py3-none-any.whl/wwbot/msg/file_msg.py
@@ -0,0 +1,31 @@
+#!python3.9
+
+import json
+from . import Message
+from xml.etree.ElementTree import Element
+
+class FileMessage(Message):
+
+    # message type 
+    type:str = 'file'
+    # handler key 
+    key:str = 'file'
+
+    def __init__(self, to_username: str, from_username: str, agent_id: str, media_id: str, **kwargs) -> None:
+        super().__init__(to_username, from_username, agent_id, **kwargs)
+        # media id for the file 
+        self.media_id:str = media_id
+
+    def to_xml(self) -> str:
+        '''Represent file message in XML format'''
+        raise NotImplementedError('Unable to reply with a file message in current WeWork')
+
+    def to_json(self) -> str:
+        '''Represent file message in JSON format'''
+        return json.dumps({"touser":self.to_username,"msgtype":"file","agentid":self.agent_id,"file":{"media_id":self.media_id},"safe":1 if self.safe else 0,"enable_id_trans":1 if self.enable_id_trans else 0,"enable_duplicate_check":1 if self.enable_duplicate_check else 0,"duplicate_check_interval":self.duplicate_check_interval})
+    
+    @classmethod
+    def from_xml(cls, xml_tree: Element):
+        '''Parse file message from XML object'''
+        raise NotImplementedError('Unable to receive and parse a file message in current WeWork')
+
