5a1980217c1051c5a2c9703e4d4bfdfa347caa3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 14:03:19 +0000

    Add package tabswitcher/0.0.1/tabswitcher-0.0.1.tar.gz

---
+++ packages/tabswitcher/tabswitcher-0.0.1.tar.gz/tabswitcher-0.0.1/src/tabswitcher/colors.py
@@ -0,0 +1,24 @@
+from .Settings import Settings
+
+settings = Settings()
+def getTextColor():
+    if(settings.get_dark_mode()):
+        return "#ffffff"
+    return "#161618"
+
+def getBackgroundColor():
+    if(settings.get_dark_mode()):
+        return "#161618"
+    return "#ffffff"
+
+def getSelectedColor():
+    if(settings.get_dark_mode()):
+        return "#818181"
+    return "#818181"
+
+def getWindowBackgroundColor():
+    if(settings.get_show_background()):
+        if(settings.get_dark_mode()):
+            return "#121212"
+        return "#dddddd"
+    return "transparent"
