f912421dd9a9598812cfe9be1e61f15092885cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 04:03:00 +0000

    Add package arena-client/0.3.2/arena_client-0.3.2.tar.gz

---
+++ packages/arena-client/arena_client-0.3.2.tar.gz/arena_client-0.3.2/src/arena/models/settings.py
@@ -0,0 +1,8 @@
+from typing import Literal
+from pydantic import BaseModel
+
+# A LM config setting
+class LMConfig(BaseModel):
+    pii_removal: Literal["masking", "replace"] | None = None
+    judge_evaluation: bool = False
+    judge_with_pii: bool = False
