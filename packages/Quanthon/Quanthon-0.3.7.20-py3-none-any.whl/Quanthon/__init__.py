2c51545787a0ab84dbe175a02f4f24bb98842674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:18 +0000

    Add package Quanthon/0.3.7.20/Quanthon-0.3.7.20-py3-none-any.whl

---
+++ packages/Quanthon/Quanthon-0.3.7.20-py3-none-any.whl/Quanthon/__init__.py
@@ -0,0 +1,8 @@
+from Quanthon.base import Qubits
+from Quanthon.expectation import cal_expectation
+from Quanthon.utils import pauli_sum
+from Quanthon.algorithms import VQE, AdaptVQE
+from Quanthon.mappers import jordan_wigner, pauli_decomposition
+from Quanthon.physics import Hamiltonian
+from Quanthon.ansatzs import HardwareEfficientAnsatz, QubitAdaptAnsatz
+from Quanthon.exponential import exponential_pauli
