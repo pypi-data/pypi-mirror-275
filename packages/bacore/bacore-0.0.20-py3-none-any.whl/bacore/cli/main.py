647255cfd410127f5ef68171b184f25503047eb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 15:04:08 +0000

    Add package bacore/0.0.20/bacore-0.0.20-py3-none-any.whl

---
+++ packages/bacore/bacore-0.0.20-py3-none-any.whl/bacore/cli/main.py
@@ -0,0 +1,8 @@
+"""BACore CLI entrypoint module."""
+from bacore.cli import create, serve, upgrade
+from typer import Typer
+
+app = Typer(rich_markup_mode="rich", add_completion=False)
+app.add_typer(create.app, name="create", help="Create a project")
+app.add_typer(serve.app, name="serve", help="Serve documentation")
+app.add_typer(upgrade.app, name="upgrade", help="Upgrade")
