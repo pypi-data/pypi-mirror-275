947c44ac3cfebccdd1f0b646aa5403e3e1b3369b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 15:03:53 +0000

    Add package cadprox/1.7/cadprox-1.7.tar.gz

---
+++ packages/cadprox/cadprox-1.7.tar.gz/cadprox-1.7/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cadprox',
+    version='1.7',  # Increment the version number
+    author='Piotr Tamu (Thriveroute)',
+    author_email='your.email@example.com',
+    description='A tool to manage Caddy reverse proxy configurations with Cloudflare DNS.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/cadprox',  # Update with your GitHub repository URL
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+    install_requires=[
+        'requests',
+        'dnspython',
+        'boto3',
+        'python-dotenv'
+    ],
+    entry_points={
+        'console_scripts': [
+            'cadprox=app.main:main',
+        ],
+    },
+)
