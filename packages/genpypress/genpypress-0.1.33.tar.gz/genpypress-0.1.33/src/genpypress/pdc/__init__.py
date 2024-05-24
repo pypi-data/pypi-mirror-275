107ef727d4de20f8cc8edc50b8423973550a24f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 15:03:51 +0000

    Add package genpypress/0.1.33/genpypress-0.1.33.tar.gz

---
+++ packages/genpypress/genpypress-0.1.33.tar.gz/genpypress-0.1.33/src/genpypress/pdc/__init__.py
@@ -0,0 +1,3 @@
+from genpypress.pdc.from_pdc import config  # noqa: F401
+from genpypress.pdc.from_pdc import from_pdc, ora_cursor, ora_session  # noqa: F401
+from genpypress.pdc.model import *  # noqa: F403 F401
