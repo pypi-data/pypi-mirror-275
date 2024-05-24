7ad274fd0395802658ba494269006783060321bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 05:03:23 +0000

    Add package bibcite/0.2.0/bibcite-0.2.0-py3-none-any.whl

---
+++ packages/bibcite/bibcite-0.2.0-py3-none-any.whl/bibcite/cli.py
@@ -0,0 +1,29 @@
+import argparse
+import pyperclip
+from tabulate import tabulate
+from bibcite.work import Work
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate BibTeX citations from a title.')
+    parser.add_argument('title', type=str, help='The title of the work to generate a BibTeX citation for.')
+    args = parser.parse_args()
+
+    text = "Bibcite: Bibtex citations from the command line\n"
+    table = [[text]]
+    print(tabulate(table, tablefmt="grid"))
+
+    title = args.title
+    try:
+        print(f'- Searching for title \"{title}\"')
+        work = Work.from_query(title=title)
+    except Exception as e:
+        print(f'- An error occurred while trying to find work with title \"{title}\": {repr(e)}')
+        return
+
+    bibtex = work.to_bibtex()
+    print(f'- Found BibTeX citation: \n{bibtex}')
+    pyperclip.copy(bibtex)
+    print(f'- Copied to clipboard!')
+
+if __name__ == "__main__":
+    main()
