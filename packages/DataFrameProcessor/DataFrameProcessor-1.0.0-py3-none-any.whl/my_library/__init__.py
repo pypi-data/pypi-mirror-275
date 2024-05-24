fe53ac7e878844c2f4e6a2a997f3e2f20f60bda4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 05:03:19 +0000

    Add package DataFrameProcessor/1.0.0/DataFrameProcessor-1.0.0-py3-none-any.whl

---
+++ packages/DataFrameProcessor/DataFrameProcessor-1.0.0-py3-none-any.whl/my_library/__init__.py
@@ -0,0 +1,12 @@
+
+__version__ = '1.0.0'
+
+from .missing_value_handler import MissingValueHandler 
+from .outlier_handler import OutlierHandler 
+from .scaler import Scaler
+from .text_cleaner import TextCleaner
+from .feature_engineer import FeatureEngineer
+from .date_time_handler import DateTimeHandeler
+from .search import Search
+from .categorical_encoder import CategoricalEncoder
+
---
+++ packages/DataFrameProcessor/DataFrameProcessor-1.0.0-py3-none-any.whl/my_library/categorical_encoder.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder   
+
+class CategoricalEncoder:
+    @staticmethod
+    def hot_encode(dataframe, columns):
+        """
+        One-hot encode categorical columns in a DataFrame.
+
+        Parameters:
+        dataframe (pandas.DataFrame): The DataFrame containing the columns to encode.
+        columns (list): A list of column names to one-hot encode.
+
+        Returns:
+        pandas.DataFrame: The DataFrame with one-hot encoded categorical columns.
+        """
+        return pd.get_dummies(dataframe, columns=columns)
+
+    @staticmethod
+    def label_encoder(dataframe, columns):
+        """
+        Encode categorical columns in a DataFrame using LabelEncoder.
+
+        Parameters:
+        dataframe (pandas.DataFrame): The DataFrame containing the columns to encode.
+        columns (list): A list of column names to encode.
+        """
+        labelEncoder = LabelEncoder()
+        for column in columns:
+            dataframe[column] = labelEncoder.fit_transform(dataframe[column])
