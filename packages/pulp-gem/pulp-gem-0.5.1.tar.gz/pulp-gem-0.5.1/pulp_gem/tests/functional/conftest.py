1a90a45b148e17439d84d6a18d1c7552c81da976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 04:03:12 +0000

    Add package pulp-gem/0.5.1/pulp-gem-0.5.1.tar.gz

---
+++ packages/pulp-gem/pulp-gem-0.5.1.tar.gz/pulp-gem-0.5.1/pulp_gem/tests/functional/conftest.py
@@ -0,0 +1,127 @@
+import uuid
+
+import pytest
+from pulp_gem.tests.functional.constants import GEM_FIXTURE_URL
+from pulpcore.client.pulp_gem import (
+    ApiClient,
+    ContentGemApi,
+    DistributionsGemApi,
+    PublicationsGemApi,
+    RemotesGemApi,
+    RepositoriesGemApi,
+    RepositoriesGemVersionsApi,
+)
+from pulp_gem.tests.functional.constants import GEM_URL
+
+# Api Bindings fixtures
+
+
+@pytest.fixture(scope="session")
+def gem_client(_api_client_set, bindings_cfg):
+    api_client = ApiClient(bindings_cfg)
+    _api_client_set.add(api_client)
+    yield api_client
+    _api_client_set.remove(api_client)
+
+
+@pytest.fixture(scope="session")
+def gem_content_api_client(gem_client):
+    return ContentGemApi(gem_client)
+
+
+@pytest.fixture(scope="session")
+def gem_distribution_api_client(gem_client):
+    return DistributionsGemApi(gem_client)
+
+
+@pytest.fixture(scope="session")
+def gem_publication_api_client(gem_client):
+    return PublicationsGemApi(gem_client)
+
+
+@pytest.fixture(scope="session")
+def gem_repository_api_client(gem_client):
+    return RepositoriesGemApi(gem_client)
+
+
+@pytest.fixture(scope="session")
+def gem_repository_version_api_client(gem_client):
+    return RepositoriesGemVersionsApi(gem_client)
+
+
+@pytest.fixture(scope="session")
+def gem_remote_api_client(gem_client):
+    return RemotesGemApi(gem_client)
+
+
+# Factory fixtures
+
+
+@pytest.fixture(scope="class")
+def gem_repository_factory(gem_repository_api_client, gen_object_with_cleanup):
+    """A factory to generate a Gem Repository with auto-deletion after the test run."""
+
+    def _gem_repository_factory(**kwargs):
+        extra_args = {}
+        if pulp_domain := kwargs.pop("pulp_domain", None):
+            extra_args["pulp_domain"] = pulp_domain
+        data = {"name": str(uuid.uuid4())}
+        data.update(kwargs)
+        return gen_object_with_cleanup(gem_repository_api_client, data, **extra_args)
+
+    return _gem_repository_factory
+
+
+@pytest.fixture(scope="class")
+def gem_distribution_factory(gem_distribution_api_client, gen_object_with_cleanup):
+    """A factory to generate a Gem Distribution with auto-deletion after the test run."""
+
+    def _gem_distribution_factory(**kwargs):
+        extra_args = {}
+        if pulp_domain := kwargs.pop("pulp_domain", None):
+            extra_args["pulp_domain"] = pulp_domain
+        data = {"base_path": str(uuid.uuid4()), "name": str(uuid.uuid4())}
+        data.update(kwargs)
+        return gen_object_with_cleanup(gem_distribution_api_client, data, **extra_args)
+
+    return _gem_distribution_factory
+
+
+@pytest.fixture(scope="class")
+def gem_publication_factory(gem_publication_api_client, gen_object_with_cleanup):
+    """A factory to generate a Gem Publication with auto-deletion after the test run."""
+
+    def _gem_publication_factory(**kwargs):
+        extra_args = {}
+        if pulp_domain := kwargs.pop("pulp_domain", None):
+            extra_args["pulp_domain"] = pulp_domain
+        # XOR check on repository and repository_version
+        assert bool("repository" in kwargs) ^ bool("repository_version" in kwargs)
+        return gen_object_with_cleanup(gem_publication_api_client, kwargs, **extra_args)
+
+    return _gem_publication_factory
+
+
+@pytest.fixture(scope="class")
+def gem_remote_factory(gem_remote_api_client, gen_object_with_cleanup):
+    """A factory to generate a Gem Remote with auto-deletion after the test run."""
+
+    def _gem_remote_factory(*, url=GEM_FIXTURE_URL, policy="immediate", **kwargs):
+        extra_args = {}
+        if pulp_domain := kwargs.pop("pulp_domain", None):
+            extra_args["pulp_domain"] = pulp_domain
+        data = {"url": url, "policy": policy, "name": str(uuid.uuid4())}
+        data.update(kwargs)
+        return gen_object_with_cleanup(gem_remote_api_client, data, **extra_args)
+
+    return _gem_remote_factory
+
+
+@pytest.fixture(scope="session")
+def gem_content_artifact(tmp_path_factory, http_get):
+    """A file containing amber-1.0.0.gem."""
+
+    temp_file = tmp_path_factory.mktemp("pulp_gem") / "amber-1.0.0.gem"
+    content = http_get(GEM_URL)
+    temp_file.write_bytes(content)
+    return temp_file
---
+++ packages/pulp-gem/pulp-gem-0.5.1.tar.gz/pulp-gem-0.5.1/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as requirements:
+    requirements = requirements.readlines()
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="pulp-gem",
+    version="0.5.1",
+    description="Gemfile plugin for the Pulp Project",
+    long_description=long_description,
+    license="GPLv2+",
+    author="Pulp Project Developers",
+    author_email="pulp-dev@redhat.com",
+    url="https://pulpproject.org/",
+    python_requires=">=3.8",
+    install_requires=requirements,
+    include_package_data=True,
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    classifiers=(
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: POSIX :: Linux",
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ),
+    entry_points={"pulpcore.plugin": ["pulp_gem = pulp_gem:default_app_config"]},
+    project_urls={
+        "Documentation": "https://docs.pulpproject.org/pulp_gem/",
+        "Source": "https://github.com/pulp/pulp_gem",
+        "Tracker": "https://github.com/pulp/pulp_gem/issues",
+    },
+)
