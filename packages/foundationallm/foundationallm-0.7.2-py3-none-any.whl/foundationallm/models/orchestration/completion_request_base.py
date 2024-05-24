fa88bc5a6d32b083e74f4a425ceb9e71b5b8d95d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:57 +0000

    Add package foundationallm/0.7.2/foundationallm-0.7.2-py3-none-any.whl

---
+++ packages/foundationallm/foundationallm-0.7.2-py3-none-any.whl/foundationallm/models/orchestration/completion_request_base.py
@@ -0,0 +1,14 @@
+import uuid
+from typing import List, Optional
+from pydantic import BaseModel
+from foundationallm.models.orchestration import MessageHistoryItem, OrchestrationSettings
+
+class CompletionRequestBase(BaseModel):
+    """
+    Orchestration completion request.
+    """
+    request_id: Optional[str] = str(uuid.uuid4())
+    session_id: Optional[str] = None
+    user_prompt: str    
+    message_history: Optional[List[MessageHistoryItem]] = []
+    settings: Optional[OrchestrationSettings] = None
