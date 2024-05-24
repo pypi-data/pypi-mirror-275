22911f51b272ac6ef5b82b1678fa65e69bca2ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 16:04:52 +0000

    Add package mapieng/0.2.0/mapieng-0.2.0.tar.gz

---
+++ packages/mapieng/mapieng-0.2.0.tar.gz/mapieng-0.2.0/setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README_en.md', encoding='utf-8') as f:
+        return f.read()
+
+setup(
+    name='mapieng',
+    version='0.2.0',
+    packages=find_packages(),
+    include_package_data=True,
+    description='mapi engineers',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    author='bschoi',
+    url='https://github.com/MIDASIT-Co-Ltd/engineers-api-python',
+    install_requires=['mdutils', "numpy", "matplotlib",],
+    )
