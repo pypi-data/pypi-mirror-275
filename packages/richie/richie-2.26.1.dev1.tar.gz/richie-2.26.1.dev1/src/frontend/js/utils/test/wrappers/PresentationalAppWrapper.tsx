d74c4a145ff57524ced6d9883e89c20626bada6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 04:03:16 +0000

    Add package richie/2.26.1.dev1/richie-2.26.1.dev1.tar.gz

---
+++ packages/richie/richie-2.26.1.dev1.tar.gz/richie-2.26.1.dev1/src/frontend/js/utils/test/wrappers/PresentationalAppWrapper.tsx
@@ -0,0 +1,18 @@
+import { PropsWithChildren } from 'react';
+import { CunninghamProvider } from '@openfun/cunningham-react';
+import { IntlWrapper, IntlWrapperProps } from './IntlWrapper';
+import { RouterWrapper, RouterWrapperProps } from './RouterWrapper';
+
+export const PresentationalAppWrapper = ({
+  children,
+  intlOptions,
+  routerOptions,
+}: PropsWithChildren<{ intlOptions?: IntlWrapperProps; routerOptions?: RouterWrapperProps }>) => {
+  return (
+    <IntlWrapper {...intlOptions}>
+      <CunninghamProvider>
+        <RouterWrapper {...routerOptions}>{children}</RouterWrapper>
+      </CunninghamProvider>
+    </IntlWrapper>
+  );
+};
