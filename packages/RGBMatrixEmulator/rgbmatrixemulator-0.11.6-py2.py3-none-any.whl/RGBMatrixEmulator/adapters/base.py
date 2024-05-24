c7686536007a5193b1bd09e19e38ad898b28afda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 16:03:57 +0000

    Add package RGBMatrixEmulator/0.11.6/rgbmatrixemulator-0.11.6-py2.py3-none-any.whl

---
+++ packages/RGBMatrixEmulator/rgbmatrixemulator-0.11.6-py2.py3-none-any.whl/RGBMatrixEmulator/adapters/base.py
@@ -0,0 +1,111 @@
+import numpy as np
+
+from PIL import Image, ImageDraw, ImageEnhance
+from RGBMatrixEmulator import version
+from RGBMatrixEmulator.graphics import Color
+
+def draw_circle_mask(drawer, x, y, pixel_size, color):
+    drawer.ellipse(
+        (x, y, x + pixel_size - 1, y + pixel_size - 1),
+        fill=color,
+        outline=color,
+    )
+
+def draw_square_mask(drawer, x, y, pixel_size, color):
+    drawer.rectangle(
+        (x, y, x + pixel_size, y + pixel_size),
+        fill=color,
+        outline=color,
+        )
+
+class BaseAdapter:
+
+    SUPPORTS_ALTERNATE_PIXEL_STYLE = False
+    INSTANCE = None
+
+    def __init__(self, width, height, options):
+        self.width   = width
+        self.height  = height
+        self.options = options
+        self.__black = Image.new("RGB", self.options.window_size(), "black")
+        self.__mask = self.__draw_mask()
+        self.loaded  = False
+
+    def __draw_mask(self):
+        mask = Image.new("L", self.options.window_size())
+        drawer = ImageDraw.Draw(mask)
+        pixel_size = self.options.pixel_size
+        width, height = self.options.window_size()
+        color = int((self.options.brightness * 255) / 100)
+        draw_mask_shape = (draw_circle_mask
+            if self.options.pixel_style == "circle" else draw_square_mask)
+        for y in range(0, height, pixel_size):
+            for x in range(0, width, pixel_size):
+                draw_mask_shape(drawer, x, y, pixel_size, color)
+
+        return mask
+
+    @classmethod
+    def get_instance(cls, *args, **kwargs):
+        if cls.INSTANCE is None:
+            instance = cls(*args, **kwargs)
+            cls.INSTANCE = instance
+        
+        return cls.INSTANCE
+
+    def adjust_pixel_brightness(self, pixel, to_int = True):
+        alpha = self.options.brightness / 100.0
+        return Color.adjust_brightness(pixel, alpha, to_int = to_int)
+
+    def pixel_out_of_bounds(self, x, y):
+        if x < 0 or x >= self.width:
+            return True
+
+        if y < 0 or y >= self.height:
+            return True
+
+        return False
+
+    def _get_masked_image(self, pixels):
+        image = Image.fromarray(np.array(pixels, dtype=np.uint8), "RGB")
+        image = image.resize(self.options.window_size(), Image.NEAREST)
+        enhancer = ImageEnhance.Brightness(image)
+        image = enhancer.enhance(self.options.brightness / 100.0)
+
+        return Image.composite(image, self.__black, self.__mask)
+
+    def emulator_details_text(self):
+        details_text = 'RGBME v{} - {}x{} Matrix | {}x{} Chain | {}px per LED ({}) | {}'
+
+        return details_text.format(version.__version__,
+                                   self.options.cols,
+                                   self.options.rows,
+                                   self.options.chain_length,
+                                   self.options.parallel,
+                                   self.options.pixel_size,
+                                   self.options.pixel_style.upper(),
+                                   self.__class__.__name__)
+
+    # This method is required for the pygame adapter but nothing else, so just skip it if not defined.
+    def check_for_quit_event(self):
+        pass
+
+    #############################################################
+    # These methods must be implemented by BaseAdapter subclasses
+    #############################################################
+    def load_emulator_window(self):
+        '''
+        Initialize the external dependency as a graphics display.
+
+        This method is fired when the emulated canvas is initialized.
+        '''
+        raise NotImplementedError
+
+    def draw_to_screen(self, _pixels):
+        '''
+        Accepts a 2D array of pixels of size height x width.
+
+        Implements drawing each pixel to the screen via the external dependency loaded in load_emulator_window.
+        Before drawing, use adjust_pixel_brightness() on each pixel if your display adapter supports it.
+        '''
+        raise NotImplementedError
---
+++ packages/RGBMatrixEmulator/rgbmatrixemulator-0.11.6-py2.py3-none-any.whl/RGBMatrixEmulator/adapters/pygame_adapter.py
@@ -0,0 +1,71 @@
+import numpy as np
+import os
+import sys
+
+# Try to suppress the pygame load warning if able.
+try:
+    os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+except Exception:
+    pass
+
+import pygame
+
+from PIL import Image
+from pygame.locals import QUIT
+from RGBMatrixEmulator.adapters.base import BaseAdapter
+from RGBMatrixEmulator.graphics import Color
+from RGBMatrixEmulator.logger import Logger
+
+
+class PygameAdapter(BaseAdapter):
+
+    SUPPORTS_ALTERNATE_PIXEL_STYLE = True
+
+    def __init__(self, width, height, options):
+        super().__init__(width, height, options)
+        self.__surface = None
+
+    def load_emulator_window(self):
+        if self.loaded:
+            return
+
+        Logger.info('Loading {}'.format(self.emulator_details_text()))
+        self.__surface = pygame.display.set_mode(self.options.window_size())
+        pygame.init()
+
+        self.__set_emulator_icon()
+        pygame.display.set_caption(self.emulator_details_text())
+
+        self.loaded = True
+
+    def draw_to_screen(self, pixels):
+        image = self._get_masked_image(pixels)
+        pygame_surface = pygame.image.fromstring(image.tobytes(),
+            self.options.window_size(), "RGB")
+        self.__surface.blit(pygame_surface, (0, 0))
+
+        pygame.display.flip()
+
+    def check_for_quit_event(self):
+        # We don't have events, but this will keep the emulator from appearing as if it's not responding.
+        # This also enables closing the window to kill the emulator
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+    def __set_emulator_icon(self):
+        emulator_path = os.path.abspath(os.path.dirname(__file__))
+        icon_path = os.path.join(emulator_path, '..', 'icon.png')
+        icon = pygame.image.load(os.path.normpath(icon_path))
+
+        pygame.display.set_icon(icon)
+
+    def __pygame_pixel(self, col, row):
+        return pygame.Rect(
+            col * self.options.pixel_size,
+            row * self.options.pixel_size,
+            self.options.pixel_size,
+            self.options.pixel_size
+        )
+
