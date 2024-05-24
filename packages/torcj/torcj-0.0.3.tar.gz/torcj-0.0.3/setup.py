07fafe92096c4f1060c643c7731b469f60f85d03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:31 +0000

    Add package torcj/0.0.3/torcj-0.0.3.tar.gz

---
+++ packages/torcj/torcj-0.0.3.tar.gz/torcj-0.0.3/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name="torcj",
+    version="0.0.3",
+    author="Example",
+    description="An example package",
+    packages=["torcj"],
+)
---
+++ packages/torcj/torcj-0.0.3.tar.gz/torcj-0.0.3/torcj/main.py
@@ -0,0 +1,51 @@
+import os
+import json
+
+
+class TextWriter:
+    def __init__(self, json_file: str) -> None:
+        with open(json_file, "r") as file:
+            self.data = json.load(file)
+
+    def print_names(self) -> None:
+        print([i for i in self.data])
+
+    def print_file(self, name) -> str:
+        print(self.data[name])
+        return self.data[name]
+
+    def write_file(self, name: str) -> None:
+        with open("main.py", "w") as f:
+            f.write(self.data[name])
+
+
+class TextStore:
+    def read_python_files(directory: str) -> None:
+        python_files = [
+            file
+            for file in os.listdir(directory)
+            if file.endswith(".py") or file.endswith(".ipynb")
+        ]
+        files_data = {}
+
+        for file_name in python_files:
+            file_path = os.path.join(directory, file_name)
+            with open(file_path, "r") as file:
+                content = file.read()
+                files_data[file_name] = content
+
+        return files_data
+
+    def save_to_json(data, output_file):
+        with open(output_file, "w") as json_file:
+            json.dump(data, json_file, indent=4)
+
+
+if __name__ == "__main__":
+    base_dir = os.path.dirname(__file__)
+    directory_path = os.path.join(base_dir, "programs")
+    json_output_file = os.path.join(base_dir, "storage.json")
+
+    files_data = TextStore.read_python_files(directory_path)
+    TextStore.save_to_json(files_data, json_output_file)
+    print("Data successfully stored in", json_output_file)
