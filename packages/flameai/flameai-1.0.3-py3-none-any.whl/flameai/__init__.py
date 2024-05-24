b1dbd06390dd45df3233640ca71f4f4f96497365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 05:03:47 +0000

    Add package flameai/1.0.3/flameai-1.0.3-py3-none-any.whl

---
+++ packages/flameai/flameai-1.0.3-py3-none-any.whl/flameai/__init__.py
@@ -0,0 +1,15 @@
+from flameai import util
+from flameai import preprocessing
+from flameai import train
+from flameai import metrics
+from flameai import mining
+from flameai import plot
+
+__all__ = [
+    'util',
+    'preprocessing',
+    'train',
+    'metrics',
+    'mining',
+    'plot',
+]
---
+++ packages/flameai/flameai-1.0.3-py3-none-any.whl/flameai/cmd.py
@@ -0,0 +1,42 @@
+import os
+import subprocess
+import click
+
+from .util import set_logger
+from ._env import check_hive_env
+
+
+logger = set_logger()
+
+
+@click.command()
+@click.option('-n', '--name', default='World', help='Name to greet')
+def hey(name: str) -> None:
+    """Print Hey, {name}!"""
+    click.echo(f"Hey, {name}!")
+
+
+@click.command()
+@click.argument('file_name', type=str)
+def hive_cli(file_name: str) -> None:
+    """Execute Hive query and redirect the output to a CSV file."""
+    if not os.path.isfile(f'{file_name}.hql'):
+        logger.warning(f'{file_name}.hql not found.')
+    elif check_hive_env() != 0:
+        logger.warning("Hive not found. Please install Hive and add it to your PATH.")
+    else:
+        command = f'hive -f {file_name}.hql > {file_name}.csv'
+        logger.info(f'Run `{command}`')
+
+        try:
+            res = subprocess.run(command, shell=True, text=True)
+            if res.returncode != 0:
+                logger.warning(f'Failed to execute query.')
+                logger.error(f'Error: {res.stderr}')
+                logger.error(f'returncode: {res.returncode}')
+        except Exception as e:
+            logger.error(f'An Error occurred: {e}')
+
+
+if __name__ == "__main__":
+    hive_cli()
