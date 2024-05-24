83be11ca268f070167a3cf1b4d70a817553cfb9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 05:03:19 +0000

    Add package modelscan/0.7.4/modelscan-0.7.4-py3-none-any.whl

---
+++ packages/modelscan/modelscan-0.7.4-py3-none-any.whl/modelscan/skip.py
@@ -0,0 +1,44 @@
+import logging
+from enum import Enum
+
+
+logger = logging.getLogger("modelscan")
+
+
+class SkipCategories(Enum):
+    SCAN_NOT_SUPPORTED = 1
+    BAD_ZIP = 2
+    MODEL_CONFIG = 3
+    H5_DATA = 4
+    NOT_IMPLEMENTED = 5
+    MAGIC_NUMBER = 6
+
+
+class Skip:
+    scan_name: str
+    category: SkipCategories
+    message: str
+    source: str
+
+    def __init__(self) -> None:
+        pass
+
+    def __str__(self) -> str:
+        raise NotImplementedError()
+
+
+class ModelScanSkipped:
+    def __init__(
+        self,
+        scan_name: str,
+        category: SkipCategories,
+        message: str,
+        source: str,
+    ) -> None:
+        self.scan_name = scan_name
+        self.category = category
+        self.message = message
+        self.source = str(source)
+
+    def __str__(self) -> str:
+        return f"The following file {self.source} was skipped during a {self.scan_name} scan: \n{self.message}"
