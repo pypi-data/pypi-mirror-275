4d6e26556d4188ded5e2d2fd3b7ac0fab8818f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 04:03:09 +0000

    Add package imagespec-fast-builder/0.0.20/imagespec_fast_builder-0.0.20.tar.gz

---
+++ packages/imagespec-fast-builder/imagespec_fast_builder-0.0.20.tar.gz/imagespec_fast_builder-0.0.20/experiments/check_builder.py
@@ -0,0 +1,18 @@
+from flytekit import ImageSpec
+
+from imagespec_fast_builder import FastImageBuilder
+from flytekit.image_spec.image_spec import ImageBuildEngine
+
+folding_img = ImageSpec(
+    name="unionbio-protein",
+    python_version="3.11",
+    conda_channels=["bioconda", "conda-forge"],
+    conda_packages=["prodigal", "biotite", "biopython", "py3Dmol"],
+    registry="ghcr.io/unionai-oss",
+    platform="linux/amd64",
+)
+# Generic way to build an image, if registry is set, then the image is automatically pushed
+# ImageBuildEngine().build(image_spec)
+
+# My image builder has a private not push command
+FastImageBuilder()._build_image(folding_img, push=False)
