00f9ad3e9baf1d924a46708695f5bbd3047496f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 04:03:11 +0000

    Add package sema4ai-action-server/0.11.0/sema4ai_action_server-0.11.0.tar.gz

---
+++ packages/sema4ai-action-server/sema4ai_action_server-0.11.0.tar.gz/sema4ai_action_server-0.11.0/src/sema4ai/action_server/_cli_helpers.py
@@ -0,0 +1,37 @@
+def add_data_args(parser, defaults):
+    parser.add_argument(
+        "-d",
+        "--datadir",
+        metavar="PATH",
+        default="",
+        help=(
+            "Directory to store the data for operating the actions server "
+            "(by default a datadir will be generated based on the current directory)."
+        ),
+    )
+    parser.add_argument(
+        "--db-file",
+        help=(
+            "The name of the database file, relative to the datadir "
+            "(default: %(default)s)"
+        ),
+        default=defaults["db_file"],
+    )
+
+
+def add_verbose_args(parser, defaults):
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        default=False,
+        help="Be more talkative (default: %(default)s)",
+    )
+
+
+def add_json_output_args(parser):
+    parser.add_argument(
+        "--json",
+        help="Write output to stdout in json format",
+        action="store_true",
+    )
