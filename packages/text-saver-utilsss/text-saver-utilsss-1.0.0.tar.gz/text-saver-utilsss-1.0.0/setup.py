6d8cbd17df8d171fefa657d00b6fd97f3d71bf7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 16:05:06 +0000

    Add package text-saver-utilsss/1.0.0/text-saver-utilsss-1.0.0.tar.gz

---
+++ packages/text-saver-utilsss/text-saver-utilsss-1.0.0.tar.gz/text-saver-utilsss-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+setup(
+  name='text-saver-utilsss',
+  version='1.0.0',
+  description='This is my first module',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  packages=find_packages(),
+  install_requires=['requests>=2.25.1', 'pytelegrambotapi>=4.18.1'],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='example python',
+  python_requires='>=3.11'
+)
