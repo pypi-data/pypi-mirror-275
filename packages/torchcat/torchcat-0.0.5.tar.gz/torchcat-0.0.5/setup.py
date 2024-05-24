07721059e97167346f4342ba1cf9c7ac7a665626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 16:04:59 +0000

    Add package torchcat/0.0.5/torchcat-0.0.5.tar.gz

---
+++ packages/torchcat/torchcat-0.0.5.tar.gz/torchcat-0.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open('README.rst', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='torchcat',
+    version='0.0.5',
+    author='kaiyu',
+    author_email='2971934557@qq.com',
+    license='GPL',
+    url='https://pypi.org/project/torchcat/',
+    description='用于简化 torch 模型训练的工具',
+    long_description=long_description,
+    packages=['torchcat'],
+    install_requires=['numpy', 'torchsummary'],
+)
+
+'''
+python -m build
+
+python -m twine upload --repository pypi dist/*
+'''
