5c9c049c1bd5dd1ff7334c91949451aa98f7e6ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:36 +0000

    Add package mfprocess/1.0.14/mfprocess-1.0.14-py3-none-any.whl

---
+++ packages/mfprocess/mfprocess-1.0.14-py3-none-any.whl/mfprocess/mes.py
@@ -0,0 +1,32 @@
+from pyvis.network import Network
+import pandas as pd
+
+def ex():
+  rels = [
+      
+      ["Fred", "George"],
+      ["Harry", "Rita"],
+      ["Fred", "Ginny"],
+      ["Tom", "Ginny"],
+      ["Harry", "Ginny"]
+      
+  ]
+  
+  for rel in rels:
+      source, dest = rel
+      net.add_node(source)
+      net.add_node(dest)
+      net.add_edge(source, dest)
+  net.toggle_physics(False)
+  return net.html
+
+def ex2(df):
+  rels = df.to_numpy()
+  
+  for rel in rels:
+      source, dest = rel
+      net.add_node(source)
+      net.add_node(dest)
+      net.add_edge(source, dest)
+  net.toggle_physics(False)
+  return net.html
