58674d6f33321a00ecf31594876782679e0dfe4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 07:03:20 +0000

    Add package uav-collision-avoidance/0.8.5/uav_collision_avoidance-0.8.5-py3-none-any.whl

---
+++ packages/uav-collision-avoidance/uav_collision_avoidance-0.8.5-py3-none-any.whl/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/tests/test_headless.py
@@ -0,0 +1,23 @@
+import sys
+import pytest
+from PySide6.QtWidgets import QApplication
+from main import *
+from . import Simulation, SimulationSettings
+
+def test_headless():
+    with pytest.raises(SystemExit) as e:
+        main("headless")
+    assert e.value.code == 0
+
+def test_headless_csv():
+    with pytest.raises(SystemExit) as e:
+        app = QApplication()
+        SimulationSettings.__init__()
+        sim = Simulation(headless = True)
+        assert sim.load_simulation_data_from_file("data/simulation-2024-05-15-17-37-59.csv", test_id = 0, avoid_collisions = False)
+        sim.run_headless(avoid_collisions = False)
+        assert sim.load_simulation_data_from_file("data/simulation-2024-05-15-17-37-59.csv", test_id = 0, avoid_collisions = True)
+        sim.run_headless(avoid_collisions = True)
+        QApplication.shutdown(app)
+        sys.exit(0)
+    assert e.value.code == 0
