e69fe2af7ca70cf3a590117512f9bb5e34f12e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:52 +0000

    Add package npeccv6/0.0.3/npeccv6-0.0.3-py3-none-any.whl

---
+++ packages/npeccv6/npeccv6-0.0.3-py3-none-any.whl/npeccv6/__init__.py
@@ -0,0 +1,9 @@
+version = "0.0.1"
+author = "Hubert Waleńczak, Nick Belterman, Amyr Lourensz, Marwa Rouah, Cristian Stîngă"
+
+from .model import *
+from .data import *
+from .predict import *
+from .postprocessing import *
+from .train import *
+from .utils import *
