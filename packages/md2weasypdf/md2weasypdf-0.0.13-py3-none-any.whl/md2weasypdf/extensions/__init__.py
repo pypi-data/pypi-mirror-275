3bddb947e7531b9aeff65ac868857bbac3ec6958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 16:03:58 +0000

    Add package md2weasypdf/0.0.13/md2weasypdf-0.0.13-py3-none-any.whl

---
+++ packages/md2weasypdf/md2weasypdf-0.0.13-py3-none-any.whl/md2weasypdf/extensions/__init__.py
@@ -0,0 +1,12 @@
+from markdown.extensions.abbr import AbbrExtension
+from markdown.extensions.fenced_code import FencedCodeExtension
+from markdown.extensions.tables import TableExtension
+
+from .checkbox import CheckboxExtension
+from .footnotes import FootnoteExtension
+from .mermaid import MermaidExtension
+from .subscript import SubscriptExtension
+from .table_caption import TableCaptionExtension
+from .textbox import TextboxExtension
+from .toa import ToaExtension
+from .toc import TocExtension
