d9a94e6b0e68ef4f00c878f1c5a69928350453a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 06:04:19 +0000

    Add package sdl-webui-lite/0.1.2/sdl-webui-lite-0.1.2.tar.gz

---
+++ packages/sdl-webui-lite/sdl-webui-lite-0.1.2.tar.gz/sdl-webui-lite-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sdl-webui-lite',
+    version='0.1.2',
+    packages=find_packages(),
+    include_package_data=True,
+    description='This web UI aims to ease up the control of any Python-based SDLs by displaying functions and their parameters dynamically.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Ivory Zhang',
+    author_email='ivoryzhang@chem.ubc.ca',
+    license='MIT',
+    install_requires=[
+        'Flask-SocketIO',  # List all dependencies here
+    ],
+    url='https://gitlab.com/heingroup/sdl_webui_lite'
+)
