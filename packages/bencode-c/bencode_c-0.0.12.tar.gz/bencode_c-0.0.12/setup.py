cd61a6483377313fd8a59cc56482acd65506cf2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 05:03:31 +0000

    Add package bencode-c/0.0.12/bencode_c-0.0.12.tar.gz

---
+++ packages/bencode-c/bencode_c-0.0.12.tar.gz/bencode_c-0.0.12/setup.py
@@ -0,0 +1,18 @@
+from glob import glob
+
+from setuptools import setup, Extension, find_packages
+
+module = Extension(
+    "bencode_c._bencode",
+    sources=glob("./src/bencode_c/*.c"),
+    include_dirs=["./src/bencode_c"],
+    py_limited_api=True,
+)
+
+setup(
+    ext_modules=[module],
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    package_data={"": ["*.h", "*.c", "*.pyi", "py.typed"]},
+    options={"bdist_wheel": {"py_limited_api": "cp38"}},
+)
