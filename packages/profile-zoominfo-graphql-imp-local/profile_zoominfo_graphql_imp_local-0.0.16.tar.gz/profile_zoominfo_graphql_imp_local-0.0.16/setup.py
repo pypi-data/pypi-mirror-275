e3e6231cf7ad43fb9d472939ced5f7b8b77d839d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:13 +0000

    Add package profile-zoominfo-graphql-imp-local/0.0.16/profile_zoominfo_graphql_imp_local-0.0.16.tar.gz

---
+++ packages/profile-zoominfo-graphql-imp-local/profile_zoominfo_graphql_imp_local-0.0.16.tar.gz/profile_zoominfo_graphql_imp_local-0.0.16/setup.py
@@ -0,0 +1,29 @@
+import setuptools  
+
+PACKAGE_NAME = "profile-zoominfo-graphql-imp-local"
+package_dir = PACKAGE_NAME.replace("-", "_")
+
+setuptools.setup(
+    name=PACKAGE_NAME,  # https://pypi.org/project/profile-zoominfo-graphql-imp-local
+    version='0.0.16',
+    author="Circles",
+    author_email="info@circlez.ai",
+    url=f"https://github.com/circles-zone/{PACKAGE_NAME}-python-package",
+    packages=[package_dir],
+    package_dir={package_dir: f"{package_dir}/src"},
+    package_data={package_dir: ['*.py']},
+    long_description="Profile ZoomInfo GraphQL Implementation Local Python Package",
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "zoomus>=1.2.1",
+        "language-remote>=0.0.20",
+        "logger-local>=0.0.135",
+        "profile-local>=0.0.64",
+        "python-sdk-remote>=0.0.93",
+    ]
+)
