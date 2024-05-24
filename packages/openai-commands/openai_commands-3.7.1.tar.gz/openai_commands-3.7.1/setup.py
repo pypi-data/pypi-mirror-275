f50425cd67bbdd121c04a4091ba167187709e0fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 15:03:58 +0000

    Add package openai-commands/3.7.1/openai_commands-3.7.1.tar.gz

---
+++ packages/openai-commands/openai_commands-3.7.1.tar.gz/openai_commands-3.7.1/setup.py
@@ -0,0 +1,11 @@
+from openai_commands import NAME, VERSION, DESCRIPTION
+from blueness.pypi import setup
+
+setup(
+    filename=__file__,
+    repo_name="openai-commands",
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=[NAME],
+)
