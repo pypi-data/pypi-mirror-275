f14389b376a308f33a665b857be0af9a13971e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:44 +0000

    Add package composio-autogen/0.2.63/composio_autogen-0.2.63.tar.gz

---
+++ packages/composio-autogen/composio_autogen-0.2.63.tar.gz/composio_autogen-0.2.63/composio_autogen/__init__.py
@@ -0,0 +1,12 @@
+from composio import Action, App, Tag
+
+from .autogen_toolspec import ComposioToolset, client
+
+
+__all__ = (
+    "Action",
+    "App",
+    "Tag",
+    "client",
+    "ComposioToolset",
+)
