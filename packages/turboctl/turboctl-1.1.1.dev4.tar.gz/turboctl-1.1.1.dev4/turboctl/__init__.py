b2a5fa38f3e24b6b843c7ba07ad0cea7d5a8371e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:31 +0000

    Add package turboctl/1.1.1.dev4/turboctl-1.1.1.dev4.tar.gz

---
+++ packages/turboctl/turboctl-1.1.1.dev4.tar.gz/turboctl-1.1.1.dev4/turboctl/__init__.py
@@ -0,0 +1,9 @@
+from datetime import date
+
+
+__version__ = '1.1.1.dev4'
+"""The current version of the TurboCtl project."""
+
+
+copyright = f'2019-{date.today().year} University of Helsinki Fusor Team'
+"""Automatically updating copyright information."""
