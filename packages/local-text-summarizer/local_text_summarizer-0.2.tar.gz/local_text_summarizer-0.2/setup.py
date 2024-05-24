1cfb5347a7e6d3b6ec1dc2156ae2a2a08281ad2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 05:03:32 +0000

    Add package local-text-summarizer/0.2/local_text_summarizer-0.2.tar.gz

---
+++ packages/local-text-summarizer/local_text_summarizer-0.2.tar.gz/local_text_summarizer-0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='local_text_summarizer',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'nltk',
+    ],
+    entry_points={
+        'console_scripts': [
+            'summarize=local_text_summarizer.summarize:main',
+        ],
+    },
+    author='Fabian Anguiano',
+    author_email='fabiananguiano@gmail.com',
+    description='A package for simple local text summarization.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/tunahorse/local_text_summarizer',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
