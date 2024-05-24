a7bfc80e1595c0d8abf4e512611e5e90827cdb24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 14:03:18 +0000

    Add package torchao-nightly/2024.5.24/torchao_nightly-2024.5.24-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/torchao-nightly/torchao_nightly-2024.5.24-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/torchao/quantization/utils.py
@@ -0,0 +1,116 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+from typing import Dict, Optional, Tuple
+
+import torch
+from torch.utils._python_dispatch import TorchDispatchMode
+from packaging import version
+from functools import reduce
+from math import gcd
+
+
+__all__ = [
+    "find_multiple",
+    "compute_error",
+    "_apply_logging_hook",
+    "get_model_size_in_bytes",
+    "TORCH_VERSION_AFTER_2_3",
+]
+
+
+def find_multiple(n: int, *args: Tuple[int]) -> int:
+    k: int = reduce(lambda x, y: x * y // gcd(x, y), args + (1,))  # type: ignore[9]
+    if n % k == 0:
+        return n
+    return n + k - (n % k)
+
+
+# basic SQNR
+
+
+def compute_error(x, y):
+    Ps = torch.linalg.norm(x)
+    Pn = torch.linalg.norm(x - y)
+    return 20 * torch.log10(Ps / Pn)
+
+
+# logger for fqn + op + shape
+# note: not safe for any kind of multithreading
+_cur_fqn: Optional[str] = None
+
+
+def _get_logging_hook(fqn):
+
+    def forward_hook(module, input):
+        global _cur_fqn
+        _cur_fqn = fqn
+
+    return forward_hook
+
+
+def _apply_logging_hook(model):
+    for name, mod in model.named_modules():
+        mod.register_forward_pre_hook(_get_logging_hook(name))
+
+
+# collections.defaultdict printing is weird with lambdas, so hand writing for now
+_fqn_to_op_to_shape_to_count: Dict[
+    Optional[str], Dict[Optional[str], Dict[Optional[str], int]]
+] = {}
+
+
+class LoggingTensorMode(TorchDispatchMode):
+
+    def __torch_dispatch__(self, func, types, args=(), kwargs=None):
+        if kwargs is None:
+            kwargs = {}
+        rs = func(*args, **kwargs)
+        global _cur_fqn
+        op_name: str = f"{func.__module__}.{func.__name__}"
+        shape_str = ""
+        for arg in args:
+            if isinstance(arg, torch.Tensor):
+                shape_str += str(list(arg.shape)) + ", "
+        if shape_str != "":
+            shape_str = shape_str[:-2]
+
+        if _cur_fqn not in _fqn_to_op_to_shape_to_count:
+            _fqn_to_op_to_shape_to_count[_cur_fqn] = {}
+        if op_name not in _fqn_to_op_to_shape_to_count[_cur_fqn]:
+            _fqn_to_op_to_shape_to_count[_cur_fqn][op_name] = {}
+        if shape_str not in _fqn_to_op_to_shape_to_count[_cur_fqn][op_name]:
+            _fqn_to_op_to_shape_to_count[_cur_fqn][op_name][shape_str] = 0
+        _fqn_to_op_to_shape_to_count[_cur_fqn][op_name][shape_str] += 1
+
+        return rs
+
+
+# https://discuss.pytorch.org/t/finding-model-size/130275
+
+
+def get_model_size_in_bytes(model):
+    s = 0
+    for p in model.parameters():
+        s += p.nelement() * p.element_size()
+    for b in model.buffers():
+        s += b.nelement() * b.element_size()
+    return s
+
+# TODO: quantization namespace is not the right place ot have this
+if version.parse(torch.__version__) >= version.parse("2.4.0.dev"):
+    TORCH_VERSION_AFTER_2_4 = True
+else:
+    TORCH_VERSION_AFTER_2_4 = False
+
+if version.parse(torch.__version__) >= version.parse("2.3.0.dev"):
+    TORCH_VERSION_AFTER_2_3 = True
+else:
+    TORCH_VERSION_AFTER_2_3 = False
+
+if version.parse(torch.__version__) >= version.parse("2.2.0.dev"):
+    TORCH_VERSION_AFTER_2_2 = True
+else:
+    TORCH_VERSION_AFTER_2_2 = False
