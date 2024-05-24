4ce122ea0aebbe2ef1b8100200f2b52c846bca6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 17:03:50 +0000

    Add package biobb-flexdyn/4.2.0/biobb_flexdyn-4.2.0.tar.gz

---
+++ packages/biobb-flexdyn/biobb_flexdyn-4.2.0.tar.gz/biobb_flexdyn-4.2.0/biobb_flexdyn/__init__.py
@@ -0,0 +1,3 @@
+name = "biobb_flexdyn"
+__all__ = ["flexdyn"]
+__version__ = "4.2.0"
---
+++ packages/biobb-flexdyn/biobb_flexdyn-4.2.0.tar.gz/biobb_flexdyn-4.2.0/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="biobb_flexdyn",
+    version="4.2.0",
+    author="Biobb developers",
+    author_email="adam.hospital@irbbarcelona.org",
+    description="biobb_flexdyn is a BioBB category for studies on the conformational landscape of native proteins.",
+    long_description="biobb_flexdyn allows the generation of protein conformational ensembles from 3D structures and the analysis of its molecular flexibility.",
+    long_description_content_type="text/markdown",
+    keywords="3D-Bioinfo ELIXIR FlexDyn Bioinformatics Workflows BioExcel Compatibility Flexibility Ensembles Protein Structure",
+    url="https://github.com/bioexcel/biobb_flexdyn",
+    project_urls={
+        "Documentation": "http://biobb-flexdyn.readthedocs.io/en/latest/",
+        "Bioexcel": "https://bioexcel.eu/"
+    },
+    packages=setuptools.find_packages(exclude=['docs', 'test']),
+    include_package_data=True,
+    install_requires=['biobb_common==4.2.0'],
+    python_requires='>=3.8',
+    entry_points={
+        "console_scripts": [
+            "concoord_dist = biobb_flexdyn.flexdyn.concoord_dist:main",
+            "concoord_disco = biobb_flexdyn.flexdyn.concoord_disco:main",
+            "imod_imode = biobb_flexdyn.flexdyn.imod_imode:main",
+            "imod_imove = biobb_flexdyn.flexdyn.imod_imove:main",
+            "imod_imc = biobb_flexdyn.flexdyn.imod_imc:main",
+            "nolb_nma = biobb_flexdyn.flexdyn.nolb_nma:main",
+            "prody_anm = biobb_flexdyn.flexdyn.prody_anm:main"
+        ]
+    },
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+    ),
+)
