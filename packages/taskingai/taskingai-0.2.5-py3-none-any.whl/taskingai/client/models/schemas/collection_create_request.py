383eb65fd78441c5a7ac2a10ad14e0bd5e4584ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 16:04:58 +0000

    Add package taskingai/0.2.5/taskingai-0.2.5-py3-none-any.whl

---
+++ packages/taskingai/taskingai-0.2.5-py3-none-any.whl/taskingai/client/models/schemas/collection_create_request.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# collection_create_request.py
+
+"""
+This script is automatically generated for TaskingAI python client
+Do not modify the file manually
+
+Author: James Yao
+Organization: TaskingAI
+License: Apache 2.0
+"""
+
+from pydantic import BaseModel, Field
+from typing import Dict
+
+
+__all__ = ["CollectionCreateRequest"]
+
+
+class CollectionCreateRequest(BaseModel):
+    name: str = Field("")
+    description: str = Field("")
+    capacity: int = Field(1000)
+    embedding_model_id: str = Field(...)
+    metadata: Dict[str, str] = Field({})
