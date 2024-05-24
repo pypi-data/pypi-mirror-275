bf4a9050f11bec60babf29a0ff9b8657cf20e8dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 04:03:04 +0000

    Add package panda-server-sdk/1.0.0/panda_server_sdk-1.0.0-py3-none-any.whl

---
+++ packages/panda-server-sdk/panda_server_sdk-1.0.0-py3-none-any.whl/panda_server_sdk/panda_server_sdk_keys.py
@@ -0,0 +1,16 @@
+"""
+Python SDK external package keys
+"""
+
+
+class TestingPandaPythonSDKKEYS():  # pylint: disable=too-few-public-methods
+    """
+    Class that contains default keys from .env file
+    """
+    TP_TITLE = 'TP_TITLE'
+    TP_BRANCH = 'TP_BRANCH'
+    TP_BROWSER = 'TP_BROWSER'
+    TP_RERUNS = 'TP_RERUNS'
+    TP_PARALLELS = 'TP_PARALLELS'
+    TP_CASES = 'TP_CASES'
+    TP_ARGUMENTS = 'TP_ARGUMENTS'
