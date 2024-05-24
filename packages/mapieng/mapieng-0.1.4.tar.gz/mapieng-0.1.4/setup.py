9fa56ec0110c1a84ad447a373b1a28a2d9db6153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 16:04:26 +0000

    Add package mapieng/0.1.4/mapieng-0.1.4.tar.gz

---
+++ packages/mapieng/mapieng-0.1.4.tar.gz/mapieng-0.1.4/setup.py
@@ -0,0 +1,29 @@
+import sys
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README_en.md', encoding='utf-8') as f:
+        return f.read()
+
+
+# platform에 따른 install_requires
+install_requires = []
+if "pyodide" in sys.modules:
+    install_requires = ['mdutils', "numpy", "matplotlib"]
+else:
+    install_requires = ['mdutils', "numpy", "matplotlib"]
+
+
+setup(
+    name='mapieng',
+    version='0.1.4',
+    packages=find_packages(),
+    include_package_data=True,
+    description='mapi engineers',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    author='bschoi',
+    url='https://github.com/MIDASIT-Co-Ltd/engineers-api-python',
+    install_requires=install_requires
+    )
