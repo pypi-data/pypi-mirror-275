9e469a1f1471c2393ebe0a1ad9f9ec60b39e635f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 04:03:02 +0000

    Add package ai-games/0.1.3/ai_games-0.1.3.tar.gz

---
+++ packages/ai-games/ai_games-0.1.3.tar.gz/ai_games-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+# Read the contents of your README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ai_games',
+    version='0.1.3',
+    author='Coding with BM',
+    description='AI Games: Collection of interactive games with AI opponents for Python.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/bzm10/ai_games',
+    packages=find_packages(),
+    install_requires=[
+        'pygame'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
