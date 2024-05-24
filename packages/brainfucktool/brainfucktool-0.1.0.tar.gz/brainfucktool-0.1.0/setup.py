bc2e7adf0ae51ed6a259fe3decff5da20495883e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 17:03:59 +0000

    Add package brainfucktool/0.1.0/brainfucktool-0.1.0.tar.gz

---
+++ packages/brainfucktool/brainfucktool-0.1.0.tar.gz/brainfucktool-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+name='brainfucktool',
+version='0.1.0',
+author='omid',
+author_email='omid377773@gmail.com',
+description='This is the most complete "brainfuck" language tool',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3'
+],
+python_requires='>=3.8',
+)
