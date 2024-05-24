ad1affc74c5720ba4c264f0eaabcf94f92557cbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 07:03:20 +0000

    Add package drowzee/0.1.1/drowzee-0.1.1.tar.gz

---
+++ packages/drowzee/drowzee-0.1.1.tar.gz/drowzee-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+# Leer el contenido del archivo README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="drowzee", 
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Ubaldo Jóse",
+    description="Una biblioteca para consultar cursos de hack4u.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://hack4u.io",
+)
