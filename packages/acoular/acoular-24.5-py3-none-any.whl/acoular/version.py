2ae5c3e3fe738afc09f4e0afb71c59e7c3bbe8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 16:04:57 +0000

    Add package acoular/24.5/acoular-24.5-py3-none-any.whl

---
+++ packages/acoular/acoular-24.5-py3-none-any.whl/acoular/version.py
@@ -0,0 +1,8 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Acoular Development Team.
+# ------------------------------------------------------------------------------
+
+# separate file to find out about version without importing the acoular lib
+__author__ = 'Acoular Development Team'
+__date__ = '21 May 2024'
+__version__ = '24.05'
