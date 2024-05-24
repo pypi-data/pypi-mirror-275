70f3f347bfd84c0b387353ee77e30727c8c735e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 05:03:07 +0000

    Add package panevo-django-celery-email/4.0.0/panevo_django_celery_email-4.0.0.tar.gz

---
+++ packages/panevo-django-celery-email/panevo_django_celery_email-4.0.0.tar.gz/panevo_django_celery_email-4.0.0/djcelery_email/__about__.py
@@ -0,0 +1,16 @@
+__all__ = [
+    '__title__', '__summary__', '__uri__', '__version__', '__author__',
+    '__email__', '__license__', '__copyright__',
+]
+
+__title__ = 'panevo-django-celery-email'
+__summary__ = 'An async Django email backend using celery (fork)'
+__uri__ = 'https://github.com/panevo/django-celery-email'
+
+__version__ = '4.0.0'
+
+__author__ = 'Karlo Krakan'
+__email__ = 'karlo.krakan@panevo.com'
+
+__license__ = 'BSD'
+__copyright__ = 'Copyright 2015 {0}'.format(__author__)
