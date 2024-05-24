dad9a83f74a6aaa7925ce2b3d93e0ab619676189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 17:03:52 +0000

    Add package odoo14-addon-report-invoice-modifications/14.0.0.0.2/odoo14_addon_report_invoice_modifications-14.0.0.0.2-py3-none-any.whl

---
+++ packages/odoo14-addon-report-invoice-modifications/odoo14_addon_report_invoice_modifications-14.0.0.0.2-py3-none-any.whl/odoo/addons/report_invoice_modifications/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Report invoice modifications",
+    'version': '14.0.0.0.2',
+    'depends': ['account'],
+    'author': "Coopdevs Treball SCCL",
+    'website': 'https://coopdevs.org',
+    'category': "Cooperative management",
+    'description': "Report invoice modifications",
+    "license": "AGPL-3",
+    'data': [
+        "views/report_invoice.xml",
+    ],
+}
