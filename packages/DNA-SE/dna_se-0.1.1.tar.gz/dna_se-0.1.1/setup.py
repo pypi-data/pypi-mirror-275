3520de47055ecf469ecb8945d5d5d73702c958d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 15:03:58 +0000

    Add package DNA-SE/0.1.1/dna_se-0.1.1.tar.gz

---
+++ packages/DNA-SE/dna_se-0.1.1.tar.gz/dna_se-0.1.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DNA_SE", # Replace with your own username
+    version="0.1.1",
+    author="Qinshuo",
+    author_email="u3008680@connect.hku.hk",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/liuqs111/DNA-SE",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
