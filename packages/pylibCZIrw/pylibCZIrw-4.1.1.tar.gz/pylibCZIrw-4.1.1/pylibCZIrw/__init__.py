72cac339faca048707e6ca9c7cce447e699ed522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 14:03:24 +0000

    Add package pylibCZIrw/4.1.1/pylibCZIrw-4.1.1.tar.gz

---
+++ packages/pylibCZIrw/pylibCZIrw-4.1.1.tar.gz/pylibCZIrw-4.1.1/pylibCZIrw/__init__.py
@@ -0,0 +1,18 @@
+# pylibCZIrw is a Python wrapper for libCZI
+# Copyright (C) 2022 Carl Zeiss Microscopy GmbH
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# To obtain a commercial version please contact Carl Zeiss Microscopy GmbH.
+"""Contains sources and tests for this package."""
