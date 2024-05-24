542562f3bacbb9ea08d07310edebfec283892dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 06:04:19 +0000

    Add package nntool/0.3.3/nntool-0.3.3-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/nntool/nntool-0.3.3-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/nntool/utils/utils_module.pyi
@@ -0,0 +1,8 @@
+def get_current_time() -> str: ...
+def get_output_path(append_date: bool = ...) -> str:
+    """Get output path based on environment variable OUTPUT_PATH
+
+    :param append_date: append a children folder with the date time, defaults to True
+    :return: output path
+    """
+    ...
