10b7b1203a66db6830a96f8f13f7e6fcede7a2c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:04 +0000

    Add package drevalpy/1.0.0/drevalpy-1.0.0.tar.gz

---
+++ packages/drevalpy/drevalpy-1.0.0.tar.gz/drevalpy-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='drevalpy',
+    version='1.0.0',
+    author=' ',
+    description='Drug Response Evaluation of cancer cell line drug response models in a fair setting.',
+    long_description='todo',
+    long_description_content_type='text/markdown',
+    url='https://github.com/biomedbigdata/drp_model_suite',
+    packages=find_packages(), 
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.9', 
+    install_requires=[
+        'pandas',  
+        'numpy',     
+        'ray[tune]',
+        'scikit-learn',
+        'scipy',
+        'matplotlib',
+        'plotly'
+    ],
+    include_package_data=True,
+    package_data={'': ['models/Baselines/hyperparameters.yaml',
+                       'models/SimpleNeuralNetwork/hyperparameters.yaml']},
+)
