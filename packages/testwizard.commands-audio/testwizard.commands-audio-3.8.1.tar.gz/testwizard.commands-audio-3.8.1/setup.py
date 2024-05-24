539c794296d96c4209dd68af7b862914a8971fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:24 +0000

    Add package testwizard.commands-audio/3.8.1/testwizard.commands-audio-3.8.1.tar.gz

---
+++ packages/testwizard.commands-audio/testwizard.commands-audio-3.8.1.tar.gz/testwizard.commands-audio-3.8.1/setup.py
@@ -0,0 +1,80 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testwizard.commands-audio",
+    version="3.8.1",
+    author="Resillion - Belgium",
+    author_email="testwizard-support@resillion.com",
+    description="Testwizard Audio commands",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.resillion.com/testwizard/",
+    packages=['testwizard.commands_audio'],
+    install_requires=[
+        'testwizard.commands-core==3.8.1'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.3",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
