054932b2cee1d8e4b82891dc1d809af1b0df5440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 04:03:04 +0000

    Add package panda-server-sdk/1.0.0/panda_server_sdk-1.0.0.tar.gz

---
+++ packages/panda-server-sdk/panda_server_sdk-1.0.0.tar.gz/panda_server_sdk-1.0.0/setup.py
@@ -0,0 +1,14 @@
+"""
+Python SDK external package setup
+"""
+
+# Imports
+from setuptools import setup, find_packages
+
+# Setup
+setup(
+    name='panda_server_sdk',
+    version='1.0.0',
+    description='Python SDK for Panda Server',
+    packages=find_packages()
+)
