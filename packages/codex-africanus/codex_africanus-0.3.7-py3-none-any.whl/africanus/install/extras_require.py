a1ba438eaeaae927b2ea4f8d79539329052ce56f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:43 +0000

    Add package codex-africanus/0.3.7/codex_africanus-0.3.7-py3-none-any.whl

---
+++ packages/codex-africanus/codex_africanus-0.3.7-py3-none-any.whl/africanus/install/extras_require.py
@@ -0,0 +1,10 @@
+# flake8: noqaextras_require = {
+   'cuda': ['cupy >= 9.0.0', 'jinja2 >= 2.10'],
+   'dask': ['dask[array] >= 2.2.0'],
+   'jax': ['jax >= 0.2.11', 'jaxlib >= 0.1.65'],
+   'scipy': ['scipy >= 1.4.0'],
+   'astropy': ['astropy >= 4.0'],
+   'python-casacore': ['python-casacore >= 3.4.0, != 3.5.0'],
+   'ducc0': ['ducc0 >= 0.9.0'],
+   'testing': ['pytest', 'flaky'],
+}
