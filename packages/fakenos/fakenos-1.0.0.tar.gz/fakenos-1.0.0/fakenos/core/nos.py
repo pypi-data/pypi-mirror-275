85f5de4c06e6758cf13d1f5c7bf64752d3377d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:19 +0000

    Add package fakenos/1.0.0/fakenos-1.0.0.tar.gz

---
+++ packages/fakenos/fakenos-1.0.0.tar.gz/fakenos-1.0.0/fakenos/core/nos.py
@@ -0,0 +1,227 @@
+"""
+Network Operating Systems (NOS). Base class to build NOS plugins instances to use with FakeNOS.
+"""
+
+import logging
+from typing import Optional, List
+import importlib.util
+import os
+import yaml
+
+from fakenos.core.pydantic_models import ModelNosAttributes
+
+log = logging.getLogger(__name__)
+
+available_platforms: List[str] = [
+    "alcatel_aos",
+    "alcatel_sros",
+    "allied_telesis_awplus",
+    "arista_eos",
+    "aruba_os",
+    "avaya_ers",
+    "avaya_vsp",
+    "broadcom_icos",
+    # "brocade_fastiron",
+    "brocade_netiron",
+    "checkpoint_gaia",
+    # "ciena_saos",
+    "cisco_asa",
+    "cisco_ftd",
+    "cisco_ios",
+    "cisco_nxos",
+    "cisco_s300",
+    "cisco_xr",
+    "dell_force10",
+    # "dell_powerconnect",
+    "dlink_ds",
+    "eltex",
+    "ericsson_ipos",
+    "extreme_exos",
+    # "fortinet",
+    "hp_comware",
+    "hp_procurve",
+    "huawei_smartax",
+    "huawei_vrp",
+    "ipinfusion_ocnos",
+    "juniper_junos",
+    # "juniper_screenos",
+    "linux",
+    # "mikrotik_routeros",
+    # "paloalto_panos",
+    # "ruckus_fastiron",
+    "ubiquiti_edgerouter",
+    "ubiquiti_edgeswitch",
+    "vyatta_vyos",
+    "yamaha",
+    "zyxel_os",
+]
+
+
+class Nos:
+    """
+    Base class to build NOS plugins instances to use with FakeNOS.
+    """
+
+    # pylint: disable=too-many-arguments
+    def __init__(
+        self,
+        name: str = "FakeNOS",
+        commands: dict = None,
+        initial_prompt: str = "FakeNOS>",
+        filename: Optional[str] = None,
+        dict_args: Optional[dict] = None,
+    ) -> None:
+        """
+        Method to instantiate Nos Instance
+
+        :param name: NOS plugin name
+        :param commands: dictionary of NOS commands
+        :param initial_prompt: NOS initial prompt
+        """
+        self.name = name
+        self.commands = commands or {}
+        self.initial_prompt = initial_prompt
+        if filename:
+            self.from_file(filename)
+        elif dict_args:
+            self.from_dict(dict_args)
+
+        self.validate()
+
+    def validate(self) -> None:
+        """
+        Method to validate NOS attributes: commands, name,
+        initial prompt - using Pydantic models,
+        raises ValidationError on failure.
+        """
+        ModelNosAttributes(**self.__dict__)
+        log.debug("%s NOS attributes validation succeeded", self.name)
+
+    def from_dict(self, data: dict) -> None:
+        """
+        Method to build NOS from dictionary data.
+
+        Sample NOS dictionary::
+
+            nos_plugin_dict = {
+                "name": "MyFakeNOSPlugin",
+                "initial_prompt": "{base_prompt}>",
+                "commands": {
+                    "terminal width 511": {
+                        "output": "",
+                        "help": "Set terminal width to 511",
+                        "prompt": "{base_prompt}>",
+                    },
+                    "terminal length 0": {
+                        "output": "",
+                        "help": "Set terminal length to 0",
+                        "prompt": "{base_prompt}>",
+                    },
+                    "show clock": {
+                        "output": "MyFakeNOSPlugin system time is 00:00:00",
+                        "help": "Show system time",
+                        "prompt": "{base_prompt}>",
+                    },
+                },
+            }
+
+        :param data: NOS dictionary
+        """
+        self.name = data.get("name", self.name)
+        self.commands.update(data.get("commands", self.commands))
+        self.initial_prompt = data.get("initial_prompt", self.initial_prompt)
+
+    def _from_yaml(self, data: str) -> None:
+        """
+        Method to build NOS from YAML data.
+
+        Sample NOS YAML file content::
+
+            name: "MyFakeNOSPlugin"
+            initial_prompt: "{base_prompt}>"
+            commands:
+                terminal width 511: {
+                    "output": "",
+                    "help": "Set terminal width to 511",
+                    "prompt": "{base_prompt}>",
+                }
+                terminal length 0: {
+                    "output": "",
+                    "help": "Set terminal length to 0",
+                    "prompt": "{base_prompt}>",
+                }
+                show clock: {
+                    "output": "MyFakeNOSPlugin system time is 00:00:00",
+                    "help": "Show system time",
+                    "prompt": "{base_prompt}>",
+                }
+
+        :param data: YAML structured text
+        """
+        with open(data, "r", encoding="utf-8") as f:
+            self.from_dict(yaml.safe_load(f))
+
+    def _from_module(self, data: str) -> None:
+        """
+        Method to import NOS data from python file or python module.
+
+        Loads from the .py file using the recipe:
+        https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly
+
+        Sample Python NOS plugin file::
+
+            name = "MyFakeNOSPlugin"
+
+            INITIAL_PROMPT = "{base_prompt}>"
+
+            commands = {
+                "terminal width 511": {
+                    "output": "",
+                    "help": "Set terminal width to 511",
+                    "prompt": "{base_prompt}>",
+                },
+                "terminal length 0": {
+                    "output": "",
+                    "help": "Set terminal length to 0",
+                    "prompt": "{base_prompt}>",
+                },
+                "show clock": {
+                    "output": "MyFakeNOSPlugin system time is 00:00:00",
+                    "help": "Show system time",
+                    "prompt": "{base_prompt}>",
+                },
+            }
+
+        :param data: OS path string to Python .py file
+        """
+        spec = importlib.util.spec_from_file_location("nos_module", data)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        self.name = getattr(module, "name", self.name)
+        self.commands.update(getattr(module, "commands", self.commands))
+        self.initial_prompt = getattr(module, "INITIAL_PROMPT", self.initial_prompt)
+
+    def from_file(self, data: str) -> None:
+        """
+        Method to load NOS from YAML or Python file
+
+        :param data: OS path string to `.yaml/.yml` or `.py` file with NOS data
+        """
+        if not self.is_file_ending_correct(data):
+            raise ValueError(
+                f'Unsupported "{data}" file extension.\
+                              Supported: .py, .yml, .yaml'
+            )
+        if not os.path.isfile(data):
+            raise FileNotFoundError(data)
+        if data.endswith((".yaml", ".yml")):
+            self._from_yaml(data)
+        elif data.endswith(".py"):
+            self._from_module(data)
+
+    def is_file_ending_correct(self, data: str) -> None:
+        """
+        Method to check if file extension is correct and load NOS data.
+        Correct types are: .yaml, .yml and .py
+        """
+        return data.endswith((".yaml", ".yml", ".py"))
---
+++ packages/fakenos/fakenos-1.0.0.tar.gz/fakenos-1.0.0/fakenos/plugins/shell/cmd_shell.py
@@ -0,0 +1,168 @@
+"""
+Custom shell class to interact with NOS.
+"""
+
+from cmd import Cmd
+import logging
+import traceback
+import copy
+
+log = logging.getLogger(__name__)
+
+
+# pylint: disable=too-many-instance-attributes
+class CMDShell(Cmd):
+    """
+    Custom shell class to interact with NOS.
+    """
+
+    use_rawinput = False
+
+    commands = {
+        "exit": {"output": True, "help": "Exit commands shell"},
+        "_default_": {
+            "output": "Unknown command",
+            "help": "Output to print for unknown commands",
+        },
+    }
+
+    # pylint: disable=too-many-arguments
+    def __init__(
+        self,
+        stdin,
+        stdout,
+        nos,
+        nos_inventory_config,
+        base_prompt,
+        is_running,
+        intro="Custom SSH Shell",
+        ruler="",
+        completekey="tab",
+        newline="\r\n",
+    ):
+        self.nos = nos
+        self.ruler = ruler
+        self.intro = intro
+        self.base_prompt = base_prompt
+        self.newline = newline
+        self.prompt = nos.initial_prompt.format(base_prompt=base_prompt)
+        self.is_running = is_running
+
+        # form commands
+        self.commands = {
+            **copy.deepcopy(self.commands),
+            **copy.deepcopy(nos.commands or {}),
+            **copy.deepcopy(nos_inventory_config.get("commands", {})),
+        }
+
+        # call the base constructor of cmd.Cmd, with our own stdin and stdout
+        super().__init__(
+            completekey=completekey,
+            stdin=stdin,
+            stdout=stdout,
+        )
+
+    def start(self):
+        """Method to start the shell"""
+        self.cmdloop()
+
+    def stop(self):
+        """Method to stop the shell"""
+        self.stdin.write("exit" + self.newline)
+
+    def writeline(self, value):
+        """Method to write a line to stdout with newline at the end"""
+        for line in str(value).splitlines():
+            self.stdout.write(line + self.newline)
+
+    def emptyline(self):
+        """This method to do nothing if empty line entered"""
+
+    def precmd(self, line):
+        return line
+
+    # pylint: disable=unused-argument
+    def postcmd(self, stop, line):
+        """Method to return stop value to stop the shell"""
+        return stop
+
+    # pylint: disable=unused-argument
+    def do_help(self, arg):
+        """Method to return help for commands"""
+        lines = {}  # dict of {cmd: cmd_help}
+        width = 0  # record longest command width for padding
+        # form help for all commands
+        for cmd, cmd_data in self.commands.items():
+            # skip special commands
+            if cmd.startswith("_") and cmd.endswith("_"):
+                continue
+            # skip commands that does not match current prompt
+            if not self._check_prompt(cmd_data.get("prompt")):
+                continue
+            lines[cmd] = cmd_data.get("help", "")
+            width = max(width, len(cmd))
+        # form help lines
+        help_msg = []
+        for k, v in lines.items():
+            padding = " " * (width - len(k)) + "  "
+            help_msg.append(f"{k}{padding}{v}")
+        self.writeline(self.newline.join(help_msg))
+
+    def _check_prompt(self, prompt_):
+        """
+        Helper method to check if prompt_ matches current prompt
+
+        :param prompt_: (string or None)  prompt to check
+        """
+        # prompt_ is None if no 'prompt' key defined for command
+        if prompt_ is None:
+            return True
+        if isinstance(prompt_, str):
+            return self.prompt == prompt_.format(base_prompt=self.base_prompt)
+        if isinstance(prompt_, list):
+            return any(self.prompt == i.format(base_prompt=self.base_prompt) for i in prompt_)
+        return False
+
+    def default(self, line):
+        """Method called if no do_xyz methods found"""
+        log.debug("shell.default '%s' running command '%s'", self.base_prompt, [line])
+        ret = self.commands["_default_"]["output"]
+        try:
+            cmd_data = self.commands[line]
+            if "alias" in cmd_data:
+                cmd_data = {**self.commands[cmd_data.pop("alias")], **cmd_data}
+            if self._check_prompt(cmd_data.get("prompt")):
+                ret = cmd_data["output"]
+                if callable(ret):
+                    ret = ret(
+                        base_prompt=self.base_prompt,
+                        current_prompt=self.prompt,
+                        command=line,
+                    )
+                    if isinstance(ret, dict):
+                        if "new_prompt" in ret:
+                            self.prompt = ret["new_prompt"].format(base_prompt=self.base_prompt)
+                        ret = ret["output"]
+                if "new_prompt" in cmd_data:
+                    self.prompt = cmd_data["new_prompt"].format(base_prompt=self.base_prompt)
+            else:
+                log.warning(
+                    "'%s' command prompt '%s' not matching current prompt '%s'",
+                    line,
+                    cmd_data.get("prompt", "").format(base_prompt=self.base_prompt),
+                    self.prompt,
+                )
+        except KeyError:
+            log.error("shell.default '%s' command '%s' not found", self.base_prompt, [line])
+        # pylint: disable=broad-except
+        except (Exception,) as e:
+            log.error("An error occurred: %s", str(e))
+            ret = traceback.format_exc()
+            ret = ret.replace("\n", self.newline)
+        # check if need to exit
+        if ret is True or not self.is_running.is_set():
+            return True
+        if ret is not None:
+            ret = ret.format(base_prompt=self.base_prompt)
+            self.writeline(ret)
+        return False
