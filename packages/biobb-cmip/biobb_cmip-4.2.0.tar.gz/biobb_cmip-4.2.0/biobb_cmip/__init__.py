06af6cbede29d06d8671ad7227c67a8a54b67be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 16:04:57 +0000

    Add package biobb-cmip/4.2.0/biobb_cmip-4.2.0.tar.gz

---
+++ packages/biobb-cmip/biobb_cmip-4.2.0.tar.gz/biobb_cmip-4.2.0/biobb_cmip/__init__.py
@@ -0,0 +1,3 @@
+name = "biobb_cmip"
+__all__ = ["cmip"]
+__version__ = "4.2.0"
---
+++ packages/biobb-cmip/biobb_cmip-4.2.0.tar.gz/biobb_cmip-4.2.0/biobb_cmip/cmip/__init__.py
@@ -0,0 +1,2 @@
+name = "cmip"
+__all__ = ["cmip_titration", "cmip_run", "cmip_prepare_structure", "cmip_prepare_pdb", "cmip_ignore_residues"]
