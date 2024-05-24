3719a48bad689ba9f117bfc611369c3f10bfbec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 16:04:36 +0000

    Add package spaceone-cost-analysis/2.0.dev122/spaceone_cost_analysis-2.0.dev122-py3-none-any.whl

---
+++ packages/spaceone-cost-analysis/spaceone_cost_analysis-2.0.dev122-py3-none-any.whl/spaceone/cost_analysis/model/data_source_account/database.py
@@ -0,0 +1,35 @@
+from mongoengine import *
+
+from spaceone.core.model.mongo_model import MongoModel
+
+
+class DataSourceAccount(MongoModel):
+    account_id = StringField(
+        max_length=255, required=True, unique_with="data_source_id"
+    )
+    data_source_id = StringField(max_length=255, required=True)
+    name = StringField(max_length=255)
+    is_sync = BooleanField(default=False)
+    v_workspace_id = StringField(max_length=40, generate_id="v-workspace", unique=True)
+    workspace_id = StringField(max_length=40, default=None, null=True)
+    domain_id = StringField(max_length=40)
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+
+    meta = {
+        "updatable_fields": [
+            "name",
+            "is_sync",
+            "workspace_id",
+            "updated_at",
+        ],
+        "minimal_fields": [
+            "account_id",
+            "data_source_id",
+            "name",
+            "is_sync",
+            "workspace_id",
+        ],
+        "ordering": ["-created_at"],
+        "indexes": ["domain_id", "data_source_id", "account_id"],
+    }
