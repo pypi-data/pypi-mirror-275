9cc62bc80d057c34eff0cd7d672e00710ea2ea13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:28 +0000

    Add package lincs/2.0.0a0/lincs-2.0.0a0-cp39-cp39-macosx_11_0_x86_64.whl

---
+++ packages/lincs/lincs-2.0.0a0-cp39-cp39-macosx_11_0_x86_64.whl/lincs/liblincs/learning/mrsort-by-weights-profiles-breed.cpp
@@ -0,0 +1,164 @@
+// Copyright 2023-2024 Vincent Jacques
+
+#include "mrsort-by-weights-profiles-breed.hpp"
+
+#include <map>
+#include <numeric>
+
+#include "../chrones.hpp"
+#include "../unreachable.hpp"
+#include "exception.hpp"
+
+
+namespace lincs {
+
+LearnMrsortByWeightsProfilesBreed::LearningData::LearningData(
+    const Problem& problem,
+    const Alternatives& learning_set,
+    const unsigned models_count_,
+    const unsigned random_seed
+) :
+  PreProcessedLearningSet(problem, learning_set),
+  models_count(models_count_),
+  urbgs(models_count),
+  iteration_index(0),
+  model_indexes(models_count),
+  accuracies(models_count, zeroed),
+  profile_ranks(models_count, boundaries_count, criteria_count, uninitialized),
+  weights(models_count, criteria_count, uninitialized)
+{
+  CHRONE();
+
+  std::iota(model_indexes.begin(), model_indexes.end(), 0);
+
+  for (unsigned model_index = 0; model_index != models_count; ++model_index) {
+    urbgs[model_index].seed(random_seed * (model_index + 1));
+  }
+}
+
+Model LearnMrsortByWeightsProfilesBreed::LearningData::get_model(const unsigned model_index) const {
+  CHRONE();
+
+  assert(model_index < models_count);
+
+  std::vector<float> model_weights;
+  model_weights.reserve(criteria_count);
+  for (unsigned criterion_index = 0; criterion_index != criteria_count; ++criterion_index) {
+    model_weights.push_back(weights[model_index][criterion_index]);
+  }
+  SufficientCoalitions coalitions = SufficientCoalitions(SufficientCoalitions::Weights(model_weights));
+
+  std::vector<PreProcessedBoundary> boundaries;
+  boundaries.reserve(boundaries_count);
+  for (unsigned boundary_index = 0; boundary_index != boundaries_count; ++boundary_index) {
+    std::vector<unsigned> boundary_profile;
+    boundary_profile.reserve(criteria_count);
+    for (unsigned criterion_index = 0; criterion_index != criteria_count; ++criterion_index) {
+      const unsigned profile_rank = profile_ranks[model_index][boundary_index][criterion_index];
+      boundary_profile.push_back(profile_rank);
+    }
+    boundaries.emplace_back(boundary_profile, coalitions);
+  }
+
+  return post_process(boundaries);
+}
+
+Model LearnMrsortByWeightsProfilesBreed::perform() {
+  CHRONE();
+
+  profiles_initialization_strategy.initialize_profiles(0, learning_data.models_count);
+
+  while (true) {
+    // Improve
+    weights_optimization_strategy.optimize_weights(0, learning_data.models_count);
+    profiles_improvement_strategy.improve_profiles(0, learning_data.models_count);
+
+    // @todo(Feature, later) Rework this main loop. Its current problems:
+    //   - we return models that have gone through a last profiles improvement, but their weights have not been optimized
+    //   - we decide to stop the learning based on the accuracy of those models in this weird state
+    // Beware, if optimize_weights is run after improve_profiles, it must also be run during the breeding strategy.
+
+    // Sort model_indexes by increasing model accuracy
+    for (unsigned model_index = 0; model_index != learning_data.models_count; ++model_index) {
+      learning_data.accuracies[model_index] = compute_accuracy(model_index);
+    }
+    std::sort(
+      learning_data.model_indexes.begin(), learning_data.model_indexes.end(),
+      [this](unsigned left_model_index, unsigned right_model_index) {
+        return learning_data.accuracies[left_model_index] < learning_data.accuracies[right_model_index];
+      }
+    );
+
+    // Succeed?
+    if (learning_data.get_best_accuracy() == learning_data.alternatives_count || termination_strategy.terminate()) {
+      for (auto observer : observers) {
+        observer->before_return();
+      }
+      return learning_data.get_model(learning_data.model_indexes.back());
+    }
+
+    // Breed
+    // @todo(Feature, later) Keep the best model and reinit half of the others
+    breeding_strategy.breed();
+
+    // Observe
+    for (auto observer : observers) {
+      observer->after_iteration();
+    }
+
+    ++learning_data.iteration_index;
+  }
+
+  unreachable();
+}
+
+unsigned LearnMrsortByWeightsProfilesBreed::compute_accuracy(const unsigned model_index) {
+  unsigned accuracy = 0;
+
+  for (unsigned alternative_index = 0; alternative_index != learning_data.alternatives_count; ++alternative_index) {
+    if (is_correctly_assigned(model_index, alternative_index)) {
+      ++accuracy;
+    }
+  }
+
+  return accuracy;
+}
+
+bool LearnMrsortByWeightsProfilesBreed::is_correctly_assigned(
+    const unsigned model_index,
+    const unsigned alternative_index) {
+  const unsigned expected_assignment = learning_data.assignments[alternative_index];
+  const unsigned actual_assignment = get_assignment(learning_data, model_index, alternative_index);
+
+  return actual_assignment == expected_assignment;
+}
+
+unsigned LearnMrsortByWeightsProfilesBreed::get_assignment(const LearningData& learning_data, const unsigned model_index, const unsigned alternative_index) {
+  // @todo(Performance, later) Evaluate if it's worth storing and updating the models' assignments
+  // (instead of recomputing them here)
+  // Same question in accuracy-heuristic-on-gpu.cu
+  assert(model_index < learning_data.models_count);
+  assert(alternative_index < learning_data.alternatives_count);
+
+  // Not parallelizable in this form because the loop gets interrupted by a return. But we could rewrite it
+  // to always perform all its iterations, and then it would be yet another map-reduce, with the reduce
+  // phase keeping the maximum 'category_index' that passes the weight threshold.
+  for (unsigned category_index = learning_data.categories_count - 1; category_index != 0; --category_index) {
+    const unsigned profile_index = category_index - 1;
+    float weight_at_or_better_than_profile = 0;
+    for (unsigned criterion_index = 0; criterion_index != learning_data.criteria_count; ++criterion_index) {
+      const unsigned alternative_rank = learning_data.performance_ranks[criterion_index][alternative_index];
+      const unsigned profile_rank = learning_data.profile_ranks[model_index][profile_index][criterion_index];
+      const bool is_better = alternative_rank >= profile_rank;
+      if (is_better) {
+        weight_at_or_better_than_profile += learning_data.weights[model_index][criterion_index];
+      }
+    }
+    if (weight_at_or_better_than_profile >= 1) {
+      return category_index;
+    }
+  }
+  return 0;
+}
+
+}  // namespace lincs
---
+++ packages/lincs/lincs-2.0.0a0-cp39-cp39-macosx_11_0_x86_64.whl/lincs/liblincs/learning/mrsort-by-weights-profiles-breed/improve-profiles/accuracy-heuristic-on-cpu.cpp
@@ -0,0 +1,262 @@
+// Copyright 2023-2024 Vincent Jacques
+
+#include "accuracy-heuristic-on-cpu.hpp"
+
+#include "../../../chrones.hpp"
+#include "../../../randomness-utils.hpp"
+
+
+namespace lincs {
+
+void ImproveProfilesWithAccuracyHeuristicOnCpu::improve_profiles(
+  const unsigned model_indexes_begin,
+  const unsigned model_indexes_end
+) {
+  CHRONE();
+
+  #pragma omp parallel for
+  for (int model_indexes_index = model_indexes_begin; model_indexes_index < model_indexes_end; ++model_indexes_index) {
+    const unsigned model_index = learning_data.model_indexes[model_indexes_index];
+    improve_model_profiles(model_index);
+  }
+}
+
+void ImproveProfilesWithAccuracyHeuristicOnCpu::improve_model_profiles(const unsigned model_index) {
+  Array1D<Host, unsigned> criterion_indexes(learning_data.criteria_count, uninitialized);
+  // Not worth parallelizing because criteria_count is typically small
+  for (unsigned crit_idx_idx = 0; crit_idx_idx != learning_data.criteria_count; ++crit_idx_idx) {
+    criterion_indexes[crit_idx_idx] = crit_idx_idx;
+  }
+
+  // Not parallel because iteration N+1 relies on side effect in iteration N
+  // (We could challenge this aspect of the algorithm described by Sobrie)
+  for (unsigned profile_index = 0; profile_index != learning_data.boundaries_count; ++profile_index) {
+    shuffle(learning_data.urbgs[model_index], ref(criterion_indexes));
+    improve_model_profile(model_index, profile_index, criterion_indexes);
+  }
+}
+
+void ImproveProfilesWithAccuracyHeuristicOnCpu::improve_model_profile(
+  const unsigned model_index,
+  const unsigned profile_index,
+  ArrayView1D<Host, const unsigned> criterion_indexes
+) {
+  // Not parallel because iteration N+1 relies on side effect in iteration N
+  // (We could challenge this aspect of the algorithm described by Sobrie)
+  for (unsigned crit_idx_idx = 0; crit_idx_idx != learning_data.criteria_count; ++crit_idx_idx) {
+    improve_model_profile(model_index, profile_index, criterion_indexes[crit_idx_idx]);
+  }
+}
+
+void ImproveProfilesWithAccuracyHeuristicOnCpu::improve_model_profile(
+  const unsigned model_index,
+  const unsigned profile_index,
+  const unsigned criterion_index
+) {
+  const float lowest_destination_rank =
+    profile_index == 0 ?
+      0 :
+      learning_data.profile_ranks[model_index][profile_index - 1][criterion_index];
+  const float highest_destination_rank =
+    profile_index == learning_data.boundaries_count - 1 ?
+      learning_data.values_counts[criterion_index] - 1 :
+      learning_data.profile_ranks[model_index][profile_index + 1][criterion_index];
+
+  assert(lowest_destination_rank <= highest_destination_rank);
+  if (lowest_destination_rank == highest_destination_rank) {
+    assert(learning_data.profile_ranks[model_index][profile_index][criterion_index] == lowest_destination_rank);
+    return;
+  }
+
+  unsigned best_destination_rank = learning_data.profile_ranks[model_index][profile_index][criterion_index];
+  float best_desirability = Desirability().value();
+
+  if (highest_destination_rank - lowest_destination_rank >= max_destinations_count) {
+    // We could try uniformly spread-out destinations instead of uniform random ones
+    for (unsigned destination_index = 0; destination_index != max_destinations_count; ++destination_index) {
+      const unsigned destination_rank = std::uniform_int_distribution<unsigned>(lowest_destination_rank, highest_destination_rank)(learning_data.urbgs[model_index]);
+      const float desirability = compute_move_desirability(
+        model_index,
+        profile_index,
+        criterion_index,
+        destination_rank).value();
+      // Single-key reduce (divide and conquer?) (atomic compare-and-swap?)
+      if (desirability > best_desirability) {
+        best_desirability = desirability;
+        best_destination_rank = destination_rank;
+      }
+    }
+  } else {
+    for (unsigned destination_rank = lowest_destination_rank; destination_rank <= highest_destination_rank; ++destination_rank) {
+      const float desirability = compute_move_desirability(
+        model_index,
+        profile_index,
+        criterion_index,
+        destination_rank).value();
+      // Single-key reduce (divide and conquer?) (atomic compare-and-swap?)
+      if (desirability > best_desirability) {
+        best_desirability = desirability;
+        best_destination_rank = destination_rank;
+      }
+    }
+  }
+
+  // @todo(Project management, later) Desirability can be as high as 2. The [0, 1] interval is a weird choice.
+  if (std::uniform_real_distribution<float>(0, 1)(learning_data.urbgs[model_index]) <= best_desirability) {
+    learning_data.profile_ranks[model_index][profile_index][criterion_index] = best_destination_rank;
+  }
+}
+
+Desirability ImproveProfilesWithAccuracyHeuristicOnCpu::compute_move_desirability(
+  const unsigned model_index,
+  const unsigned profile_index,
+  const unsigned criterion_index,
+  const unsigned destination_rank
+) {
+  Desirability d;
+
+  for (unsigned alternative_index = 0; alternative_index != learning_data.alternatives_count; ++alternative_index) {
+    update_move_desirability(
+      model_index,
+      profile_index,
+      criterion_index,
+      destination_rank,
+      alternative_index,
+      &d);
+  }
+
+  return d;
+}
+
+void ImproveProfilesWithAccuracyHeuristicOnCpu::update_move_desirability(
+  const unsigned model_index,
+  const unsigned profile_index,
+  const unsigned criterion_index,
+  const unsigned destination_rank,
+  const unsigned alternative_index,
+  Desirability* desirability
+) {
+  const unsigned current_rank = learning_data.profile_ranks[model_index][profile_index][criterion_index];
+  const float weight = learning_data.weights[model_index][criterion_index];
+
+  const unsigned alternative_rank = learning_data.performance_ranks[criterion_index][alternative_index];
+  const unsigned learning_assignment = learning_data.assignments[alternative_index];
+  const unsigned model_assignment = LearnMrsortByWeightsProfilesBreed::get_assignment(learning_data, model_index, alternative_index);
+
+  // @todo(Project management, later) Factorize with get_assignment
+  // (Same remark in accuracy-heuristic-on-gpu.cu)
+  float weight_at_or_better_than_profile = 0;
+  // There is a criterion parameter above, *and* a local criterion just here
+  for (unsigned crit_index = 0; crit_index != learning_data.criteria_count; ++crit_index) {
+    const unsigned alternative_rank = learning_data.performance_ranks[crit_index][alternative_index];
+    const unsigned profile_rank = learning_data.profile_ranks[model_index][profile_index][crit_index];
+    const bool is_better = alternative_rank >= profile_rank;
+    if (is_better) {
+      weight_at_or_better_than_profile += learning_data.weights[model_index][crit_index];
+    }
+  }
+
+  // These imbricated conditionals could be factorized, but this form has the benefit
+  // of being a direct translation of the top of page 78 of Sobrie's thesis.
+  // Correspondance:
+  // - learning_assignment: bottom index of A*
+  // - model_assignment: top index of A*
+  // - profile_index: h
+  // - destination_value: b_j +/- \delta
+  // - current_value: b_j
+  // - alternative_value: a_j
+  // - weight_at_or_better_than_profile: \sigma
+  // - weight: w_j
+  // - 1: \lambda
+  if (destination_rank > current_rank) {
+    if (
+      learning_assignment == profile_index
+      && model_assignment == profile_index + 1
+      && destination_rank > alternative_rank
+      && alternative_rank >= current_rank
+      && weight_at_or_better_than_profile - weight < 1
+    ) {
+      ++desirability->v;
+    }
+    if (
+      learning_assignment == profile_index
+      && model_assignment == profile_index + 1
+      && destination_rank > alternative_rank
+      && alternative_rank >= current_rank
+      && weight_at_or_better_than_profile - weight >= 1
+    ) {
+      ++desirability->w;
+    }
+    if (
+      learning_assignment == profile_index + 1
+      && model_assignment == profile_index + 1
+      && destination_rank > alternative_rank
+      && alternative_rank >= current_rank
+      && weight_at_or_better_than_profile - weight < 1
+    ) {
+      ++desirability->q;
+    }
+    if (
+      learning_assignment == profile_index + 1
+      && model_assignment == profile_index
+      && destination_rank > alternative_rank
+      && alternative_rank >= current_rank
+    ) {
+      ++desirability->r;
+    }
+    if (
+      learning_assignment < profile_index
+      && model_assignment > profile_index
+      && destination_rank > alternative_rank
+      && alternative_rank >= current_rank
+    ) {
+      ++desirability->t;
+    }
+  } else {
+    if (
+      learning_assignment == profile_index + 1
+      && model_assignment == profile_index
+      && alternative_rank > destination_rank
+      && current_rank > alternative_rank
+      && weight_at_or_better_than_profile + weight >= 1
+    ) {
+      ++desirability->v;
+    }
+    if (
+      learning_assignment == profile_index + 1
+      && model_assignment == profile_index
+      && alternative_rank > destination_rank
+      && current_rank > alternative_rank
+      && weight_at_or_better_than_profile + weight < 1
+    ) {
+      ++desirability->w;
+    }
+    if (
+      learning_assignment == profile_index
+      && model_assignment == profile_index
+      && alternative_rank > destination_rank
+      && current_rank > alternative_rank
+      && weight_at_or_better_than_profile + weight >= 1
+    ) {
+      ++desirability->q;
+    }
+    if (
+      learning_assignment == profile_index
+      && model_assignment == profile_index + 1
+      && alternative_rank >= destination_rank
+      && current_rank > alternative_rank
+    ) {
+      ++desirability->r;
+    }
+    if (
+      learning_assignment > profile_index + 1
+      && model_assignment < profile_index + 1
+      && alternative_rank > destination_rank
+      && current_rank >= alternative_rank
+    ) {
+      ++desirability->t;
+    }
+  }
+}
+
+}  // namespace lincs
