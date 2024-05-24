dcb0a9ad0f832590b260c2ac048fdd350ae125a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 04:03:04 +0000

    Add package tiled/0.1.0a122/tiled-0.1.0a122-py3-none-any.whl

---
+++ packages/tiled/tiled-0.1.0a122-py3-none-any.whl/tiled/_tests/utils.py
@@ -0,0 +1,66 @@
+import contextlib
+import getpass
+import uuid
+from enum import IntEnum
+
+import httpx
+import pytest
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import create_async_engine
+
+from ..client import context
+from ..client.base import BaseClient
+
+
+@contextlib.contextmanager
+def fail_with_status_code(status_code):
+    with pytest.raises(httpx.HTTPStatusError) as info:
+        yield info
+    assert info.value.response.status_code == status_code
+
+
+@contextlib.asynccontextmanager
+async def temp_postgres(uri):
+    if uri.endswith("/"):
+        uri = uri[:-1]
+    # Create a fresh database.
+    engine = create_async_engine(uri)
+    database_name = f"tiled_test_disposable_{uuid.uuid4().hex}"
+    async with engine.connect() as connection:
+        await connection.execute(
+            text("COMMIT")
+        )  # close the automatically-started transaction
+        await connection.execute(text(f"CREATE DATABASE {database_name};"))
+        await connection.commit()
+    yield f"{uri}/{database_name}"
+    # Drop the database.
+    async with engine.connect() as connection:
+        await connection.execute(
+            text("COMMIT")
+        )  # close the automatically-started transaction
+        await connection.execute(text(f"DROP DATABASE {database_name};"))
+        await connection.commit()
+
+
+@contextlib.contextmanager
+def enter_password(password):
+    """
+    Override getpass, used like:
+
+    >>> with enter_password(...):
+    ...     # Run code that calls getpass.getpass().
+    """
+
+    original_prompt = context.PROMPT_FOR_REAUTHENTICATION
+    original_getpass = getpass.getpass
+    context.PROMPT_FOR_REAUTHENTICATION = True
+    setattr(getpass, "getpass", lambda: password)
+    yield
+    setattr(getpass, "getpass", original_getpass)
+    context.PROMPT_FOR_REAUTHENTICATION = original_prompt
+
+
+class URL_LIMITS(IntEnum):
+    HUGE = 80_000
+    DEFAULT = BaseClient.URL_CHARACTER_LIMIT
+    TINY = 10
