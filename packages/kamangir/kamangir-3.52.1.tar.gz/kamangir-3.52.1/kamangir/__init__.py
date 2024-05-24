ca42823fefd0c7e1d04e9b34a429138cae3f9ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 15:04:13 +0000

    Add package kamangir/3.52.1/kamangir-3.52.1.tar.gz

---
+++ packages/kamangir/kamangir-3.52.1.tar.gz/kamangir-3.52.1/kamangir/__init__.py
@@ -0,0 +1,7 @@
+NAME = "kamangir"
+
+ICON = "📜"
+
+DESCRIPTION = f"{ICON} github/kamangir"
+
+VERSION = "3.52.1"
---
+++ packages/kamangir/kamangir-3.52.1.tar.gz/kamangir-3.52.1/kamangir/content.py
@@ -0,0 +1,94 @@
+from abcli import DESCRIPTION as abcli_DESCRIPTION
+from articraft import DESCRIPTION as articraft_DESCRIPTION
+from hubblescope import DESCRIPTION as hubble_DESCRIPTION
+from gizai import DESCRIPTION as giza_DESCRIPTION
+from openai_cli import DESCRIPTION as openai_cli_DESCRIPTION
+from notebooks_and_scripts import DESCRIPTION as nbs_DESCRIPTION
+from roofAI import DESCRIPTION as roofAI_DESCRIPTION
+from vancouver_watching import DESCRIPTION as vancouver_watching_DESCRIPTION
+
+content = {
+    "cols": 4,
+    "items": {
+        "notebooks-and-scripts": {
+            "image": "https://github.com/kamangir/assets/raw/main/nbs/3x4.jpg?raw=true",
+            "url": "https://github.com/kamangir/notebooks-and-scripts",
+            "description": nbs_DESCRIPTION,
+        },
+        "Vancouver-Watching": {
+            "image": "https://user-images.githubusercontent.com/1007567/196573547-b1c71b3b-7fac-4d2c-bba0-a87b063830da.png",
+            "url": "https://github.com/kamangir/Vancouver-Watching",
+            "description": vancouver_watching_DESCRIPTION,
+        },
+        "giza": {
+            "image": "https://github.com/kamangir/giza/raw/main/assets/giza.png",
+            "url": "https://github.com/kamangir/giza",
+            "description": giza_DESCRIPTION,
+        },
+        "roofAI": {
+            "image": "https://github.com/kamangir/assets/blob/main/2023-10-28-16-28-36-88493-predict.gif?raw=true",
+            "url": "https://github.com/kamangir/roofAI",
+            "description": roofAI_DESCRIPTION,
+        },
+        "openai_cli": {
+            "image": "https://github.com/kamangir/openai_cli/raw/main/assets/carrot.png",
+            "url": "https://github.com/kamangir/openai_cli",
+            "description": openai_cli_DESCRIPTION,
+        },
+        "hubble": {
+            "image": "https://github.com/kamangir/hubble/raw/main/assets/hst/u4ge0106r_c0m.gif",
+            "url": "https://github.com/kamangir/hubble",
+            "description": hubble_DESCRIPTION,
+        },
+        "blue-stability": {
+            "image": "https://github.com/kamangir/AI-ART/raw/main/blue-stability/blue_stability.gif",
+            "url": "https://github.com/kamangir/blue-stability",
+            "description": "bash cli for stability.ai API's stable diffusion inference",
+        },
+        "aiart": {
+            "image": "https://github.com/kamangir/openai_cli/raw/main/assets/DALL-E.png?raw=1",
+            "url": "https://github.com/kamangir/aiart",
+            "description": articraft_DESCRIPTION,
+        },
+        "abcli": {
+            "image": "https://github.com/kamangir/awesome-bash-cli/raw/main/assets/marquee.png",
+            "url": "https://github.com/kamangir/awesome-bash-cli",
+            "description": abcli_DESCRIPTION,
+        },
+        "ferfereh": {
+            "image": "https://user-images.githubusercontent.com/1007567/221448494-d57e08c1-625b-499e-a576-81894f112d6a.jpg",
+            "url": "https://github.com/kamangir/ferfereh",
+            "description": "3d-printed graffiti",
+        },
+        "Kanata": {
+            "image": "https://kamangir-public.s3.ca-central-1.amazonaws.com/Canadians_v11.gif",
+            "url": "https://github.com/kamangir/Kanata",
+            "description": "a multi-screen video feed that is comprised of a matrix of animated faces that slide to the right.",
+        },
+        "dec82": {
+            "image": "https://github.com/kamangir/blue-bracket/raw/main/images/dec82-6.jpg",
+            "url": "https://github.com/kamangir/dec82",
+            "description": "A wearable Raspberry Pi + Grove / Qwiic + Camera.",
+        },
+        "blue-rvr": {
+            "image": "https://github.com/kamangir/blue-rvr/raw/master/abcli/assets/marquee.jpeg",
+            "url": "https://github.com/kamangir/blue-rvr",
+            "description": "a bash cli for Sphero RVR SDK - runs deep learning vision models on a Raspberry Pi using Python and TensorFlow.",
+        },
+        "blue-bracket": {
+            "image": "https://github.com/kamangir/blue-bracket/raw/main/images/marquee.jpg",
+            "url": "https://github.com/kamangir/blue-bracket",
+            "description": "a parametric 3d-printed bracket to build hardware for machine vision & ai on raspberry pi and jetson nano on the edge.",
+        },
+        "blue-sbc": {
+            "image": "https://github.com/kamangir/blue-bracket/raw/main/images/blue3-1.jpg",
+            "url": "https://github.com/kamangir/blue-sbc",
+            "description": "python + bash bootstrap for edge computing on single board computers.",
+        },
+        "template": {
+            "image": "TBA",
+            "url": "TBA",
+            "description": "TBA",
+        },
+    },
+}
