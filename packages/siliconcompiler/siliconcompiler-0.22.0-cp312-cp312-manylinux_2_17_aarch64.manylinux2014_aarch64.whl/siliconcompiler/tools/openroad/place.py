17c0b38323e4094587a1fa3a53fb8502f49a9bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 05:03:47 +0000

    Add package siliconcompiler/0.22.0/siliconcompiler-0.22.0-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/siliconcompiler/siliconcompiler-0.22.0-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/siliconcompiler/tools/openroad/place.py
@@ -0,0 +1,49 @@
+
+from siliconcompiler.tools.openroad.openroad import setup as setup_tool
+from siliconcompiler.tools.openroad.openroad import build_pex_corners
+from siliconcompiler.tools.openroad.openroad import post_process as or_post_process
+from siliconcompiler.tools.openroad.openroad import pre_process as or_pre_process
+from siliconcompiler.tools.openroad.openroad import _set_reports
+
+
+def setup(chip):
+    '''
+    Perform global and detail placements along with design violation repairs
+    '''
+
+    # Generic tool setup.
+    setup_tool(chip)
+
+    tool = 'openroad'
+    design = chip.top()
+    step = chip.get('arg', 'step')
+    index = chip.get('arg', 'index')
+    task = chip._get_task(step, index)
+
+    if not chip.valid('input', 'layout', 'def') or \
+       not chip.get('input', 'layout', 'def', step=step, index=index):
+        chip.add('tool', tool, 'task', task, 'input', design + '.def', step=step, index=index)
+
+    _set_reports(chip, [
+        'setup',
+        'unconstrained',
+        'power',
+        'drv_violations',
+        'fmax',
+
+        # Images
+        'placement_density',
+        'routing_congestion',
+        'power_density',
+        'ir_drop',
+        'optimization_placement'
+    ])
+
+
+def pre_process(chip):
+    or_pre_process(chip)
+    build_pex_corners(chip)
+
+
+def post_process(chip):
+    or_post_process(chip)
