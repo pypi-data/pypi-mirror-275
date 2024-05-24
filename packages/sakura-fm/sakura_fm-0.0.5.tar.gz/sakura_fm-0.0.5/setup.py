697daacd239498b75b9ba79d04c66a8aee3473c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 17:03:51 +0000

    Add package sakura-fm/0.0.5/sakura_fm-0.0.5.tar.gz

---
+++ packages/sakura-fm/sakura_fm-0.0.5.tar.gz/sakura_fm-0.0.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.5"
+
+def get_long_description():
+    with open("README.md", encoding="UTF-8") as f:
+        long_description = f.read()
+        return long_description
+
+setup(
+    name='sakura_fm',
+    version=VERSION,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description_content_type='text/markdown',
+    author='Aditya',
+    author_email='adityaraj6311@gmail.com',
+    description='A simple scraper package for chatting with bots from sakura.fm',
+    long_description=get_long_description(),
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'pymongo'
+    ],
+    keywords=['sakura', 'sakura.fm', 'sakura scraper', 'sakurai'],
+    entry_points="""
+        [console_scripts]
+        sakura=sakura.sakura:main
+    """
+)
