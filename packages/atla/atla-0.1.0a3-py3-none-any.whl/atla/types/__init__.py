bb034530df799702a11139d0f1881a972ae6ddef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 16:04:35 +0000

    Add package atla/0.1.0a3/atla-0.1.0a3-py3-none-any.whl

---
+++ packages/atla/atla-0.1.0a3-py3-none-any.whl/atla/types/__init__.py
@@ -0,0 +1,6 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .evaluate_create_params import EvaluateCreateParams as EvaluateCreateParams
+from .evaluate_create_response import EvaluateCreateResponse as EvaluateCreateResponse
