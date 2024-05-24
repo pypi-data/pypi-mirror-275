73fc6c6e57ae37e9d499d024375d63495c2b4ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 05:03:21 +0000

    Add package movoid-xml-data/1.1.0/movoid_xml_data-1.1.0-py3-none-any.whl

---
+++ packages/movoid-xml-data/movoid_xml_data-1.1.0-py3-none-any.whl/movoid_xml_data/__init__.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : __init__
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/5/19 17:10
+# Description   : 
+"""
+from .label_data import LabelData
