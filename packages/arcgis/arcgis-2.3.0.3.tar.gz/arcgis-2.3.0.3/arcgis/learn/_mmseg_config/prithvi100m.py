06efebc4d103a52c4fe7af7d76ba7d96ac476ee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 06:04:17 +0000

    Add package arcgis/2.3.0.3/arcgis-2.3.0.3.tar.gz

---
+++ packages/arcgis/arcgis-2.3.0.3.tar.gz/arcgis-2.3.0.3/arcgis/learn/_mmseg_config/prithvi100m.py
@@ -0,0 +1,57 @@
+bands = [0, 1, 2, 3, 4, 5]
+nframes = 3
+
+norm_cfg = dict(type="BN", requires_grad=True)
+model = dict(
+    type="TemporalEncoderDecoder",
+    frozen_backbone=True,
+    backbone=dict(
+        type="TemporalViTEncoder",
+        pretrained="https://huggingface.co/ibm-nasa-geospatial/Prithvi-100M/resolve/main/Prithvi_100M.pt",
+        img_size=224,
+        patch_size=16,
+        num_frames=nframes,
+        tubelet_size=1,
+        in_chans=len(bands),
+        embed_dim=768,
+        depth=12,
+        num_heads=12,
+        mlp_ratio=4.0,
+        norm_pix_loss=False,
+    ),
+    neck=dict(
+        type="ConvTransformerTokensToEmbeddingNeck",
+        embed_dim=768 * nframes,
+        output_embed_dim=768 * nframes,
+        drop_cls_token=True,
+        Hp=14,
+        Wp=14,
+    ),
+    decode_head=dict(
+        in_channels=768 * nframes,
+        type="FCNHead",
+        in_index=-1,
+        channels=256,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=0.1,
+        norm_cfg=dict(type="BN", requires_grad=True),
+        align_corners=False,
+        loss_decode=dict(type="CrossEntropyLoss", use_sigmoid=False, loss_weight=0.4),
+    ),
+    auxiliary_head=dict(
+        in_channels=768 * nframes,
+        type="FCNHead",
+        in_index=-1,
+        channels=256,
+        num_convs=2,
+        concat_input=False,
+        dropout_ratio=0.1,
+        norm_cfg=dict(type="BN", requires_grad=True),
+        align_corners=False,
+        loss_decode=dict(type="CrossEntropyLoss", use_sigmoid=False, loss_weight=0.4),
+    ),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode="whole"),
+)
