c3cb38e297fda8b62326042ffa6e283dcb8ac575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:37 +0000

    Add package nextline-alert/0.1.6/nextline_alert-0.1.6.tar.gz

---
+++ packages/nextline-alert/nextline_alert-0.1.6.tar.gz/nextline_alert-0.1.6/tests/test_emitter.py
@@ -0,0 +1,74 @@
+import asyncio
+import json
+import time
+
+import pytest
+import respx
+from nextline import Nextline
+
+from nextline_alert.emitter import Emitter
+
+
+def func_success():
+    time.sleep(0.001)
+
+
+def func_raise_ignore():
+    time.sleep(0.001)
+    raise KeyboardInterrupt
+
+
+def func_raise():
+    time.sleep(0.001)
+    raise ValueError('test')
+
+
+@respx.mock
+@pytest.mark.parametrize('func', [func_success, func_raise_ignore])
+async def test_emit_no_alert(func) -> None:
+    nextline = Nextline(func)
+
+    url = 'http://localhost:5000/alerts'
+    platform = 'pytest'
+    emitter = Emitter(url=url, platform=platform)
+    assert nextline.register(emitter)
+
+    # Mock the HTTP POST request
+    route = respx.post(url).respond(status_code=200)
+
+    # Run a script that does not raise an exception
+    async with nextline:
+        event = asyncio.Event()
+        await nextline.run_continue_and_wait(event)
+
+    # Assert the HTTP POST request
+    assert not route.called
+
+
+@respx.mock
+async def test_emit_alert() -> None:
+    nextline = Nextline(func_raise)
+
+    url = 'http://localhost:5000/alerts'
+    platform = 'pytest'
+    emitter = Emitter(url=url, platform=platform)
+    assert nextline.register(emitter)
+
+    # Mock the HTTP POST request
+    route = respx.post(url).respond(status_code=200)
+
+    # Run a script that raises an exception
+    async with nextline:
+        event = asyncio.Event()
+        await nextline.run_continue_and_wait(event)
+
+    # Assert the HTTP POST request
+    assert route.called
+    assert len(route.calls) == 1
+    call = route.calls[0]
+    assert url == call.request.url
+    data = json.loads(call.request.content)
+    labels = data['alerts'][0]['labels']
+    description = data['alerts'][0]['annotations']['description']
+    assert platform == labels['platform']
+    assert 'ValueError' in description
