bf8bfd6475a8564c8a98299b8d29fd2f7104ad43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 04:03:12 +0000

    Add package shipyard-airtable/0.1.2/shipyard_airtable-0.1.2-py3-none-any.whl

---
+++ packages/shipyard-airtable/shipyard_airtable-0.1.2-py3-none-any.whl/shipyard_airtable/cli/authtest.py
@@ -0,0 +1,11 @@
+import os
+import sys
+from shipyard_airtable import AirtableClient
+
+
+def main():
+    sys.exit(AirtableClient(api_key=os.environ.get("AIRTABLE_API_KEY")).connect())
+
+
+if __name__ == "__main__":
+    main()
