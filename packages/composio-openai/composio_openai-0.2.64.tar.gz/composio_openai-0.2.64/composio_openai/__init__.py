91dbf126d108420880e5291c8c61132c15080698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:11 +0000

    Add package composio-openai/0.2.64/composio_openai-0.2.64.tar.gz

---
+++ packages/composio-openai/composio_openai-0.2.64.tar.gz/composio_openai-0.2.64/composio_openai/__init__.py
@@ -0,0 +1,12 @@
+from composio import Action, App, Tag
+
+from .openai_toolspec import ComposioToolset, OpenaiStyleToolsetBase
+
+
+__all__ = (
+    "Action",
+    "App",
+    "Tag",
+    "ComposioToolset",
+    "OpenaiStyleToolsetBase",
+)
