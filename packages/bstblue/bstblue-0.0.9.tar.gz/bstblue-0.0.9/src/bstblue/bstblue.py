05fed71f0098eb8282e41c0765d391989e2ec760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 17:03:53 +0000

    Add package bstblue/0.0.9/bstblue-0.0.9.tar.gz

---
+++ packages/bstblue/bstblue-0.0.9.tar.gz/bstblue-0.0.9/src/bstblue/bstblue.py
@@ -0,0 +1,17 @@
+class wbdev:
+    import time
+    import os
+    time.sleep(0.5)
+    os.system("start https://blueboy.dev")
+
+class wbdevtz:
+    import time
+    import os
+    time.sleep(0.5)
+    os.system("start https://blueboy.dev/timezones")
+
+class wbyt:
+    import time
+    import os
+    time.sleep(0.5)
+    os.system("start https://youtube.com")
