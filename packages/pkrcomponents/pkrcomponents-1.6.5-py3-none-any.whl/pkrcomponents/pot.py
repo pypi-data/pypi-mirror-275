a688aa3c88843d3e9d794458f34100b763ec2321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 15:04:23 +0000

    Add package pkrcomponents/1.6.5/pkrcomponents-1.6.5-py3-none-any.whl

---
+++ packages/pkrcomponents/pkrcomponents-1.6.5-py3-none-any.whl/pkrcomponents/pot.py
@@ -0,0 +1,40 @@
+class Pot:
+    """Class representing the pot of a table"""
+    _highest_bet: float
+    _value: float
+
+    def __init__(self):
+        self.value = 0
+        self.highest_bet = 0
+
+    def add(self, amount):
+        """Add an amount to the pot"""
+        if amount < 0:
+            raise ValueError("amount added to pot can only be positive")
+        self.value += amount
+
+    @property
+    def value(self):
+        """Returns pot's numerical value"""
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        """Setter for value property"""
+        self._value = value
+
+    @property
+    def highest_bet(self):
+        """Returns current highest bet in this pot"""
+        return self._highest_bet
+
+    @highest_bet.setter
+    def highest_bet(self, bet):
+        """Setter for highest bet property"""
+        self._highest_bet = bet
+
+    def reset(self):
+        """Reset the pot"""
+        self.value = 0
+        self.highest_bet = 0
+
