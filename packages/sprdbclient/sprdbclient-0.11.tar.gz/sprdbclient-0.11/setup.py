b9928273c20168fa24d65368abcc280388463135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 16:04:15 +0000

    Add package sprdbclient/0.11/sprdbclient-0.11.tar.gz

---
+++ packages/sprdbclient/sprdbclient-0.11.tar.gz/sprdbclient-0.11/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sprdbclient',
+    version='0.11',
+    packages=[''],
+    url='',
+    license='',
+    author='asxe',
+    author_email='2973918177@qq.com',
+    description='用于连接sprdb数据库。'
+)
