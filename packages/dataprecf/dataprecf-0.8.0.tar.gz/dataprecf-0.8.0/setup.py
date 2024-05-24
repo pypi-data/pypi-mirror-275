33ce741d5450325f333fa93d9e90a14ec6ca55a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 05:03:10 +0000

    Add package dataprecf/0.8.0/dataprecf-0.8.0.tar.gz

---
+++ packages/dataprecf/dataprecf-0.8.0.tar.gz/dataprecf-0.8.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dataprecf',
+    version='0.8.0',
+    packages=find_packages(where='dataprecf'),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'scikit-learn',
+        'nltk',
+    ],
+    description='A data preprocessor for machine learning projects.',
+    author='fatmanur caliskan, cihan yilmaz',
+    author_email='fatmanur.caliskan@stu.fsm.edu.tr, cihan.yilmaz@stu.fsm.edu.tr',
+    package_dir={'': 'dataprecf'},
+    include_package_data=True,
+    python_requires='>=3.6',
+)
