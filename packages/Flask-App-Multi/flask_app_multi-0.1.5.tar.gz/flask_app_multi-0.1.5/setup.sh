f229e85ffa7df0c0b21f5afb6b4a25ccbb82e0ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 14:03:37 +0000

    Add package Flask-App-Multi/0.1.5/flask_app_multi-0.1.5.tar.gz

---
+++ packages/Flask-App-Multi/flask_app_multi-0.1.5.tar.gz/flask_app_multi-0.1.5/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install virtualenv
+pip install virtualenv
+
+# Create a virtual environment
+virtualenv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install packages
+pip install requests
+
+
