6d17572fcf75ba91fceb38f3a871d3e80f137982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 16:03:58 +0000

    Add package oktmoparser/1.3/oktmoparser-1.3.tar.gz

---
+++ packages/oktmoparser/oktmoparser-1.3.tar.gz/oktmoparser-1.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+
+setup(
+    name='oktmoparser',
+    version='1.3',
+    author='letimvkocmoc',
+    author_email='letimvkocmoc@gmail.com',
+    description='Simple and useful parser that helps you get actual All-Russian Classifier of Municipal Territories (OKTMO)',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/letimvkocmoc/oktmo-parser',
+    classifiers=[
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    keywords='oktmo parser municipal territory classifier',
+    project_urls={
+        'GitGub': 'https://github.com/letimvkocmoc'
+    },
+    packages=find_packages(),
+    install_requires=['requests']
+)
