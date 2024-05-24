caa84ea61a2643ed2995f0f3f55d96bc15eb2c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:24 +0000

    Add package testwizard.test/3.8.1/testwizard.test-3.8.1.tar.gz

---
+++ packages/testwizard.test/testwizard.test-3.8.1.tar.gz/testwizard.test-3.8.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_namespace_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="testwizard.test",
+    version="3.8.1",
+    author="Resillion - Belgium",
+    author_email="testwizard-support@resillion.com",
+    description="Testwizard test",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.resillion.com/testwizard/",
+    packages=find_namespace_packages(),
+    install_requires=[
+          'testwizard.core==3.8.1',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.3",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
