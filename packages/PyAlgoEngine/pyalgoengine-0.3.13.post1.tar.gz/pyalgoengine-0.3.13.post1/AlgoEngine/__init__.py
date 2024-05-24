cc3f67dd894a759ac91148b88de4a41eadfa9231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 15:04:24 +0000

    Add package PyAlgoEngine/0.3.13.post1/pyalgoengine-0.3.13.post1.tar.gz

---
+++ packages/PyAlgoEngine/pyalgoengine-0.3.13.post1.tar.gz/pyalgoengine-0.3.13.post1/AlgoEngine/__init__.py
@@ -0,0 +1,16 @@
+__version__ = "0.3.13.post1"
+
+import traceback
+
+from . import Engine
+from . import Strategies
+
+Engine.LOGGER.info(f'AlgoEngine version {__version__}')
+
+# import addon module
+try:
+    from . import EngineAddon
+
+    Engine.LOGGER.info(f'AlgoEngine_Addons import successful, version {EngineAddon.__version__}')
+except ImportError:
+    Engine.LOGGER.debug(f'Install AlgoEngine_Addons to use Statistics module\n{traceback.format_exc()}')
