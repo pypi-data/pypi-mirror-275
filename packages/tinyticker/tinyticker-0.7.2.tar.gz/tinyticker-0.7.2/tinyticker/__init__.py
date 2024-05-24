ba26d6dd361843a3bbcfe45b1d055465890a6fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 15:04:25 +0000

    Add package tinyticker/0.7.2/tinyticker-0.7.2.tar.gz

---
+++ packages/tinyticker/tinyticker-0.7.2.tar.gz/tinyticker-0.7.2/tinyticker/__init__.py
@@ -0,0 +1,22 @@
+import logging
+
+from .config import TickerConfig, TinytickerConfig, SequenceConfig
+from .display import Display
+from .sequence import Sequence
+from .tickers import Ticker, TickerCrypto, TickerStock
+
+logger = logging.getLogger(__name__)
+
+logger.addHandler(logging.NullHandler())
+
+__version__ = "0.7.2"
+__all__ = [
+    "Display",
+    "Sequence",
+    "SequenceConfig",
+    "Ticker",
+    "TickerConfig",
+    "TickerCrypto",
+    "TickerStock",
+    "TinytickerConfig",
+]
---
+++ packages/tinyticker/tinyticker-0.7.2.tar.gz/tinyticker-0.7.2/tinyticker/waveshare_lib/models.py
@@ -0,0 +1,72 @@
+from dataclasses import dataclass
+from typing import Type, Union
+
+from . import (
+    epd2in13,
+    epd2in13_V2,
+    epd2in13_V3,
+    epd2in13_V4,
+    epd2in13b_V3,
+    epd2in13b_V4,
+    epd2in13bc,
+    epd2in7,
+    epd2in7_V2,
+)
+from ._base import EPDHighlight, EPDMonochrome
+
+
+@dataclass
+class EPDModel:
+    name: str
+    class_: Union[Type[EPDMonochrome], Type[EPDHighlight]]
+    desc: str
+
+
+MODELS = [
+    EPDModel(
+        name="EPD",
+        class_=epd2in13.EPD,
+        desc="Black and White 2.13 inch",
+    ),
+    EPDModel(
+        name="EPD_v2",
+        class_=epd2in13_V2.EPD,
+        desc="Black and White 2.13 inch V2",
+    ),
+    EPDModel(
+        name="EPD_v3",
+        class_=epd2in13_V3.EPD,
+        desc="Black and White 2.13 inch V3",
+    ),
+    EPDModel(
+        name="EPD_v4",
+        class_=epd2in13_V4.EPD,
+        desc="Black and White 2.13 inch V4",
+    ),
+    EPDModel(
+        name="EPDb_v3",
+        class_=epd2in13b_V3.EPD,
+        desc="Black, White and Red 2.13 inch V3",
+    ),
+    EPDModel(
+        name="EPDb_v4",
+        class_=epd2in13b_V4.EPD,
+        desc="Black, White and Red 2.13 inch V4",
+    ),
+    EPDModel(
+        name="EPDbc",
+        class_=epd2in13bc.EPD,
+        desc="Black, White and Yellow 2.13 inch",
+    ),
+    EPDModel(
+        name="EPD_2in7",
+        class_=epd2in7.EPD,
+        desc="Black and White 2.7 inch",
+    ),
+    EPDModel(
+        name="EPD_2in7_v2",
+        class_=epd2in7_V2.EPD,
+        desc="Black and White 2.7 inch V2",
+    ),
+]
+MODELS = {model.name: model for model in MODELS}
