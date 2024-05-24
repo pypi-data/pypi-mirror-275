8d032ea7d78dbca00e04747c3148d16f419cf96d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 05:03:55 +0000

    Add package pdftext/0.3.8/pdftext-0.3.8-py3-none-any.whl

---
+++ packages/pdftext/pdftext-0.3.8-py3-none-any.whl/pdftext/extraction.py
@@ -0,0 +1,90 @@
+from functools import partial
+from typing import List
+from concurrent.futures import ProcessPoolExecutor
+import math
+import pypdfium2 as pdfium
+
+from pdftext.inference import inference
+from pdftext.model import get_model
+from pdftext.pdf.chars import get_pdfium_chars
+from pdftext.pdf.utils import unnormalize_bbox
+from pdftext.postprocessing import merge_text, sort_blocks, postprocess_text, handle_hyphens
+from pdftext.settings import settings
+
+
+def _get_page_range(pdf_path, model, page_range):
+    pdf_doc = pdfium.PdfDocument(pdf_path)
+    text_chars = get_pdfium_chars(pdf_doc, page_range)
+    pages = inference(text_chars, model)
+    return pages
+
+
+def _get_pages(pdf_path, model=None, page_range=None, workers=None):
+    if model is None:
+        model = get_model()
+
+    pdf_doc = pdfium.PdfDocument(pdf_path)
+    if page_range is None:
+        page_range = range(len(pdf_doc))
+
+    if workers is not None:
+        workers = min(workers, len(page_range) // settings.WORKER_PAGE_THRESHOLD) # It's inefficient to have too many workers, since we batch in inference
+
+    if workers is None or workers <= 1:
+        text_chars = get_pdfium_chars(pdf_doc, page_range)
+        return inference(text_chars, model)
+
+    func = partial(_get_page_range, pdf_path, model)
+    page_range = list(page_range)
+
+    pages_per_worker = math.ceil(len(page_range) / workers)
+    page_range_chunks = [page_range[i * pages_per_worker:(i + 1) * pages_per_worker] for i in range(workers)]
+
+    with ProcessPoolExecutor(max_workers=workers) as executor:
+        pages = list(executor.map(func, page_range_chunks))
+
+    ordered_pages = [page for sublist in pages for page in sublist]
+
+    return ordered_pages
+
+
+def plain_text_output(pdf_path, sort=False, model=None, hyphens=False, page_range=None, workers=None) -> str:
+    text = paginated_plain_text_output(pdf_path, sort=sort, model=model, hyphens=hyphens, page_range=page_range, workers=workers)
+    return "\n".join(text)
+
+
+def paginated_plain_text_output(pdf_path, sort=False, model=None, hyphens=False, page_range=None, workers=None) -> List[str]:
+    pages = _get_pages(pdf_path, model, page_range, workers=workers)
+    text = []
+    for page in pages:
+        text.append(merge_text(page, sort=sort, hyphens=hyphens).strip())
+    return text
+
+
+def dictionary_output(pdf_path, sort=False, model=None, page_range=None, keep_chars=False, workers=None):
+    pages = _get_pages(pdf_path, model, page_range, workers=workers)
+    for page in pages:
+        for block in page["blocks"]:
+            bad_keys = [key for key in block.keys() if key not in ["lines", "bbox"]]
+            for key in bad_keys:
+                del block[key]
+            for line in block["lines"]:
+                bad_keys = [key for key in line.keys() if key not in ["bbox", "spans"]]
+                for key in bad_keys:
+                    del line[key]
+                for span in line["spans"]:
+                    span["bbox"] = unnormalize_bbox(span["bbox"], page["width"], page["height"])
+                    span["text"] = postprocess_text(span["text"])
+                    span["text"] = handle_hyphens(span["text"], keep_hyphens=True)
+
+                    if not keep_chars:
+                        del span["chars"]
+                    else:
+                        for char in span["chars"]:
+                            char["bbox"] = unnormalize_bbox(char["bbox"], page["width"], page["height"])
+
+                line["bbox"] = unnormalize_bbox(line["bbox"], page["width"], page["height"])
+            block["bbox"] = unnormalize_bbox(block["bbox"], page["width"], page["height"])
+        if sort:
+            page["blocks"] = sort_blocks(page["blocks"])
+    return pages
---
+++ packages/pdftext/pdftext-0.3.8-py3-none-any.whl/pdftext/inference.py
@@ -0,0 +1,197 @@
+from itertools import chain
+
+import sklearn
+
+from pdftext.pdf.utils import LINE_BREAKS, TABS, SPACES
+from pdftext.settings import settings
+
+
+def update_current(current, new_char):
+    bbox = new_char["bbox"]
+    if "bbox" not in current:
+        current_bbox = bbox.copy()
+        current["bbox"] = current_bbox
+    else:
+        current_bbox = current["bbox"]
+        current_bbox[0] = min(bbox[0], current_bbox[0])
+        current_bbox[1] = min(bbox[1], current_bbox[1])
+        current_bbox[2] = max(bbox[2], current_bbox[2])
+        current_bbox[3] = max(bbox[3], current_bbox[3])
+    current["center_x"] = (current_bbox[0] + current_bbox[2]) / 2
+    current["center_y"] = (current_bbox[1] + current_bbox[3]) / 2
+
+
+def create_training_row(char_info, prev_char, currblock, currline):
+    char = char_info["char"]
+
+    # Store variables used multiple times
+    char_x1, char_y1, char_x2, char_y2 = char_info["bbox"]
+    prev_x1, prev_y1, prev_x2, prev_y2 = prev_char["bbox"]
+    char_center_x = (char_x2 + char_x1) / 2
+    char_center_y = (char_y2 + char_y1) / 2
+    x_gap = char_x1 - prev_x2
+    y_gap = char_y1 - prev_y2
+
+    char_font = char_info["font"]
+    prev_font = prev_char["font"]
+    font_match = all(
+        [char_font[key] == prev_font[key] for key in ["name", "size", "weight", "flags"]] +
+        [char_info["rotation"] == prev_char["rotation"]]
+    )
+    is_space = any([
+        char in SPACES,
+        char in TABS,
+    ])
+
+    training_row = {
+        "is_newline": char in LINE_BREAKS,
+        "is_space": is_space,
+        "x_gap": x_gap,
+        "y_gap": y_gap,
+        "font_match": font_match,
+        "x_outer_gap": char_x2 - prev_x1,
+        "y_outer_gap": char_y2 - prev_y1,
+        "line_x_center_gap": char_center_x - currline["center_x"],
+        "line_y_center_gap": char_center_y - currline["center_y"],
+        "line_x_gap": char_x1 - currline["bbox"][2],
+        "line_y_gap": char_y1 - currline["bbox"][3],
+        "line_x_start_gap": char_x1 - currline["bbox"][0],
+        "line_y_start_gap": char_y1 - currline["bbox"][1],
+        "block_x_center_gap": char_center_x - currblock["center_x"],
+        "block_y_center_gap": char_center_y - currblock["center_y"],
+        "block_x_gap": char_x1 - currblock["bbox"][2],
+        "block_y_gap": char_y1 - currblock["bbox"][3],
+        "block_x_start_gap": char_x1 - currblock["bbox"][0],
+        "block_y_start_gap": char_y1 - currblock["bbox"][1]
+    }
+
+    return training_row
+
+
+def update_span(line, span):
+    if len(span["chars"]) > 0:
+        span["font"] = span["chars"][0]["font"]
+        span["rotation"] = span["chars"][0]["rotation"]
+        char_bboxes = [char["bbox"] for char in span["chars"]]
+        span["bbox"] = [min([bbox[0] for bbox in char_bboxes]),
+                        min([bbox[1] for bbox in char_bboxes]),
+                        max([bbox[2] for bbox in char_bboxes]),
+                        max([bbox[3] for bbox in char_bboxes])]
+        span["text"] = "".join([char["char"] for char in span["chars"]])
+        span["char_start_idx"] = span["chars"][0]["char_idx"]
+        span["char_end_idx"] = span["chars"][-1]["char_idx"]
+
+    # Remove unneeded keys from the characters
+    for char in span["chars"]:
+        del_keys = [k for k in list(char.keys()) if k not in ["char", "bbox"]]
+        for key in del_keys:
+            del char[key]
+    line["spans"].append(span)
+    span = {"chars": []}
+    return span
+
+
+def update_line(block, line):
+    block["lines"].append(line)
+    line = {"spans": []}
+    return line
+
+
+def update_block(blocks, block):
+    blocks["blocks"].append(block)
+    block = {"lines": []}
+    return block
+
+
+def infer_single_page(text_chars, block_threshold=settings.BLOCK_THRESHOLD):
+    prev_char = None
+    prev_font_info = None
+
+    blocks = {
+        "blocks": [],
+        "page": text_chars["page"],
+        "rotation": text_chars["rotation"],
+        "bbox": text_chars["bbox"],
+        "width": text_chars["width"],
+        "height": text_chars["height"],
+    }
+    block = {"lines": []}
+    line = {"spans": []}
+    span = {"chars": []}
+    for i, char_info in enumerate(text_chars["chars"]):
+        font_info = f"{char_info['font']['name']}_{char_info['font']['size']}_{char_info['font']['weight']}_{char_info['font']['flags']}_{char_info['rotation']}"
+        if prev_char:
+            training_row = create_training_row(char_info, prev_char, block, line)
+            sorted_keys = sorted(training_row.keys())
+            training_row = [training_row[key] for key in sorted_keys]
+
+            prediction_probs = yield training_row
+            # First item is probability of same line/block, second is probability of new line, third is probability of new block
+            if prediction_probs[0] >= .5:
+                # Ensure we update spans properly for font info when predicting no new line
+                if prev_font_info != font_info:
+                    span = update_span(line, span)
+            elif prediction_probs[2] > block_threshold:
+                span = update_span(line, span)
+                line = update_line(block, line)
+                block = update_block(blocks, block)
+            elif prev_char["char"] in LINE_BREAKS: # Look for newline character as a forcing signal for a new line
+                span = update_span(line, span)
+                line = update_line(block, line)
+            elif prev_font_info != font_info:
+                span = update_span(line, span)
+
+        span["chars"].append(char_info)
+        update_current(line, char_info)
+        update_current(block, char_info)
+
+        prev_char = char_info
+        prev_font_info = font_info
+    if len(span["chars"]) > 0:
+        update_span(line, span)
+    if len(line["spans"]) > 0:
+        update_line(block, line)
+    if len(block["lines"]) > 0:
+        update_block(blocks, block)
+
+    return blocks
+
+
+def inference(text_chars, model):
+    # Create generators and get first training row from each
+    generators = [infer_single_page(text_page) for text_page in text_chars]
+    next_prediction = {}
+
+    page_blocks = {}
+    while len(page_blocks) < len(generators):
+        training_data = {}
+        for page_idx, page_generator in enumerate(generators):
+            if page_idx in page_blocks:
+                continue
+
+            try:
+                if page_idx not in next_prediction:
+                    training_row = next(page_generator)
+                else:
+                    training_row = page_generator.send(next_prediction[page_idx])
+                    del next_prediction[page_idx]
+                training_data[page_idx] = training_row
+            except StopIteration as e:
+                blocks = e.value
+                page_blocks[page_idx] = blocks
+
+        if len(page_blocks) == len(generators):
+            break
+
+        training_idxs = sorted(training_data.keys())
+        training_rows = [training_data[idx] for idx in training_idxs]
+
+        # Disable nan, etc, validation for a small speedup
+        with sklearn.config_context(assume_finite=True):
+            predictions = model.predict_proba(training_rows)
+        for pred, page_idx in zip(predictions, training_idxs):
+            next_prediction[page_idx] = pred
+    sorted_keys = sorted(page_blocks.keys())
+    page_blocks = [page_blocks[key] for key in sorted_keys]
+    assert len(page_blocks) == len(text_chars)
+    return page_blocks
