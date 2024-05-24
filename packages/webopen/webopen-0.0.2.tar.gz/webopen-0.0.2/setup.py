aa6f55af0f65f10f268ec29d26ae09c1abf1180c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 16:04:59 +0000

    Add package webopen/0.0.2/webopen-0.0.2.tar.gz

---
+++ packages/webopen/webopen-0.0.2.tar.gz/webopen-0.0.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='webopen',
+    version='0.0.2',
+    author='Bhuvaneshwar_Marri',
+    author_email='bhuvaneshwarmarri@gmail.com',
+    description='A small example package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/BhuvaneshwarMarri/webopen',
+    project_urls={
+        'Homepage': 'https://github.com/BhuvaneshwarMarri/webopen',
+        'Issues': 'https://github.com/BhuvaneshwarMarri/webopen/issues'
+    },
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["add", "search","delete"],
+    python_requires=">=3.8",
+    install_requires=[
+        "argparse",
+        "click",
+    ],
+    entry_points={
+        'console_scripts': [
+            'webopen = webopen.main:main',
+        ],
+    },
+)
---
+++ packages/webopen/webopen-0.0.2.tar.gz/webopen-0.0.2/src/webopen/main.py
@@ -0,0 +1,92 @@
+import webbrowser
+import argparse
+from webopen.db.connection import get_db
+from webopen.db.models import create_tables
+import click
+
+def add_data(website, username, password):
+    with get_db() as conn:
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO creds (website,username,password) VALUES (?,?,?)', (website, username, password,))
+        conn.commit()
+        click.echo("Data added successfully")
+
+def credential_add():
+    create_tables()
+    website = click.prompt("Enter website name")
+    username = click.prompt("Enter username for website")
+    password = click.prompt("Enter password for website")
+    add_data(website, username, password)
+
+def search_data(website):
+    create_tables()
+    with get_db() as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+        SELECT website, username, password
+        FROM creds
+        WHERE website=?
+        ''', (website,))
+        data = cursor.fetchone()
+        if data:
+            labels = ["Website", "Username", "Password"]
+            for i in range(3):
+                click.echo(f"{labels[i]} : {data[i]}")
+            return True
+        else:
+            click.echo("No data found for the specified website.")
+            return False
+
+def credential_search():
+    website = click.prompt("Enter website name to search")
+    search_data(website)
+
+def delete_data(website):
+    create_tables()
+    with get_db() as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+        DELETE FROM creds
+        WHERE website=?
+        ''', (website,))
+        conn.commit()
+        click.echo("Data deleted successfully")
+
+def credential_delete():
+    website = click.prompt("Enter website name to delete")
+    x=search_data(website)
+    if x:
+        choice = click.prompt("Enter yes to delete this credential else type no")
+        if choice == "yes":
+            delete_data(website)
+        else:
+            click.echo("Successfully exited from delete process")
+    
+def open_link(website):
+    webbrowser.open(f"http://{website}")
+        
+def main():
+    
+    parser = argparse.ArgumentParser(description="Manage credentials and open links.")
+    subparsers = parser.add_subparsers(dest="command", help="Commands")
+    
+    parser.add_argument("-link", dest="link_name", help="The name of the link to open")
+    subparsers.add_parser("add", help="Add a new credential")
+    subparsers.add_parser("search", help="Search for a credential")
+    subparsers.add_parser("delete",help="Delete a particular credential")
+    args = parser.parse_args()
+    
+    if args.link_name:
+        open_link(args.link_name)
+    elif args.command == "add":
+        credential_add()
+    elif args.command == "search":
+        credential_search()
+    elif args.command == "delete":
+        credential_delete()
+    else:
+        parser.print_help()
+
+if __name__ == "__main__":
+    create_tables()
+    main()
