825fb6ec2ac610e901287fe58096a81a8604d3bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 15:04:21 +0000

    Add package blue-stability/4.28.1/blue_stability-4.28.1.tar.gz

---
+++ packages/blue-stability/blue_stability-4.28.1.tar.gz/blue_stability-4.28.1/blue_stability/__init__.py
@@ -0,0 +1,7 @@
+NAME = "blue_stability"
+
+ICON = "🟦"
+
+DESCRIPTION = f"{ICON} bash cli for stability.ai API's stable diffusion inference"
+
+VERSION = "4.28.1"
---
+++ packages/blue-stability/blue_stability-4.28.1.tar.gz/blue_stability-4.28.1/setup.py
@@ -0,0 +1,12 @@
+from blue_stability import NAME, VERSION, DESCRIPTION
+from blueness.pypi import setup
+
+
+setup(
+    filename=__file__,
+    repo_name="blue-stability",
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=[NAME],
+)
