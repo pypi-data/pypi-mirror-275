57617f469ec367480d97c93a8b4088790c778e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 16:04:51 +0000

    Add package pygradflow/0.5.0/pygradflow-0.5.0-py3-none-any.whl

---
+++ packages/pygradflow/pygradflow-0.5.0-py3-none-any.whl/pygradflow/runners/instance.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+
+class Instance(ABC):
+    def __init__(self, name, num_vars, num_cons):
+        self.name = name
+        self.num_vars = num_vars
+        self.num_cons = num_cons
+
+    @property
+    def size(self):
+        return self.num_vars + self.num_cons
+
+    @abstractmethod
+    def solve(self, params):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def problem(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def x0(self):
+        raise NotImplementedError()
---
+++ packages/pygradflow/pygradflow-0.5.0-py3-none-any.whl/pygradflow/step/__init__.py
@@ -0,0 +1,30 @@
+from pygradflow.iterate import Iterate
+from pygradflow.params import Params, StepSolverType
+from pygradflow.problem import Problem
+from pygradflow.step.asymmetric_step_solver import AsymmetricStepSolver
+from pygradflow.step.extended_step_solver import ExtendedStepSolver
+from pygradflow.step.standard_step_solver import StandardStepSolver
+from pygradflow.step.step_solver import StepSolver
+from pygradflow.step.symmetric_step_solver import SymmetricStepSolver
+
+
+def step_solver(
+    problem: Problem, params: Params, iterate: Iterate, dt: float, rho: float
+) -> StepSolver:
+    assert dt > 0.0
+    assert rho > 0.0
+
+    if params.step_solver is not None:
+        return params.step_solver(problem, params, iterate, dt, rho)
+
+    step_solver_type = params.step_solver_type
+
+    if step_solver_type == StepSolverType.Standard:
+        return StandardStepSolver(problem, params, iterate, dt, rho)
+    elif step_solver_type == StepSolverType.Extended:
+        return ExtendedStepSolver(problem, params, iterate, dt, rho)
+    elif step_solver_type == StepSolverType.Symmetric:
+        return SymmetricStepSolver(problem, params, iterate, dt, rho)
+    else:
+        assert step_solver_type == StepSolverType.Asymmetric
+        return AsymmetricStepSolver(problem, params, iterate, dt, rho)
