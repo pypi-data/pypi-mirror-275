00da884810e7c61115a57fd6fed29cd925d67346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-24 03:03:21 +0000

    Add package torcj/0.0.9/torcj-0.0.9.tar.gz

---
+++ packages/torcj/torcj-0.0.9.tar.gz/torcj-0.0.9/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+    name="torcj",
+    version="0.0.9",
+    author="Example",
+    description="An example package",
+    packages=["torcj"],
+    package_data={"torcj": ["storage.json"]},
+)
