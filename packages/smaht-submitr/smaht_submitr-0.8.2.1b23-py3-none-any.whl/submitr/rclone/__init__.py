e10ba3ba0daf208f627a37f037f3d22e6ad4664b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 05:04:21 +0000

    Add package smaht-submitr/0.8.2.1b23/smaht_submitr-0.8.2.1b23-py3-none-any.whl

---
+++ packages/smaht-submitr/smaht_submitr-0.8.2.1b23-py3-none-any.whl/submitr/rclone/__init__.py
@@ -0,0 +1,5 @@
+from submitr.rclone.rcloner import RCloner  # noqa
+from submitr.rclone.rclone_config import RCloneConfig, RCloneCredentials  # noqa
+from submitr.rclone.rclone_amazon import AmazonCredentials, RCloneAmazon  # noqa
+from submitr.rclone.rclone_google import GoogleCredentials, RCloneGoogle  # noqa
+from submitr.rclone.rclone_utils import cloud_path  # noqa
