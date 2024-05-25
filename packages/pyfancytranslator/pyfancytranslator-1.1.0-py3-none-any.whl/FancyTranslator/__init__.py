731959748afe7e1765e35f2b0ebbc22db072a13c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 05:04:38 +0000

    Add package pyfancytranslator/1.1.0/pyfancytranslator-1.1.0-py3-none-any.whl

---
+++ packages/pyfancytranslator/pyfancytranslator-1.1.0-py3-none-any.whl/FancyTranslator/__init__.py
@@ -0,0 +1,10 @@
+from .classes import Translator, Translation
+
+# Backward compatibility
+Language = Translation
+
+__version__ = '1.0.3'
+__author__ = 'Ashenguard'
+__email__ = 'ashenguard@agmstudio.xyz'
+
+__all__ = ['Translator', 'Translation']
