bc926b3faddcaebddf714ac5b883abb8b796022d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 15:04:13 +0000

    Add package dariopdf/1.2/dariopdf-1.2.tar.gz

---
+++ packages/dariopdf/dariopdf-1.2.tar.gz/dariopdf-1.2/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(  # 3 essential arguments
+    name="dariopdf",
+    version=1.2,
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=["tests", "data"])  # tell what packages are going to be distributed
+)
+
+"""(where: StrPath = ".", exclude: Iterable[str] = (), include: Iterable[str] = ("*", )) -> list[str]"""
+""" excluded tests and data from distribution"""
