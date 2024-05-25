d187fbed7cb5e4527ae5b9198daa7c0ea6f2fc99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 14:04:06 +0000

    Add package starstream/0.0.1/starstream-0.0.1-py3-none-any.whl

---
+++ packages/starstream/starstream-0.0.1-py3-none-any.whl/starstream/Hinode.py
@@ -0,0 +1,96 @@
+from .utils import asyncFITS, datetime_interval
+from datetime import timedelta
+from bs4 import BeautifulSoup
+from io import BytesIO
+import numpy as np
+import asyncio
+import glob
+import aiofiles
+from itertools import chain
+import os
+
+
+class Hinode:
+    class XRT:
+        batch_size: int = 1
+
+        def __init__(self, filetype: str = "png") -> None:
+            self.filetype = filetype
+            self.path = lambda name: f"./data/Hinode/XRT/{name}.{filetype}"
+            self.xrt_folder_path = "./data/Hinode/XRT/"
+            self.download_urls = []
+            self.url = (
+                lambda date, hour: f"https://xrt.cfa.harvard.edu/level1/{date[:4]}/{date[4:6]}/{date[6:]}/H{hour[:2]}00/"
+            )
+            os.makedirs(self.xrt_folder_path, exist_ok=True)
+
+        def check_tasks(self, scrap_date):
+            scrap_date = datetime_interval(
+                scrap_date[0], scrap_date[-1], timedelta(hours=1), "%Y%m%d-%H%M"
+            )
+            self.new_scrap_date_list = [
+                date.split("-")
+                for date in scrap_date
+                if len(glob.glob(self.path(f'{date.split("-")[0]}*'))) == 0
+            ]
+
+        def get_scrap_tasks(self, session):
+            return [
+                self.scrap_names(session, date, hour)
+                for date, hour in self.new_scrap_date_list
+            ]
+
+        async def scrap_names(self, session, date, hour):
+            async with session.get(self.url(date, hour), ssl=False) as response:
+                html = await response.text()
+                soup = BeautifulSoup(html, "html.parser")
+                fits_links = soup.find_all(
+                    "a", href=lambda href: href and href.endswith(".fits")
+                )
+                download_urls = [
+                    self.url(date, hour) + link["href"] for link in fits_links
+                ]
+
+            await asyncio.gather(
+                *[self.download_url(session, url) for url in download_urls]
+            )
+
+        def fits_processing(self, fits_file, path):
+            image = fits_file[0].data
+            np.save(path, image)
+
+        async def download_url(self, session, url):
+            async with session.get(url, ssl=False) as response:
+                data = await response.read()
+                if self.filetype != "fits":
+                    await asyncFITS(
+                        BytesIO(data), self.fits_processing, self.path(url[-22:-9])
+                    )
+                else:
+                    async with aiofiles.open(self.path(url[-22:-9]), "wb") as file:
+                        await file.write(data)
+
+        async def downloader_pipeline(self, scrap_date, session):
+            self.check_tasks(scrap_date)
+            if len(self.new_scrap_date_list) == 0:
+                print("Already downloaded!")
+            else:
+                scrap_tasks = self.get_scrap_tasks(session)
+                for i in range(0, len(scrap_tasks), self.batch_size):
+                    await asyncio.gather(*scrap_tasks[i : i + self.batch_size])
+
+        def get_hour_images(self, date):
+            query_c = "*" + "_".join(date.split("-"))[:-4] + "**"
+            return glob.glob(self.path(query_c))
+
+        async def data_prep(self, scrap_date):
+            scrap_date = datetime_interval(
+                scrap_date[0],
+                scrap_date[-1],
+                step_size=timedelta(days=1),
+            )
+            return [
+                *chain.from_iterable(
+                    [glob.glob(self.path(f"{date}*")) for date in scrap_date]
+                )
+            ]
---
+++ packages/starstream/starstream-0.0.1-py3-none-any.whl/starstream/OMNI.py
@@ -0,0 +1,74 @@
+from .utils import asyncCDF, datetime_interval
+from ._base import CDAWeb
+import pandas as pd
+import aiofiles
+import asyncio
+import os
+from dateutil.relativedelta import relativedelta
+
+
+class OMNI(CDAWeb):
+    def __init__(self) -> None:
+        super().__init__()
+        self.url = (
+            lambda date: f"https://cdaweb.gsfc.nasa.gov/sp_phys/data/omni/hro2_5min/{date[:4]}/omni_hro2_5min_{date}01_v01.cdf"
+        )
+        self.phy_obs = [
+            "BX_GSE",
+            "BY_GSE",
+            "BZ_GSE",
+            "Mach_num",
+            "Mgs_mach_num",
+            "PR-FLX_10",
+            "PR-FLX_30",
+            "PR-FLX_60",
+            "proton_density",
+            "flow_speed",
+            "Vx",
+            "Vy",
+            "Vz",
+        ]
+        self.variables = self.phy_obs
+        self.csv_path = lambda date: f"./data/OMNI/HRO2/{date}.csv"
+        self.cdf_path = lambda date: f"./data/OMNI/HRO2/{date}.cdf"
+        os.makedirs("./data/OMNI/HRO2/", exist_ok=True)
+
+    def check_tasks(self, scrap_date):
+        scrap_date = datetime_interval(*scrap_date, relativedelta(months=1), "%Y%m")
+        self.new_scrap_date_list = [
+            date for date in scrap_date if not os.path.exists(self.csv_path(date))
+        ]
+
+    async def download_url(self, session, date):
+        async with session.get(self.url(date), ssl=False) as response:
+            cdf_data = await response.read()
+            async with aiofiles.open(self.cdf_path(date), mode="wb") as f:
+                await f.write(cdf_data)
+
+    def get_download_tasks(self, session):
+        return [self.download_url(session, date) for date in self.new_scrap_date_list]
+
+    async def preprocessing(self, date):
+        await asyncCDF(self.cdf_path(date), self.default_cda_processing, date)
+        os.remove(self.cdf_path(date))
+
+    def get_preprocessing_tasks(self):
+        return [self.preprocessing(date) for date in self.new_scrap_date_list]
+
+    async def downloader_pipeline(self, scrap_date, session):
+        self.check_tasks(scrap_date)
+        await asyncio.gather(*self.get_download_tasks(session))
+        await asyncio.gather(*self.get_preprocessing_tasks())
+
+    async def get_df(self, date):
+        df = await asyncio.get_event_loop().run_in_executor(
+            None, pd.read_csv, self.csv_path(date)
+        )
+        return pd.from_pandas(df)
+
+    def get_dfs(self, scrap_date):
+        return [self.get_df(date) for date in scrap_date]
+
+    async def data_prep(self, scrap_date):
+        dfs = await asyncio.gather(*self.get_dfs(scrap_date))
+        return pd.concat(dfs)
