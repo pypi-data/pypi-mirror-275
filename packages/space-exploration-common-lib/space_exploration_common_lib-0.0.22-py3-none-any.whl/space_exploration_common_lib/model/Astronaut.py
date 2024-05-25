413ec833c268b91c11b55bdf45b3bf34ab34e92e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 05:04:39 +0000

    Add package space-exploration-common-lib/0.0.22/space_exploration_common_lib-0.0.22-py3-none-any.whl

---
+++ packages/space-exploration-common-lib/space_exploration_common_lib-0.0.22-py3-none-any.whl/space_exploration_common_lib/model/Astronaut.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class Astronaut(BaseModel):
+    astronaut_first_name: str
+    astronaut_last_name: str
+    program_names: List[str]
+    image_url: str
