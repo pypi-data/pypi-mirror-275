6b01136ce5d81f284dcf0dfa910c0db9ecfcdb4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 04:04:49 +0000

    Add package lambdapdk/0.1.22/lambdapdk-0.1.22.tar.gz

---
+++ packages/lambdapdk/lambdapdk-0.1.22.tar.gz/lambdapdk-0.1.22/lambdapdk/__init__.py
@@ -0,0 +1,15 @@
+import siliconcompiler.package as sc_package
+
+
+__version__ = "0.1.22"
+
+
+def register_data_source(chip):
+    sc_package.register_python_data_source(
+        chip,
+        "lambdapdk",
+        "lambdapdk",
+        "https://github.com/siliconcompiler/lambdapdk/archive/refs/tags/",
+        alternative_ref=f"v{__version__}",
+        python_module_path_append=".."
+    )
