0fa62f73d3a7500b0b03c38abef2f0c48d4d8428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 15:04:55 +0000

    Add package online-scheduler/0.1.1/online_scheduler-0.1.1.tar.gz

---
+++ packages/online-scheduler/online_scheduler-0.1.1.tar.gz/online_scheduler-0.1.1/online_scheduler/__init__.py
@@ -0,0 +1,10 @@
+from online_scheduler import *
+
+__copyright__    = 'Copyright (C) 2018 Your Name'
+__version__      = '0.1.2'
+__license__      = 'BSD-3-Clause'
+__author__       = 'Your Name'
+__author_email__ = 'Your@Email'
+__url__          = 'http://github.com/account/repository'
+
+__all__ = ['online_scheduler', ...]
