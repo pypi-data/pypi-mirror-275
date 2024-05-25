0f34cfe98441af0b198fcec9e4ed6913c19d555d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 06:04:12 +0000

    Add package Scrapelytix/0.0.2/Scrapelytix-0.0.2-py3-none-any.whl

---
+++ packages/Scrapelytix/Scrapelytix-0.0.2-py3-none-any.whl/Scrapelytix/__init__.py
@@ -0,0 +1,3 @@
+from Scrapelytix.extraction import pass_data, player_data
+from Scrapelytix.filter import analyze_passes, analyze_shots
+from Scrapelytix.plot import pass_network, prg_passes, shot_map
