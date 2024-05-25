06ccbda36319061bf3de430fea3ce5c67af77087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 04:04:36 +0000

    Add package pbx-code-owners/1.2.0/pbx_code_owners-1.2.0-py3-none-any.whl

---
+++ packages/pbx-code-owners/pbx_code_owners-1.2.0-py3-none-any.whl/code_owners/config.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+
+DEFAULT_BRANCH = os.environ.get("DEFAULT_BRANCH", "master")
+CODEOWNERS_FILE = Path(os.getcwd()) / "codeowners.yml"
+
+
+CI_SERVER_URL = os.environ["CI_SERVER_URL"]
+CI_PROJECT_ID = os.environ["CI_PROJECT_ID"]
+CI_MERGE_REQUEST_IID = os.environ["CI_MERGE_REQUEST_IID"]
+GITLAB_TOKEN = os.environ["CODE_OWNERS_GITLAB_TOKEN"]
+
+CI_COMMIT_BRANCH = os.environ["CI_MERGE_REQUEST_SOURCE_BRANCH_NAME"]
+
+THREADS_PER_PAGE = 100
+
+GITLAB_DISCUSSIONS_URL = (
+    f"{CI_SERVER_URL}/api/v4/projects/{CI_PROJECT_ID}"
+    f"/merge_requests/{CI_MERGE_REQUEST_IID}/discussions?per_page={THREADS_PER_PAGE}"
+)
