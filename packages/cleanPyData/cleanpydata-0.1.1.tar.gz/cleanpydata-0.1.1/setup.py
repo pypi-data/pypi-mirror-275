fca6ab935032aa0cdbcaa807ce58659efe3745fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 15:04:11 +0000

    Add package cleanPyData/0.1.1/cleanpydata-0.1.1.tar.gz

---
+++ packages/cleanPyData/cleanpydata-0.1.1.tar.gz/cleanpydata-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='cleanPyData',
+    version='0.1.1',
+    description='A package for data cleaning and preprocessing',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Use 'text/x-rst' if you're using reStructuredText
+    author='Kaddu Livingstone',
+    author_email='kaddulivingston@gmail.com',
+    url='https://github.com/Livingston-k/cleanPyData',
+    packages=find_packages(),
+    install_requires=[
+        'pandas>=1.0',
+        'scikit-learn>=0.24'
+    ],
+    tests_require=[
+        'pytest>=6.0'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
