5e7ba3848e42337043c1958ae72f75bf64757d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 14:04:02 +0000

    Add package articraft/2.5.1/articraft-2.5.1.tar.gz

---
+++ packages/articraft/articraft-2.5.1.tar.gz/articraft-2.5.1/setup.py
@@ -0,0 +1,11 @@
+from articraft import NAME, VERSION, DESCRIPTION
+from blueness.pypi import setup
+
+setup(
+    filename=__file__,
+    repo_name="aiart",
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=[NAME],
+)
