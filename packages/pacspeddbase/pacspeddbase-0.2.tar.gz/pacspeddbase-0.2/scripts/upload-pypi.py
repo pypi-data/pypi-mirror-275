bb89ef4412c70f6c3ad57263db394ee5fa6fd1dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 15:04:04 +0000

    Add package pacspeddbase/0.2/pacspeddbase-0.2.tar.gz

---
+++ packages/pacspeddbase/pacspeddbase-0.2.tar.gz/pacspeddbase-0.2/scripts/upload-pypi.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+import sys
+
+if __name__ == '__main__':
+    option = sys.argv[1]
+    if option in ['view', 'show', 'list']:
+        subprocess.run(['gh', 'workflow', 'view'])
+    elif option in ['setup', 'pypi']:
+        subprocess.run(['python', '-m', 'pip', 'install', 'twine'])
+        token = sys.argv[2]
+        HOME = os.environ.get('HOME')
+        PYPIRC = os.path.join(HOME, '.pypirc')
+        with open(PYPIRC, 'w') as f:
+            text1 = "[pypi]\n"
+            text2 = "username = __token__\n"
+            text3 = f"password = {token}\n"
+            f.write(text1)
+            f.write(text2)
+            f.write(text3)
+    elif option in ['upload', 'publish']:
+        pid = sys.argv[2]
+        cdir = os.getcwd()
+        build = os.path.join(cdir, 'build')
+        os.makedirs(build, exist_ok=True)
+        os.chdir(build)
+        subprocess.run(['gh', 'run', 'download', pid])
+        folder = os.listdir(build)
+        for fold in folder:
+            files = os.listdir(fold)
+            for file in files:
+                filepath = os.path.join(fold, file)
+                subprocess.run(['python', '-m', 'twine', 'upload', filepath])
+        os.chdir(cdir)
+        os.remove(build)
