6697f4ea445394a97494678cf09ee6af4c4bdf96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 04:04:37 +0000

    Add package scaleway/2.3.1/scaleway-2.3.1.tar.gz

---
+++ packages/scaleway/scaleway-2.3.1.tar.gz/scaleway-2.3.1/scaleway/tem/v1alpha1/__init__.py
@@ -0,0 +1,83 @@
+# This file was automatically generated. DO NOT EDIT.
+# If you have any remark or suggestion do not hesitate to open an issue.
+from .types import DomainLastStatusRecordStatus
+from .types import DomainReputationStatus
+from .types import DomainStatus
+from .content import DOMAIN_TRANSIENT_STATUSES
+from .types import EmailFlag
+from .types import EmailRcptType
+from .types import EmailStatus
+from .content import EMAIL_TRANSIENT_STATUSES
+from .types import ListEmailsRequestOrderBy
+from .types import DomainRecordsDMARC
+from .types import EmailTry
+from .types import DomainRecords
+from .types import DomainReputation
+from .types import DomainStatistics
+from .types import CreateEmailRequestAddress
+from .types import CreateEmailRequestAttachment
+from .types import CreateEmailRequestHeader
+from .types import Email
+from .types import DomainLastStatusDkimRecord
+from .types import DomainLastStatusDmarcRecord
+from .types import DomainLastStatusSpfRecord
+from .types import Domain
+from .types import CancelEmailRequest
+from .types import CheckDomainRequest
+from .types import CreateDomainRequest
+from .types import CreateEmailRequest
+from .types import CreateEmailResponse
+from .types import DomainLastStatus
+from .types import GetDomainLastStatusRequest
+from .types import GetDomainRequest
+from .types import GetEmailRequest
+from .types import GetStatisticsRequest
+from .types import ListDomainsRequest
+from .types import ListDomainsResponse
+from .types import ListEmailsRequest
+from .types import ListEmailsResponse
+from .types import RevokeDomainRequest
+from .types import Statistics
+from .api import TemV1Alpha1API
+
+__all__ = [
+    "DomainLastStatusRecordStatus",
+    "DomainReputationStatus",
+    "DomainStatus",
+    "DOMAIN_TRANSIENT_STATUSES",
+    "EmailFlag",
+    "EmailRcptType",
+    "EmailStatus",
+    "EMAIL_TRANSIENT_STATUSES",
+    "ListEmailsRequestOrderBy",
+    "DomainRecordsDMARC",
+    "EmailTry",
+    "DomainRecords",
+    "DomainReputation",
+    "DomainStatistics",
+    "CreateEmailRequestAddress",
+    "CreateEmailRequestAttachment",
+    "CreateEmailRequestHeader",
+    "Email",
+    "DomainLastStatusDkimRecord",
+    "DomainLastStatusDmarcRecord",
+    "DomainLastStatusSpfRecord",
+    "Domain",
+    "CancelEmailRequest",
+    "CheckDomainRequest",
+    "CreateDomainRequest",
+    "CreateEmailRequest",
+    "CreateEmailResponse",
+    "DomainLastStatus",
+    "GetDomainLastStatusRequest",
+    "GetDomainRequest",
+    "GetEmailRequest",
+    "GetStatisticsRequest",
+    "ListDomainsRequest",
+    "ListDomainsResponse",
+    "ListEmailsRequest",
+    "ListEmailsResponse",
+    "RevokeDomainRequest",
+    "Statistics",
+    "TemV1Alpha1API",
+]
