b7e3ddacabaa9e60923e7b35472f248dfc69d85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 05:04:39 +0000

    Add package reddemcee/0.6.3/reddemcee-0.6.3.tar.gz

---
+++ packages/reddemcee/reddemcee-0.6.3.tar.gz/reddemcee-0.6.3/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="reddemcee",
+    version="0.6.3",
+    author="ReddTea",
+    author_email="redd@tea.com",
+    description="An Adaptative Parallel Tempering wrapper for emcee 3 for personal use",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=['numpy', 'matplotlib>=3.5.1', 'emcee>=3', 'tqdm', 'pandas'],
+    python_requires=">=3.6",
+)
---
+++ packages/reddemcee/reddemcee-0.6.3.tar.gz/reddemcee-0.6.3/tests/test_reddemcee.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    pass
+except ImportError:
+    raise ImportError('You don t have the package ... installed. Try pip install ....')
+
+def func(foo, bar):
+	pass
