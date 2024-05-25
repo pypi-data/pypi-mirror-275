d488823c41642a4b5c4df4e9c7a3e2a7e180b06c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 05:04:32 +0000

    Add package UFO041/1.0.0/ufo041-1.0.0.tar.gz

---
+++ packages/UFO041/ufo041-1.0.0.tar.gz/ufo041-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='UFO041',
+version='1.0.0',
+author='Fanboy041',
+author_email='hani.ph.003@gmail.com',
+description='An animated UFO in console',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.12.3',
+)
