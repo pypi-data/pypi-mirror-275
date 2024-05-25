56c47c9bbe03e50c084483e07df296d0570b1ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 04:04:57 +0000

    Add package discontinuum/0.2/discontinuum-0.2.tar.gz

---
+++ packages/discontinuum/discontinuum-0.2.tar.gz/discontinuum-0.2/src/discontinuum/engines/pymc.py
@@ -0,0 +1,162 @@
+"""Data transformations to improve optimization"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import numpy as np
+import pymc as pm
+from xarray import DataArray
+
+from discontinuum.engines.base import BaseModel, is_fitted
+
+if TYPE_CHECKING:
+    from typing import Dict, Optional
+
+
+class LatentGP(BaseModel):
+    def __init__(
+        self,
+        model_config: Optional[Dict] = None,
+    ):
+        """ """
+        super().__init__(model_config=model_config)
+
+    def fit(self, covariates, target=None):
+        pass
+
+
+class MarginalGP(BaseModel):
+    def __init__(
+        self,
+        model_config: Optional[Dict] = None,
+    ):
+        """ """
+        super().__init__(model_config=model_config)
+
+    def fit(self, covariates, target=None):
+        """Fit the model to data.
+
+        Parameters
+        ----------
+        X : array-like
+            Feature matrix.
+        y : array-like
+            Target values.
+        """
+        self.is_fitted = True
+        # preprocessing: setup data manager
+        self.dm.fit(target=target, covariates=covariates)
+        self.X = self.dm.X
+        self.y = self.dm.y
+
+        self.model = self.build_model(self.X, self.y)
+
+        self.mp = pm.find_MAP(method="BFGS", model=self.model)
+
+    @is_fitted
+    def predict(self, covariates, diag=True, pred_noise=False) -> DataArray:
+        """Uses the fitted model to make predictions on new data."""
+        Xnew = self.dm.Xnew(covariates)
+
+        mu, cov = self.gp.predict(
+            Xnew, point=self.mp, diag=diag, pred_noise=pred_noise, model=self.model
+        )
+
+        target = self.dm.y_t(mu)
+        # TODO the reshape should be done in the pipeline
+        se = self.dm.error_pipeline.inverse_transform(cov.reshape(-1, 1))
+
+        return target, se
+
+    @is_fitted
+    def predict_grid(self, covariate: str, index="time", t_step=12):
+        """Predict on a grid of points.
+
+        Parameters
+        ----------
+        covariate_dim : int, optional
+            Dimension to predict on. The default is 1.
+        t_step : int, optional
+            Time steps per year. The default is 12.
+        """
+        time_dim = self.dm.get_dim(index)
+        covariate_dim = self.dm.get_dim(covariate)
+
+        x_max = self.dm.X.max(axis=0)
+        x_min = self.dm.X.min(axis=0)
+        x_range = x_max - x_min
+
+        n_cov = 18
+        n_time = np.round(x_range[time_dim] * t_step).astype(int)
+
+        x_time = np.linspace(x_min[time_dim], x_max[time_dim], n_time)
+        x_cov = np.linspace(x_min[covariate_dim], x_max[covariate_dim], n_cov)
+
+        # TODO check dependency
+        # tested with this on WSL with pymc v5.14.0
+        # X_grid = pm.math.cartesian(x_cov[:, None], x_time[None, :])
+        X_grid = pm.math.cartesian(x_time, x_cov)
+
+        mu, _ = self.gp.predict(X_grid, point=self.mp, diag=True, pred_noise=True, model=self.model)
+
+        target = self.dm.y_t(mu)
+        # TODO return a Dataset with the correct shape
+        target = target.data.reshape(n_time, n_cov)  # TODO might need to flip these
+
+        index = x_time
+        covariate = self.dm.covariate_pipelines[covariate].inverse_transform(x_cov.reshape(-1, 1))
+
+        return target, index, covariate
+
+    @is_fitted
+    def sample(self, covariates, n=1000, diag=False, pred_noise=False, method="cholesky", tol=1e-6) -> DataArray:
+        """Sample from the posterior distribution of the model.
+
+        Parameters
+        ----------
+        covariates : Dataset
+            Covariates for prediction.
+        n : int, optional
+            Number of samples to draw.
+        method{ ‘svd’, ‘eigh’, ‘cholesky’}, optional
+            Method to use for covariance matrix decomposition. The default is ‘cholesky’.
+        tol : float, optional
+            Tolerance when checking the singular values in covariance matrix. The default is 1e-6.
+        """
+        Xnew = self.dm.Xnew(covariates)
+        mu, cov = self.gp.predict(
+            Xnew, point=self.mp, diag=diag, pred_noise=pred_noise, model=self.model
+        )
+
+        rng = np.random.default_rng()
+        sim = rng.multivariate_normal(mu, cov, size=n, method=method, tol=tol)
+
+        # TODO modify transform to handle samples/draws HACK
+        temp = self.dm.y_t(sim)
+        data = temp.data.reshape(n, -1)
+        attrs = temp.attrs
+        da = DataArray(
+            data,
+            coords={"time": covariates.time, "draw": np.arange(n)},
+            dims=["draw", "time"],
+            attrs=attrs,
+        )
+
+        return da
+
+    def build_model(self, X, y, **kwargs):
+        """
+        Creates an instance of pm.Model based on provided data and model_config, and
+        attaches it to self.
+
+        The subclass method must instantiate self.model and self.gp.
+
+        Raises
+        ------
+        NotImplementedError
+        """
+        self.model = None
+        self.gp = None
+
+        raise NotImplementedError("This method must be implemented in a subclass")
---
+++ packages/discontinuum/discontinuum-0.2.tar.gz/discontinuum-0.2/src/loadest_gp/model.py
@@ -0,0 +1,94 @@
+import numpy as np
+import pymc as pm
+from discontinuum.data_manager import DataManager
+from discontinuum.engines.pymc import MarginalGP
+from discontinuum.pipeline import LogStandardPipeline, TimePipeline
+
+from loadest_gp.plot import LoadestPlotMixin
+
+
+class LoadestGP(MarginalGP, LoadestPlotMixin):
+    """
+    Gaussian Process implementation of the LOAD ESTimation (LOADEST) model
+
+    This model currrently uses the marginal likelihood implementation, which is
+    fast but does not account for censored data. Censored data require a slower
+    latent variable implementation.
+    """
+    def __init__(self):
+        """ """
+        super().__init__()
+        covariate_pipelines = {
+            "time": TimePipeline,
+            "flow": LogStandardPipeline
+        }
+        target_pipeline = LogStandardPipeline
+
+        self.dm = DataManager(
+            target_pipeline=target_pipeline,
+            covariate_pipelines=covariate_pipelines
+        )
+
+    def build_model(self, X, y) -> pm.Model:
+        """ """
+        n_d = X.shape[1]  # number of dimensions
+        dims = np.arange(n_d)
+        time_dim = [dims[0]]
+        cov_dims = dims[1:]
+
+        with pm.Model() as model:
+            # seasonal trend
+            eta_per = pm.HalfNormal("eta_per", sigma=1, initval=1)  # was 2
+            ls_pdecay = pm.LogNormal("ls_pdecay", mu=2, sigma=1)
+            # https://peterroelants.github.io/posts/gaussian-process-kernels/
+            period = pm.Normal("period", mu=1, sigma=0.05)
+            ls_psmooth = pm.LogNormal("ls_psmooth", mu=1, sigma=1)  # 14 sec at 0,0.5
+
+            cov_seasonal = (
+                eta_per**2
+                * pm.gp.cov.Periodic(n_d, period, ls_psmooth, active_dims=time_dim)
+                * pm.gp.cov.Matern52(n_d, ls_pdecay, active_dims=time_dim)
+            )
+            gp_seasonal = pm.gp.Marginal(cov_func=cov_seasonal)
+
+            # longterm trend
+            # eta_trend =  pm.HalfNormal("eta_trend", sigma=1) # was 2
+            eta_trend = pm.Exponential(
+                "eta_trend", scale=1.5
+            )  # Exponential might to limit outliers dictating a trend
+            ls_trend = pm.LogNormal("ls_trend", mu=2, sigma=1)
+            cov_trend = eta_trend**2 * pm.gp.cov.ExpQuad(n_d, ls_trend, active_dims=time_dim)
+            gp_trend = pm.gp.Marginal(cov_func=cov_trend)
+
+            # covariate trend
+            # could include time with a different prior on ls
+            eta_covariates = pm.HalfNormal("eta_covariates", sigma=2)
+            ls_covariates = pm.LogNormal(
+                "ls_covariates",
+                mu=-1.1,
+                sigma=1,
+                initval=[0.5],
+                shape=n_d-1,  # exclude time
+                )
+            cov_covariates = eta_covariates**2 \
+                * pm.gp.cov.ExpQuad(n_d, ls=ls_covariates, active_dims=cov_dims)
+            gp_covariates = pm.gp.Marginal(cov_func=cov_covariates)
+
+            # residual trend
+            eta_res = pm.Exponential("eta_res", scale=0.2)
+            ls_res = pm.LogNormal("ls_res", mu=-1.1, sigma=1, shape=n_d)
+            cov_res = eta_res**2 * pm.gp.cov.ExpQuad(n_d, ls_res, active_dims=dims)
+            gp_res = pm.gp.Marginal(cov_func=cov_res)
+
+            gp = gp_trend + gp_seasonal + gp_covariates + gp_res
+
+            # noise_model
+            # set to a small value to ensure cov is positive definite
+            # or use a HalfNormal prior
+            cov_noise = pm.gp.cov.WhiteNoise(sigma=0.1)
+
+            y_ = gp.marginal_likelihood("y", X=X, y=y, sigma=cov_noise)  # noqa: F841
+
+            self.gp = gp
+
+        return model
