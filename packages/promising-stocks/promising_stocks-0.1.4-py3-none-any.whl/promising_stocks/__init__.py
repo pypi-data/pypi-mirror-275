8bda13b01f98612bf14549f8c1647d2cba68eb92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 03:03:55 +0000

    Add package promising-stocks/0.1.4/promising_stocks-0.1.4-py3-none-any.whl

---
+++ packages/promising-stocks/promising_stocks-0.1.4-py3-none-any.whl/promising_stocks/__init__.py
@@ -0,0 +1,4 @@
+from .main import promising_stocks
+
+__all__ = ["promising_stocks"]
+__version__ = '0.1.4'
---
+++ packages/promising-stocks/promising_stocks-0.1.4-py3-none-any.whl/promising_stocks/main.py
@@ -0,0 +1,13 @@
+# ライブラリのインポート
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import pandas_datareader.stooq as pdr
+from pandas_datareader import data
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+class promising_stocks():
+
+  def stooq(self, s_code):
+    return data.DataReader(s_code + '.JP', 'stooq')
