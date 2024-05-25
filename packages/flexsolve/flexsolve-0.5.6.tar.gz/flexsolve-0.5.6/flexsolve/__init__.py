d24ed65429049342259e1a8d17f1fa3d14ad93eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 04:04:47 +0000

    Add package flexsolve/0.5.6/flexsolve-0.5.6.tar.gz

---
+++ packages/flexsolve/flexsolve-0.5.6.tar.gz/flexsolve-0.5.6/flexsolve/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 22:47:51 2019
+
+@author: yoelr
+"""
+from . import open_solvers
+from . import bounded_solvers
+from . import iterative_solvers
+from . import problem_list
+from . import profiler
+from . import problem
+from . import utils
+
+__all__ = (*open_solvers.__all__,
+           *bounded_solvers.__all__,
+           *iterative_solvers.__all__,
+           *problem_list.__all__,
+           *problem.__all__,
+           *profiler.__all__,
+           'utils',
+)
+
+from .open_solvers import *
+from .bounded_solvers import *
+from .iterative_solvers import *
+from .problem_list import *
+from .problem import *
+from .profiler import *
+
+__version__ = '0.5.6'
