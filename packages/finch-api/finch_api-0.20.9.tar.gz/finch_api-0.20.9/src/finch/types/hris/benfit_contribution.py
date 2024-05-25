1e48867f8ad98203b95a45fea82b5d4f60c721f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 05:04:27 +0000

    Add package finch-api/0.20.9/finch_api-0.20.9.tar.gz

---
+++ packages/finch-api/finch_api-0.20.9.tar.gz/finch_api-0.20.9/src/finch/types/hris/benfit_contribution.py
@@ -0,0 +1,8 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+
+
+from .benefit_contribution import BenefitContribution
+
+BenfitContribution = BenefitContribution
+"""use `BenefitContribution` instead"""
