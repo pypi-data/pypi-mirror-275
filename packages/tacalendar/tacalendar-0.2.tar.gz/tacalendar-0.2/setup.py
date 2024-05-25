31ba956484cfde29f22c4b73bae90926221fa0ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 04:04:37 +0000

    Add package tacalendar/0.2/tacalendar-0.2.tar.gz

---
+++ packages/tacalendar/tacalendar-0.2.tar.gz/tacalendar-0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='tacalendar',
+    version='0.2',
+    description='Handles date time manipulation with simplicity',
+    author='Tousif Anaam',
+    author_email='tousifanaam@gmail.com',
+    license='GNU',
+    py_modules=['tacalendar'],
+    keywords=['ttime', 'tcalendar', 'tcalendar_time', 'tc', 'tt'],
+    url='https://github.com/tousifanaam/tcalendar',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
