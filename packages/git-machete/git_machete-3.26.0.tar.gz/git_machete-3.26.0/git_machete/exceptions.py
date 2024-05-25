169e9be6bb0a0d5cd7d0296ea61a54e55b028ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 03:04:02 +0000

    Add package git-machete/3.26.0/git_machete-3.26.0.tar.gz

---
+++ packages/git-machete/git_machete-3.26.0.tar.gz/git_machete-3.26.0/git_machete/exceptions.py
@@ -0,0 +1,37 @@
+from enum import IntEnum
+
+from git_machete import utils
+
+
+class InteractionStopped(Exception):
+    def __init__(self) -> None:
+        pass
+
+
+class UnderlyingGitException(Exception):
+    def __init__(self, msg: str, apply_fmt: bool = True) -> None:
+        self.msg: str = utils.fmt(msg) if apply_fmt else msg
+
+    def __str__(self) -> str:
+        return str(self.msg)
+
+
+class MacheteException(Exception):
+    def __init__(self, msg: str, apply_fmt: bool = True) -> None:
+        self.msg: str = utils.fmt(msg) if apply_fmt else msg
+
+    def __str__(self) -> str:
+        return str(self.msg)
+
+
+class UnexpectedMacheteException(MacheteException):
+    def __init__(self, msg: str, apply_fmt: bool = False) -> None:
+        super().__init__(f"{msg}\n\nConsider posting an issue at https://github.com/VirtusLab/git-machete/issues/new", apply_fmt=apply_fmt)
+
+
+class ExitCode(IntEnum):
+    SUCCESS = 0
+    MACHETE_EXCEPTION = 1
+    ARGUMENT_ERROR = 2
+    KEYBOARD_INTERRUPT = 3
+    END_OF_FILE_SIGNAL = 4
