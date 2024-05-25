750a88fb14e56043b6c77752be082eb6e7918d33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 14:04:11 +0000

    Add package aqora-cli/0.1.15/aqora_cli-0.1.15.tar.gz

---
+++ packages/aqora-cli/aqora_cli-0.1.15.tar.gz/aqora_cli-0.1.15/src/lib.rs
@@ -0,0 +1,27 @@
+mod colors;
+mod commands;
+mod compress;
+mod credentials;
+mod dirs;
+mod download;
+mod error;
+mod evaluate;
+mod graphql_client;
+mod id;
+mod ipynb;
+mod manifest;
+#[cfg(feature = "extension-module")]
+mod module;
+mod print;
+mod process;
+mod progress_bar;
+mod python;
+mod readme;
+mod revert_file;
+pub mod sentry;
+mod shutdown;
+
+pub use commands::{Cli, CliExit};
+
+#[cfg(feature = "extension-module")]
+pub use module::*;
