eacf89415627bdadffe07f003b70bf21c872cbfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 15:04:11 +0000

    Add package numpydantic/1.1.0/numpydantic-1.1.0-py3-none-any.whl

---
+++ packages/numpydantic/numpydantic-1.1.0-py3-none-any.whl/numpydantic/__init__.py
@@ -0,0 +1,16 @@
+# ruff: noqa: E402
+# ruff: noqa: F401
+# ruff: noqa: I001
+# ruff: noqa: D104
+from numpydantic.monkeypatch import apply_patches
+
+apply_patches()
+
+from numpydantic.ndarray import NDArray
+from numpydantic.meta import update_ndarray_stub
+
+from nptyping import Shape
+
+update_ndarray_stub()
+
+__all__ = ["NDArray", "Shape"]
