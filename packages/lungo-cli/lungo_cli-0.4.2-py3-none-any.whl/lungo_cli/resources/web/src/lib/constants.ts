e6c9dc4e5d5c92219d655393aad7b5efd32f828d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 05:04:05 +0000

    Add package lungo-cli/0.4.2/lungo_cli-0.4.2-py3-none-any.whl

---
+++ packages/lungo-cli/lungo_cli-0.4.2-py3-none-any.whl/lungo_cli/resources/web/src/lib/constants.ts
@@ -0,0 +1,6 @@
+import { env } from "$env/dynamic/public"
+
+export const HOSTNAME = env.PUBLIC_HOSTNAME
+export const HTTPS_PORT = env.PUBLIC_HTTPS_PORT
+export const SITE_SUBTITLE = env.PUBLIC_SITE_SUBTITLE
+export const SITE_TITLE = env.PUBLIC_SITE_TITLE
