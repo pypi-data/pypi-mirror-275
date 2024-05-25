5503097556c0f8d2b25af64b18ce4fbe14e135b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 14:04:16 +0000

    Add package autowsgr/0.2.2.34/autowsgr-0.2.2.34.tar.gz

---
+++ packages/autowsgr/autowsgr-0.2.2.34.tar.gz/autowsgr-0.2.2.34/autowsgr/constants/data_roots.py
@@ -0,0 +1,10 @@
+from os.path import dirname, join
+
+DATA_ROOT = join(dirname(dirname(__file__)), "data")
+IMG_ROOT = join(DATA_ROOT, "images")
+MAP_ROOT = join(DATA_ROOT, "map")
+SETTING_ROOT = join(DATA_ROOT, "settings")
+
+BIN_ROOT = join(dirname(DATA_ROOT), "bin")
+TUNNEL_ROOT = join(BIN_ROOT, "image_recognize")
+ADB_ROOT = join(BIN_ROOT, "adb")
