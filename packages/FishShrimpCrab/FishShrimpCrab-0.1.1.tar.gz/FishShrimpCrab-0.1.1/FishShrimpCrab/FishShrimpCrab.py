cbd7547a31a5b9986814f16ca90677129cce4d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 15:04:18 +0000

    Add package FishShrimpCrab/0.1.1/FishShrimpCrab-0.1.1.tar.gz

---
+++ packages/FishShrimpCrab/FishShrimpCrab-0.1.1.tar.gz/FishShrimpCrab-0.1.1/FishShrimpCrab/FishShrimpCrab.py
@@ -0,0 +1,24 @@
+
+def play():
+	print ("fish,shrimp,crap,cock,money,8")
+	
+	import random 
+	
+	cmove = ("fish","ha","crap","cock","money","8")
+	player1 = input("press 1 to start: ")
+	
+	while player1 == "1":
+		result1 = random.choice(cmove)
+		result2 = random.choice(cmove)
+		result3 = random.choice(cmove)
+	
+		print(result1)
+		print(result2)
+		print(result3)
+	
+		new_game = input("Press y to start a new match(y/n)")
+		if new_game == "y":
+			p1ayer1 = 1 
+		else:
+			player1 = 0
+			print("Thanks For Playing")
