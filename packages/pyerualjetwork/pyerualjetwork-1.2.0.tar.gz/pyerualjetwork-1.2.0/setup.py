73b738c1ccfec9983ed83b0d2ae83034d22cb0a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 04:04:26 +0000

    Add package pyerualjetwork/1.2.0/pyerualjetwork-1.2.0.tar.gz

---
+++ packages/pyerualjetwork/pyerualjetwork-1.2.0.tar.gz/pyerualjetwork-1.2.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "pyerualjetwork",
+      version = "1.2.0",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "Advanced python deep learning library.(Documentation in desc. Examples in GİTHUB: HCB06)",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'pruning learning artficial neural networks']
+      
+      )
