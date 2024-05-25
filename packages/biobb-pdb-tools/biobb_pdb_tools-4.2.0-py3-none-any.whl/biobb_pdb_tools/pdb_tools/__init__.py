4378647bf4a15306e24d08a98a9b4f1b85381396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 03:05:02 +0000

    Add package biobb-pdb-tools/4.2.0/biobb_pdb_tools-4.2.0-py3-none-any.whl

---
+++ packages/biobb-pdb-tools/biobb_pdb_tools-4.2.0-py3-none-any.whl/biobb_pdb_tools/pdb_tools/__init__.py
@@ -0,0 +1,2 @@
+name = "pdb_tools"
+__all__ = ["biobb_pdb_fetch", "biobb_pdb_reres", "biobb_pdb_chain", "biobb_pdb_chainxseg", "biobb_pdb_delhetatm", "biobb_pdb_merge", "biobb_pdb_mkensemble", "biobb_pdb_seg", "biobb_pdb_splitmodel", "biobb_pdb_splitseg", "biobb_pdb_tidy", "biobb_pdb_uniqname"]
