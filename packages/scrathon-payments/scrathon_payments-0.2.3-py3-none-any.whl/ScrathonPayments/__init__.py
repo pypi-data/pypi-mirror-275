97abad9d3b2efb57feab6108817f8c04dd4bec4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 04:04:44 +0000

    Add package scrathon-payments/0.2.3/scrathon_payments-0.2.3-py3-none-any.whl

---
+++ packages/scrathon-payments/scrathon_payments-0.2.3-py3-none-any.whl/ScrathonPayments/__init__.py
@@ -0,0 +1,53 @@
+import scratchattach as scratch3
+import requests
+
+convert = ["false", "true"]
+
+class Scrathon:
+
+    def __init__(self, username, client: scratch3.CloudRequests):
+        self.client = client
+        self.username = "None"
+        try:
+            scratch3.get_user(username)
+            userexists = True
+        except scratch3.exceptions.UserNotFound:
+            userexists = False
+        if userexists:
+            if requests.post("http://45.140.188.129:6623/userexist/", json={"username": username}).json() == {"userexist": "True"}:
+                self.username = str(username)
+            else:
+                raise ValueError("User {} did not use Scrathon before!".format(username))
+        else:
+            raise ValueError("User {} does not exist on scratch!".format(username))
+
+        if self.username != "None":
+            print("Started Scrathon as {}! All funds earned will go to mentioned user".format(self.username))
+
+        @client.request
+        def purchase(price):
+            request = requests.post("http://45.140.188.129:6623/transaction", json={
+                "price": price,
+                "buyer": client.get_requester(),
+                "seller": self.username
+            })
+            try:
+                success = request.json().get("success")
+            except Exception as e:
+                print(f"Didn't recognise json: {request.text}")
+                raise e from None
+            return convert[success]
+            
+        @client.request
+        def purchasecheck(price):
+            request = requests.post("http://45.140.188.129:6623/checkpurchase", json={
+                "price": price,
+                "buyer": client.get_requester(),
+                "seller": self.username
+            })
+            try:
+                success = request.json().get("success")
+            except Exception as e:
+                print(f"Didn't recognise json: {request.text}")
+                raise e from None
+            return convert[success]
