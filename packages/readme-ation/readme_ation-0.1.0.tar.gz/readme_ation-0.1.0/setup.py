f24d1a088692fbe8eaac856d92b930b99d1d1833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 15:04:14 +0000

    Add package readme-ation/0.1.0/readme_ation-0.1.0.tar.gz

---
+++ packages/readme-ation/readme_ation-0.1.0.tar.gz/readme_ation-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="readme-ation",
+    version="0.1.0",
+        py_modules=['my_script'],
+    install_requires=[
+        # List dependencies here, e.g. 'requests>=2.25.1'
+    ],
+    entry_points={
+        'console_scripts': [
+            'my-script = my_script:main',  # Assuming you have a main function in my_script.py
+        ],
+    },
+    author='Charles Feinn',
+    description='A README.md generation tool',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/your-repo',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
