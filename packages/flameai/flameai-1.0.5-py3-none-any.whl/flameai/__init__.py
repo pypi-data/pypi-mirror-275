24a2ea2287c1c0e7c8888934b92553969655f79d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 05:04:41 +0000

    Add package flameai/1.0.5/flameai-1.0.5-py3-none-any.whl

---
+++ packages/flameai/flameai-1.0.5-py3-none-any.whl/flameai/__init__.py
@@ -0,0 +1,16 @@
+from flameai import metrics
+from flameai import mining
+from flameai import plot
+from flameai import preprocessing
+from flameai import train
+from flameai import util
+
+
+__all__ = [
+    'metrics',
+    'mining',
+    'plot',
+    'preprocessing',
+    'train',
+    'util',
+]
