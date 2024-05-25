e3f4fa32341bdfe6fbed70a60343d77d56fcbc0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 04:04:36 +0000

    Add package enzope/0.0.22/enzope-0.0.22-py3-none-any.whl

---
+++ packages/enzope/enzope-0.0.22-py3-none-any.whl/enzope/kernels/locks.py
@@ -0,0 +1,20 @@
+from numba import cuda
+
+
+@cuda.jit(device=True)
+def double_lock(mutex, i, j):
+    first, second = (i, j) if i < j else (j, i)
+
+    while cuda.atomic.cas(mutex, first, 0, 1) != 0:
+        pass
+    while cuda.atomic.cas(mutex, second, 0, 1) != 0:
+        pass
+
+    cuda.threadfence()
+
+
+@cuda.jit(device=True)
+def double_unlock(mutex, i, j):
+    cuda.threadfence()
+    cuda.atomic.exch(mutex, j, 0)
+    cuda.atomic.exch(mutex, i, 0)
