9276cb23b38801a5d3175efc3a4c2c9b65c0b619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 14:04:04 +0000

    Add package lyriks/0.2.0/lyriks-0.2.0-py3-none-any.whl

---
+++ packages/lyriks/lyriks-0.2.0-py3-none-any.whl/lyriks/__init__.py
@@ -0,0 +1,24 @@
+import glob
+from os import path
+
+from .cli import parse_arguments
+from .lyrics_fetcher import LyricsFetcher
+
+
+def main():
+    args = parse_arguments()
+
+    collection_path = args.path
+
+    fetcher = LyricsFetcher(args.dry_run, args.force)
+
+    files = glob.iglob(path.join(collection_path, '**/*.*'), recursive=True)
+    for file in files:
+        if path.isdir(file):
+            continue
+        if not file.lower().endswith('.flac') and not file.lower().endswith('.mp3'):
+            continue
+        fetcher.fetch_lyrics(file)
+
+    if args.report:
+        fetcher.write_report(filename=args.report)
---
+++ packages/lyriks/lyriks-0.2.0-py3-none-any.whl/lyriks/cli.py
@@ -0,0 +1,24 @@
+import argparse
+
+PROGNAME = 'lyriks'
+VERSION = '0.2.0'
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(prog=PROGNAME, description='A command line tool that fetches lyrics from Genie.')
+    path_help = f"""
+                The path to the music collection.
+                {PROGNAME} will recursively search for music files in this directory.
+                """
+    parser.add_argument('path', type=str, help=path_help)
+    parser.add_argument('-n', '--dry-run', action='store_true', help='fetch lyrics without writing them to files')
+    parser.add_argument('-f', '--force',
+                        action='store_true',
+                        help='force fetching lyrics for all tracks, even if they already have them'
+                             ' - THIS WILL OVERWRITE EXISTING LYRICS FILES!')
+    parser.add_argument('-R', '--report',
+                        nargs='?', const='report.html',
+                        help='write a HTML report of releases missing album URLs to a file (default: report.html)')
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + VERSION)
+
+    return parser.parse_args()
