0c9b56cfeb16f2db22ee1d546fd3fa1635d8671c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 04:04:56 +0000

    Add package logyca/0.1.11rc1/logyca-0.1.11rc1-py3-none-any.whl

---
+++ packages/logyca/logyca-0.1.11rc1-py3-none-any.whl/logyca/utils/helpers/stringshelpers.py
@@ -0,0 +1,49 @@
+from urllib import parse
+
+def buildUrl(url1:str,url2:str)->str:
+    '''Description    
+    Build a from url correctly
+    :param url1: http(s)://initial/string
+    :param url2: following url path
+    :return str: url
+    '''
+    return parse.urljoin(url1, url2)
+
+def convert_string_to_boolean(truth_value: str) -> bool:
+    # TODO - Deprecated 2024/05/24
+    print("Soon this function will be retired: convert_string_to_boolean(), please change it to the following: parse_bool()")
+    lowercased_value = str(truth_value).lower()
+
+    if lowercased_value in ("yes", "true", "1", "on"):
+        return True
+    elif lowercased_value in ("no", "false", "0", "off"):
+        return False
+    else:
+        raise ValueError(f"Invalid boolean string value: {truth_value}"
+        )
+
+def parse_bool(bool_value: str|int|bool|None) -> bool | None:
+    true_values = {"true", "1"}
+    false_values = {"false", "0"}
+
+    if isinstance(bool_value,bool):
+        return bool_value
+    
+    if isinstance(bool_value,int):
+        if bool_value == 1:
+            bool_value = "1"
+        elif bool_value== 0:
+            bool_value = "0"
+        else:
+            return None
+
+    if isinstance(bool_value,str):
+        bool_value = str(bool_value).lower()
+        if bool_value in true_values:
+            return True
+        elif bool_value in false_values:
+            return False
+        else:
+            return None
+
+    return None
