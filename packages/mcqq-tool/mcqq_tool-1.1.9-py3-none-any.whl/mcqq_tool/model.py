fa1e192570484a8c9725c54562ff7df47b10ee8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 16:04:05 +0000

    Add package mcqq-tool/1.1.9/mcqq_tool-1.1.9-py3-none-any.whl

---
+++ packages/mcqq-tool/mcqq_tool-1.1.9-py3-none-any.whl/mcqq_tool/model.py
@@ -0,0 +1,7 @@
+from typing import List, Optional, Dict
+
+QQ_GROUP_ID_LIST: Optional[Dict[str, List[str]]] = {}
+QQ_GUILD_ID_LIST: Optional[Dict[str, List[str]]] = {}
+
+ONEBOT_GROUP_ID_LIST: Optional[Dict[str, List[str]]] = {}
+ONEBOT_GUILD_ID_LIST: Optional[Dict[str, List[str]]] = {}
