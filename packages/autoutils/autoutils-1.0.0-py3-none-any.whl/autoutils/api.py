e1875cb3d437f13203d20e1c9d77e51a56acd498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 06:03:54 +0000

    Add package autoutils/1.0.0/autoutils-1.0.0-py3-none-any.whl

---
+++ packages/autoutils/autoutils-1.0.0-py3-none-any.whl/autoutils/api.py
@@ -0,0 +1,102 @@
+import dataclasses
+import json
+import logging
+import time
+from typing import Optional, Union, List, Dict
+
+from requests import Session, Response
+
+logger = logging.getLogger(__name__)
+
+
+@dataclasses.dataclass
+class ServerResponse:
+    """
+        Server response
+    """
+    response: "Response" = None
+    status_code: int = 503
+
+    def __post_init__(self):
+        if self.response is not None:
+            self.status_code = self.response.status_code
+
+    @property
+    def json_response(self) -> Optional[Union[List, Dict]]:
+        if self.response is None:
+            return None
+        try:
+            return self.response.json()
+        except json.JSONDecodeError:
+            pass
+
+
+@dataclasses.dataclass
+class BaseApi:
+    name: str
+    base_url: str
+    connection_timeout: int = 60
+    validate_cert: bool = True
+    user_agent: str = "autoutils"
+    retry_count: int = 1
+    retry_delay: int = 3
+    token_name: str = "Bearer"
+    supported_http_methods: List[str] = None
+    token: str = None
+    proxies: dict = None
+    username: str = None
+    password: str = None
+
+    def __post_init__(self):
+        if not self.supported_http_methods:
+            self.supported_http_methods = ["GET", "PUT", "DELETE", "POST", "PATCH"]
+        self.session = Session()
+
+    def send(self, method: str, path: str, content: dict = None, files=None,
+             query_params: dict = None, is_json=True):
+        method = method.upper()
+        if method not in self.supported_http_methods:
+            raise Exception(f"Unsupported HTTP method: {method}")
+        if query_params is None:
+            query_params = {}
+        headers = {
+            "User-Agent": self.user_agent,
+        }
+        if self.token:
+            headers["Authorization"] = f"{self.token_name} {self.token}"
+
+        endpoint = self.base_url + path
+        logger.info(f"{self.name} api: method {method} with url {endpoint} start")
+        if is_json:
+            send_data = {
+                "json": content
+            }
+        else:
+            send_data = {
+                "data": content,
+                "files": files
+            }
+        if self.username and self.password:
+            send_data["auth"] = (self.username, self.password)
+        retry_count = 0
+        while retry_count < self.retry_count:
+            try:
+                response = self.session.request(
+                    method, endpoint,
+                    params=query_params,
+                    headers=headers,
+                    verify=self.validate_cert,
+                    timeout=self.connection_timeout,
+                    proxies=self.proxies,
+                    **send_data
+                )
+                logger.info(
+                    f"{self.name} api: method {method} with url {endpoint} end. status code {response.status_code}")
+                return ServerResponse(response=response)
+            except Exception as e:
+                logger.error(f"{self.name} api: method {method} with url {endpoint} has error {e}")
+            retry_count += 1
+            if retry_count < self.retry_count:
+                logger.error(f"{self.name} api: method {method} with url {endpoint} wait {self.retry_delay}")
+                time.sleep(self.retry_delay)
+        return ServerResponse()
---
+++ packages/autoutils/autoutils-1.0.0-py3-none-any.whl/autoutils/script.py
@@ -0,0 +1,148 @@
+"""
+    All Work with linux scripting
+"""
+__author__ = ('Reza Zeiny <rezazeiny1998@gmail.com>',)
+
+import logging
+import random
+import string
+import subprocess
+from threading import Thread
+
+logger = logging.getLogger(__name__)
+
+PIPE = -1
+STDOUT = -2
+DEVNULL = -3
+
+
+def id_generator(size: int = 6, chars: list = string.ascii_uppercase + string.ascii_lowercase + string.digits) -> str:
+    """
+        Generate random string
+    Args:
+        size (int): size of random string
+        chars (list): define select set
+
+    Returns:
+        (str) : random string
+    """
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+class ShellScript:
+    """
+        Shell Script
+    """
+
+    def __init__(self, command, *, shell=False, text=True, split_stdout=True, split_stderr=True, timeout=None,
+                 background=False, quiet=False, extra_data=None):
+        self.__used = False
+        self.command = command
+        self.is_running = False
+        self.process_id = None
+        self.exit_code = None
+        self.stdout = None
+        self.stderr = None
+        self.error = None
+
+        self.shell = shell
+        self.split_stdout = split_stdout
+        self.split_stderr = split_stderr
+        self.text = text
+        self.timeout = timeout
+        self.background = background
+        self.quiet = quiet
+        self.extra_data = extra_data
+
+    def _log(self, level, message, *args):
+        if logger.isEnabledFor(level):
+            # noinspection PyProtectedMember
+            logger._log(level, f"run {self.command}: {message}", args)
+
+    def _run_job(self):
+        self.is_running = True
+        self._log(logging.DEBUG, "Start")
+        if self.quiet:
+            stderr = DEVNULL
+            stdout = DEVNULL
+        else:
+            stdout = PIPE
+            stderr = PIPE
+        try:
+            process = subprocess.Popen(self.command, shell=self.shell, text=self.text, stdout=stdout, stderr=stderr)
+            self.process_id = process.pid
+            self._on_start()
+            try:
+                stdout, stderr = process.communicate(timeout=self.timeout)
+            except Exception as e:
+                process.kill()
+                raise e
+            if stdout is not None:
+                if self.text:
+                    self.stdout = stdout
+                else:
+                    self.stdout = stdout.decode()
+                if self.split_stdout:
+                    self.stdout = self.stdout.split("\n")[:-1]
+            if stderr is not None:
+                if self.text:
+                    self.stderr = stderr
+                else:
+                    self.stderr = stderr.decode()
+                if self.split_stderr:
+                    self.stderr = self.stderr.split("\n")[:-1]
+            self.exit_code = process.poll()
+            self.is_running = False
+            if self.exit_code == 0:
+                self._log(logging.DEBUG, "Completed")
+                self._on_success()
+            else:
+                self._log(logging.DEBUG, f"Error Code {self.exit_code}")
+                self._on_error()
+        except Exception as e:
+            self.is_running = False
+            self.error = e
+            self._log(logging.DEBUG, f"Exception {e}")
+            self._on_exception()
+        return self
+
+    def _on_start(self):
+        """
+            If command is starting
+        """
+        pass
+
+    def _on_error(self):
+        """
+            If exit code of running command is not zero call this function
+        """
+        pass
+
+    def _on_success(self):
+        """
+            If exit code of running command is zero call this function
+        """
+        pass
+
+    def _on_exception(self):
+        """
+            If acquire any exception while running command call this function
+        """
+        pass
+
+    def run(self):
+        """
+            Run Script
+        """
+        if self.__used:
+            raise Exception("this command was run before. make a new instance")
+        self.__used = True
+
+        if self.background:
+            job = Thread(target=self._run_job)
+            job.start()
+            while self.process_id is None and self.error is None:
+                pass
+        else:
+            self._run_job()
+        return self
