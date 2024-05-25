1ee2303fb4b59054315677c9fcb9f1b153c083ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 15:04:41 +0000

    Add package QuLab/2.0.3/qulab-2.0.3.tar.gz

---
+++ packages/QuLab/qulab-2.0.3.tar.gz/qulab-2.0.3/qulab/__main__.py
@@ -0,0 +1,26 @@
+import click
+
+from .monitor.__main__ import main as monitor
+from .scan.recorder import record
+from .sys.net.cli import dht
+from .visualization.__main__ import plot
+
+
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+def hello():
+    """Print hello world."""
+    click.echo('hello, world')
+
+
+main.add_command(monitor)
+main.add_command(plot)
+main.add_command(dht)
+main.add_command(record)
+
+if __name__ == '__main__':
+    main()
