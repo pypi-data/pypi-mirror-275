bd4eb2212a353dd545b46fb421bbef9c32424d1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 15:05:01 +0000

    Add package etypes/1.0.2/etypes-1.0.2-py3-none-any.whl

---
+++ packages/etypes/etypes-1.0.2-py3-none-any.whl/etypes/etypes.py
@@ -0,0 +1,10 @@
+class EncryptedSecret(str):
+    """Encrypted version of a DecryptedSecret"""
+    pass
+
+class DecryptedSecret(str):
+    """Decrypted version of a EncryptedSecret"""
+    pass
+
+class EnvString(str):
+    pass
