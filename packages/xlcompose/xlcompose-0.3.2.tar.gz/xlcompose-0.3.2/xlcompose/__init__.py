0f6da050e5d9eddaf2424d1cc777061be10544de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 14:04:16 +0000

    Add package xlcompose/0.3.2/xlcompose-0.3.2.tar.gz

---
+++ packages/xlcompose/xlcompose-0.3.2.tar.gz/xlcompose-0.3.2/xlcompose/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.3.2'
+from xlcompose.core import (
+    Tabs, Sheet, Row, Column,
+    DataFrame, Series, CSpacer, RSpacer, Title, Image, VSpacer, HSpacer)
+from xlcompose.templates import load_json, load_yaml, EvalExtension
