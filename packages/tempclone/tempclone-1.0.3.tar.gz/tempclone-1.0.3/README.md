4cbfa39f8056f61d058a349164300dcef05a2009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 03:04:21 +0000

    Add package tempclone/1.0.3/tempclone-1.0.3.tar.gz

---
+++ packages/tempclone/tempclone-1.0.3.tar.gz/tempclone-1.0.3/README.md
@@ -0,0 +1,17 @@
+# Temp Clone
+
+Temp Clone is a command-line interface (CLI) tool to automate the creation of new projects from GitHub templates. This tool helps streamline the setup process for new projects by cloning from predefined templates.
+
+## Features
+
+- Configure GitHub access token and user nickname.
+- List available templates from a specified GitHub user or organization.
+- Create a new project repository from a selected template.
+- Automatically clone the newly created repository.
+
+## Installation
+
+You can install `tempclone` using `pip`:
+
+```bash
+pip install tempclone
---
+++ packages/tempclone/tempclone-1.0.3.tar.gz/tempclone-1.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="tempclone",
+    version="1.0.3",
+    author="Matheus",
+    author_email="matheusmlfg@gmail.com",
+    description="CLI to automate the creation of new projects from GitHub templates",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/tempclone",  # URL do repositório do seu projeto
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "click",
+        "requests"
+    ],
+    entry_points={
+        'console_scripts': [
+            'tempclone=tempclone.cli:cli',
+        ],
+    },
+)
