4061d7265243d9e83cb3fca219bb5e31eea93242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 15:04:14 +0000

    Add package biochain/0.0.1/biochain-0.0.1.tar.gz

---
+++ packages/biochain/biochain-0.0.1.tar.gz/biochain-0.0.1/biochain/base.py
@@ -0,0 +1,17 @@
+"""
+biochain base module.
+
+This is the principal module of the biochain project.
+here you put your main classes and objects.
+
+Be creative! do whatever you want!
+
+If you want to replace this with a Flask application run:
+
+    $ make init
+
+and then choose `flask` as template.
+"""
+
+# example constant variable
+NAME = "biochain"
