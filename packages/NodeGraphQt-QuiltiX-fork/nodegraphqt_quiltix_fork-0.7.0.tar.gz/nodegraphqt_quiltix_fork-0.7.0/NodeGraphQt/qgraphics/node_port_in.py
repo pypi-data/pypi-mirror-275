74545cc314859b9e60fe3ffbd3b441ae000c54c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 05:04:30 +0000

    Add package NodeGraphQt-QuiltiX-fork/0.7.0/nodegraphqt_quiltix_fork-0.7.0.tar.gz

---
+++ packages/NodeGraphQt-QuiltiX-fork/nodegraphqt_quiltix_fork-0.7.0.tar.gz/nodegraphqt_quiltix_fork-0.7.0/NodeGraphQt/qgraphics/node_port_in.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+from qtpy import QtCore, QtGui, QtWidgets
+
+from NodeGraphQt.constants import NodeEnum
+from NodeGraphQt.qgraphics.node_base import NodeItem
+
+
+class PortInputNodeItem(NodeItem):
+    """
+    Input Port Node item.
+
+    Args:
+        name (str): name displayed on the node.
+        parent (QtWidgets.QGraphicsItem): parent item.
+    """
+
+    def __init__(self, name='group port', parent=None):
+        super(PortInputNodeItem, self).__init__(name, parent)
+        self._icon_item.setVisible(False)
+        self._text_item.set_locked(True)
+        self._x_item.text = 'Port Locked'
+
+    def _paint_horizontal(self, painter, option, widget):
+        painter.save()
+        painter.setPen(QtCore.Qt.NoPen)
+        painter.setBrush(QtCore.Qt.NoBrush)
+
+        # base background.
+        margin = 0
+        rect = self.boundingRect()
+        rect = QtCore.QRectF(rect.left() + margin,
+                             rect.top() + margin,
+                             rect.width() - (margin * 2),
+                             rect.height() - (margin * 2))
+
+        radius = 4.0
+        painter.setBrush(QtGui.QColor(self.backgroundColor))
+        painter.drawRoundedRect(rect, radius, radius)
+
+        # light overlay on background when selected.
+        if self.selected:
+            painter.setBrush(QtGui.QColor(*NodeEnum.SELECTED_COLOR.value))
+            painter.drawRoundedRect(rect, radius, radius)
+
+        # node name background.
+        padding = 0, 0
+        text_rect = self._text_item.boundingRect()
+        text_rect = QtCore.QRectF(text_rect.x() + padding[0],
+                                  rect.y() + padding[1],
+                                  rect.width() - padding[0] - margin,
+                                  text_rect.height() - (padding[1] * 2))
+
+        text_rect2 = QtCore.QRectF(text_rect.x() + padding[0],
+                                  rect.y() + 3 + padding[1],
+                                  rect.width() - padding[0] - margin,
+                                  text_rect.height() - (padding[1] * 2))
+        # if self.selected:
+        #     painter.setBrush(QtGui.QColor(*NodeEnum.SELECTED_COLOR.value))
+        # else:
+        #     painter.setBrush(QtGui.QColor(0, 0, 0, 80))
+        # painter.setBrush(QtGui.QColor(self.titleBackground))
+        # painter.drawRoundedRect(text_rect, 3.0, 3.0)
+        # painter.drawRect(text_rect2)
+
+        # node border
+        if self.selected:
+            border_width = 1.2
+            border_color = QtGui.QColor(
+                *NodeEnum.SELECTED_BORDER_COLOR.value
+            )
+        else:
+            border_width = 0.8
+            border_color = QtGui.QColor(*self.border_color)
+
+        border_rect = QtCore.QRectF(rect.left(), rect.top(),
+                                    rect.width(), rect.height())
+
+        pen = QtGui.QPen(border_color, border_width)
+        pen.setCosmetic(self.viewer().get_zoom() < 0.0)
+        path = QtGui.QPainterPath()
+        path.addRoundedRect(border_rect, radius, radius)
+        painter.setBrush(QtCore.Qt.NoBrush)
+        painter.setPen(pen)
+        painter.drawPath(path)
+
+        painter.restore()
+
+    def set_proxy_mode(self, mode):
+        super(PortInputNodeItem, self).set_proxy_mode(mode)
+        self._icon_item.setVisible(False)
---
+++ packages/NodeGraphQt-QuiltiX-fork/nodegraphqt_quiltix_fork-0.7.0.tar.gz/nodegraphqt_quiltix_fork-0.7.0/README.md
@@ -0,0 +1,34 @@
+## NodeGraphQt
+
+NodeGraphQt is a node graph UI framework for PySide2 that can be implemented and re-purposed into 
+applications.
+
+<img src="/docs/_images/screenshot.png" width="100%" title="NodeGraphQt">
+
+
+#### Documentation
+
+<a href="https://jchanvfx.github.io/NodeGraphQt" target="_blank">https://jchanvfx.github.io/NodeGraphQt</a>
+
+See the [basic_example.py](/examples/basic_example.py) python script from this repo.
+
+More examples can be found in the API documentation:<br>
+https://jchanvfx.github.io/NodeGraphQt/api/html/examples/ex_overview.html#simple-example
+
+#### Vertical Layout
+
+<img src="/docs/_images/vertical_layout.png" width="600" title="Vertical Layout">
+
+#### Pipe Layout
+
+<img src="/docs/_images/pipe_layout_types.gif" width="600" title="Pipe Layout">
+
+#### Slice Connections
+
+<img src="/docs/_images/slicer.png" width="500" title="Pipe Slicer">
+
+#### Custom Widgets
+<img src="/docs/_images/prop_bin.png" width="600" title="Properties Bin">
+<img src="/docs/_images/nodes_palette.png" width="350" title="Nodes Paletten">
+
+
