10954c753603b05ecb63f2170978134207966776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 15:05:03 +0000

    Add package ctrip-app-ui/0.7.8/ctrip-app-ui-0.7.8.tar.gz

---
+++ packages/ctrip-app-ui/ctrip-app-ui-0.7.8.tar.gz/ctrip-app-ui-0.7.8/capp_ui/fee.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+# ---------------------------------------------------------------------------------------------------------
+# ProjectName:  ctrip-app-ui
+# FileName:     fee.py
+# Description:  TODO
+# Author:       mfkifhss2023
+# CreateDate:   2024/04/24
+# Copyright ©2011-2024. Hunan xxxxxxx Company limited. All rights reserved.
+# ---------------------------------------------------------------------------------------------------------
+"""
+from decimal import Decimal
+
+flight_fee = {
+    "fuel_fee": Decimal("70.00"),  # 燃油费
+    "airport_fee": Decimal("50.00")  # 机建费
+}
