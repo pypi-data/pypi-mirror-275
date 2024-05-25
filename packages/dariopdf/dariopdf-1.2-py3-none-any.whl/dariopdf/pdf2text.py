6ae065e74de346ea9c36ac10bc567bb7a5ff59d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 15:04:13 +0000

    Add package dariopdf/1.2/dariopdf-1.2-py3-none-any.whl

---
+++ packages/dariopdf/dariopdf-1.2-py3-none-any.whl/dariopdf/pdf2text.py
@@ -0,0 +1,19 @@
+"""  This module provides functions to convert a PDF to text. """
+
+
+class Converter:
+    """ A simple converter for converting PDFs to text. """
+
+    def convert(self, path):
+        """ Covert the given PDF to text.
+
+        Parameters:
+        path(str): the path to a pdf file.
+
+        Returns:
+        str: The content of the pdf file as text.
+        """
+        print("pdf2text")
+
+
+pdf2text.convert()
