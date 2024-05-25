224ee0d0c4d65517a491b7626c1de5a10d59f269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 14:04:15 +0000

    Add package pazok/0.0.4/pazok-0.0.4.tar.gz

---
+++ packages/pazok/pazok-0.0.4.tar.gz/pazok-0.0.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pazok',
+    version='0.0.4',
+    author='b_azo',
+    author_email='husseun.selt@gmail.com',
+    description='A short description of my package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'argparse',
+        're',
+        'shlex',
+        'random',
+        'collections',
+        'urllib',
+        'pyperclip',
+        'rich',
+        'fake_useragent',
+        'colorama',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
