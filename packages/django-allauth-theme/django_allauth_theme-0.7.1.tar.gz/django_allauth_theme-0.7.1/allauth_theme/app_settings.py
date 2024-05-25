fc3b695902fca30189e3c46ae27daae4d70fec1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 04:04:22 +0000

    Add package django-allauth-theme/0.7.1/django_allauth_theme-0.7.1.tar.gz

---
+++ packages/django-allauth-theme/django_allauth_theme-0.7.1.tar.gz/django_allauth_theme-0.7.1/allauth_theme/app_settings.py
@@ -0,0 +1,40 @@
+from django.conf import settings
+
+
+class AppSettings:
+    @property
+    def DAT_WELCOME_TITLE(self):
+        return getattr(settings, "DAT_WELCOME_TITLE", "Welcome")
+
+    @property
+    def DAT_TAILWIND_CSS(self):
+        return getattr(settings, "DAT_TAILWIND_CSS", "css/tailwind.css")
+
+    @property
+    def DAT_SIGNUP_CSS(self):
+        return getattr(settings, "DAT_SIGNUP_CSS", "css/signup.css")
+
+    @property
+    def DAT_WELCOME_TITLE_MOBILE(self):
+        return getattr(settings, "DAT_WELCOME_TITLE_MOBILE", "Welcome mobile")
+
+    @property
+    def DAT_GOOGLE_ENABLE_ONETAP_LOGIN(self):
+        return getattr(settings, "DAT_GOOGLE_ENABLE_ONETAP_LOGIN", False)
+
+    @property
+    def DAT_GOOGLE_CLIENT_ID(self):
+        return getattr(settings, "DAT_GOOGLE_CLIENT_ID", "google_client_id")
+
+    @property
+    def DAT_BASE_URL(self):
+        return getattr(settings, "DAT_BASE_URL", "http://localhost:8000")
+
+    @property
+    def DAT_WELCOME_TEXT(self):
+        return getattr(
+            settings,
+            "DAT_WELCOME_TEXT",
+            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor"
+            " invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua",
+        )
