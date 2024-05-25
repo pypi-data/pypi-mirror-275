bf72b57b65ec47e3cf3c702e0941890c7cd9c833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 03:03:55 +0000

    Add package slurmray/3.6.0/slurmray-3.6.0-py3-none-any.whl

---
+++ packages/slurmray/slurmray-3.6.0-py3-none-any.whl/slurmray/assets/slurmray_server_template.py
@@ -0,0 +1,19 @@
+from slurmray.RayLauncher import RayLauncher
+
+if __name__ == "__main__":
+
+    launcher = RayLauncher(
+        project_name="server",
+        func=None,
+        args=None,
+        modules={{MODULES}},
+        node_nbr={{NODE_NBR}},
+        use_gpu={{USE_GPU}},
+        memory={{MEMORY}},
+        max_running_time={{MAX_RUNNING_TIME}},
+        server_run=False,
+        server_ssh=None,
+        server_username=None,
+    )
+
+    result = launcher()
