c7bbc04248376305d396136d29ecc79e786572d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 16:04:04 +0000

    Add package sagemath-standard/10.4b7/sagemath_standard-10.4b7.tar.gz

---
+++ packages/sagemath-standard/sagemath_standard-10.4b7.tar.gz/sagemath_standard-10.4b7/sage/coding/all.py
@@ -0,0 +1,12 @@
+
+from sage.misc.lazy_import import lazy_import as _lazy_import
+
+_lazy_import("sage.coding.code_constructions", ["permutation_action",
+                                                "walsh_matrix"])
+
+_lazy_import("sage.coding.linear_code", "LinearCode")
+
+# Functions removed from the global namespace
+
+_lazy_import('sage.coding', 'codes_catalog', 'codes')
+_lazy_import('sage.coding', 'channels_catalog', 'channels')
