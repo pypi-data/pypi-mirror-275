7b7e4a9c9952f12be3de8cd6d10c670432a9b6fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 15:05:02 +0000

    Add package TestMonitoringSystem/0.1.0/testmonitoringsystem-0.1.0.tar.gz

---
+++ packages/TestMonitoringSystem/testmonitoringsystem-0.1.0.tar.gz/testmonitoringsystem-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TestMonitoringSystem",
+    version="0.1.0",
+    packages=find_packages(),
+    author="yuta morimoto",
+    author_email="s2222081@stu.musashino-u.ac.jp",
+    description="TestMonitoringSystem",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yuta0726/TestMonitoringSystem",
+    python_requires=">=3.12.3",
+)
