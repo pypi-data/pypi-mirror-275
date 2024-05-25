0af92592a6c13ebdabd4a979047c9dc295f61c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 05:04:26 +0000

    Add package ivan-pro/1.1.2/ivan_pro-1.1.2-py3-none-any.whl

---
+++ packages/ivan-pro/ivan_pro-1.1.2-py3-none-any.whl/ivan/plugins/__init__.py
@@ -0,0 +1,17 @@
+from .update import update
+from .keys import keys
+from .find import find
+from .ip import ip
+from .display import display
+from .export import export
+from .scan import scan
+
+
+def plugin_loader(group):
+    group.add_command(update)
+    group.add_command(keys)
+    group.add_command(find)
+    group.add_command(ip)
+    group.add_command(display)
+    group.add_command(export)
+    group.add_command(scan)
