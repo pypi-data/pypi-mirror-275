1c8a01e4fce2374141a1f8a8dc455fe640d88723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 03:04:00 +0000

    Add package mistral-common/1.1.0/mistral_common-1.1.0.tar.gz

---
+++ packages/mistral-common/mistral_common-1.1.0.tar.gz/mistral_common-1.1.0/src/mistral_common/base.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class MistralBase(BaseModel):
+    """
+    Base class for all Mistral Pydantic models.
+    """
+
+    model_config = ConfigDict(
+        extra="forbid",
+        validate_default=True,
+        use_enum_values=True
+    )
