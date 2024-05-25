4c608fed04df99551678135af706e17062daf814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 03:04:47 +0000

    Add package MusicSeparationYT/1.0.0/musicseparationyt-1.0.0.tar.gz

---
+++ packages/MusicSeparationYT/musicseparationyt-1.0.0.tar.gz/musicseparationyt-1.0.0/README.md
@@ -0,0 +1,5 @@
+# Example Package
+
+This is a music download and separate tool.
+
+please input your url.
---
+++ packages/MusicSeparationYT/musicseparationyt-1.0.0.tar.gz/musicseparationyt-1.0.0/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+# パッケージの基本情報
+name = "MusicSeparationYT"  # パッケージ名
+version = "1.0.0"  # バージョン番号
+description = "this tool music download and seprate"  # パッケージの説明
+author = "Y-Ryohei"  # 作者名
+author_email = ""  # 作者のメールアドレス
+url = "https://github.com/Y-Ryohei/MusicSeparationYT"  # リポジトリURL
+license = "MIT"  # ライセンス
+
+# インストールに必要な依存関係
+requirements = [
+  'pytube'
+  'moviepy',
+  'demucs',
+]
+
+# パッケージに含めるモジュール
+packages = [
+]
+
+# その他の情報
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3.6",
+    "Topic :: Software Development :: Libraries",
+]
+
+# セットアップ処理
+setuptools.setup(
+    name=name,
+    version=version,
+    description=description,
+    author=author,
+    author_email=author_email,
+    url=url,
+    license=license,
+    install_requires=requirements,
+    packages=packages,
+    classifiers=classifiers,
+)
