c66a041014fe14ad6048d6f8a5e3ed74b0b66eb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 03:05:03 +0000

    Add package tscat/0.3.0/tscat-0.3.0-py3-none-any.whl

---
+++ packages/tscat/tscat-0.3.0-py3-none-any.whl/tscat/__init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for Time Series Catalogues."""
+
+__author__ = """Patrick Boettcher"""
+__email__ = 'p@yai.se'
+__version__ = '0.3.0'
+
+from .base import create_event, create_catalogue, \
+    add_events_to_catalogue, remove_events_from_catalogue, \
+    save, discard, has_unsaved_changes, \
+    get_catalogues, get_events, \
+    _Catalogue, _Event, Session, EventQueryInformation
+
+from .import_export import export_votable, export_json, \
+    import_json, import_votable, \
+    __canonicalize_json_import as canonicalize_json_import, \
+    __import_canonicalized_dict as import_canonicalized_dict
