072e86f11a4e925620a457ffc8fb86d72e5fb0cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 03:04:00 +0000

    Add package mpltoolbox/24.5.1/mpltoolbox-24.5.1-py3-none-any.whl

---
+++ packages/mpltoolbox/mpltoolbox-24.5.1-py3-none-any.whl/mpltoolbox/__init__.py
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) Scipp contributors (https://github.com/scipp)
+
+# flake8: noqa
+
+from .event import DummyEvent
+from .tool import Tool
+from .lines import Lines
+from .rectangles import Rectangles
+from .ellipses import Ellipses
+from .points import Points
+from .polygons import Polygons
+from .hspans import Hspans
+from .vspans import Vspans
