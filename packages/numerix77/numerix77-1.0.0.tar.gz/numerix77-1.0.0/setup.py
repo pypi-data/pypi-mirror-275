4d906e4ac3927075d464d33b8b56a2ef4a1fd56c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 03:04:29 +0000

    Add package numerix77/1.0.0/numerix77-1.0.0.tar.gz

---
+++ packages/numerix77/numerix77-1.0.0.tar.gz/numerix77-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name='numerix77',
+    version='1.0.0',
+    packages=['Basic_library'],
+    description='A simple Python library for basic mathematical operations',
+    url='',
+    license='MIT',
+)
