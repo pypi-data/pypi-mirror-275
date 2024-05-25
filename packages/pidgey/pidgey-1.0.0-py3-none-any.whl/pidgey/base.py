b0f49513697f46c2c414cf26b741cf1790d3f913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 05:04:38 +0000

    Add package pidgey/1.0.0/pidgey-1.0.0-py3-none-any.whl

---
+++ packages/pidgey/pidgey-1.0.0-py3-none-any.whl/pidgey/base.py
@@ -0,0 +1,60 @@
+from abc import abstractmethod
+
+import astropy.coordinates as coord
+import astropy.units as u
+from iext import ExtendImports
+
+
+class Backend(ExtendImports):
+    def __imports__():
+        ...
+
+    def __init__(self):
+        self._args = None
+        self._result = None
+
+    @property
+    @abstractmethod
+    def ORBIT_TYPE(self):
+        pass
+
+    @abstractmethod
+    def _compute_orbit(
+        self, skycoord, pot, dt, steps, pattern_speed=0 * u.km / u.s / u.kpc
+    ):
+        pass
+
+    def compute_orbit(
+        self, skycoord, pot, dt, steps, pattern_speed=0 * u.km / u.s / u.kpc
+    ):
+        if not isinstance(skycoord, coord.SkyCoord):
+            raise TypeError(
+                "coord must be passed in as a astropy.coordinates.SkyCoord object."
+                f"{skycoord} is not a astropy.coordinates.SkyCoord object."
+            )
+        skycoord.representation_type = "cartesian"
+        if not (
+            hasattr(skycoord, "v_x")
+            and hasattr(skycoord, "v_y")
+            and hasattr(skycoord, "v_z")
+        ):
+            raise TypeError(
+                "coord must have 3-D velocity information to compute an orbit."
+            )
+        if not isinstance(dt, u.Quantity):
+            raise TypeError(
+                "dt must be passed in as a astropy.units.Quantity object."
+                f"{dt} is not a astropy.coordinates.SkyCoord object."
+            )
+        self._args = skycoord, pot, dt, steps
+        self._result = self._compute_orbit(skycoord, pot, dt, steps, pattern_speed)
+        return self._extract_points(self._result, pattern_speed)
+
+    @abstractmethod
+    def _extract_points(self, orbit, pattern_speed=0 * u.km / u.s / u.kpc):
+        pass
+
+    def get_points(self):
+        if not isinstance(self._result, self.ORBIT_TYPE):
+            raise TypeError("No computed orbit to retrive points from.")
+        return self._extract_points(self._result)
---
+++ packages/pidgey/pidgey-1.0.0-py3-none-any.whl/pidgey/gala_backend.py
@@ -0,0 +1,33 @@
+import astropy.units as u
+
+from .base import Backend
+
+
+class GalaBackend(Backend):
+    def __imports__():
+        from gala import dynamics, potential, units
+
+    def ORBIT_TYPE(self):
+        return self.dynamics.orbit.Orbit
+
+    def _compute_orbit(
+        self, skycoord, pot, dt, steps, pattern_speed=0 * u.km / u.s / u.kpc
+    ):
+        pos = [skycoord.x.value, skycoord.y.value, skycoord.z.value] * skycoord.x.unit
+        vel = [
+            skycoord.v_x.value,
+            skycoord.v_y.value,
+            skycoord.v_z.value,
+        ] * skycoord.v_x.unit
+        ics = self.dynamics.PhaseSpacePosition(pos, vel)
+
+        orbit = pot.integrate_orbit(ics, dt=dt, n_steps=steps - 1)
+        return orbit
+
+    def _extract_points(self, orbit, pattern_speed=0 * u.km / u.s / u.kpc):
+        frame = self.potential.ConstantRotatingFrame(
+            Omega=[0, 0, pattern_speed.value] * pattern_speed.unit,
+            units=self.units.galactic,
+        )
+        orbit = orbit.to_frame(frame)
+        return orbit.data.T
