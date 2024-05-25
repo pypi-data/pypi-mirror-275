5fffbcda7e70aa152ef2dc8ff923443d7877c43c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 15:05:10 +0000

    Add package better-web3/4.0.0b6/better_web3-4.0.0b6.tar.gz

---
+++ packages/better-web3/better_web3-4.0.0b6.tar.gz/better_web3-4.0.0b6/better_web3/__init__.py
@@ -0,0 +1,13 @@
+from .chain import Chain
+from .contract import Contract
+from .caip_2 import get_chains, get_chain
+from . import utils
+
+
+__all__ = [
+    "Chain",
+    "Contract",
+    "get_chains",
+    "get_chain",
+    "utils",
+]
