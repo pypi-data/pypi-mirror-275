34e4fc7af2e1849a5eb74fbfc60dee9fa84f18c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 03:04:38 +0000

    Add package unprint/0.0.2/unprint-0.0.2.tar.gz

---
+++ packages/unprint/unprint-0.0.2.tar.gz/unprint-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from time import time
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="unprint",
+    version="0.0.2",
+    author="Liu Zuo Lin",
+    author_email="zlliu246@gmail.com",
+    description="A simple package to unprint lines in the terminal",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+)
