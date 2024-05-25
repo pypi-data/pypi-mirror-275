6a981b2191f1fce79100848ff591d1f9a9d3a135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-25 04:04:52 +0000

    Add package aitelegrambot/0.8.4/aitelegrambot-0.8.4.tar.gz

---
+++ packages/aitelegrambot/aitelegrambot-0.8.4.tar.gz/aitelegrambot-0.8.4/CHANGELOG.md
@@ -0,0 +1,98 @@
+# CHANGELOG
+
+## 0.0.0
+
+- Add README, LICENSE, and pyproject.toml.
+
+## 0.1.0
+
+- Add basic functionality.
+- Rename project to aitelegrambot from rationalai-telegram-bot.
+
+## 0.1.1
+
+- Add script for pipx.
+- Add additonal instructions in README.
+
+## 0.1.2
+
+- Use a seperate module for constant messages.
+
+## 0.1.3
+
+- Fix bug because of which the command was being sent to the LLM.
+
+## 0.2.0
+
+- The bot now replies to the message which ran the infer command.
+
+## 0.3.0
+
+- The bot has an administration feature which allows the admin to:
+  1. Change the current model.
+  2. Pull a model.
+  3. Delete a model.
+  4. List models.
+
+## 0.3.1
+
+- Fix bug with list models command: Error and reply when no models are
+  available.
+
+## 0.4.0
+
+- Add support for ~streaming responses~ (Receiving the messages, as
+  the LLM generates them).
+
+_The following two releases are just very basic bug fixes which should
+  have been properly tested._
+## 0.4.1
+
+- Fix bugs
+
+## 0.4.2
+
+- Fix bugs
+
+## 0.5.0
+
+- Add support for both, streaming and non-streaming inference.
+- Default to non-streaming.
+- New option to customize streaming or non-streaming.
+
+## 0.5.1
+
+- Fix bug
+
+## 0.5.2
+
+- Fix bug
+
+## 0.6.0
+
+- add help command.
+- Reply to the user even when streaming messages is disabled.
+- Fix bugs with pull and delete commands.
+
+## 0.7.0
+
+- add support for multiple administrators.
+
+## 0.8.0
+
+- add current model indication in list_models command.
+- fix error in /help command.
+
+## 0.8.1
+
+- remove debugging statements from /list_models command.
+
+## 0.8.2
+- fix BadRequest response error when using streaming inference.
+- add help message when user sends an empty message to /infer commands.
+
+## 0.8.3
+- Fix error in /infer command.
+
+## 0.8.4
+- Fix behaviour of /infer command.
---
+++ packages/aitelegrambot/aitelegrambot-0.8.4.tar.gz/aitelegrambot-0.8.4/src/aitelegrambot/bot.py
@@ -0,0 +1,118 @@
+# The GPLv3 License (GPLv3)
+
+# Copyright © 2024 aitelegrambot authors.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+This module defines the TelegramBot class.
+"""
+
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    Application,
+)
+from ollama import Client
+from aitelegrambot.commandhandlers import (
+    NormalCommandHandlers,
+    CommandHandlers,
+    AdministrationCommandHandlers,
+    OllamaState,
+)
+
+
+class TelegramBot:
+    """
+    A class that implements a Telegram bot using the Ollama framework.
+    """
+
+    def __init__(
+        self,
+        ollama_host: str,
+        bot_token: str,
+        default_model: str,
+        administrator_user_ids: list[int],
+        enable_streaming_response: bool,
+        message_chunk_size: int,
+    ):
+        """
+        Initializes an instance of TelegramBot.
+
+        Arguments:
+        ==========
+        ollama_host: Host address of the `ollama` service.
+        bot_token: Bot token for telegram.
+        default_model: The default model to load when the bot is
+        initialized.
+        administrator_user_ids: Telegram user ids of the administrators.
+        enable_streaming_response: Enable streaming response option.
+        message_chunk_size: The number of words to be send at a time.
+        """
+        ollama_state: OllamaState = OllamaState(
+            Client(host=ollama_host), default_model, message_chunk_size
+        )
+        self.normal_command_handlers: CommandHandlers = NormalCommandHandlers(
+            ollama_state
+        )
+        self.normal_command_handlers.inference = (
+            self.normal_command_handlers.stream_inference
+            if enable_streaming_response
+            else self.normal_command_handlers.basic_inference
+        )
+
+        self.administrative_command_handlers: CommandHandlers = (
+            AdministrationCommandHandlers(ollama_state, administrator_user_ids)
+        )
+
+        self.application: Application = ApplicationBuilder().token(bot_token).build()
+
+    def run(self):
+        """
+        Run the bot.
+        """
+        self.application.add_handler(
+            CommandHandler("start", self.normal_command_handlers.start),
+        )
+        self.application.add_handler(
+            CommandHandler("help", self.normal_command_handlers.help),
+        )
+        self.application.add_handler(
+            CommandHandler("infer", self.normal_command_handlers.inference),
+        )
+        self.application.add_handler(
+            CommandHandler(
+                "list_models", self.administrative_command_handlers.list_models
+            )
+        )
+        self.application.add_handler(
+            CommandHandler(
+                "change_model",
+                self.administrative_command_handlers.change_model,
+            )
+        )
+        self.application.add_handler(
+            CommandHandler(
+                "pull_model",
+                self.administrative_command_handlers.pull_model,
+            )
+        )
+        self.application.add_handler(
+            CommandHandler(
+                "remove_model",
+                self.administrative_command_handlers.remove_model,
+            )
+        )
+
+        self.application.run_polling()
