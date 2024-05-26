fbd5a65c58eb3d6858c764512875f823f4420e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 03:04:03 +0000

    Add package image-collect/0.0.1/image_collect-0.0.1.tar.gz

---
+++ packages/image-collect/image_collect-0.0.1.tar.gz/image_collect-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='image_collect',
+    version='0.0.1',
+    py_modules=['image_collect'],
+    install_requires=["BeautifulSoup4","deep_translator"],
+)
