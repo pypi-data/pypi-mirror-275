8becb0f996272581a0f78c1c9646b769008fed62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 03:03:57 +0000

    Add package botrun-google-docs-reader/4.25.254/botrun_google_docs_reader-4.25.254.tar.gz

---
+++ packages/botrun-google-docs-reader/botrun_google_docs_reader-4.25.254.tar.gz/botrun_google_docs_reader-4.25.254/README.md
@@ -0,0 +1,3 @@
+### 2024-05-25 by bowen
+輸入任意一個 google doc id 就能讀取純文字出來
+可以運用在 _autogen_prefix.botrun 讀取
---
+++ packages/botrun-google-docs-reader/botrun_google_docs_reader-4.25.254.tar.gz/botrun_google_docs_reader-4.25.254/botrun_google_docs_reader.py
@@ -0,0 +1,36 @@
+import argparse
+
+import google.auth
+from dotenv import load_dotenv
+from googleapiclient.discovery import build
+
+load_dotenv()
+
+
+def botrun_google_docs_reader(doc_id: str) -> str:
+    credentials, project = google.auth.default()
+    service = build('docs', 'v1', credentials=credentials)
+    document = service.documents().get(documentId=doc_id).execute()
+    doc_content = document.get('body').get('content')
+
+    text = ''
+    for element in doc_content:
+        if 'paragraph' in element:
+            for text_run in element['paragraph']['elements']:
+                if 'textRun' in text_run:
+                    text += text_run['textRun']['content']
+
+    return text
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Download Google Doc and read content.')
+    parser.add_argument('--doc_id', type=str, default='1uV4gjH3yTfvXweXn8nXhQ7Tx2nBm1fPl9WNpWzo2H3w',
+                        help='Google Doc ID')
+    args = parser.parse_args()
+    print("botrun_google_docs_reader.py, args.doc_id:", args.doc_id)
+    print(botrun_google_docs_reader(args.doc_id))
+
+
+if __name__ == "__main__":
+    main()
