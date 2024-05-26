3519d4ee013fe5f74f8e31537d2b858edd58ca1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 03:04:04 +0000

    Add package AzizKitten/11.2.0/AzizKitten-11.2.0-py3-none-any.whl

---
+++ packages/AzizKitten/AzizKitten-11.2.0-py3-none-any.whl/AzizKitten/factorial.py
@@ -0,0 +1,16 @@
+def factorial(x):
+    from .exp import exp
+    from .integrate import integrate
+    if type(x) is not complex:
+        if int(x) == x:
+            if x < 0:
+                raise ValueError("Factorial of a negative integer is undefined.")
+            else:
+                ans = 1
+                for i in range(1,x+1):
+                    ans *= i
+                return ans
+    else:
+        def integrand(t):
+            return t**x * exp(-t)
+        return integrate(integrand, 0, 100)
