787ed4834211177b83f93d266ed759190947c1f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 03:03:57 +0000

    Add package django-my-web-console/0.1.1/django-my-web-console-0.1.1.tar.gz

---
+++ packages/django-my-web-console/django-my-web-console-0.1.1.tar.gz/django-my-web-console-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-my-web-console',
+    version='0.1.1',
+    description='A simple web console based on Django.',
+    author='yuhua.yang',
+    packages=find_packages(),
+)
