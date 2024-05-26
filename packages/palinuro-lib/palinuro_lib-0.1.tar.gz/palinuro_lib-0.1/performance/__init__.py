be94eb15bca1a2b4f328871e1e45ae9e50007303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 04:04:09 +0000

    Add package palinuro-lib/0.1/palinuro_lib-0.1.tar.gz

---
+++ packages/palinuro-lib/palinuro_lib-0.1.tar.gz/palinuro_lib-0.1/performance/__init__.py
@@ -0,0 +1,3 @@
+from palinuro_lib.performance import nav_pro, cumulative_return, volatility, sharpe_ratio, downside_volatility, sortino_ratio, max_drawdown, stats_table, value_at_risk, distribution_properties, historical_correlation
+
+__all__ = ['nav_pro', 'cumulative_return', 'volatility', 'sharpe_ratio', 'downside_volatility', 'sortino_ratio', 'max_drawdown', 'stats_table', 'value_at_risk', 'distribution_properties', 'historical_correlation']
---
+++ packages/palinuro-lib/palinuro_lib-0.1.tar.gz/palinuro_lib-0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='palinuro_lib',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib==3.8.4',
+        'matplotlib-inline==0.1.7',
+        'numpy==1.26.4',
+        'pandas==2.2.2',
+        'scipy==1.13.0',
+       ' seaborn==0.13.2',
+    ],
+    author='Vincenzo Brigandi',
+    author_email='brigandi.vincenzo@yahoo.com',
+    description='A library for trading in a Macro Hedge Fund',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/vcnzbrgd/palinuro_lib',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
