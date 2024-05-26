e06549a78d8057acaae6e2ae59cf112a5d180181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 03:03:59 +0000

    Add package vialin/0.0.1/vialin-0.0.1.tar.gz

---
+++ packages/vialin/vialin-0.0.1.tar.gz/vialin-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    readme = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Port c++ to python easily!'
+
+setup(
+    name="vialin",
+    version=VERSION,
+    author="Schkimansky",
+    author_email="<ahmadchawla1432@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=readme,
+    packages=find_packages(),
+    install_requires=[],
+    keywords='c++, python, python c++, port, porter, bind, binder, binding, bind c++'.split(', '),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
