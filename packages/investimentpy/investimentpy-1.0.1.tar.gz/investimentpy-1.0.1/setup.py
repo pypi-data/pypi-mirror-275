c78a761c920a00d3e609f1d9bf7e798d5f0ad7b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 03:04:00 +0000

    Add package investimentpy/1.0.1/investimentpy-1.0.1.tar.gz

---
+++ packages/investimentpy/investimentpy-1.0.1.tar.gz/investimentpy-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='investimentpy',
+    version='1.0.1',
+    packages=find_packages(),
+    description='Uma biblioteca para análise de investimentos',
+    author='Thiago S Adriano',
+    author_email='prof.thiagoadriano@gmail.com',    
+    license='MIT',  
+    long_description=long_description,
+    long_description_content_type='text/markdown' 
+)
