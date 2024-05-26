15584a787cb79124fdd50df7cf0186ddda6c6299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 03:04:06 +0000

    Add package bot-managers/0.0.1017/bot-managers-0.0.1017.tar.gz

---
+++ packages/bot-managers/bot-managers-0.0.1017.tar.gz/bot-managers-0.0.1017/bot_managers/__init__.py
@@ -0,0 +1,7 @@
+from .bots_manager import ListeningTGBotManager, ResponsibleTGBotManager
+from .client_manager import TelethonClientManager
+from .telethon.client import PatchedTelegramClient
+from .telethon.errors import TelethonErrorHandler
+from .updater import aio_update_tgbot, update_tgbot
+from .utils import AsyncDict, AsyncList, ShuttingDown
+from .voices import VoiceTranscription
---
+++ packages/bot-managers/bot-managers-0.0.1017.tar.gz/bot-managers-0.0.1017/bot_managers/__version__.py
@@ -0,0 +1,3 @@
+VERSION = (0, 0, 1017)
+
+__version__ = '.'.join(map(str, VERSION))
