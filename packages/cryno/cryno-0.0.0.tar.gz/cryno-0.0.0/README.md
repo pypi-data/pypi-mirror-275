30eea04eb315af4e8c33a156cd4ea3a7a1ece698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 03:04:19 +0000

    Add package cryno/0.0.0/cryno-0.0.0.tar.gz

---
+++ packages/cryno/cryno-0.0.0.tar.gz/cryno-0.0.0/README.md
@@ -0,0 +1,21 @@
+# Donations
+It takes me a long time to make these libraries. If you would like to support my work, Then you can follow my patreon :)
+
+https://www.patreon.com/Schkimansky
+
+# Library
+This is a powerful python image manipulation library.
+
+```python
+import cryno
+
+# example comin soon
+```
+
+# Installation
+```bash
+pip install cryno
+```
+
+# Documentation
+Coming soon. not finished yet and is in planning stage
---
+++ packages/cryno/cryno-0.0.0.tar.gz/cryno-0.0.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    readme = "\n" + fh.read()
+
+VERSION = '0.0.0'
+DESCRIPTION = 'A python image processing library'
+
+setup(
+    name="cryno",
+    version=VERSION,
+    author="Schkimansky",
+    author_email="<ahmadchawla1432@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=readme,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'image', 'processing', 'data', 'image data', 'image processing', 'image processor', 'image manipulation', 'editor', 'image editor'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
