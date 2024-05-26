f4539e6992d28ef89bb361ad30c0c6db85047fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 03:04:06 +0000

    Add package bot-managers/0.0.972/bot-managers-0.0.972.tar.gz

---
+++ packages/bot-managers/bot-managers-0.0.972.tar.gz/bot-managers-0.0.972/bot_managers/updater.py
@@ -0,0 +1,53 @@
+import asyncio
+from typing import Union
+
+from aio_rabbitmq import RabbitMQSettings
+from tg_logger import BaseLogger
+
+from .utils import (ShuttingDown, TGBotAbstractModel, configure_rabbitmq,
+                    safe_get_loop)
+
+
+async def aio_update_tgbot(
+        tgbot: TGBotAbstractModel,
+        rabbitmq_settings: RabbitMQSettings,
+        logger: BaseLogger,
+        timeout: int = 2,
+        expiration: int = 15,
+) -> None:
+    from .settings import NUMBER_OF_MANAGERS
+    loop = safe_get_loop()
+    rabbitmq = configure_rabbitmq(rabbitmq_settings, logger, loop)
+    channel = await rabbitmq.get_one_time_use_channel()
+    await rabbitmq.send_message(
+        f'{rabbitmq_settings.prefix}update_listening',
+        channel,
+        tgbot,
+        expiration=expiration,
+    )
+    logger.update_tgbot('info', tgbot, 'listening')
+    for number in range(NUMBER_OF_MANAGERS):
+        await asyncio.sleep(timeout)
+        msg = f'Set to cache for update {tgbot.tgbot_id=}, {number=}'
+        logger.update_tgbot('info', msg)
+        await rabbitmq.send_message(
+            f'{rabbitmq_settings.prefix}update_responsible_{number}',
+            channel,
+            tgbot,
+            expiration=expiration,
+        )
+
+
+def update_tgbot(
+        tgbot: Union[TGBotAbstractModel, ShuttingDown],
+        rabbitmq_settings: RabbitMQSettings,
+        logger: BaseLogger,
+        timeout: int = 2,
+        expiration: int = 15,
+):
+    loop = safe_get_loop()
+    loop.run_until_complete(
+        aio_update_tgbot(
+            tgbot, rabbitmq_settings, logger, timeout, expiration
+        )
+    )
