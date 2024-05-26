2ac8fdbc1b1c936d2332600ea7c3157f1f48be3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 03:04:09 +0000

    Add package rendercv/1.10/rendercv-1.10-py3-none-any.whl

---
+++ packages/rendercv/rendercv-1.10-py3-none-any.whl/rendercv/__init__.py
@@ -0,0 +1,8 @@
+"""RenderCV package.
+
+RenderCV is a $\\LaTeX$ CV/resume framework that generates a CV from a JSON/YAML input
+file. Its primary motivation is to allow the separation between a CV's content and
+design.
+"""
+
+__version__ = "1.10"
