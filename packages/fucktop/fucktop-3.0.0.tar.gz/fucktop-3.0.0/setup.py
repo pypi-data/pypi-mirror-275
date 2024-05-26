4c58606cc15f4a50111b8f8b6056072c7fb7373d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 03:04:07 +0000

    Add package fucktop/3.0.0/fucktop-3.0.0.tar.gz

---
+++ packages/fucktop/fucktop-3.0.0.tar.gz/fucktop-3.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='fucktop',
+    version='3.0.0',
+    description='一个免费而热搜工具(喜欢关注:python学霸微信公众号)',
+    author='Python学霸',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author_email='python@xueba.com',
+    py_modules=['fucktop'],
+    install_requires=['mechanicalsoup'],)
