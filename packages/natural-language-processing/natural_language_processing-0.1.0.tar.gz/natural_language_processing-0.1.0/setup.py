a0802573018b7745188d03a040304956fe9952e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 14:04:30 +0000

    Add package natural-language-processing/0.1.0/natural_language_processing-0.1.0.tar.gz

---
+++ packages/natural-language-processing/natural_language_processing-0.1.0.tar.gz/natural_language_processing-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    author= "Dear Norathee",
+    description="<add short describtion here>",
+    name="natural_language_processing",
+    version="0.1.0",
+    packages=find_packages(),
+    license="MIT",
+    install_requires=["xlwings"],
+
+    # example
+    # install_requires=['pandas>=1.0',
+    # 'scipy==1.1',
+    # 'matplotlib>=2.2.1,<3'],
+    
+
+)
