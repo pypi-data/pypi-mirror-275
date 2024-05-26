ac7a0193d9764f91b9266d54ffa2a3579cfc010e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 14:04:24 +0000

    Add package imagededup-cli/1.0.4/imagededup-cli-1.0.4.tar.gz

---
+++ packages/imagededup-cli/imagededup-cli-1.0.4.tar.gz/imagededup-cli-1.0.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="imagededup-cli",  # Package name
+    version="1.0.4",  # Initial version
+    author="Tadeas Fort",
+    author_email="taddy.fort@gmail.com",
+    description="A tool to remove duplicate jpg images",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tadeasf/imagededup-cli",  # Project URL
+    packages=find_packages(),
+    install_requires=[
+        "rich-click",
+        "tk",
+        "tqdm",
+        "loguru",
+        "imagededup",
+    ],
+    entry_points={
+        "console_scripts": [
+            "imagededup-cli = imagededup_cli.imagededup_cli:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
