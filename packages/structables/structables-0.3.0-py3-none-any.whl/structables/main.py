770623cf93524712d2e408077f313a61e14858d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 03:04:08 +0000

    Add package structables/0.3.0/structables-0.3.0-py3-none-any.whl

---
+++ packages/structables/structables-0.3.0-py3-none-any.whl/structables/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from flask import Flask
+
+from .config import Config
+from .routes import init_routes
+from .utils.data import update_data
+from .utils.helpers import get_typesense_api_key
+
+app = Flask(__name__, template_folder="templates", static_folder="static")
+app.config.from_object(Config)
+app.typesense_api_key = get_typesense_api_key()
+
+init_routes(app)
+
+if __name__ == "__main__":
+    app.run(port=app.config['PORT'], host=app.config['LISTEN_HOST'], debug=app.config['DEBUG'])
+
+# Initialize data when the server starts
+update_data(app)
