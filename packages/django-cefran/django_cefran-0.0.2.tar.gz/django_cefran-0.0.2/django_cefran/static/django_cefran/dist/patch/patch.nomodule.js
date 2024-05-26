a97f3a64e4afb75654336c2fca59e325b3fbe540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 14:04:24 +0000

    Add package django-cefran/0.0.2/django_cefran-0.0.2.tar.gz

---
+++ packages/django-cefran/django_cefran-0.0.2.tar.gz/django_cefran-0.0.2/django_cefran/static/django_cefran/dist/patch/patch.nomodule.js
@@ -0,0 +1,41 @@
+/*! CEFRAN v0.0.1 | SPDX-License-Identifier: MIT | License-Filename: LICENSE.md | restricted use (see terms and conditions) */
+
+(function () {
+  'use strict';
+
+  var config = {
+    prefix: 'cefran',
+    namespace: 'cefran',
+    version: '0.0.1'
+  };
+
+  var patch = {
+    namespace: 'a4e35ba2a938ba9d007689dbf3f46acbb9807869'
+  };
+
+  var executor = {};
+  var promise = new Promise(function (resolve, reject) {
+    executor.resolve = resolve;
+    executor.reject = reject;
+  });
+
+  window[patch.namespace] = {
+    configuration: window[config.namespace],
+    promise: promise
+  };
+
+  var patchInternals = function () {
+    var api = window[config.namespace];
+    if (!api || !api.internals) {
+      requestAnimationFrame(patchInternals);
+      return;
+    }
+    if (api.inspector.trace) { api.inspector.log = api.inspector.trace; }
+
+    executor.resolve();
+  };
+
+  patchInternals();
+
+})();
+//# sourceMappingURL=patch.nomodule.js.map
