cb522ed00c8bda173867d6d41a8d446d132dc8cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 14:04:20 +0000

    Add package pref-voting/1.3.8/pref_voting-1.3.8-py3-none-any.whl

---
+++ packages/pref-voting/pref_voting-1.3.8-py3-none-any.whl/pref_voting/voting_method_properties.py
@@ -0,0 +1,40 @@
+'''
+    File: voting_methods.py
+    Author: Wes Holliday (wesholliday@berkeley.edu) and Eric Pacuit (epacuit@umd.edu)
+    Date: November 6, 2021
+    Update: January 15, 2023
+    
+    The VotingMethodProperties class to encapsulate properties of voting methods.
+'''
+from enum import Enum
+
+class ElectionTypes(Enum): 
+    PROFILE = "Profile"
+    PROFILE_WITH_TIES = "ProfileWithTies"
+    TRUNCATED_LINEAR_PROFILE = "ProfileWithTies"
+    MAJORITY_GRAPH = "MajorityGraph"
+    MARGIN_GRAPH = "MarginGraph"
+
+class VotingMethodProperties:
+    """
+    Class to encapsulate properties of voting methods.
+
+    Attributes:
+        condorcet_consistent (bool): Indicates if the voting method always elects a Condorcet winner when one exists.
+    """
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def satisfied(self):
+        return [key for key, value in self.__dict__.items() if value is True]
+    
+    def violated(self):
+        return [key for key, value in self.__dict__.items() if value is False]
+
+    def __getitem__(self, prop):
+        return self.__dict__.get(prop, None)
+
+    def __str__(self):
+        properties = [f"{key}: {'Satisfied' if value is True else ('Violated' if value is False else 'N/A')}" for key, value in self.__dict__.items()]
+        return "\n".join(properties)
