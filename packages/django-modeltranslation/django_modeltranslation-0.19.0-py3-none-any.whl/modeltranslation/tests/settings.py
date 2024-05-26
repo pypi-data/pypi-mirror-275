07632196e5b895e13178cec40f03a501abcc60c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 14:04:25 +0000

    Add package django-modeltranslation/0.19.0/django_modeltranslation-0.19.0-py3-none-any.whl

---
+++ packages/django-modeltranslation/django_modeltranslation-0.19.0-py3-none-any.whl/modeltranslation/tests/settings.py
@@ -0,0 +1,61 @@
+import os
+import warnings
+
+warnings.simplefilter("always", DeprecationWarning)
+
+
+def _get_database_config():
+    db = os.getenv("DB", "sqlite")
+    host = os.getenv("DB_HOST", "localhost")
+    conf = {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+    if db == "mysql":
+        conf.update(
+            {
+                "ENGINE": "django.db.backends.mysql",
+                "NAME": os.getenv("MYSQL_DATABASE", "modeltranslation"),
+                "USER": os.getenv("MYSQL_USER", "root"),
+                "PASSWORD": os.getenv("MYSQL_PASSWORD", "password"),
+                "HOST": host,
+            }
+        )
+    elif db == "postgres":
+        conf.update(
+            {
+                "ENGINE": "django.db.backends.postgresql",
+                "USER": os.getenv("POSTGRES_USER", "postgres"),
+                "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
+                "NAME": os.getenv("POSTGRES_DB", "modeltranslation"),
+                "HOST": host,
+            }
+        )
+    return conf
+
+
+DATABASES = {"default": _get_database_config()}
+
+INSTALLED_APPS = (
+    "django.contrib.contenttypes",
+    "django.contrib.auth",
+    "modeltranslation",
+    "modeltranslation.tests",
+)
+
+LANGUAGES = (("de", "Deutsch"), ("en", "English"))
+LANGUAGE_CODE = "de"
+
+USE_I18N = True
+USE_TZ = False
+MIDDLEWARE_CLASSES = ()
+
+MODELTRANSLATION_DEFAULT_LANGUAGE = "de"
+MODELTRANSLATION_AUTO_POPULATE = False
+MODELTRANSLATION_FALLBACK_LANGUAGES = ()
+
+ROOT_URLCONF = "modeltranslation.tests.urls"
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
+TEST_NON_SERIALIZED_APPS = ("django.contrib.auth",)
