96819d8d1d0a936e992f243f6ce3c7c6fe8208ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 15:04:29 +0000

    Add package xssbase/1.0.7/xssbase-1.0.7.tar.gz

---
+++ packages/xssbase/xssbase-1.0.7.tar.gz/xssbase-1.0.7/xssbase/cli.py
@@ -0,0 +1,41 @@
+import argparse
+import os
+import sys
+import platform
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from .main import test_xss_payloads
+from .utils import download_chromedriver
+
+def main():
+    # Check if the operating system is Windows
+    if platform.system() != 'Windows':
+        print("This tool only supports Windows.")
+        sys.exit(1)
+
+    # Define the argument parser
+    parser = argparse.ArgumentParser(
+        description="XSSbase - A professional tool for scanning XSS vulnerabilities.",
+        epilog="Author: Fidal\nEmail: mrfidal@proton.me\nGitHub: https://github.com/mr-fidal\n\n"
+               "Copyright 2024 Fidal. All rights reserved. "
+               "Unauthorized copying of this tool, via any medium is strictly prohibited."
+    )
+    parser.add_argument('--url', required=True, help='URL to test for XSS vulnerability')
+    args = parser.parse_args()
+
+    # Download the chromedriver if it doesn't exist
+    download_chromedriver()
+
+    # Set up Chrome WebDriver
+    service = Service('chromedriver-win64/chromedriver.exe')  # Specify the path to chromedriver executable
+    driver = webdriver.Chrome(service=service)
+
+    try:
+        # Test XSS payloads
+        test_xss_payloads(driver, args.url)
+    finally:
+        # Close the browser
+        driver.quit()
+
+if __name__ == "__main__":
+    main()
