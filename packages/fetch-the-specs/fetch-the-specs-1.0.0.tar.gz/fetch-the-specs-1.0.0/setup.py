6bd27cca52af6466fe0c857f6084cd9501a4f78d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 04:03:47 +0000

    Add package fetch-the-specs/1.0.0/fetch-the-specs-1.0.0.tar.gz

---
+++ packages/fetch-the-specs/fetch-the-specs-1.0.0.tar.gz/fetch-the-specs-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fetch-the-specs',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'psutil',
+        'rich'
+    ],
+    entry_points={
+        'console_scripts': [
+            'fetch-the-specs=fetch_the_specs.sysinfo:main'
+        ]
+    },
+    author='Ibrahim Hasnat',
+    description='A command-line tool to report system information in a colorful format.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+    url='https://github.com/ibrahimhasnat/fetch-the-specs/'
+)
