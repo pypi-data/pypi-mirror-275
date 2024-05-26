dda5748a1c430746215ba365621dbe712dee5061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 14:04:24 +0000

    Add package s3-toolkit/0.1.0/s3_toolkit-0.1.0.tar.gz

---
+++ packages/s3-toolkit/s3_toolkit-0.1.0.tar.gz/s3_toolkit-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    author= "Dear Norathee",
+    description="package to help you manage file in AWS s3 storage",
+    name="s3_toolkit",
+    version="0.1.0",
+    packages=find_packages(),
+    license="MIT",
+    install_requires=[""],
+
+    # example
+    # install_requires=['pandas>=1.0',
+    # 'scipy==1.1',
+    # 'matplotlib>=2.2.1,<3'],
+    
+
+)
