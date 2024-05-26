dba15840d771aac971f94080ff1303565f575f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 15:04:33 +0000

    Add package my-cli-code-explorer/0.1.4/my_cli_code_explorer-0.1.4.tar.gz

---
+++ packages/my-cli-code-explorer/my_cli_code_explorer-0.1.4.tar.gz/my_cli_code_explorer-0.1.4/setup.py
@@ -0,0 +1,23 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="my_cli_code_explorer",  # Ensure your package name is consistent
+    version="0.1.4",
+    packages=find_packages(),
+    install_requires=[
+        "typer[all]",  # Install Typer with its dependencies
+    ],
+    entry_points={
+        "console_scripts": [
+            "my_cli_code_explorer=my_cli_code_explorer.main:app",  # Correct the entry point
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
