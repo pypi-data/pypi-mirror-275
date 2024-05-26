3ddcfa99bfc33f83c33eeed6e7e7c5aa0937f1df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 14:04:10 +0000

    Add package sravanthi/0.1.0/sravanthi-0.1.0.tar.gz

---
+++ packages/sravanthi/sravanthi-0.1.0.tar.gz/sravanthi-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='sravanthi',
+version='0.1.0',
+author='sravanthi',
+author_email='boomer@gmail.com',
+description='lekhivanthi',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
