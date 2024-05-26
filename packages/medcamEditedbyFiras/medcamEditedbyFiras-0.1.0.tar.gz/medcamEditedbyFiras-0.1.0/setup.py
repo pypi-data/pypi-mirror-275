194ccd624b06b853e774bbf2e158a19d32b77e5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 15:04:24 +0000

    Add package medcamEditedbyFiras/0.1.0/medcamEditedbyFiras-0.1.0.tar.gz

---
+++ packages/medcamEditedbyFiras/medcamEditedbyFiras-0.1.0.tar.gz/medcamEditedbyFiras-0.1.0/setup.py
@@ -0,0 +1,61 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+from setuptools.command.install import install
+class MyInstall(install):
+    def run(self):
+        import subprocess, sys
+        subprocess.call([sys.executable, "-m", "pip", "install", "opencv-python"])
+        install.run(self)
+
+setuptools.setup(
+    name="medcamEditedbyFiras",
+    version="0.1.0",
+    author="Karol Gotkowski (edited by Firas Darwish)",
+    author_email="KarolGotkowski@gmx.de (edited by fbd2014@nyu.edu)",
+    description="An easy to use library that makes model predictions more interpretable for humans.",
+    long_description="An easy to use library that makes model predictions more interpretable for humans. M3d-CAM allows the generation of attention maps with multiple methods like Guided Backpropagation, Grad-Cam, Guided Grad-Cam and Grad-Cam++.",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'certifi',
+        'cycler',
+        'decorator',
+        'future',
+        'imageio',
+        'kiwisolver',
+        'Mako',
+        'Markdown',
+        'MarkupSafe',
+        'matplotlib',
+        'networkx',
+        'nibabel',
+        'numpy',
+        'packaging',
+        'pandas',
+        'Pillow',
+        'pyparsing',
+        'python-dateutil',
+        'pytz',
+        'PyWavelets',
+        'scikit-image',
+        'scipy',
+        'SimpleITK',
+        'six',
+    ],
+    # extras_require={
+    #     'extras': PostInstallExtrasInstaller.extras_install_by_default,
+    # },
+    cmdclass={
+        'install': MyInstall,
+    },
+)
