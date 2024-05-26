0d334cc54a85f312a3ad45d03d51151093686e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 14:04:24 +0000

    Add package pypdfzipcracker/1.1/pypdfzipcracker-1.1.tar.gz

---
+++ packages/pypdfzipcracker/pypdfzipcracker-1.1.tar.gz/pypdfzipcracker-1.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+# Read the contents of README.md for the long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pypdfzipcracker',
+    version='1.1',  # Incremented version number
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'PyPDF2',
+        'pyzipper',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pdfzipcrack=pdfzipcracker.pdfzipcrack:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    description="A tool to crack encrypted PDF and ZIP files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Specify that README is in Markdown format
+)
