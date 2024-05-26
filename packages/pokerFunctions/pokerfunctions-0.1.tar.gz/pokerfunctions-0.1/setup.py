0f81f76b9cd7a7ef98b71af81483b82ffc320534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 14:04:24 +0000

    Add package pokerFunctions/0.1/pokerfunctions-0.1.tar.gz

---
+++ packages/pokerFunctions/pokerfunctions-0.1.tar.gz/pokerfunctions-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pokerFunctions',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['re', 'random', 'time', 'itertools', 'unittests' ],
+    include_package_data=True,
+    description='A package for poker functions',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Jonah Aden',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/pokerFunctions',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
