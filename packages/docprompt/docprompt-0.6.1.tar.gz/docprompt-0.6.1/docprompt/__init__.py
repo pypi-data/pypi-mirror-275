909d090bcc1e27abe68ed973e4aaefa818a753a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 14:04:38 +0000

    Add package docprompt/0.6.1/docprompt-0.6.1.tar.gz

---
+++ packages/docprompt/docprompt-0.6.1.tar.gz/docprompt-0.6.1/docprompt/__init__.py
@@ -0,0 +1,28 @@
+"""Top-level package for Docprompt."""
+
+__author__ = """Frankie Colson"""
+__email__ = "frank@pageleaf.io"
+__version__ = "0.6.1"
+
+from docprompt.schema.document import Document, PdfDocument  # noqa
+from docprompt.schema.layout import NormBBox, TextBlock  # noqa
+from docprompt.schema.pipeline import DocumentCollection, DocumentNode, PageNode  # noqa
+from docprompt.utils import load_document, load_documents, hash_from_bytes  # noqa
+from docprompt.rasterize import ProviderResizeRatios
+
+Document.model_rebuild()
+
+
+__all__ = [
+    "Document",
+    "PdfDocument",
+    "DocumentCollection",
+    "DocumentNode",
+    "NormBBox",
+    "PageNode",
+    "TextBlock",
+    "load_document",
+    "load_documents",
+    "hash_from_bytes",
+    "ProviderResizeRatios",
+]
