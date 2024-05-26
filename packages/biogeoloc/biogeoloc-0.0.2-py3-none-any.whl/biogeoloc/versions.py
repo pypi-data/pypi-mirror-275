92f17124dd034c46ce4f5ca9325703de839151b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 04:03:47 +0000

    Add package biogeoloc/0.0.2/biogeoloc-0.0.2-py3-none-any.whl

---
+++ packages/biogeoloc/biogeoloc-0.0.2-py3-none-any.whl/biogeoloc/versions.py
@@ -0,0 +1,17 @@
+def get_versions():
+    return versions[0]["number"]
+
+versions = [
+    {
+        "number": "0.0.2",
+        "features": [
+            "1. added merge function.",
+        ],
+    },
+    {
+        "number": "0.0.1",
+        "features": [
+            "1. initial version.",
+        ],
+    },
+]
