d4a808b0166ad798838f5f66356c613f835f7cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 03:04:15 +0000

    Add package pypi-shell/0.1.0/pypi_shell-0.1.0.tar.gz

---
+++ packages/pypi-shell/pypi_shell-0.1.0.tar.gz/pypi_shell-0.1.0/pypi_project/config.py
@@ -0,0 +1,8 @@
+import os
+
+PACKAGE_NAME = "pypi-shell"
+COMMAND_NAME = "pypi"
+PACKAGE_VERSION = '0.1.0'
+PACKAGE_PATH = os.path.dirname(__file__)
+DATA_PATH = os.path.join(PACKAGE_PATH, 'data')
+os.makedirs(DATA_PATH, exist_ok=True)
