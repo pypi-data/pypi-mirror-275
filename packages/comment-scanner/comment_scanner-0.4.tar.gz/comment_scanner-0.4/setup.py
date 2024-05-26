c5a6c05da9f4d5fbf9b5efbcb33dc3e943d2b66f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 03:04:02 +0000

    Add package comment-scanner/0.4/comment_scanner-0.4.tar.gz

---
+++ packages/comment-scanner/comment_scanner-0.4.tar.gz/comment_scanner-0.4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as readme_file:
+        return readme_file.read()
+
+
+setup(
+    name='comment_scanner',
+    version='0.4',
+    install_requires=[
+        'python-magic-bin>=0.4.14',
+    ],
+    description='Parse comments from various source code files',
+    author='Faiz Alam',
+    author_email='mohdfaizalam53@gmail.com',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    packages=['comment_scanner', 'comment_scanner.parsers'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Documentation',
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: OS Independent"
+    ],
+    url='https://github.com/FaizAlam/comment-scanner',
+    python_requires='>=3.7',
+    zip_safe=False,
+    entry_points={
+        "console_scripts": [
+            "comment_scanner = comment_scanner.comment_scanner:main",
+        ],
+    },
+
+)
