bd1eabb80402ac93651206cc82d49fa50353d317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 03:04:04 +0000

    Add package hybridkf/1.0.42/hybridkf-1.0.42.tar.gz

---
+++ packages/hybridkf/hybridkf-1.0.42.tar.gz/hybridkf-1.0.42/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hybridkf',
+    version='1.0.42',
+    packages=find_packages(),
+    description='''An advanced implementation of the Kalman filter that handles and processes (pragmatic) continuous time-model state observations with discrete time-measurements for state estimation.'''
+    ,
+    author='akain0',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/akain0/hybridkf',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
