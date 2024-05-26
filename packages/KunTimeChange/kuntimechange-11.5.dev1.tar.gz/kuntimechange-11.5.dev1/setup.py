1f1a3a0e5e3c7ed65bd1e098327bd0388c458a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 03:04:05 +0000

    Add package KunTimeChange/11.5.dev1/kuntimechange-11.5.dev1.tar.gz

---
+++ packages/KunTimeChange/kuntimechange-11.5.dev1.tar.gz/kuntimechange-11.5.dev1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='KunTimeChange',
+    version='11.5.dev1',
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+    author='W0518',
+    author_email='2647346053@qq.com',
+    description='It will not exit after run it.',
+    license='MIT',
+    keywords='sample setuptools development',
+    url='https://github.com/ziyin-wang/ikuntime'
+)
