0217423e2b0604988d0527d87ca554a9c1503ce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 03:04:09 +0000

    Add package templinktool/1.0.0/templinktool-1.0.0.tar.gz

---
+++ packages/templinktool/templinktool-1.0.0.tar.gz/templinktool-1.0.0/setup.py
@@ -0,0 +1,27 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='templinktool',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Flask',
+        'pyngrok',
+        'werkzeug',
+    ],
+    entry_points={
+        'console_scripts': [
+            'linkm=main:main',      # linkm-start commandmmand
+        ],
+    },
+    author='sdidd',
+    author_email='sdiddtayade@gmail.com',
+    description='Creates a temporary server to share files',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/sdidd/templinkMaker.git',
+    license='MIT',
+)
