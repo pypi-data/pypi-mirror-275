0c7685b1816cead80185c157228bba0968e963b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 14:04:09 +0000

    Add package elevenlabs-bulk/0.1.0/elevenlabs_bulk-0.1.0.tar.gz

---
+++ packages/elevenlabs-bulk/elevenlabs_bulk-0.1.0.tar.gz/elevenlabs_bulk-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    author= "Dear Norathee",
+    description="create multiple audio files at once",
+    name="elevenlabs_bulk",
+    version="0.1.0",
+    packages=find_packages(),
+    license="MIT",
+    install_requires=["pandas",
+        "os_toolkit>=0.1.1",
+        "py_string_tool>=0.1.3",
+        "python_wizard>=0.1.1"],
+
+    # example
+    # install_requires=['pandas>=1.0',
+    # 'scipy==1.1',
+    # 'matplotlib>=2.2.1,<3'],
+    
+
+)
