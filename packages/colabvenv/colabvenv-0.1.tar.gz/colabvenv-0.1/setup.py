1b5fa2555dba90b87c5b60369ded9823d3052213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 04:04:59 +0000

    Add package colabvenv/0.1/colabvenv-0.1.tar.gz

---
+++ packages/colabvenv/colabvenv-0.1.tar.gz/colabvenv-0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='colabvenv',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={},
+    author='Athanasios Delis, Stylianos Kandylakis',
+    author_email='athanasiosdelis@gmail.com, stelkcand@gmail.com',
+    description='A package that creates python 3.8 virtual environments on Google  Colab and Jupyter Notebooks in Linux OS or WSL Systems.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/athasdelis/colabvenv',
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'Operating System :: Microsoft :: Windows :: Windows 11',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+    ],
+    python_requires='>=3.6',
+)
