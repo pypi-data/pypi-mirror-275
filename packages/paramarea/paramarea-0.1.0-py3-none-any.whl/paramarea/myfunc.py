ccf5c474feaf816340fc1d98cddee9a0cc1309c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 03:04:01 +0000

    Add package paramarea/0.1.0/paramarea-0.1.0-py3-none-any.whl

---
+++ packages/paramarea/paramarea-0.1.0-py3-none-any.whl/paramarea/myfunc.py
@@ -0,0 +1,14 @@
+import math
+
+def circumcircle( radius : float ) -> float :
+    return math.pi * 2 * radius 
+
+def areacircle( radius:float ) -> float :
+    return math.pi * ( radius ** 2 )
+
+def perirect( length:float , breadth:float )-> float :
+    return 2 * ( length + breadth )
+
+def arearect ( length:float , breadth :float  )-> float :
+    return length * breadth
+
