911649f3de53a2f5271092d9108c912dc3ad6dcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 14:04:34 +0000

    Add package data-manipulation/0.40/data_manipulation-0.40.tar.gz

---
+++ packages/data-manipulation/data_manipulation-0.40.tar.gz/data_manipulation-0.40/data_manipulation/mysql_connector_python_.py
@@ -0,0 +1,96 @@
+import logging
+from typing import Optional, Union
+
+import mysql.connector
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__file__)
+
+
+def create_connection(
+    host: str,
+    dbname: str,
+    user: str,
+    password: str,
+    port=3306,
+) -> mysql.connector.connection_cext.CMySQLConnection:
+    """
+    Return MySQL connection
+
+    https://github.com/mysql/mysql-connector-python
+
+    Parameters
+    ----------
+    host : str
+        Database host address
+    dbname : str
+        Database name
+    user : str
+        User used to authenticate
+    password : str
+        Password used to authenticate
+    port : int, optional
+        Connection port number, by default 3306
+
+    Returns
+    -------
+    mysql.connector.connection_cext.CMySQLConnection
+        Connection object
+    """
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host,
+            database=dbname,
+            user=user,
+            password=password,
+            port=port,
+        )
+        logger.info("MySQL database connected ...")
+    except mysql.connector.errors.Error as e:
+        logger.error(f"{e}")
+    return connection
+
+
+def execute_query(
+    connection: mysql.connector.connection_cext.CMySQLConnection,
+    sql_query: str,
+    data: Union[dict, tuple],
+    commit=True,
+) -> Optional[int]:
+    """
+    Execute and commit MySQL query
+
+    Parameters
+    ----------
+    connection : mysql.connector.connection_cext.CMySQLConnection
+        mysql connection class
+    sql_query : str
+        SQL query
+    data : Union[dict, tuple]
+        _description_
+    commit : bool, optional
+        Make database change persistent, by default True
+
+    Returns
+    -------
+    Optional[int]
+        Query id should be int
+    """
+    cursor = connection.cursor()
+    try:
+        cursor.execute(sql_query, data)
+        if commit:
+            connection.commit()
+            logger.info("MySQL committed ...")
+            id = cursor.lastrowid
+            cursor.close()
+            return id
+    except mysql.connector.errors.Error as e:
+        logger.error(f"{e}")
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
