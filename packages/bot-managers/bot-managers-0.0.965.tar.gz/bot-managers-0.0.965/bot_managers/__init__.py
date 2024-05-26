a726af78f13a3122f4cc75cfb64011e1ac006b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-26 03:04:06 +0000

    Add package bot-managers/0.0.965/bot-managers-0.0.965.tar.gz

---
+++ packages/bot-managers/bot-managers-0.0.965.tar.gz/bot-managers-0.0.965/bot_managers/__init__.py
@@ -0,0 +1,7 @@
+from .bots_manager import (ListeningTGBotManager, ResponsibleTGBotManager,
+                           TGBotManager)
+from .client_manager import TelethonClientManager
+from .telethon import PatchedTelegramClient, TelethonErrorHandler
+from .updater import aio_update_tgbot, update_tgbot
+from .utils import AsyncDict, AsyncList, ShuttingDown
+from .voices import VoiceTranscription
---
+++ packages/bot-managers/bot-managers-0.0.965.tar.gz/bot-managers-0.0.965/bot_managers/updater.py
@@ -0,0 +1,49 @@
+import asyncio
+from typing import Union
+
+from aio_rabbitmq import RabbitMQSettings
+from tg_logger import BaseLogger
+
+from .utils import (ShuttingDown, TGBotAbstractModel, configure_rabbitmq,
+                    safe_get_loop)
+
+
+async def aio_update_tgbot(
+        tgbot: TGBotAbstractModel,
+        rabbitmq_settings: RabbitMQSettings,
+        logger: BaseLogger,
+) -> None:
+    from .settings import NUMBER_OF_MANAGERS
+    loop = safe_get_loop()
+    rabbitmq = configure_rabbitmq(rabbitmq_settings, logger, loop)
+    channel = await rabbitmq.get_one_time_use_channel()
+    await rabbitmq.send_message(
+        f'{rabbitmq_settings.prefix}update_ListeningTGBotManager',
+        channel,
+        tgbot,
+        expiration=15,
+    )
+    logger.update_tgbot('info', tgbot, 'listening')
+    for number in range(NUMBER_OF_MANAGERS):
+        await asyncio.sleep(3)
+        logger.update_tgbot('info', tgbot, number)
+        await rabbitmq.send_message(
+            f'{rabbitmq_settings.prefix}update_ResponsibleTGBotManager'
+            f'_{number}',
+            channel,
+            tgbot,
+            expiration=15,
+        )
+
+
+def update_tgbot(
+        tgbot: Union[TGBotAbstractModel, ShuttingDown],
+        rabbitmq_settings: RabbitMQSettings,
+        logger: BaseLogger
+):
+    loop = safe_get_loop()
+    loop.run_until_complete(
+        aio_update_tgbot(
+            tgbot, rabbitmq_settings, logger
+        )
+    )
