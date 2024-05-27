5dd9cccc8ca0df6bcc96f037d3304666cb8f051e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 14:05:48 +0000

    Add package tpdk/2.0.204/tpdk-2.0.204-py3-none-any.whl

---
+++ packages/tpdk/tpdk-2.0.204-py3-none-any.whl/tpdk/api/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+# import apis into api package
+from tpdk.api.branding_api import BrandingApi
+from tpdk.api.notification_api import NotificationApi
+from tpdk.api.organization_api import OrganizationApi
+from tpdk.api.persona_api import PersonaApi
+from tpdk.api.resolution_center_api import ResolutionCenterApi
+from tpdk.api.user_api import UserApi
+from tpdk.api.webhook_api import WebhookApi
+
