eb13dc2e11bb9d6de00a91c955de02da4fe6864d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 03:04:34 +0000

    Add package movoid-xml-data-pyside/1.0.0/movoid_xml_data_pyside-1.0.0-py3-none-any.whl

---
+++ packages/movoid-xml-data-pyside/movoid_xml_data_pyside-1.0.0-py3-none-any.whl/movoid_xml_data_pyside/lib/__init__.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : __init__.py
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/5/24 2:04
+# Description   : 
+"""
+from .app import MainApp
