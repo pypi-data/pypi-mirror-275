0ac67b362cda53d36528ac9f92227336c7b8c62d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 15:04:19 +0000

    Add package mia-vgg/0.0.10/mia_vgg-0.0.10-py3-none-any.whl

---
+++ packages/mia-vgg/mia_vgg-0.0.10-py3-none-any.whl/mia_vgg/mia.py
@@ -0,0 +1,13 @@
+from sklearn.ensemble import RandomForestClassifier
+from pathlib import Path
+import pickle
+
+def mia():
+    path = Path("data", "mia.pickle")
+    with open
+    clf = RandomForestClassifier()
+    clf.fit(
+
+if __name__=="__main__":
+    pass
+
