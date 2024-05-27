d6f382d2db7e79af4360e6fa821f6ddb78671326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 15:04:31 +0000

    Add package common-test-ceair/0.0.2/common_test_ceair-0.0.2.tar.gz

---
+++ packages/common-test-ceair/common_test_ceair-0.0.2.tar.gz/common_test_ceair-0.0.2/setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='common_test_ceair',
+    version="0.0.2",
+    author="runhua.shi",
+    install_requires=[
+        'requests'
+    ],
+)
