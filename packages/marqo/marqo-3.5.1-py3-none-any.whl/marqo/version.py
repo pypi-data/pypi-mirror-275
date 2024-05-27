520d4da6792b3b5347f780780a34262b9ee9421c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 14:05:59 +0000

    Add package marqo/3.5.1/marqo-3.5.1-py3-none-any.whl

---
+++ packages/marqo/marqo-3.5.1-py3-none-any.whl/marqo/version.py
@@ -0,0 +1,14 @@
+__marqo_version__ = "2.7.0"
+__marqo_release_page__ = f"https://github.com/marqo-ai/marqo/releases/tag/{__marqo_version__}"
+
+__minimum_supported_marqo_version__ = "2.6.0"
+
+
+def supported_marqo_version() -> str:
+    return f"This Marqo Python client is built for Marqo release ({__marqo_version__}) \n" \
+           f"{__marqo_release_page__} \n" \
+           f"The minimum supported Marqo version for this client is ({__minimum_supported_marqo_version__}) \n"
+
+
+def minimum_supported_marqo_version() -> str:
+    return __minimum_supported_marqo_version__
