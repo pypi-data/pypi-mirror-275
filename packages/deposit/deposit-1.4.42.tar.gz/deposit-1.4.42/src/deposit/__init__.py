16389dce912824865249a43554c458e28ca30089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 04:05:23 +0000

    Add package deposit/1.4.42/deposit-1.4.42.tar.gz

---
+++ packages/deposit/deposit-1.4.42.tar.gz/deposit-1.4.42/src/deposit/__init__.py
@@ -0,0 +1,12 @@
+version_info = (1, 4, 42)
+
+__version__ = '.'.join(map(str, version_info))
+__title__ = 'Deposit'
+__date__ = "26.5.2024"
+
+from deposit.store.store import Store
+
+from deposit.store.abstract_dtype import AbstractDType
+from deposit.store.ddatetime import DDateTime
+from deposit.store.dgeometry import DGeometry
+from deposit.store.dresource import DResource
