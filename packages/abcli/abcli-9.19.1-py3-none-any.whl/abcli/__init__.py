99a59c63406db65f9fa0a50ff2f7a458fc542d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 04:04:58 +0000

    Add package abcli/9.19.1/abcli-9.19.1-py3-none-any.whl

---
+++ packages/abcli/abcli-9.19.1-py3-none-any.whl/abcli/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+NAME = "abcli"
+
+VERSION = "9.19.1"
+
+DESCRIPTION = "🚀 a language to speak AI."
+
+
+def fullname() -> str:
+    abcli_git_branch = os.getenv("abcli_git_branch", "")
+    return "abcli-{}{}".format(
+        VERSION,
+        f"-{abcli_git_branch}" if abcli_git_branch else "",
+    )
