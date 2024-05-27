f11229ee41383bb021a311416ead839b31288575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 15:04:17 +0000

    Add package NetHyTech-DeepTTS/1.0/NetHyTech-DeepTTS-1.0.tar.gz

---
+++ packages/NetHyTech-DeepTTS/NetHyTech-DeepTTS-1.0.tar.gz/NetHyTech-DeepTTS-1.0/NetHyTech_DeepTTS/__init__.py
@@ -0,0 +1,38 @@
+import playsound
+import requests
+import os
+
+# Voices
+
+
+a = "aura-asteria-en" # Sophia (Female US English
+b = "aura-luna-en" # Emily (Female US English) 
+c = "aura-stella-en" # Rachel (Female US English)
+d = "aura-athena-en" # Eliza (Female UK English)
+e = "aura-hera-en" # Pam  (Female US English) 
+f = "aura-orion-en" # Kevin  (Male US English) 
+g = "aura-arcas-en" # Jeff (Male US English) 
+h = "aura-perseus-en" # Alex (Male US English) 
+i = "aura-angus-en" # Rory (Male Irish English) 
+j = "aura-orpheus-en" # John (Male US English) 
+k = "aura-helios-en" # Pete (Male UK English) 
+l = "aura-zeus-en" # James (Male US English) 
+
+
+def speak(text, model):
+        filename :str="data.mp3"
+        url = "https://api.deepai.org/speech_response"
+
+        payload = {
+            "model": model,
+            "text": text
+        }
+        response = requests.post(url, json=payload)
+        if response.status_code != 200: return f"Error: {response.status_code} - {response.text}"
+        else:
+            with open(filename, 'wb') as f:
+                f.write(response.content)
+            playsound.playsound(filename)
+            os.remove(filename)
+
+
