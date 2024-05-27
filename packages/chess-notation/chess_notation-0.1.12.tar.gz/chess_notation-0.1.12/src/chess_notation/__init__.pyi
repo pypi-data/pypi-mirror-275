70337de64f971b9b024301483c226336535113c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 16:04:28 +0000

    Add package chess-notation/0.1.12/chess_notation-0.1.12.tar.gz

---
+++ packages/chess-notation/chess_notation-0.1.12.tar.gz/chess_notation-0.1.12/src/chess_notation/__init__.pyi
@@ -0,0 +1,12 @@
+from .language import Language, LANGUAGES, translate
+from .styles import Styles, PawnCapture, PieceCapture, CapturedPiece, Castle, Check, Mate, style
+from .represent import representations, KingEffectStyles, MotionStyles, all_representations
+from .notation import Notation, random_notation, styled
+from . import language, styles, represent
+
+__all__ = [
+  'Language', 'LANGUAGES', 'Notation', 'random_notation', 'style', 'translate',
+  'Styles', 'PawnCapture', 'PieceCapture', 'CapturedPiece', 'Castle', 'Check', 'Mate',
+  'representations', 'KingEffectStyles', 'MotionStyles', 'all_representations',
+  'language', 'styles', 'represent', 'styled',
+]
