40319df14df6da25e446eefe42664ecc72d3a9da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 15:04:59 +0000

    Add package bharatocr-client/0.3.0/bharatocr_client-0.3.0.tar.gz

---
+++ packages/bharatocr-client/bharatocr_client-0.3.0.tar.gz/bharatocr_client-0.3.0/bharatocrclient/__init__.py
@@ -0,0 +1,28 @@
+# author:    Kunal Kumar Kushwaha
+# website:   http://www.essentia.dev
+
+# import the required packages
+from bharatocrclient.clients.api import (
+    pan,
+    aadhaar,
+    login,
+    register,
+    driving_licence,
+    passport,
+    voterid,
+    water_bill,
+    vehicle_registration,
+)
+
+
+__all__ = [
+    "pan",
+    "aadhaar",
+    "login",
+    "register",
+    "driving_licence",
+    "passport",
+    "voterid",
+    "water_bill",
+    "vehicle_registration",
+]
