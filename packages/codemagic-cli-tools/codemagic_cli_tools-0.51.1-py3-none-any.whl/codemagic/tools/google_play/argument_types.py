0ac8e70ea4c4b1ec1052a264fd4d9b5b13915501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 15:04:20 +0000

    Add package codemagic-cli-tools/0.51.1/codemagic_cli_tools-0.51.1-py3-none-any.whl

---
+++ packages/codemagic-cli-tools/codemagic_cli_tools-0.51.1-py3-none-any.whl/codemagic/tools/google_play/argument_types.py
@@ -0,0 +1,16 @@
+import json
+
+from codemagic import cli
+
+
+class CredentialsArgument(cli.EnvironmentArgumentValue[str]):
+    environment_variable_key = "GCLOUD_SERVICE_ACCOUNT_CREDENTIALS"
+
+    @classmethod
+    def _is_valid(cls, value: str) -> bool:
+        try:
+            json_content = json.loads(value)
+        except json.decoder.JSONDecodeError:
+            return False
+        else:
+            return json_content.get("type") == "service_account"
