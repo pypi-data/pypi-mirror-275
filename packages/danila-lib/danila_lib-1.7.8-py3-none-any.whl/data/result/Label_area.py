6a185120034c12a7d7c345ad62a7c7a279277a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 15:05:00 +0000

    Add package danila-lib/1.7.8/danila_lib-1.7.8-py3-none-any.whl

---
+++ packages/danila-lib/danila_lib-1.7.8-py3-none-any.whl/data/result/Label_area.py
@@ -0,0 +1,14 @@
+from data.result.Class_text import Class_text
+
+
+class Label_area:
+    def __init__(self):
+        self.labels = dict(
+            {
+                        Class_text.number : '',
+                        Class_text.prod : '',
+                        Class_text.year : ''
+            }
+        )
+
+
---
+++ packages/danila-lib/danila_lib-1.7.8-py3-none-any.whl/data/result/Rama_prod.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Rama_Prod(Enum):
+    bejickaya = 0
+    ruzhimmash = 1
+    no_rama = 2
