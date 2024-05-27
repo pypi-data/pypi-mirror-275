fb1975e628fefdc6cbfff7d05ad0e332a808d3b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 03:04:51 +0000

    Add package movoid-xml-data/1.1.1/movoid_xml_data-1.1.1.tar.gz

---
+++ packages/movoid-xml-data/movoid_xml_data-1.1.1.tar.gz/movoid_xml_data-1.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='movoid_xml_data',
+    version='1.1.1',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='movoid',
+    author_email='bobrobotsun@163.com',
+    description='',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "lxml",
+        "cssselect"
+    ],
+)
