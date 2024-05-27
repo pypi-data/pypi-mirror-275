310d78c7f10fd4f0b6e0c1e4bed78cc651fe1323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 15:04:54 +0000

    Add package superb-ai-curate/1.3.1/superb_ai_curate-1.3.1.tar.gz

---
+++ packages/superb-ai-curate/superb_ai_curate-1.3.1.tar.gz/superb_ai_curate-1.3.1/spb_curate/curate/api/enums.py
@@ -0,0 +1,44 @@
+from enum import Enum
+
+
+class IouType(str, Enum):
+    """
+    Types of IoU.
+    """
+
+    BOX = "bbox"
+    POLYGON = "segm"
+
+
+class JobType(Enum):
+    """
+    Available types of a job.
+    """
+
+    ANNOTATION_IMPORT = "ANNOTATION_IMPORT"
+    DELETE_IMAGES = "DELETE_IMAGES"
+    IMAGE_IMPORT = "IMAGE_IMPORT"
+    IMPORT_PREDICTIONS = "IMPORT_PREDICTIONS"
+    UPDATE_SLICE = "UPDATE_SLICE"
+    UPDATE_SLICE_BY_QUERY = "UPDATE_SLICE_BY_QUERY"
+
+    def __str__(self):
+        return self.value
+
+
+class Split(str, Enum):
+    """
+    Types of Split.
+    """
+
+    TRAIN = "TRAIN"
+    VAL = "VAL"
+
+
+class SearchFieldMappingType(str, Enum):
+    ANNOTATION_CLASS = "annotations.class_count"
+    ANNOTATION_METADATA = "annotations.metadata"
+    IMAGE_METADATA = "images.metadata"
+
+    def __str__(self):
+        return self.value
