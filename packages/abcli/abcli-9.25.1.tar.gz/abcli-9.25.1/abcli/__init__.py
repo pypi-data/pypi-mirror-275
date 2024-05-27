e2a11ea98596ce313a313ab498cfd586dec711bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 04:05:47 +0000

    Add package abcli/9.25.1/abcli-9.25.1.tar.gz

---
+++ packages/abcli/abcli-9.25.1.tar.gz/abcli-9.25.1/abcli/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+NAME = "abcli"
+
+VERSION = "9.25.1"
+
+DESCRIPTION = "🚀 a language to speak AI."
+
+REPO_NAME = "awesome-bash-cli"
+
+
+def fullname() -> str:
+    abcli_git_branch = os.getenv("abcli_git_branch", "")
+    return "abcli-{}{}".format(
+        VERSION,
+        f"-{abcli_git_branch}" if abcli_git_branch else "",
+    )
