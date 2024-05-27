7263ec55171f849c3f83b5c143f21a259ba7fb6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 04:09:24 +0000

    Add package inline-snapshot/0.10.1/inline_snapshot-0.10.1.tar.gz

---
+++ packages/inline-snapshot/inline_snapshot-0.10.1.tar.gz/inline_snapshot-0.10.1/inline_snapshot/__init__.py
@@ -0,0 +1,7 @@
+from ._external import external
+from ._external import outsource
+from ._inline_snapshot import snapshot
+
+__all__ = ["snapshot", "external", "outsource"]
+
+__version__ = "0.10.1"
