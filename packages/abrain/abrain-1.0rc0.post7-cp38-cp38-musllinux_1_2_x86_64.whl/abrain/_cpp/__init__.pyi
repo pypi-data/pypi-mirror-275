ac6c3cc68533af162222dec80edfe3a87c58adab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 04:05:40 +0000

    Add package abrain/1.0rc0.post7/abrain-1.0rc0.post7-cp38-cp38-musllinux_1_2_x86_64.whl

---
+++ packages/abrain/abrain-1.0rc0.post7-cp38-cp38-musllinux_1_2_x86_64.whl/abrain/_cpp/__init__.pyi
@@ -0,0 +1,10 @@
+"""
+Docstring main module
+"""
+from __future__ import annotations
+from . import config
+from . import genotype
+from . import misc
+from . import phenotype
+__all__ = ['config', 'genotype', 'misc', 'phenotype']
+__version__: str = '1.0rc0.post7'
