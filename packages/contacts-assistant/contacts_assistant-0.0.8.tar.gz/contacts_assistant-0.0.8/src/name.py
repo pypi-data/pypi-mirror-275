e3267cb3dd3eb9ffe2290e5ba0d431c39f8713cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 03:04:36 +0000

    Add package contacts-assistant/0.0.8/contacts_assistant-0.0.8.tar.gz

---
+++ packages/contacts-assistant/contacts_assistant-0.0.8.tar.gz/contacts_assistant-0.0.8/src/name.py
@@ -0,0 +1,22 @@
+from src.field import Field
+
+class Name(Field):
+    """
+    A class to represent and validate a name.
+
+    Inherits from Field to provide a consistent interface for different types of fields.
+
+    Attributes:
+        value (str): The validated name.
+
+    Methods:
+        __init__(name): Initializes the Name with a given name string.
+    """
+    def __init__(self, name):
+        """
+        Initializes the Name instance with the given name string.
+
+        Args:
+            name (str): The name string to be assigned to the field.
+        """
+        self.value = name
