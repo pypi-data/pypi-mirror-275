1a76e2c74fc53a63e144cabf5043f6f66b95c224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 03:04:41 +0000

    Add package contacts-assistant/0.0.11/contacts_assistant-0.0.11.tar.gz

---
+++ packages/contacts-assistant/contacts_assistant-0.0.11.tar.gz/contacts_assistant-0.0.11/src/contacts_assistant/constants.py
@@ -0,0 +1,21 @@
+DATE_FORMAT = "%d.%m.%Y"
+CONTACTS_BOOK_FILENAME = "./contacts_book.pkl"
+NOTEBOOK_FILENAME = "./notebook.json"
+MENU_BORDER = f"{'-'*72}\n"
+DEMO_CONTACTS = 10
+GREETING_BANNER = """
+  ___          _     _              _     _           _   
+ / _ \        (_)   | |            | |   | |         | |  
+/ /_\ \___ ___ _ ___| |_ __ _ _ __ | |_  | |__   ___ | |_ 
+|  _  / __/ __| / __| __/ _` | '_ \| __| | '_ \ / _ \| __|
+| | | \__ \__ \ \__ \ || (_| | | | | |_  | |_) | (_) | |_ 
+\_| |_/___/___/_|___/\__\__,_|_| |_|\__| |_.__/ \___/ \__|
+"""
+INPUT_STYLE = {
+    "completion-menu.completion": "bg:#008888 #ffffff",
+    "completion-menu.completion.current": "bg:#00aaaa #000000",
+    "scrollbar.background": "bg:#88aaaa",
+    "scrollbar.button": "bg:#222222",
+    "prompt": "#47C959",
+    "prompt.arg.text": "#00aaaa",
+}
