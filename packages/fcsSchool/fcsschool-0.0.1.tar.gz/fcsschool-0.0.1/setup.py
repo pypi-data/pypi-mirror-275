6a4fc873e121191f676513756d5a3e571d53bab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 03:04:37 +0000

    Add package fcsSchool/0.0.1/fcsschool-0.0.1.tar.gz

---
+++ packages/fcsSchool/fcsschool-0.0.1.tar.gz/fcsschool-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='fcsSchool',
+    version='0.0.1',
+    description='A very basic description',
+    long_description=open('README.txt').read(),
+    url='',  # Replace with a valid URL or remove this line if not available
+    author='A K Ghosh',
+    author_email='anantafcs@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='FutureCampusSchool',
+    packages=find_packages(),
+    install_requires=[]  # Update with actual dependencies or leave as an empty list
+)
+
