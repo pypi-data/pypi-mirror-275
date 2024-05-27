d807fac30687f93fbeee8a3d07d2778eea1405e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 03:04:30 +0000

    Add package bgutils-hddly/1.1.68/bgutils_hddly-1.1.68-py3-none-any.whl

---
+++ packages/bgutils-hddly/bgutils_hddly-1.1.68-py3-none-any.whl/bgutils/spider/SpiderData.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from bgutils.spider.BaseSpider import BaseSpider
+
+
+class SpiderData(BaseSpider):
+    id: str
+    title: str
+    url: str
+    price: str
+    pic: str
+    time_sort: str
+
+    def __init__(self, username, collector, rawurl, rawdata):
+        self.username = username
+        self.collector= collector
+        self.topic = "goods_data"
+        self.rawurl= rawurl
+        self.rawdata = rawdata
+        self.coll_time = datetime.now()
