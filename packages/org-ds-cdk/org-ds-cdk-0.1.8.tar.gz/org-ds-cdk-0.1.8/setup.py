07e8803f49a943845f9c85839159259b110ad29b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 15:05:06 +0000

    Add package org-ds-cdk/0.1.8/org-ds-cdk-0.1.8.tar.gz

---
+++ packages/org-ds-cdk/org-ds-cdk-0.1.8.tar.gz/org-ds-cdk-0.1.8/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='org-ds-cdk',
+    version='0.1.08',
+    description='DS Organization CDK Constructs',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages('../cdk_main'),
+    package_dir={'': '../cdk_main'},
+    install_requires=[
+        'aws-cdk-lib==2.142.0',
+        'constructs>=10.0.0'
+    ],
+)
