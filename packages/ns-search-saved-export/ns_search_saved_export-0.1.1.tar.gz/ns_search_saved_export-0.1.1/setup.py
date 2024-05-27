7dc61bb7b3e191e8b27f42fd40b430453a8fdbbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 03:04:31 +0000

    Add package ns-search-saved-export/0.1.1/ns_search_saved_export-0.1.1.tar.gz

---
+++ packages/ns-search-saved-export/ns_search_saved_export-0.1.1.tar.gz/ns_search_saved_export-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ns_search_saved_export',
+    version='0.1.1',
+    description='A library for interacting with NetSuite API and exporting data',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Gildder',
+    author_email='gildder@outlook.com',
+    url='https://github.com/gildder/netsuite-search-saved-export',
+    packages=find_packages(),
+    install_requires=[
+        'oauth2',
+        'requests',
+        'pandas'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
