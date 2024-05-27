14e646dee91c3c4724d36bf908d9371da8d37b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 05:04:51 +0000

    Add package almax-common/0.8.2/almax_common-0.8.2.tar.gz

---
+++ packages/almax-common/almax_common-0.8.2.tar.gz/almax_common-0.8.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name='almax_common',
+    version='0.8.2',
+    description='A common library with some of my implementations',
+    long_description='test',
+    long_description_content_type='text/markdown',
+    author='AlMax98',
+    author_email='alihaider.maqsood@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'tkinter',
+        'logging',
+        'subprocess',
+        'reportlab',
+        'threading',
+        'datetime',
+        'os',
+        'sys'
+    ],
+);
