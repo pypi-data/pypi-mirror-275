0d7fd33476ef600429e25dc7b0f9357b26147d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 04:04:54 +0000

    Add package open-webui/0.2.0.dev2/open_webui-0.2.0.dev2-py3-none-any.whl

---
+++ packages/open-webui/open_webui-0.2.0.dev2-py3-none-any.whl/open_webui/frontend/_app/immutable/chunks/navigation.01f37cd6.js
@@ -0,0 +1,2 @@
+import{h as o}from"./singletons.6ad9a8f3.js";const e=o("goto");export{e as g};
+//# sourceMappingURL=navigation.01f37cd6.js.map
---
+++ packages/open-webui/open_webui-0.2.0.dev2-py3-none-any.whl/open_webui/frontend/_app/immutable/chunks/paths.973fc9e2.js
@@ -0,0 +1,2 @@
+var s;const a=((s=globalThis.__sveltekit_yeu1v7)==null?void 0:s.base)??"";var e;const t=((e=globalThis.__sveltekit_yeu1v7)==null?void 0:e.assets)??a;export{t as a,a as b};
+//# sourceMappingURL=paths.973fc9e2.js.map
