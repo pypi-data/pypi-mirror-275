74eec474a2c9a1a85d8274ea6295738ef73a34f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 15:04:19 +0000

    Add package Peelpreter/1.1.2/peelpreter-1.1.2.tar.gz

---
+++ packages/Peelpreter/peelpreter-1.1.2.tar.gz/peelpreter-1.1.2/src/peelpreter/objectt/enviroment.py
@@ -0,0 +1,36 @@
+########################################################################################
+#    Peelpreter is a interpreter designed to interpret the language, Monkey.
+#    Copyright (C) 2024 Jeebak Samajdwar
+#
+#    This file is part of Peelpreter
+#
+#    Peelpreter is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    any later version.
+#
+#    Peelpreter is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+########################################################################################
+
+from .objectt import Object
+
+class Enviroment:
+    def __init__(self, outer) -> None:
+        self.store: dict[str, Object] = dict()
+        self.outer = outer
+    def get(self, name):
+        obj = self.store.get(name)
+        if obj is None and self.outer is not None:
+            obj = self.outer.get(name)
+        return obj
+    def set_iden(self, name, value):
+        self.store[name] = value
+        return value
+    def __repr__(self) -> str:
+        return repr(self.store)
