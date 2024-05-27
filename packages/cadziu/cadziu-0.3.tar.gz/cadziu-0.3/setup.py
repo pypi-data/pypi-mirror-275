cceec302c4d9f46a5dbc416da2961efd8891c3de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 03:04:35 +0000

    Add package cadziu/0.3/cadziu-0.3.tar.gz

---
+++ packages/cadziu/cadziu-0.3.tar.gz/cadziu-0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cadziu',
+    version='0.3',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A tool to manage Caddy reverse proxy configurations with Cloudflare DNS.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/cadziu',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'requests',
+        'python-dotenv'
+    ],
+    entry_points={
+        'console_scripts': [
+            'cadziu=app.main:main',
+        ],
+    },
+)
