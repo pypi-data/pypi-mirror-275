20494d113d3af0cd160393b01ea481ae7c92409e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 16:04:32 +0000

    Add package polars-qt/0.1.16/polars_qt-0.1.16.tar.gz

---
+++ packages/polars-qt/polars_qt-0.1.16.tar.gz/polars_qt-0.1.16/src/funcs/rolling_rank.rs
@@ -0,0 +1,41 @@
+use polars::prelude::*;
+use pyo3_polars::derive::polars_expr;
+use serde::Deserialize;
+use tea_strategy::tevec::prelude::*;
+
+#[derive(Deserialize)]
+struct TsRankKwargs {
+    window: usize,
+    min_periods: Option<usize>,
+    pct: bool,
+    rev: bool,
+}
+
+#[polars_expr(output_type=Float64)]
+fn rolling_rank(inputs: &[Series], kwargs: TsRankKwargs) -> PolarsResult<Series> {
+    let s = &inputs[0];
+    let name = s.name();
+    let out: Float64Chunked = match s.dtype() {
+        DataType::Int32 => {
+            s.i32()?
+                .ts_vrank(kwargs.window, kwargs.min_periods, kwargs.pct, kwargs.rev)
+        }
+        DataType::Int64 => {
+            s.i64()?
+                .ts_vrank(kwargs.window, kwargs.min_periods, kwargs.pct, kwargs.rev)
+        }
+        DataType::Float32 => {
+            s.f32()?
+                .ts_vrank(kwargs.window, kwargs.min_periods, kwargs.pct, kwargs.rev)
+        }
+        DataType::Float64 => {
+            s.f64()?
+                .ts_vrank(kwargs.window, kwargs.min_periods, kwargs.pct, kwargs.rev)
+        }
+        dtype => {
+            polars_bail!(InvalidOperation: "dtype {dtype} not \
+            supported for rolling_rank, expected Int32, Int64, Float32, Float64.")
+        }
+    };
+    Ok(out.with_name(name).into_series())
+}
