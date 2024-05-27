ebc3228cce898fad1842ba41184080533371f677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 03:04:36 +0000

    Add package freetar/0.7.0/freetar-0.7.0-py3-none-any.whl

---
+++ packages/freetar/freetar-0.7.0-py3-none-any.whl/freetar/backend.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request
+from flask_minify import Minify
+
+from freetar.ug import ug_search, ug_tab
+import waitress
+
+app = Flask(__name__)
+Minify(app=app, html=True, js=True, cssless=True)
+
+@app.route("/")
+def index():
+    return render_template("index.html",
+                           title="Freetar",
+                           favs=True)
+
+
+
+@app.route("/search")
+def search():
+    search_term = request.args.get("search_term")
+    if search_term:
+        search_results = ug_search(search_term)
+    else:
+        search_results = []
+    return render_template("index.html",
+                           search_term=search_term,
+                           title=f"Freetar - Search: {search_term}",
+                           search_results=search_results,)
+
+
+@app.route("/tab/<artist>/<song>")
+def show_tab(artist: str, song: str):
+    tab = ug_tab(f"{artist}/{song}")
+    return render_template("tab.html",
+                           tab=tab,
+                           title=f"{tab.artist_name} - {tab.song_name}")
+
+
+@app.route("/tab/<tabid>")
+def show_tab2(tabid: int):
+    tab = ug_tab(tabid)
+    return render_template("tab.html",
+                           tab=tab,
+                           title=f"{tab.artist_name} - {tab.song_name}")
+
+
+@app.route("/favs")
+def show_favs():
+    return render_template("index.html",
+                           title="Freetar - Favorites",
+                           favs=True)
+
+
+def main():
+    host = "0.0.0.0"
+    port = 22000
+    if __name__ == '__main__':
+        app.run(debug=True,
+                host=host,
+                port=port)
+    else:
+        print(f"Running backend on {host}:{port}")
+        waitress.serve(app, listen=f"{host}:{port}")
+
+
+if __name__ == '__main__':
+    main()
