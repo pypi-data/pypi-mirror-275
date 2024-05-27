5466dba7bced021a6a375f7ecbe27a7ab643830c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 15:04:11 +0000

    Add package bharatocr-client/0.3.1/bharatocr_client-0.3.1.tar.gz

---
+++ packages/bharatocr-client/bharatocr_client-0.3.1.tar.gz/bharatocr_client-0.3.1/bharatocrclient/clients/api.py
@@ -0,0 +1,8 @@
+from bharatocrclient.clients.pan import pan
+from bharatocrclient.clients.aadhaar import aadhaar
+from bharatocrclient.clients.driving_licence import driving_licence
+from bharatocrclient.clients.passport import passport
+from bharatocrclient.clients.auth import login, register
+from bharatocrclient.clients.voter_id import voterid
+from bharatocrclient.clients.water_bill import water_bill
+from bharatocrclient.clients.vehicle_registration import vehicle_registration
