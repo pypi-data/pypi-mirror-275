25d44356cc1f11b08f9f2d8e9c5a42cc31014571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 15:04:19 +0000

    Add package wlkata-mirobot-zuto/1.0.0/wlkata_mirobot_zuto-1.0.0.tar.gz

---
+++ packages/wlkata-mirobot-zuto/wlkata_mirobot_zuto-1.0.0.tar.gz/wlkata_mirobot_zuto-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import  setup
+import setuptools
+packages = ['wlkata_mirobot_zuto']
+setup(name='wlkata_mirobot_zuto',
+	version='1.0.0',
+	author='zuto',
+    packages=packages, 
+    package_dir={'requests': 'requests'},)
