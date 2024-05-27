cd2bff15ee45bec798283ce858f139e483728351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 14:05:58 +0000

    Add package cloudquicklabs-kg/0.0.4/cloudquicklabs_kg-0.0.4.tar.gz

---
+++ packages/cloudquicklabs-kg/cloudquicklabs_kg-0.0.4.tar.gz/cloudquicklabs_kg-0.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cloudquicklabs_kg",
+    version="0.0.4",
+    author="Rekhu Chinnarathod",
+    author_email="vrchinnarathod@gmail.com",
+    url="https://www.youtube.com/channel/UCv9MUffHWyo2GgLIDLVu0KQ",
+    description="An application that informs you of the time in different locations and timezones",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["click", "pytz"],
+    entry_points={"console_scripts": ["cloudquicklabs1 = src.main:main"]},
+)
