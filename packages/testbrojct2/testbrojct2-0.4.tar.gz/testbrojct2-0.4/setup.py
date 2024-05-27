2c675c500dd3ba9b3ee78df1caf45bf18a092c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 03:04:40 +0000

    Add package testbrojct2/0.4/testbrojct2-0.4.tar.gz

---
+++ packages/testbrojct2/testbrojct2-0.4.tar.gz/testbrojct2-0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testbrojct2',
+    version='0.4',
+    author='،NiK',
+    description='Sexy liday',
+    install_requires=[
+        'requests',
+    ],
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
