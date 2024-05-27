bbec9ce7b0198a6a7921e9ee9940c1a92ecc787d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 15:04:18 +0000

    Add package Python-EasyGraph/1.3/Python_EasyGraph-1.3-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/Python-EasyGraph/Python_EasyGraph-1.3-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/easygraph/classes/__init__.py
@@ -0,0 +1,19 @@
+try:
+    from .base import BaseHypergraph
+    from .base import load_structure
+    from .directed_graph import DiGraph
+    from .directed_graph import DiGraphC
+    from .directed_multigraph import MultiDiGraph
+    from .graph import Graph
+    from .graph import GraphC
+    from .graphviews import *
+    from .hypergraph import Hypergraph
+    from .multigraph import MultiGraph
+    from .operation import *
+
+
+except:
+    print(
+        "Warning raise in module:classes. Please install torch before you use"
+        " functions related to Hypergraph"
+    )
