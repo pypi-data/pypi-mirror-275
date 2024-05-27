8cd2ca539f3502cb08bb666b0db2be581339420c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 14:05:24 +0000

    Add package quest-decoding/1.0.5/quest-decoding-1.0.5.tar.gz

---
+++ packages/quest-decoding/quest-decoding-1.0.5.tar.gz/quest-decoding-1.0.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fr:
+    installation_requirements = fr.readlines()
+
+setuptools.setup(
+    name="quest-decoding",
+    version="1.0.5",
+    author="Goncalo Faria, Sweta Agrawal.",
+    author_email="goncalofaria.research@gmail.com, swetaagrawal20@gmail.com",
+    description="A package for sampling from intractable distributions with LLMs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/goncalo-faria/quest-decoding",
+    packages=setuptools.find_packages(),
+    install_requires=installation_requirements,
+    python_requires=">=3.10.13",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
