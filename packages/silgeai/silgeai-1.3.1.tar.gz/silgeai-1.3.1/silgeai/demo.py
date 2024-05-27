ae5299a7e339d381da011adb99db15af4ca50fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 14:05:31 +0000

    Add package silgeai/1.3.1/silgeai-1.3.1.tar.gz

---
+++ packages/silgeai/silgeai-1.3.1.tar.gz/silgeai-1.3.1/silgeai/demo.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+"""
+@Author: 风吹落叶
+@Contact: waitKey1@outlook.com
+@Version: 1.0
+@Date: 2024/5/15 15:57
+@Describe: 
+"""
+from folderAnalysis import FileAnalysis
+dirpath=r'I:\Downloads\MuseTalk3\MuseTalk1\musetalk'
+FileAnalysis(dirpath)
