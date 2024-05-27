aace47417f382c6567ae5fe52640708e828db2cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 16:04:29 +0000

    Add package imio.smartweb.core/1.2.48/imio.smartweb.core-1.2.48-py3-none-any.whl

---
+++ packages/imio.smartweb.core/imio.smartweb.core-1.2.48-py3-none-any.whl/imio/smartweb/core/contents/rest/base.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+from imio.smartweb.core.utils import get_json
+from plone import api
+from plone.dexterity.content import Container
+from plone.rest import Service
+from zope.interface import implementer
+from zope.interface import Interface
+
+import json
+
+
+class BaseEndpoint(object):
+    language = "fr"
+    remote_endpoint = ""
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+        results = get_json(self.query_url, timeout=20)
+        return results
+
+    @property
+    def query_url(self):
+        raise NotImplementedError
+
+    def convert_cached_image_scales(
+        self,
+        item,
+        modified_hash,
+        field="image",
+        scales=["vignette", "affiche"],
+        orientation="paysage",
+    ):
+        """Remove image from result dict and add generated image scales URLs
+        with cache key"""
+        for scale in scales:
+            scale_name = "_".join(filter(None, [orientation, scale]))
+            cached_scale_url = (
+                f"{item['@id']}/@@images/{field}/{scale_name}?cache_key={modified_hash}"
+            )
+            item[f"{field}_{scale}_scale"] = cached_scale_url
+        item[f"{field}_full_scale"] = (
+            f"{item['@id']}/@@images/{field}/?cache_key={modified_hash}"
+        )
+        del item[field]
+
+    def get_extra_params(self, params):
+        form = self.request.form
+        extra_params = []
+        for k, v in form.items():
+            if isinstance(v, list):
+                # handles multiple metadata_fields
+                for value in v:
+                    extra_params.append("{}={}".format(k, value))
+            else:
+                extra_params.append("{}={}".format(k, v))
+        current_lang = api.portal.get_current_language()[:2]
+        if current_lang != "fr":
+            extra_params.append("translated_in_{}=1".format(current_lang))
+        params = params + extra_params
+        return params
+
+
+class BaseService(Service):
+    def render(self):
+        response = self.request.response
+        response.setHeader("Content-type", "application/json")
+        content = self.reply()
+        return json.dumps(
+            content,
+            indent=2,
+            separators=(", ", ": "),
+        )
+
+
+class IRestView(Interface):
+    """"""
+
+
+@implementer(IRestView)
+class RestView(Container):
+    """Shared base class for REST views contents"""
---
+++ packages/imio.smartweb.core/imio.smartweb.core-1.2.48-py3-none-any.whl/imio/smartweb/core/webcomponents/src/components/News/News.jsx
@@ -0,0 +1,205 @@
+import React, { useEffect, useState, createContext, useContex } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Filters from "./Filters/Filter";
+import NewsContent from "./NewsContent/NewsContent";
+import NewsList from "./NewsList/NewsList";
+import useAxios from "../../hooks/useAxios";
+import "./News.scss";
+import "../Filters/MainFilter.scss";
+import useFilterQuery from "../../hooks/useFilterQuery";
+import { Provider, Translate } from "react-translated";
+import translation from "../../utils/translation";
+import queryString from "query-string";
+
+export const LanguageContext = createContext("fr");
+export default function News(props) {
+    return (
+        <LanguageContext.Provider value={props.currentLanguage}>
+            <BrowserRouter basename={props.viewPath}>
+                <Provider language={props.currentLanguage} translation={translation}>
+                    <NewsView
+                        queryFilterUrl={props.queryFilterUrl}
+                        queryUrl={props.queryUrl}
+                        proposeUrl={props.proposeUrl}
+                        batchSize={props.batchSize}
+                        showCategoriesOrTopics={props.showCategoriesOrTopics}
+                    />
+                </Provider>
+            </BrowserRouter>
+        </LanguageContext.Provider>
+    );
+}
+const NewsView = (props) => {
+    const { u, ...parsed } = Object.assign(
+        { b_start: 0, fullobjects: 1 },
+        queryString.parse(useFilterQuery().toString())
+    );
+    const [itemsArray, setItemsArray] = useState([]);
+    const [itemsNumber, setItemsNumber] = useState([]);
+    const [clickId, setClickId] = useState(null);
+    const [filters, setFilters] = useState(parsed);
+    const [batchStart, setBatchStart] = useState(0);
+    const [loadMoreLaunch, setLoadMoreLaunch] = useState(false);
+    const { response, error, isLoading, isMore } = useAxios(
+        {
+            method: "get",
+            url: "",
+            baseURL: props.queryUrl,
+            headers: {
+                Accept: "application/json",
+            },
+            params: filters,
+            load: loadMoreLaunch,
+        },
+        []
+    );
+
+    // set all news in state
+    useEffect(() => {
+        if (response !== null) {
+            if (isMore) {
+                setItemsArray((itemsArray) => [...itemsArray, ...response.items]);
+            } else {
+                setItemsArray(response.items);
+            }
+            setItemsNumber(response.items_total);
+        }
+    }, [response]);
+
+    // set state id when clicked on list element
+    const clickID = (id) => {
+        setClickId(id);
+    };
+
+    // set state filters when active filter selection
+    const filtersChange = (value) => {
+        setLoadMoreLaunch(false);
+        setBatchStart((batchStart) => 0);
+        setFilters(value);
+    };
+
+    // set batch
+    const loadMore = () => {
+        setBatchStart((batchStart) => batchStart + parseInt(props.batchSize));
+        setLoadMoreLaunch(true);
+    };
+    // Update filters Batch
+    useEffect(() => {
+        setFilters((prevFilters) => {
+            return {
+                ...prevFilters,
+                b_start: batchStart,
+            };
+        });
+    }, [batchStart]);
+    // coditional list render
+    let listRender;
+    if (itemsArray && itemsArray.length > 0) {
+        listRender = (
+            <NewsList
+                onChange={clickID}
+                itemsArray={itemsArray}
+                showCategoriesOrTopics={props.showCategoriesOrTopics}
+            />
+        );
+    } else if (!isLoading) {
+        listRender = (
+            <p>
+                <Translate text="Aucune actualité n'a été trouvée" />
+            </p>
+        );
+    }
+
+    const divLoader = (
+        <div className="lds-roller-container">
+            <div className="lds-roller">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+
+    return (
+        <div>
+            <div className="r-wrapper r-actu-wrapper">
+                <div className="r-result r-annuaire-result">
+                    <Routes>
+                        <Route
+                            exact
+                            path="/"
+                            element={
+                                <>
+                                    <div className="r-result-filter actu-result-filter">
+                                        <Filters
+                                            url={props.queryFilterUrl}
+                                            activeFilter={filters}
+                                            onChange={filtersChange}
+                                        />
+                                        {props.proposeUrl && (
+                                            <div className="r-add-news">
+                                                <a target="_blank" href={props.proposeUrl}>
+                                                    <Translate text="Proposer une actualité" />
+                                                </a>
+                                            </div>
+                                        )}
+                                    </div>
+                                    {itemsNumber > 0 ? (
+                                        <p className="r-results-numbers">
+                                            <span>{itemsNumber}</span>{" "}
+                                            {itemsNumber > 1 ? (
+                                                <Translate text="Actualités trouvées" />
+                                            ) : (
+                                                <Translate text="Actualité trouvée" />
+                                            )}
+                                        </p>
+                                    ) : (
+                                        <p className="r-results-numbers">
+                                            <Translate text="Aucun résultat" />
+                                        </p>
+                                    )}
+                                    <div>{listRender}</div>
+                                    <div className="r-load-more">
+                                        {itemsNumber - props.batchSize > batchStart ? (
+                                            <div>
+                                                <span className="no-more-result">
+                                                    {isLoading ? divLoader : ""}
+                                                </span>
+                                                <button onClick={loadMore} className="btn-grad">
+                                                    {isLoading ? (
+                                                        <Translate text="Chargement..." />
+                                                    ) : (
+                                                        <Translate text="Plus de résultats" />
+                                                    )}
+                                                </button>
+                                            </div>
+                                        ) : (
+                                            <span className="no-more-result">
+                                                {isLoading ? divLoader : ""}
+                                            </span>
+                                        )}
+                                    </div>
+                                </>
+                            }
+                        ></Route>
+                        <Route
+                            path={"/:name"}
+                            element={
+                                <NewsContent
+                                    onChange={clickID}
+                                    onReturn={filtersChange}
+                                    queryUrl={props.queryUrl}
+                                />
+                            }
+                        ></Route>
+                    </Routes>
+                </div>
+            </div>
+        </div>
+    );
+};
