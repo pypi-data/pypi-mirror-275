8a79db482b61b7be6d19c8da8723692484205f3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 14:06:00 +0000

    Add package tablemaster/1.2.3/tablemaster-1.2.3.tar.gz

---
+++ packages/tablemaster/tablemaster-1.2.3.tar.gz/tablemaster-1.2.3/tablemaster/__init__.py
@@ -0,0 +1,39 @@
+
+from yaml import load, dump
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+import json
+from types import SimpleNamespace
+import pandas as pd
+
+
+with open('cfg.yaml') as cfg:
+    cfg = json.loads(json.dumps(load(cfg, Loader=Loader)), object_hook=lambda d: SimpleNamespace(**d))
+
+from . import utils
+
+from .mysql import (
+    query,
+    opt,
+    ManageTable,
+    Manage_table,
+)
+
+from .gspread import (
+    gs_read_df,
+    gs_write_df,
+)
+
+from .local import (
+    read,
+    batch_read,
+    read_dfs,
+)
+
+from .feishu import (
+    fs_read_df,
+    fs_read_base,
+)
+
