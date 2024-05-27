48e9cf2dfce2533c9c4deeb823d518eea4ae01bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 15:04:24 +0000

    Add package uiautodev/0.3.7/uiautodev-0.3.7-py3-none-any.whl

---
+++ packages/uiautodev/uiautodev-0.3.7-py3-none-any.whl/uiautodev/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Created on Mon Mar 04 2024 14:28:53 by codeskyblue
+"""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("uiautodev")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
