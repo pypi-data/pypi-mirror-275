e4d404be3e3dc31295e3926be06631a6a12dc324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 03:04:31 +0000

    Add package pyerualjetwork/1.2.2/pyerualjetwork-1.2.2.tar.gz

---
+++ packages/pyerualjetwork/pyerualjetwork-1.2.2.tar.gz/pyerualjetwork-1.2.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "pyerualjetwork",
+      version = "1.2.2",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "Advanced python deep learning library.UPDATED TO 1.2.2 NEW FEATURES: GetWeights() func for TrainPLAN, TestPLAN and LoadPLAN, GetPreds() and GetAcc() funcs for TrainPLAN and TestPLAN, GetAllFromLoad() func for LoadPLAN, GetDf() for LoadPLAN (Documentation in desc. Examples in GİTHUB: HCB06)",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'pruning learning artficial neural networks']
+      
+      )
