c1b6fb43d7c226d72c4d654405cfe8e2af003935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 14:05:04 +0000

    Add package xray-disease-detector/0.1.1/xray_disease_detector-0.1.1.tar.gz

---
+++ packages/xray-disease-detector/xray_disease_detector-0.1.1.tar.gz/xray_disease_detector-0.1.1/setup.py
@@ -0,0 +1,39 @@
+# setup.py
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xray_disease_detector",
+    version="0.1.1",
+    author="Gong Zhan",
+    author_email="gongzhan34@yahoo.co.jp",
+    description="A package for detecting diseases in X-ray images using a pre-trained PyTorch model",
+    long_description=long_description,      
+    long_description_content_type="text/markdown",
+    url="https://github.com/KONMIO34/xray_disease_detector",
+    project_urls={
+        "Bug Tracker": "https://github.com/KONMIO34/xray_disease_detector/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    py_modules=["xray_disease_detector"],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=[
+        "torch",
+        "torchvision",
+        "Pillow"
+    ],
+    entry_points={
+        "console_scripts": [
+            "xray_disease_detector=xray_disease_detector:main",
+        ],
+    },
+)
