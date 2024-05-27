4aa0e68791b3124a66b263e51441e9bb1a41c812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 03:04:52 +0000

    Add package testbrojct2/0.1/testbrojct2-0.1.tar.gz

---
+++ packages/testbrojct2/testbrojct2-0.1.tar.gz/testbrojct2-0.1/code/__init__.py
@@ -0,0 +1,7 @@
+def qq(a ,b):
+    return a * b
+def qqq(a ,b):
+    return a - b
+def qqqq(a ,b):
+    return a / b
+
---
+++ packages/testbrojct2/testbrojct2-0.1.tar.gz/testbrojct2-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testbrojct2',
+    version='0.1',
+    author='you',
+    description='i love',
+    install_requires=[
+        "requests"
+    ],
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ],
+)
