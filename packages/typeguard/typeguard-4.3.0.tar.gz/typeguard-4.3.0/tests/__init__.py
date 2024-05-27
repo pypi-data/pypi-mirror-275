3eeb44d20745c1c2dda938368355438f7c8ffed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 16:04:29 +0000

    Add package typeguard/4.3.0/typeguard-4.3.0.tar.gz

---
+++ packages/typeguard/typeguard-4.3.0.tar.gz/typeguard-4.3.0/tests/__init__.py
@@ -0,0 +1,59 @@
+from typing import (
+    AbstractSet,
+    Collection,
+    Dict,
+    Generic,
+    List,
+    NamedTuple,
+    NewType,
+    Protocol,
+    TypeVar,
+    Union,
+    runtime_checkable,
+)
+
+T_Foo = TypeVar("T_Foo")
+
+TBound = TypeVar("TBound", bound="Parent")
+TConstrained = TypeVar("TConstrained", "Parent", int)
+TTypingConstrained = TypeVar("TTypingConstrained", List[int], AbstractSet[str])
+TIntStr = TypeVar("TIntStr", int, str)
+TIntCollection = TypeVar("TIntCollection", int, Collection[int])
+TParent = TypeVar("TParent", bound="Parent")
+TChild = TypeVar("TChild", bound="Child")
+
+
+class Employee(NamedTuple):
+    name: str
+    id: int
+
+
+JSONType = Union[str, float, bool, None, List["JSONType"], Dict[str, "JSONType"]]
+myint = NewType("myint", int)
+mylist = NewType("mylist", List[int])
+
+
+class FooGeneric(Generic[T_Foo]):
+    pass
+
+
+class Parent:
+    pass
+
+
+class Child(Parent):
+    def method(self, a: int) -> None:
+        pass
+
+
+class StaticProtocol(Protocol):
+    member: int
+
+    def meth(self, x: str) -> None: ...
+
+
+@runtime_checkable
+class RuntimeProtocol(Protocol):
+    member: int
+
+    def meth(self, x: str) -> None: ...
