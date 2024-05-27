4b4d45e809e216b77920edcbb3ee7f18acc28a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 03:04:39 +0000

    Add package ar/0.9/ar-0.9.tar.gz

---
+++ packages/ar/ar-0.9.tar.gz/ar-0.9/ar/tests/test_linux.py
@@ -0,0 +1,58 @@
+# pylint: disable=redefined-outer-name
+import subprocess
+from pathlib import Path
+
+import pytest
+
+from ar import Archive, ArchiveError
+
+
+ARCHIVE = Path('test_data/linux.a')
+
+
+def test_list():
+    with ARCHIVE.open('rb') as f:
+        archive = Archive(f)
+        assert ['file0.txt', 'file1.bin'] == [entry.name for entry in archive]
+
+
+def test_read_content():
+    with ARCHIVE.open('rb') as f:
+        archive = Archive(f)
+        file0 = archive.open('file0.txt')
+        assert file0.read(1) == 'H'
+        assert file0.read() == 'ello'
+
+
+def test_read_binary():
+    with ARCHIVE.open('rb') as f:
+        archive = Archive(f)
+        file0 = archive.open('file1.bin', 'rb')
+        assert file0.read() == b'\xc3\x28'
+
+
+def test_seek_basic():
+    with ARCHIVE.open('rb') as f:
+        archive = Archive(f)
+        file0 = archive.open('file0.txt')
+        file0.seek(1)
+        assert file0.read(3) == 'ell'
+
+
+def test_tell():
+    with ARCHIVE.open('rb') as f:
+        archive = Archive(f)
+        file0 = archive.open('file0.txt')
+        assert file0.tell() == 0
+        file0.read(2)
+        assert file0.tell() == 2
+        file0.read()
+        assert file0.tell() == 5
+
+
+def test_open_missing_path():
+    with ARCHIVE.open('rb') as f:
+        archive = Archive(f)
+        with pytest.raises(ArchiveError) as exception_info:
+            archive.open('missing')
+        assert str(exception_info.value) == "No such entry: missing"
---
+++ packages/ar/ar-0.9.tar.gz/ar-0.9/ar/tests/test_roundtrip.py
@@ -0,0 +1,50 @@
+# pylint: disable=redefined-outer-name
+import subprocess
+from pathlib import Path
+
+import pytest
+
+from ar import Archive, ArchiveError
+
+
+TEST_DATA = Path('test_data/')
+
+
+@pytest.fixture
+def simple_archive():
+    # Create archive
+    TEST_DATA.mkdir(exist_ok=True)
+
+    (TEST_DATA / 'file0.txt').write_text('Hello')
+    (TEST_DATA / 'file1.bin').write_bytes(b'\xc3\x28')  # invalid utf-8 characters
+    subprocess.check_call('ar r test.a file0.txt file1.bin'.split(), cwd=str(TEST_DATA))
+    return TEST_DATA / 'test.a'
+
+
+def test_list(simple_archive):
+    with simple_archive.open('rb') as f:
+        archive = Archive(f)
+        assert ['file0.txt', 'file1.bin'] == [entry.name for entry in archive]
+
+
+def test_read_content(simple_archive):
+    with simple_archive.open('rb') as f:
+        archive = Archive(f)
+        file0 = archive.open('file0.txt')
+        assert file0.read(1) == 'H'
+        assert file0.read() == 'ello'
+
+
+def test_read_binary(simple_archive):
+    with simple_archive.open('rb') as f:
+        archive = Archive(f)
+        file0 = archive.open('file1.bin', 'rb')
+        assert file0.read() == b'\xc3\x28'
+
+
+def test_seek_basic(simple_archive):
+    with simple_archive.open('rb') as f:
+        archive = Archive(f)
+        file0 = archive.open('file0.txt')
+        file0.seek(1)
+        assert file0.read(3) == 'ell'
