52a77ca011b35048fa439ef4cb0d1ce542b5a790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 04:05:12 +0000

    Add package pianosdk/16.188.1/pianosdk-16.188.1-py3-none-any.whl

---
+++ packages/pianosdk/pianosdk-16.188.1-py3-none-any.whl/pianosdk/id/models/social_additional_input_request.py
@@ -0,0 +1,16 @@
+from datetime import date, datetime
+from pydantic.main import BaseModel
+from typing import Optional
+from pianosdk.id.models.consent_model import ConsentModel
+from pianosdk.id.models.custom_field_value_dto import CustomFieldValueDto
+from typing import List
+
+
+class SocialAdditionalInputRequest(BaseModel):
+    additional_input_state: Optional[str] = None
+    consents: Optional['List[ConsentModel]'] = None
+    custom_field_values: Optional['List[CustomFieldValueDto]'] = None
+    email: Optional[str] = None
+
+
+SocialAdditionalInputRequest.model_rebuild()
