e23a324c024fda560e3f1c90d0549601a3b0338c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 04:05:25 +0000

    Add package kamangir/3.74.1/kamangir-3.74.1.tar.gz

---
+++ packages/kamangir/kamangir-3.74.1.tar.gz/kamangir-3.74.1/setup.py
@@ -0,0 +1,15 @@
+from kamangir import NAME, VERSION, DESCRIPTION, REPO_NAME
+from blueness.pypi import setup
+
+
+setup(
+    filename=__file__,
+    repo_name=REPO_NAME,
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=[NAME],
+    package_data={
+        NAME: ["config.env"],
+    },
+)
