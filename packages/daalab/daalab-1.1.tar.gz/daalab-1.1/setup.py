d93a33b1c7988a0ad7cc194831f585a6342f5a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 14:04:57 +0000

    Add package daalab/1.1/daalab-1.1.tar.gz

---
+++ packages/daalab/daalab-1.1.tar.gz/daalab-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='daalab',
+    version='1.1',
+    packages=find_packages(),
+    install_requires=[], 
+    author='Imad',
+    author_email='mdimad005@gmail.com',
+    description='A Python library for all daa lab questions',
+)
