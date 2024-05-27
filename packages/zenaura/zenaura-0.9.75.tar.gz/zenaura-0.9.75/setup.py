535a5328bdada61bf658a350790e357530c97c9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 04:04:38 +0000

    Add package zenaura/0.9.75/zenaura-0.9.75.tar.gz

---
+++ packages/zenaura/zenaura-0.9.75.tar.gz/zenaura-0.9.75/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='zenaura',
+    version='0.9.75',
+    description="Zenaura is an experimental Python library built upon PyScript, designed to empower Python developers to create stateful, component-based Single Page Applications (SPAs). By leveraging a virtual DOM implementation, Zenaura optimizes the performance, reactivity, responsiveness, and interactivity of web applications. This allows developers to build high-performance, dynamic web applications using familiar Python concepts and syntax.",
+    author="Ahmed Rakan",
+    author_email="ar.aldhafeeri11@gmail.com",
+    package_dir={"": "src"},
+    packages=["zenaura"],
+    install_requires=[
+        'bleach'
+    ],
+    test_suite='tests',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
