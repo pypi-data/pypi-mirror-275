dab5bf6b4c8ed71fca70189c92fa899860a5dd65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 04:05:49 +0000

    Add package z2m-log-parser/0.0.8/z2m_log_parser-0.0.8.tar.gz

---
+++ packages/z2m-log-parser/z2m_log_parser-0.0.8.tar.gz/z2m_log_parser-0.0.8/src/z2m_log_parser/z2m_log_parser.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+import json
+import os
+
+class MqttMessage(object):
+    def __init__(self):
+        self.topic = str
+        self.payload = json
+
+class LogEntryData(object):
+    def __init__(self):
+        self.is_mqtt_publish = bool
+        self.message = str
+        self.mqtt_message = MqttMessage()
+
+class LogEntry(object):
+    def __init__(self):
+        self.type = str
+        self.date = datetime
+        self.data = LogEntryData()
+
+def parse_logs(path):
+    parsed_lines = []
+    with open(path) as log:
+        for line in log:
+            parsed_line= LogEntry()
+            parsed_line.data.is_mqtt_publish
+            try:
+                parsed_line.type = line[0:4]
+                parsed_line.date = datetime.strptime(line[6:25], '%Y-%m-%d %H:%M:%S')
+            except:
+                try:
+                    parsed_lines[-1].data.message = parsed_lines[-1].data.message + line
+                except Exception:
+                    pass
+                continue
+            parsed_line.data.message = line[27:]
+            if parsed_line.data.message[0:12] == "MQTT publish":
+                parsed_line.data.is_mqtt_publish = True
+                parsed_line.data.mqtt_message.topic = line.split("topic")[1].rsplit(", payload")[0].strip().strip("'")
+                parsed_line.data.mqtt_message.payload = line.split("payload")[1].strip().strip("'")
+                try:
+                    parsed_line.data.mqtt_message.payload = json.loads("{\"payload\":" + "\""  + line.split("payload")[1].strip().strip("'") + "\"" + "}")
+                except:
+                    parsed_line.data.mqtt_message.payload = json.loads("{\"payload\":" + "\"Couldn't convert to JSON\"" + "}")
+            else:
+                parsed_line.data.is_mqtt_publish = True
+                parsed_line.data.mqtt_message.topic = None
+                parsed_line.data.mqtt_message.payload = None
+            parsed_lines.append(parsed_line)
+    return parsed_lines
+def parse_latest_logs(path: str):
+        events = parse_logs(path)
+        execution_path = os.path.dirname(os.path.realpath(__file__))
+        pointer_file_name = execution_path + "/eventPointer.txt"
+        last_event = datetime.strftime(events[(len(events))-1].date, '%Y-%m-%d %H:%M:%S')
+        if not os.path.exists(pointer_file_name):
+            f = open(pointer_file_name, "w")
+            f.write(last_event)
+            f.close()
+        else:
+            f = open(pointer_file_name, "r+")
+            date_st = f.read()
+            date_dt = datetime.strptime(date_st, '%Y-%m-%d %H:%M:%S')
+            if date_st and date_dt <= events[(len(events))-1].date:
+                events = [x for x in events if x.date > date_dt]
+            if events:
+                f.seek(0)
+                f.truncate()
+                f.write(last_event)
+        if any(events):
+            return events
+        return None
