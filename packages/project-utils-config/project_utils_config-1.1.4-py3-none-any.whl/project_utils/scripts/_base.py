673459a766b4d3bd7696683f800058d215162ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 15:04:25 +0000

    Add package project-utils-config/1.1.4/project_utils_config-1.1.4-py3-none-any.whl

---
+++ packages/project-utils-config/project_utils_config-1.1.4-py3-none-any.whl/project_utils/scripts/_base.py
@@ -0,0 +1,29 @@
+import asyncio
+
+from abc import ABCMeta, abstractmethod
+
+from project_utils.conf import ConfigTemplate
+
+
+class BaseScript(metaclass=ABCMeta):
+    config: ConfigTemplate
+
+    def __init__(self, config: ConfigTemplate):
+        self.config = config
+        self.loop = asyncio.get_event_loop()
+
+    def async_start(self, *args, **kwargs):
+        self.loop.run_until_complete(self.handler(*args, **kwargs))
+
+    @abstractmethod
+    async def handler(self, *args, **kwargs):
+        ...
+
+    @classmethod
+    def run(cls, config: ConfigTemplate, *args, **kwargs):
+        this: cls = cls(config)
+        return this.async_start(*args, **kwargs)
+
+    @classmethod
+    def get_instance(cls, config: ConfigTemplate):
+        return cls(config)
