f571829b8e74d473786b4145dd592b200da18d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 15:04:44 +0000

    Add package django-safe-delete-t/1.0.0/django-safe-delete-t-1.0.0.tar.gz

---
+++ packages/django-safe-delete-t/django-safe-delete-t-1.0.0.tar.gz/django-safe-delete-t-1.0.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-safe-delete-t',
+    packages=find_packages(),
+    version='1.0.0',
+    description='django safedelete',
+    author='Mei Hai',
+    author_email='864561183@qq.com',
+    url='https://gitee.com/hai_hpy/django-sofe-delete.git',
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Development Status :: 4 - Beta',
+    ],
+    install_requires=['Django', 'packaging'],
+    include_package_data=True,
+)
+'''
+pypi-AgEIcHlwaS5vcmcCJDI5M2U2YmFlLWMxNDgtNGFmYS1hYTFkLTFjYjQxYzVmN2Q1ZgACKlszLCJjZTIxZTkwNy05ZmFiLTQwZGMtYjVmNi0wYmYxMmMwNTFmMTEiXQAABiDXAlsMNfh_suX4pgqIOjW7GcGfYhcf9rY6Tz2aSd46Cg
+'''
