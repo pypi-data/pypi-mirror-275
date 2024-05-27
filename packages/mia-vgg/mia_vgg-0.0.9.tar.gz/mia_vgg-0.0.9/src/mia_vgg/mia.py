fe72bb53de18ca9d58063262894ca05e7254a10f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 15:04:23 +0000

    Add package mia-vgg/0.0.9/mia_vgg-0.0.9.tar.gz

---
+++ packages/mia-vgg/mia_vgg-0.0.9.tar.gz/mia_vgg-0.0.9/src/mia_vgg/mia.py
@@ -0,0 +1,11 @@
+from sklearn.ensemble import RandomForestClassifier
+from pathlib import Path
+
+def mia():
+    path 
+    clf = RandomForestClassifier()
+    clf.fit(
+
+if __name__=="__main__":
+    pass
+
