89152254bf129c1ab9e1a6e1fec26957f430fb1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 03:04:44 +0000

    Add package clipboardhist/1.0.5/clipboardhist-1.0.5-py3-none-any.whl

---
+++ packages/clipboardhist/clipboardhist-1.0.5-py3-none-any.whl/clipboardhist/__init__.py
@@ -0,0 +1,8 @@
+from .clipboardhist import ClipboardHist
+
+__version__ = "1.0.5"
+__author__ = "Hiro Takagi"
+__email__ = "s2222103@stu.musashino-u.ac.jp"
+__description__ = "A simple clipboard history management tool"
+
+__all__ = ["ClipboardHist"]
