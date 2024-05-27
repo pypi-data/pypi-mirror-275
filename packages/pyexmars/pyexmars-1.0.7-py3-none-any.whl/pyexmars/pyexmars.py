c22eb0d9897840fba5bc8e66ff6f6f9cdc48f19f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 15:04:32 +0000

    Add package pyexmars/1.0.7/pyexmars-1.0.7-py3-none-any.whl

---
+++ packages/pyexmars/pyexmars-1.0.7-py3-none-any.whl/pyexmars/pyexmars.py
@@ -0,0 +1,24 @@
+
+class Protocol():
+    def __init__(self):
+        self.buffers = []
+        for i in range(11):
+            self.buffers.append(0)
+
+    def makePacket(self, index, para1, para2, para3, para4):
+        self.buffers[0] = 0
+        self.buffers[1] = index
+        self.buffers[2] = para1
+        self.buffers[3] = para2
+        self.buffers[4] = para3
+        self.buffers[5] = para4
+        self.buffers[6] = 90
+        self.buffers[7] = 7
+        self.buffers[8] = 253
+        self.buffers[9] = 254
+        self.buffers[10] = 254
+
+    def makePacketMenuSetting(self, main, sub):
+        self.makePacket(0, 11, main, sub, 255)
+        return self.buffers
+    
