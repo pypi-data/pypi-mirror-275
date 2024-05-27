1ec03ca905a86d70e6ed0393fa7fae854f7c1932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 14:05:44 +0000

    Add package color-change-filter/1.1.2/color_change_filter-1.1.2.tar.gz

---
+++ packages/color-change-filter/color_change_filter-1.1.2.tar.gz/color_change_filter-1.1.2/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r",encoding="UTF-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+  name="color_change_filter",
+  version="1.1.2",
+  author="Chihiro senbonmatsu",
+  author_email="s2222020@stu.musahino-u.ac.jp",
+  description="Change color of image",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/senbonmatsu/color_changer",
+  project_urls={
+    "Bug Tracker": "https://github.com/senbonmatsu/color_changer",
+  },
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  install_requires=[
+        'opencv-python',
+        'numpy',
+    ],
+  package_dir={"": "src"},
+  py_modules=["color_change_filter"],
+  packages=setuptools.find_packages(where="src"),
+  Python_requires=">=3.6",
+  entry_points={
+    "console_scripts": [
+      "color_change_filter = color_change_filter:main",
+    ],
+  },
+)
