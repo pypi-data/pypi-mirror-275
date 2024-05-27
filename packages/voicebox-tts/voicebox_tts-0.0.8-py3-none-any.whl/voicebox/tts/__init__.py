ac2b34f26c1c4ab10c343c1f107e5b09957ad81d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 15:04:31 +0000

    Add package voicebox-tts/0.0.8/voicebox_tts-0.0.8-py3-none-any.whl

---
+++ packages/voicebox-tts/voicebox_tts-0.0.8-py3-none-any.whl/voicebox/tts/__init__.py
@@ -0,0 +1,31 @@
+from voicebox.tts.tts import TTS, FallbackTTS, RetryTTS
+
+try:
+    from voicebox.tts.amazonpolly import AmazonPolly
+except ImportError:
+    pass
+
+from voicebox.tts.cache import CachedTTS, PrerecordedTTS
+from voicebox.tts.espeakng import ESpeakConfig, ESpeakNG
+
+try:
+    from voicebox.tts.elevenlabs import ElevenLabsTTS
+except ImportError:
+    pass
+
+try:
+    from voicebox.tts.googlecloudtts import GoogleCloudTTS
+except ImportError:
+    pass
+
+try:
+    from voicebox.tts.gtts import gTTS
+except ImportError:
+    pass
+
+from voicebox.tts.picotts import PicoTTS
+
+
+def default_tts() -> TTS:
+    """Returns a new instance of the default TTS, :class:`PicoTTS`."""
+    return PicoTTS()
