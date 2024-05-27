00b3761159af76899ee1ee4049b57311bcf0e2a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 14:05:32 +0000

    Add package oresat-star-tracker/0.3.0/oresat_star_tracker-0.3.0.tar.gz

---
+++ packages/oresat-star-tracker/oresat_star_tracker-0.3.0.tar.gz/oresat_star_tracker-0.3.0/oresat_star_tracker/__main__.py
@@ -0,0 +1,35 @@
+"""Star Tracker App"""
+
+import os
+
+from olaf import app, olaf_run, olaf_setup, render_olaf_template, rest_api
+
+from . import __version__
+from .star_tracker_service import StarTrackerService
+
+
+@rest_api.app.route("/star-tracker")
+def star_tracker_template():
+    """Render the star tracker web page."""
+    return render_olaf_template("star_tracker.html", name="Star Tracker")
+
+
+def main():
+    """Star Tracker OLAF app main."""
+    path = os.path.dirname(os.path.abspath(__file__))
+
+    args, _ = olaf_setup("star_tracker_1")
+    mock_args = [i.lower() for i in args.mock_hw]
+    mock_camera = "camera" in mock_args or "all" in mock_args
+
+    app.od["versions"]["sw_version"].value = __version__
+
+    app.add_service(StarTrackerService(mock_camera))
+
+    rest_api.add_template(f"{path}/templates/star_tracker.html")
+
+    olaf_run()
+
+
+if __name__ == "__main__":
+    main()
