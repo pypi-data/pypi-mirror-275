0679df1a8c6cefa19bfa64cc3de1970c7f1145ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 03:04:48 +0000

    Add package gdtransform/0.1.0/gdtransform-0.1.0.tar.gz

---
+++ packages/gdtransform/gdtransform-0.1.0.tar.gz/gdtransform-0.1.0/src/gdtransform/constants.py
@@ -0,0 +1,5 @@
+GRID_TRANSFORMATION_FLAG = '__gd_transformation__'
+GRID_BATCH_TRANSFORMATION_FLAG = "__gd_batch_transformation__"
+GRID_TRANSFORMATION_NAME = '__gd_transformation_name__'
+
+GRID_TRANSFORMATION_BUILDER_FLAG = "__gd_transformation_builder__"
