e459ef8f0c8d8b51daa2e6ce9ec3edce2a86fc5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 14:06:01 +0000

    Add package txdpy/8.8.9/txdpy-8.8.9-py3-none-any.whl

---
+++ packages/txdpy/txdpy-8.8.9-py3-none-any.whl/txdpy/文本错别字检查.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @File  : 文本错别字检查.py
+# @Time  : 2024/1/18 10:19
+# @Author: 唐旭东
+
+# from pycorrector import Corrector
+# import re
+# from time import sleep
+# import requests
+# import json
+# from .read_data import ReadData
+
+
+class 文本错别字检查方法一():
+    """
+    网上开源的代码库
+    """
+    pass
+    # def __init__(self):
+    #     self.m = Corrector()
+    # def main(self,文本):
+    #     """
+    #     :param 文本: 要识别错别字的文本
+    #     :return: 返回[(错误文本,可能正确的文本,错误文本出现的位置),]
+    #     """
+    #     return self.m.correct_batch([文本])[0]['errors'] or None
