b393745fcdba0ce0892f02cb4548d13ae3ac851e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 03:04:38 +0000

    Add package zenaura/0.9.51/zenaura-0.9.51.tar.gz

---
+++ packages/zenaura/zenaura-0.9.51.tar.gz/zenaura-0.9.51/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='zenaura',
+    version='0.9.51',
+    description="Zenaura, light weight, super fast python full-stack development framework, in which every line of code emit the aura of python zen, build interactive SPA with pure Python, create secure and scalable endpoints.",
+    author="Ahmed Rakan",
+    author_email="ar.aldhafeeri11@gmail.com",
+    packages=['zenaura'],
+    install_requires=[
+        'bleach'
+    ],
+    test_suite='tests',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
