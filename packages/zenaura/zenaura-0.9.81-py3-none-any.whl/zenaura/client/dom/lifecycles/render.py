0d1bc9d713ec596ab3687a26d082a88acda14318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 04:05:13 +0000

    Add package zenaura/0.9.81/zenaura-0.9.81-py3-none-any.whl

---
+++ packages/zenaura/zenaura-0.9.81-py3-none-any.whl/zenaura/client/dom/lifecycles/render.py
@@ -0,0 +1,28 @@
+class RenderLifeCycle:
+    def on_mutation(self, comp) -> None:
+        """
+        Method called after the component is updated in the DOM and re-rendered.
+
+        Parameters:
+        - comp: An instance of the Component class.
+
+        Returns:
+        None
+        """
+        # Perform operations before updating
+        if hasattr(comp, 'on_mutation'):
+            comp.on_mutation()
+
+    def on_settled(self, comp) -> None:
+        """
+        Method called after the component is updated in the DOM and re-rendered.
+
+        Parameters:
+        - comp: An instance of the Component class.
+
+        Returns:
+        None
+        """
+        # Perform operations after updating
+        if hasattr(comp, 'on_settled'):
+            comp.on_settled()
