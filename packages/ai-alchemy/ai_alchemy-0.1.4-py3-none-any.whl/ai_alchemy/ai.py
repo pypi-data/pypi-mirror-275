02865253abe7bee118cc203c6900d362936f4ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 03:04:34 +0000

    Add package ai-alchemy/0.1.4/ai_alchemy-0.1.4-py3-none-any.whl

---
+++ packages/ai-alchemy/ai_alchemy-0.1.4-py3-none-any.whl/ai_alchemy/ai.py
@@ -0,0 +1,5 @@
+from ai_alchemy.core.ai import AiWrapper
+from ai_alchemy.core.ai import OpenAIWrapper
+
+AiWrapper
+OpenAIWrapper
---
+++ packages/ai-alchemy/ai_alchemy-0.1.4-py3-none-any.whl/ai_alchemy/cast.py
@@ -0,0 +1,7 @@
+from ai_alchemy.core.cast import pydantic_model_to_pydantic_model
+from ai_alchemy.core.cast import str_to_pydantic_model
+from ai_alchemy.core.cast import dict_to_pydantic_model
+
+pydantic_model_to_pydantic_model
+str_to_pydantic_model
+dict_to_pydantic_model
