bf58b3834cbff99463aca51bd44a48d14c007199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 04:05:47 +0000

    Add package NetHyTech-DeepTTS/0.2/NetHyTech-DeepTTS-0.2.tar.gz

---
+++ packages/NetHyTech-DeepTTS/NetHyTech-DeepTTS-0.2.tar.gz/NetHyTech-DeepTTS-0.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# Read the content of README.md for long description
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='NetHyTech-DeepTTS',
+    version='0.2',
+    author='Anubhav Chaturvedi',
+    author_email='chaturvedianubhav520@example.com',
+    description='A Python package for text-to-speech conversion using DeepAI API.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'playsound',
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'NetHyTech-DeepTTS = my_package.main:main'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Multimedia :: Sound/Audio :: Speech',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
