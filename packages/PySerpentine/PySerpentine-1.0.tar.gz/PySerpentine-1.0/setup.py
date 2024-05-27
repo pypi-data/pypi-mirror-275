b1701815a94bb50379546c71df279413a4a775c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 16:04:18 +0000

    Add package PySerpentine/1.0/PySerpentine-1.0.tar.gz

---
+++ packages/PySerpentine/PySerpentine-1.0.tar.gz/PySerpentine-1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+  name='PySerpentine',
+  version='1.0',
+  author='conhost',
+  author_email='conshot.dev@gmail.com',
+  description='Module for beautiful log',
+  long_description='Module for beautiful log',
+  packages=find_packages(),
+  install_requires=[],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='PySerpentine',
+  python_requires='>=3.11'
+)
