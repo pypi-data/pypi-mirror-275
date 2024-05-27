dd26a840f141e6d6957aa6842c7cf20c3d61f7cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 04:05:24 +0000

    Add package wikipathway2genes/0.0.3/wikipathway2genes-0.0.3.tar.gz

---
+++ packages/wikipathway2genes/wikipathway2genes-0.0.3.tar.gz/wikipathway2genes-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wikipathway2genes',
+    version='0.0.3',
+    author='Pr (France) Dr. rer. nat. Vijay K. ULAGANATHAN',
+    author_email=' ',
+    description='A package to convert WikiPathways IDs to genes',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'wikipathway2genes=wp2genes.wp2genes:main',
+        ],
+    },
+    install_requires=[
+        'argparse',
+        'pywikipathways',
+    ],
+)
+
