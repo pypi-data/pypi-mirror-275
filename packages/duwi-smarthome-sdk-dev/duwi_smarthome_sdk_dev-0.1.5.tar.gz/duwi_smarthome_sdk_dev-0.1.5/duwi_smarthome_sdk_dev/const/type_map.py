85eb25515f83c0b25473979e46b3667e4b9111bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-27 15:04:45 +0000

    Add package duwi-smarthome-sdk-dev/0.1.5/duwi_smarthome_sdk_dev-0.1.5.tar.gz

---
+++ packages/duwi-smarthome-sdk-dev/duwi_smarthome_sdk_dev-0.1.5.tar.gz/duwi_smarthome_sdk_dev-0.1.5/duwi_smarthome_sdk_dev/const/type_map.py
@@ -0,0 +1,81 @@
+# device_type
+type_map = {
+    "SWITCH": {
+        "1-002": "On",
+        "1-003": "On",
+        "1-005": "On",
+        "1-006": "On",
+        "107-001": "On",
+    },
+    "LIGHT": {
+        "1-001": "On",
+        "1-004": "On",
+        "3-001": "Dim",
+        "3-002": "Temp",
+        "3-003": "DimTemp",
+        "3-004": "RGBW",
+        "3-005": "RGB",
+        "3-006": "RGBCW",
+    },
+    "COVER": {
+        "4-001": "Roll",
+        "4-002": "Roll",
+        "4-003": "Shutter",
+        "4-004": "Shutter",
+    },
+    "MEDIA": {
+        "8-001": "On",
+    }
+    # "CLIMATE": {
+    #     "5-001": "HeatCool",
+    #     "5-002": "Underfloor",
+    #     "5-003": "Ventilation",
+    #     "5-004": "HeatPump",
+    #     "5-005": "Combo",
+    # },
+}
+
+sensor_type_map = {
+    "7-001-001": ["Temperature"],
+    "7-001-002": ["Temperature"],
+    "7-002-001": ["Humidity"],
+    "7-003-001": ["Light"],
+    "7-004-001": ["Formaldehyde"],
+    "7-005-001": ["Pm25"],
+    "7-006-001": ["CarbonDioxide"],
+    "7-007-001": ["AirQuality"],
+    "7-008-001": ["Human"],
+    "7-008-002": ["Human"],
+    "7-009-001": ["Trigger"],
+    "7-009-002": ["Trigger"],
+    "7-009-003": ["Trigger"],
+    "7-009-004": ["Trigger"],
+    "7-009-005": ["Trigger"],
+    "7-009-006": ["Trigger"],
+    "7-009-007": ["Trigger"],
+    "7-009-008": ["Trigger"],
+    "7-009-009": ["Trigger"],
+    "7-009-010": ["Trigger"],
+    "7-010-001": ["CarbonMonoxide"],
+    "7-011-001": ["Tvoc"],
+    "7-012-001": ["Temperature", "Humidity", "Tvoc", "Formaldehyde", "Pm25", "CarbonDioxide", "Pm10", "AirQuality"],
+    "7-012-002": ["CarbonMonoxide"],
+    "7-013-001": ["Light", "Human"],
+}
+
+group_type_map = {
+    "SWITCH": {
+        "Breaker": "On",
+    },
+    "LIGHT": {
+        "Light": "Dim",
+        "Color": "Temp",
+        "LightColor": "DimTemp",
+        "RGBW": "RGBW",
+        "RGB": "RGB",
+    },
+    "COVER": {
+        "Retractable": "Roll",
+        "Roller": "Roll",
+    },
+}
