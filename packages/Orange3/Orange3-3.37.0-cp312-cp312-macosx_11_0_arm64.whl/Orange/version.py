a824285adb30e4edcbc512ef91d2d8ec6a0b3d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 03:03:56 +0000

    Add package Orange3/3.37.0/Orange3-3.37.0-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/Orange3/Orange3-3.37.0-cp312-cp312-macosx_11_0_arm64.whl/Orange/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM ORANGE SETUP.PY
+short_version = '3.37.0'
+version = '3.37.0'
+full_version = '3.37.0'
+git_revision = 'a2a0cb00bd01442d8707c8b9405b79dc54f9dbfd'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
