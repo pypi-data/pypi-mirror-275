0362d832966f29ae2a772d0b19ebe46ed7dc8a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 15:04:26 +0000

    Add package combinedpackmsnk/0.1.0/combinedpackmsnk-0.1.0.tar.gz

---
+++ packages/combinedpackmsnk/combinedpackmsnk-0.1.0.tar.gz/combinedpackmsnk-0.1.0/README.md
@@ -0,0 +1,23 @@
+
+# combinedpackmsnk
+
+A short description of your package.
+
+## Installation
+
+You can install the package using:
+
+```sh
+pip install combinedpackmsnk
+```
+
+## Usage
+
+Here is an example of how to use the package:
+
+```python
+from combinedpackmsnk import func
+
+# Call the function
+func('path_to_your_file.csv')
+```
---
+++ packages/combinedpackmsnk/combinedpackmsnk-0.1.0.tar.gz/combinedpackmsnk-0.1.0/setup.py
@@ -0,0 +1,33 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='combinedpackmsnk',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scikit-learn',
+        'seaborn',
+        'matplotlib',
+        'pandas',
+        'nbconvert',
+        'statsmodels',
+        'ydata-profiling',
+    ],
+    entry_points={
+        'console_scripts': [],
+    },
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A short description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/your-repo-name',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
