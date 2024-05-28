1986011690d9ea4eaab81c496b8cd500dde798a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 16:05:00 +0000

    Add package gamspy/0.12.5/gamspy-0.12.5-py3-none-any.whl

---
+++ packages/gamspy/gamspy-0.12.5-py3-none-any.whl/gamspy/_symbols/symbol.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Union
+
+import gams.transfer as gt
+
+import gamspy._symbols.implicits as implicits
+from gamspy.exceptions import ValidationError
+
+if TYPE_CHECKING:
+    from gamspy import Alias, Equation, Parameter, Set, Variable
+
+    SymbolType = Union[Alias, Set, Parameter, Variable, Equation]
+
+
+class Symbol:
+    def __bool__(self):
+        raise ValidationError(
+            "A symbol cannot be used as a truth value. Use len(<symbol>.records) instead."
+        )
+
+    def gamsRepr(self: SymbolType):
+        """Representation of the symbol in GAMS"""
+
+    def getDeclaration(self: SymbolType):
+        """Declaration of the symbol in GAMS"""
+
+    def _get_domain_str(self: SymbolType):
+        if isinstance(self.domain_forwarding, bool):
+            forwarding = [self.domain_forwarding] * self.dimension
+        else:
+            forwarding = self.domain_forwarding
+
+        set_strs = []
+        for elem, is_forwarding in zip(self.domain, forwarding):
+            if isinstance(elem, (gt.Set, gt.Alias, implicits.ImplicitSet)):
+                set_str = elem.gamsRepr()
+                set_strs.append(set_str + "<" if is_forwarding else set_str)
+            elif isinstance(elem, str):
+                set_strs.append("*")
+
+        return "(" + ",".join(set_strs) + ")"
+
+    def _mark_forwarded_domain_sets(self):
+        for elem in self.domain:
+            if hasattr(elem, "modified"):
+                elem.modified = False
+                elem._is_dirty = True
