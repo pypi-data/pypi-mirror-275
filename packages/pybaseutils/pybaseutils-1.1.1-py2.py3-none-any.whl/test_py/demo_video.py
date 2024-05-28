76396b29ae57580b119d42ec58c1a3bd40918559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 03:04:08 +0000

    Add package pybaseutils/1.1.1/pybaseutils-1.1.1-py2.py3-none-any.whl

---
+++ packages/pybaseutils/pybaseutils-1.1.1-py2.py3-none-any.whl/test_py/demo_video.py
@@ -0,0 +1,26 @@
+# -*-coding: utf-8 -*-
+"""
+    @Author : PKing
+    @E-mail : 390737991@qq.com
+    @Date   : 2022-12-31 11:37:30
+    @Brief  :
+"""
+import os
+import cv2
+import numpy as np
+from pybaseutils import file_utils, image_utils
+from pybaseutils.cvutils import video_utils
+
+
+def video2frames_demo(root, out):
+    files = file_utils.get_files_list(root, postfix=["*.avi", "*.mp4", "*.flv"])
+    for video_file in files:
+        print(video_file)
+        video_utils.video2frames_similarity(video_file, out_dir=out, func=None, interval=50, thresh=0.3,vis=True)
+        # video_utils.video2frames(video_file, out_dir=out, func=None, interval=15, vis=True)
+
+
+if __name__ == "__main__":
+    root = "/home/PKing/nasdata/dataset/finger_keypoint/pen_test3/zip"
+    out = root + "-frame"
+    video2frames_demo(root, out)
