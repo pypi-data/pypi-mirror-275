60adc1a0e02f5fe16222d3a4c40b21020b2eb140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 16:05:11 +0000

    Add package py-seal/0.0.6/py_seal-0.0.6-py3-none-any.whl

---
+++ packages/py-seal/py_seal-0.0.6-py3-none-any.whl/seal/db/mysql/mysql_connector.py
@@ -0,0 +1,20 @@
+import pymysql
+import pymysql.cursors
+from seal.config.configuration import Configuration
+from seal.db.connector import DBConnector
+from seal.wrapper.singleton import singleton
+
+
+@singleton
+class MysqlConnector(DBConnector):
+    def __init__(self):
+        self.config = Configuration().config['mysql']
+        # self.pool = mysql.connector.pooling.MySQLConnectionPool(**self.config)
+
+    def get_connection(self):
+        # return self.pool.get_connection()
+        return pymysql.connect(host=self.config['host'],
+                               user=self.config['user'],
+                               password=self.config['password'],
+                               database=self.config['database'],
+                               cursorclass=pymysql.cursors.DictCursor)
