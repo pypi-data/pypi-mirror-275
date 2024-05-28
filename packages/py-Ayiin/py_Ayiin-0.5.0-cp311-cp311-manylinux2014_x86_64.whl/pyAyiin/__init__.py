53af58c7ef4a10c7e402314a9545a8ce5b83e54f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 05:04:51 +0000

    Add package py-Ayiin/0.5.0/py_Ayiin-0.5.0-cp311-cp311-manylinux2014_x86_64.whl

---
+++ packages/py-Ayiin/py_Ayiin-0.5.0-cp311-cp311-manylinux2014_x86_64.whl/pyAyiin/__init__.py
@@ -0,0 +1,52 @@
+"""
+pyromod - A monkeypatched add-on for Pyrogram
+Copyright (C) 2020 Cezar H. <https://github.com/usernein>
+
+This file is part of pyromod.
+
+pyromod is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pyromod is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pyromod.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from .config import config
+from .helpers import ikb, bki, ntb, btn, kb, kbtn, array_chunk, force_reply
+from .listen import Client, MessageHandler, CallbackQueryHandler, Message, Chat, User
+from .nav import Pagination
+from .utils import patch_into, should_patch
+
+__all__ = [
+    "config",
+    "Client",
+    "MessageHandler",
+    "Message",
+    "Chat",
+    "User",
+    "CallbackQueryHandler",
+    "patch_into",
+    "should_patch",
+    "ikb",
+    "bki",
+    "ntb",
+    "btn",
+    "kb",
+    "kbtn",
+    "array_chunk",
+    "force_reply",
+    "Pagination",
+]
+
+__copyright__ = "Copyright (C) 2022-present AyiinXd <https://github.com/AyiinXd>"
+__license__ = "GNU General Public License v3.0 (GPL-3.0)"
+__version__ = "0.5.0"
+ayiin_ver = "0.1.1"
+prem_version = "0.1"
