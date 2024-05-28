604795a5b4bb43f33592c19ee200546dd47908d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 04:04:04 +0000

    Add package jaqpotpy/1.1.1/jaqpotpy-1.1.1.tar.gz

---
+++ packages/jaqpotpy/jaqpotpy-1.1.1.tar.gz/jaqpotpy-1.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\\n" + fh.read()
+
+setup(name='jaqpotpy',
+      version='1.1.1',
+      description='Standardizing molecular modeling',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+
+      url='https://github.com/ntua-unit-of-control-and-informatics/jaqpotpy',
+      author='Unit of Process Control and Informatics | National Technical University of Athens',
+      author_email='upci.ntua@gmail.com',
+      license='MIT License',
+      packages=find_packages(exclude=["*.tests"]),
+      package_data={'jaqpotpy': ['data/*.gz']},
+      install_requires=[
+          'pandas', 'requests', 'pydantic', 'rdkit-pypi', 'mordred', 'pyjwt', 'scikit-learn', 'tqdm',
+          'skl2onnx', 'onnxruntime'
+      ],
+      zip_safe=False)
