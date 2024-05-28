2578544c29b1216d9fb1515032ae5dd9544f3b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 14:05:21 +0000

    Add package kanstream/2.0.1/kanstream-2.0.1.tar.gz

---
+++ packages/kanstream/kanstream-2.0.1.tar.gz/kanstream-2.0.1/kanstream/codec/codec.py
@@ -0,0 +1,34 @@
+from enum import Enum
+
+from .h264 import H264Encoder, H264Decoder
+from .vp9 import VP9Encoder, VP9Decoder
+
+
+class CodecType(Enum):
+    H264 = 'h264'
+    VP9 = 'vp9'
+
+
+class Codec(object):
+
+    @staticmethod
+    def create_encoder(codec_type: CodecType | str, **kwargs):
+        match codec_type:
+            case CodecType.H264 | CodecType.H264.value:
+                return H264Encoder(**kwargs)
+            case CodecType.VP9 | CodecType.VP9.value:
+                return VP9Encoder(**kwargs)
+            case _:
+                raise ValueError(f'Unsupported codec type: {codec_type}')
+
+    @staticmethod
+    def create_decoder(codec_type: CodecType, **kwargs):
+        match codec_type:
+            case CodecType.H264 | CodecType.H264.value:
+                return H264Decoder(**kwargs)
+
+            case CodecType.VP9 | CodecType.VP9.value:
+                return VP9Decoder(**kwargs)
+
+            case _:
+                raise ValueError(f'Unsupported codec type: {codec_type}')
