c648041ee1200e6237e0b8c58cbef527ecd3d3f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 03:04:10 +0000

    Add package Warg/1.4.3/Warg-1.4.3-py36-none-any.whl

---
+++ packages/Warg/Warg-1.4.3-py36-none-any.whl/warg/mixins/private.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+__author__ = "heider"
+__doc__ = r"""
+
+           Created on 9/9/22
+           """
+
+__all__ = ["PrivateAttributeMixin"]
+
+from typing import Any
+
+
+class PrivateAttributeMixin:
+    def __getitem__(self, key: str) -> Any:
+        if key.startswith("_"):
+            return None
+        return getattr(self, key, None)
