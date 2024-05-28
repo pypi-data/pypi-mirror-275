c9dc155a3ee8be413c7b8929b09e9a34d96aeb08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 05:04:29 +0000

    Add package telethon-client/0.1.0/telethon-client-0.1.0.tar.gz

---
+++ packages/telethon-client/telethon-client-0.1.0.tar.gz/telethon-client-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import shutil
+shutil.rmtree('build')
+shutil.rmtree('dist')
+shutil.rmtree('Telethon_Client.egg-info')
+setup(
+    name = "telethon-client",
+    version = "0.1.0",
+    packages = find_packages(),
+    install_requires = ["Telethon>=1.35.0"],
+    author= " LEGENDX",
+    author_email = "legendxcoder@gmail.com",
+    description = "Custom Telethon Client for My Usage",
+    long_description = open('README.md').read(),
+    long_description_content_type = "text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
