7c22dc0c87c086b302c77e26bb0e71d4d4df39e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 04:04:03 +0000

    Add package dicio-py/1.9/dicio_py-1.9.tar.gz

---
+++ packages/dicio-py/dicio_py-1.9.tar.gz/dicio_py-1.9/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dicio-py",
+    version="1.9",
+    author="Jetrom17",
+    author_email="Jeiel@duck.com",
+    description="Dicionário via CLI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jetrom17/dicio-py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
