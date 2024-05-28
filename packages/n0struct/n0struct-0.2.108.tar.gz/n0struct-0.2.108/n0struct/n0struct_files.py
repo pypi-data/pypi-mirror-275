8f5f6985aae0716b54557946c4fa11c5a3bac0b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 14:05:18 +0000

    Add package n0struct/0.2.108/n0struct-0.2.108.tar.gz

---
+++ packages/n0struct/n0struct-0.2.108.tar.gz/n0struct-0.2.108/n0struct/n0struct_files.py
@@ -0,0 +1,92 @@
+# Because of load_ini(...) uses walrus operator inside comprehensions, it was moved into separate module n0struct_comprehensions.
+# It will be imported only for Python 3.8+, so all other modules will be fully compatibility with Python 3.7
+import os
+import typing
+from pathlib import Path
+from .n0struct_utils import n0eval
+from .n0struct_utils import isnumber
+from .n0struct_logging import (
+    n0print,
+    n0debug,
+    n0debug_calc,
+    n0error,
+)
+# ******************************************************************************
+# ******************************************************************************
+def load_file(
+        file_path: str,
+        read_mode: str = 't',
+        encoding: typing.Union[str, None] = "utf-8-sig",  # with possible UTF-8 BOM (Byte Order Mark)
+) -> str:
+    if read_mode == 'b':
+        encoding = None
+    with open(file_path, 'r'+read_mode, encoding=encoding) as in_filehandler:
+        return in_filehandler.read()
+# ******************************************************************************
+def load_lines(
+        file_path: str,
+        read_mode: str = 't',
+        encoding: str = "utf-8-sig",    # with possible UTF-8 BOM (Byte Order Mark)
+        EOL: str = '\r\n',              # used only for read_mode == 'b'
+) -> typing.Generator:
+    if read_mode == 'b':
+        if isinstance(EOL, str):
+            EOL = EOL.encode("utf-8")
+        elif not isinstance(EOL, bytes):
+            raise TypeError(f"{EOL=} could be str or bytes for {read_mode=}")
+        for line in load_file(file_path, read_mode='b').split(EOL):
+            yield line
+    else:
+        with open(file_path, 'rt', encoding=encoding) as in_filehandler:
+            while True:
+                line = in_filehandler.readline()
+                if not line:
+                    break
+                yield line.rstrip('\r\n')   # strip \r, \n, \r\n
+# ******************************************************************************
+def save_file(
+        file_path: str,
+        lines: typing.Union[tuple, list, dict, str, bytes, bytearray, typing.Generator],
+        mode: str = 'wt',           # or 'at' for append
+        encoding: str = "utf-8",    # without UTF-8 BOM (Byte Order Mark)
+        EOL: str = os.linesep,
+        equal_tag: str = "=",
+):
+    Path(file_path).parent.mkdir(parents=True, exist_ok=True)
+
+    if mode in ('t', 'b'):
+        mode = 'w' + mode
+
+    if isinstance(lines, (list, tuple)):
+        output_buffer = EOL.join(lines)
+    elif isinstance(lines, dict):
+        output_buffer = EOL.join([key+equal_tag+lines[key] for key in lines])
+    elif isinstance(lines, str):
+        output_buffer = lines
+    elif isinstance(lines, (bytes, bytearray)):
+        output_buffer = lines
+        mode = mode[0] + 'b'
+    else:
+        output_buffer = str(lines)
+
+    if 'b' in mode:
+        if isinstance(output_buffer, str):
+            output_buffer = output_buffer.encode(encoding)
+        encoding = None
+
+    with open(file_path, mode, encoding=encoding) as out_filehandler:
+        out_filehandler.write(output_buffer)
+# ******************************************************************************
+
+
+def unique_file_path(file_path: str, purpose: str = "") -> Path:
+    for i in range(1000):
+        unique_file_path = Path(str(file_path) + (f".{i:03}" if i else ""))
+        if not unique_file_path.exists():
+            return unique_file_path
+    else:
+        raise FileExistsError("Impossible to find unique name for {purpose}'{file_path}'")
+# ******************************************************************************
+# ******************************************************************************
+
+
