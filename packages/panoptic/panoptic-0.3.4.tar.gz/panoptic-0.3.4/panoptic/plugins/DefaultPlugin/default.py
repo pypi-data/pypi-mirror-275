c6d111d01bd866c2ac9be1a6b5348f9b307cfb4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 16:04:28 +0000

    Add package panoptic/0.3.4/panoptic-0.3.4.tar.gz

---
+++ packages/panoptic/panoptic-0.3.4.tar.gz/panoptic-0.3.4/panoptic/plugins/DefaultPlugin/default.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+from pydantic import BaseModel
+
+from panoptic.core.project.project import Project
+from panoptic.models import ActionContext, PropertyId, PropertyType, PropertyMode, InstancePropertyValue, DbCommit
+from panoptic.models.results import ActionResult
+from panoptic.plugin import Plugin
+
+
+# class TestParams(BaseModel):
+#     eau: str = None
+#     terre: int = 0
+#     feu: float = 2.4
+#     air: Path = None
+
+
+class DefaultPlugin(Plugin):
+    def __init__(self, project: Project, plugin_path: str):
+        super().__init__(name='TestPlugin1', project=project, plugin_path=plugin_path)
+        # self.params = TestParams()
+
+        self.project.action.easy_add(self, self.convert_to_tags, ['execute'])
+
+    async def convert_to_tags(self, context: ActionContext, source: PropertyId):
+        prop = await self.project.db.add_property('PluginProp', PropertyType.string)
+        values = [InstancePropertyValue(property_id=prop.id, instance_id=i, value='I LOVE PLUGINS') for i in context.instance_ids]
+        commit = DbCommit(instance_values=values)
+        commit = await self.project.undo_queue.do(commit)
+        commit.properties = [prop]
+        return ActionResult(commit=commit)
