2dada653715916f528ad4a615d4e7ab3c3376571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 06:04:15 +0000

    Add package pyVMO/0.0.4/pyVMO-0.0.4-py3-none-any.whl

---
+++ packages/pyVMO/pyVMO-0.0.4-py3-none-any.whl/pyvmo/versions.py
@@ -0,0 +1,34 @@
+def get_versions():
+    return versions[0]["number"]
+
+
+versions = [
+    {
+        "number": "0.0.4",
+        "features": [
+            "1. Add stores QUAL, FILTER and INFO to vmo",
+        ],
+    },
+    {
+        "number": "0.0.3",
+        "features": [
+            "1. add command line interface",
+        ],
+    },
+    {
+        "number": "0.0.2",
+        "features": [
+            "1. convert vmo to bimbam",
+            "2. bug fix",
+            "3. add readme",
+        ],
+    },
+    {
+        "number": "0.0.1",
+        "features": [
+            "1. init",
+            "2. VMO builds are included in the initial release, and it is now possible to easily convert vcf files into vmo. with vmo you can easily perform matrix extraction, as well as filter the data according to MAF or Miss ratio.",
+            "3. Identity by states (IBS) calculations within a controlled memory and time range",
+        ],
+    },
+]
