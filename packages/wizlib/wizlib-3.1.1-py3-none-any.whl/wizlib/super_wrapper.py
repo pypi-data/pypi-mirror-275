d8553c22193461c9cb909251ac5e4b26e7ce7571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 14:04:27 +0000

    Add package wizlib/3.1.1/wizlib-3.1.1-py3-none-any.whl

---
+++ packages/wizlib/wizlib-3.1.1-py3-none-any.whl/wizlib/super_wrapper.py
@@ -0,0 +1,8 @@
+
+class SuperWrapper:
+    @classmethod
+    def wrap(parent_class, method):
+        def wrapper(self, *args, **kwargs):
+            parent_method = getattr(parent_class, method.__name__)
+            return parent_method(self, method, *args, **kwargs)
+        return wrapper
