8b22f105d2f172e87a2b33a542d26edf2f821396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 06:04:05 +0000

    Add package alertpix/0.3.5/alertpix-0.3.5.tar.gz

---
+++ packages/alertpix/alertpix-0.3.5.tar.gz/alertpix-0.3.5/alertpix/src/charges.py
@@ -0,0 +1,52 @@
+import requests
+from .brcode import BrCODE
+from .statics import BASEURL
+class Charge():
+    def __init__(self, link:str,  amount:int, comment:str=".", username:str=".",) -> None:
+        """
+        Args:x'
+            link (str): Username da conta da twitch que IRÁ RECEBER
+            comment (str): Mensagem que o usuário irá receber (OPCIONAL)
+            username (str): Nome que o pagante será identificado (OPCIONAL)
+            amount (int): Quantia que o usuário irá receber
+        """
+        if amount <= 100:
+            raise ValueError("Amount must be greater than 100")
+        self.link = link
+        self.comment = comment
+        self.username = username
+        self.amount = amount
+
+    def create(self):
+        request  =  requests.post(
+            url=f'{BASEURL}/create-charge',
+            json={
+                "link": self.link,
+                "amount": self.amount,
+                "comment": self.comment,
+                "userName": self.username
+            },
+            headers={"content-type": "application/json"}
+            
+        )
+        if request.status_code == 200:
+            self.charge_id = request.json()['data']["id"]
+            self.brcode = BrCODE(request.json()['data']["brCode"])   
+            return self
+        else:
+            raise Exception(f'Request returned status {request.status_code} | Response: {request.text}')
+
+    def check(self) -> bool:
+        """
+        Verifica se o pagamento foi concluído
+
+        Retorna:
+            bool: True se o pagamento foi concluído, False se não
+        """
+        request  =  requests.get(
+            url=f'{BASEURL}/check-charge?id={self.charge_id}'
+        )
+        if request.status_code == 200:
+            return False if request.json()["data"]["status"] == "pending" else True
+        else:
+            raise Exception(f'Request returned status {request.status_code} | Response: {request.text}')
---
+++ packages/alertpix/alertpix-0.3.5.tar.gz/alertpix-0.3.5/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r", encoding="UTF-8") as fh:
+    long_description = fh.read()
+
+requirements = [
+    "qrcode",
+    "requests"
+]
+
+setuptools.setup(
+    name="alertpix",
+    version="0.3.5",
+    author="Joaquim Cassano",
+    license='MIT',
+    author_email="joaquim@cassano.com.br",
+    description='Python library for communication with the alertpix api',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
+    keywords=["api", "alertpix", "payments", "pix"],
+    url="https://github.com/JoaquimCassano/Alertpix.py",
+    packages=["alertpix", "alertpix.src"],
+    package_dir={"alertpix": "alertpix", "alertpix.src": "alertpix/src"},
+    python_requires=">=3.7",
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
