3a5b940da31bb5e08bb6920903dded3f1e4c692e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 04:04:07 +0000

    Add package flamapy-sat/2.0.0.dev0/flamapy-sat-2.0.0.dev0.tar.gz

---
+++ packages/flamapy-sat/flamapy-sat-2.0.0.dev0.tar.gz/flamapy-sat-2.0.0.dev0/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="flamapy-sat",
+    version="2.0.0.dev0",
+    author="Flamapy",
+    author_email="flamapy@us.es",
+    description="flamapy-sat is a plugin to flamapy module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/flamapy/pysat_metamodel",
+    packages=setuptools.find_namespace_packages(include=['flamapy.*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    install_requires=[
+        'flamapy-fw~=2.0.0.dev0',
+        'flamapy-fm~=2.0.0.dev0',
+        'python-sat>=0.1.7.dev6'
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-mock',
+            'prospector',
+            'mypy',
+            'coverage',
+        ]
+    },
+    dependency_links=[
+        'flamapy~=1.6.0.dev0',
+    ]
+)
