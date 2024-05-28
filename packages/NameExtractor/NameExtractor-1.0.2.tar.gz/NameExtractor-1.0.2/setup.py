337250ee85f9bb639df9ca4b28c6cd24e7a8a9d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 05:04:28 +0000

    Add package NameExtractor/1.0.2/NameExtractor-1.0.2.tar.gz

---
+++ packages/NameExtractor/NameExtractor-1.0.2.tar.gz/NameExtractor-1.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='NameExtractor',
+    version='1.0.2',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    author="Defying Gravity",
+    description="Name Extractor is a small AI program to extract name and gender from characters in text.",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/xDefyingGravity/Name-Extractor/'
+)
