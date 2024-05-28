d53d9588491cb3317747c78515741cce3cb98c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 16:04:30 +0000

    Add package dashtable2/1.4.14/dashtable2-1.4.14-py3-none-any.whl

---
+++ packages/dashtable2/dashtable2-1.4.14-py3-none-any.whl/dashtable/data2rst/__init__.py
@@ -0,0 +1,8 @@
+
+from ..dashutils.aliases import DATA_SPAN, DATA_SPANS
+from .aliases import CANDIDATES_MASK_CREATOR, CHECKED_MASK_CREATOR, DATA_TABLE
+
+from .merge_all_cells import get_checked_mask_v1, get_checked_mask_v2, \
+    get_candidates_mask_v1, get_candidates_mask_v2
+
+from .data2rst import data2rst
