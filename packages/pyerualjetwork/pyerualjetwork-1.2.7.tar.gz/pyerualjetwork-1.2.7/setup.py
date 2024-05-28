f9fa9e8a82b826f86d68329b801c28018958405b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 05:05:11 +0000

    Add package pyerualjetwork/1.2.7/pyerualjetwork-1.2.7.tar.gz

---
+++ packages/pyerualjetwork/pyerualjetwork-1.2.7.tar.gz/pyerualjetwork-1.2.7/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "pyerualjetwork",
+      version = "1.2.7",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "Advanced python deep learning library. 'ThresholdSigns' changed to 'MembranThresholds' and 'ThresholdValues' changed to 'MembranPotentials'. (Documentation in desc. Examples in GİTHUB: https://github.com/HCB06/PyerualJetwork)",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'pruning learning artficial neural networks']
+      
+      )
