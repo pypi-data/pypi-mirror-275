a227361b92ae5c606e5b6a054d48dbb06b8d4cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 04:04:03 +0000

    Add package pip2deb/2.0/pip2deb-2.0.tar.gz

---
+++ packages/pip2deb/pip2deb-2.0.tar.gz/pip2deb-2.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst', 'r') as f:
+        content = f.read()
+    return content
+
+setup(
+    name="pip2deb",
+    version="2.0",
+    decription="A Simple Tool to Pack Python Project into a Debian Package, For Debian/Ubuntu and Termux",
+    long_description=readme(),
+    url='https://github.com/pacspedd/pip2deb',
+    author="PacSpedd",
+    author_email="pacspedd@outlook.com",
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8'
+    ],
+    packages=find_packages(),
+    scripts=['pip2deb']
+)
