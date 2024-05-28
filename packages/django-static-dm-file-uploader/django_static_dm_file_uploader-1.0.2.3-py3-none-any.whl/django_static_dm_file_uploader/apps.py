3da52ed6b8e69df96bb6242f10a9dcc7464fdac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 06:04:15 +0000

    Add package django-static-dm-file-uploader/1.0.2.3/django_static_dm_file_uploader-1.0.2.3-py3-none-any.whl

---
+++ packages/django-static-dm-file-uploader/django_static_dm_file_uploader-1.0.2.3-py3-none-any.whl/django_static_dm_file_uploader/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjangoStaticDmFileUploader(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_static_dm_file_uploader"
