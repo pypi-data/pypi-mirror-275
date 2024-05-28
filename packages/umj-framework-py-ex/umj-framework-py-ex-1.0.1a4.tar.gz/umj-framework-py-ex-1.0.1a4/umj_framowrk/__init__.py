8a42de91df02959299625f0e89bdcfe495fb8150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 15:04:21 +0000

    Add package umj-framework-py-ex/1.0.1a4/umj-framework-py-ex-1.0.1a4.tar.gz

---
+++ packages/umj-framework-py-ex/umj-framework-py-ex-1.0.1a4.tar.gz/umj-framework-py-ex-1.0.1a4/umj_framowrk/__init__.py
@@ -0,0 +1,10 @@
+from .kernel.interpreter import Interpreter
+from .kernel.builder import Builder
+from .kernel.common import get_object_type
+
+
+__all__ = [
+    'Interpreter',
+    'Builder',
+    'get_object_type'
+]
