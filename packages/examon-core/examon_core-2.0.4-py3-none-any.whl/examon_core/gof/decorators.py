6950a5f7bfda968765b2a7655442f27b19c41ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 03:03:50 +0000

    Add package examon-core/2.0.4/examon_core-2.0.4-py3-none-any.whl

---
+++ packages/examon-core/examon_core-2.0.4-py3-none-any.whl/examon_core/gof/decorators.py
@@ -0,0 +1,124 @@
+# **Behavioral Patterns**
+#
+# 13. Observer Pattern
+# 16. Interpreter Pattern
+# 17. Command Pattern
+# 18. Memento Pattern
+# 20. Mediator Pattern
+# 21. Iterator Pattern
+# 22. Visitor Pattern
+
+
+# Behavioural
+def chain_of_responsibility():
+    def inner_function(function):
+        return function
+
+    return inner_function
+
+
+def template_method():
+    def inner_function(function):
+        return function
+
+    return inner_function
+
+
+def strategy():
+    def inner_function(function):
+        return function
+
+    return inner_function
+
+
+def state():
+    def inner_function(function):
+        return function
+
+    return inner_function
+
+
+# Creational Patterns
+def factory_method():
+    def inner_function(function):
+        return function
+
+    return inner_function
+
+
+def singleton():
+    def inner_function(function):
+        return function
+
+    return inner_function
+
+
+def abstract_factory():
+    def inner_function(function):
+        return function
+
+    return inner_function
+
+
+def builder():
+    def inner_function(function):
+        return function
+
+    return inner_function
+
+
+def prototype():
+    def inner_function(function):
+        return function
+
+    return inner_function
+
+
+# Structural
+def flyweight():
+    def inner_function(function):
+        return function
+
+    return inner_function
+
+
+def adapter():
+    def inner_function(function):
+        return function
+
+    return inner_function
+
+
+def facade():
+    def inner_function(function):
+        return function
+
+    return inner_function
+
+
+def proxy():
+    def inner_function(function):
+        return function
+
+    return inner_function
+
+
+def decorator():
+    def inner_function(function):
+        return function
+
+    return inner_function
+
+
+def bridge():
+    def inner_function(function):
+        return function
+
+    return inner_function
+
+
+def composite():
+    def inner_function(function):
+        return function
+
+    return inner_function
