4b0194972bdd931c2de6504447afc1a95c76ba29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 04:04:39 +0000

    Add package quickQrLib/0.2.16/quickQrLib-0.2.16.tar.gz

---
+++ packages/quickQrLib/quickQrLib-0.2.16.tar.gz/quickQrLib-0.2.16/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name='quickQrLib',
+    version='0.2.16',
+    packages=find_packages(),
+    
+    install_requires=[
+        'djangorestframework-simplejwt==5.3.1',
+        'django==5.0.6',
+        'boto3==1.34.113'
+    ]
+)
