09cb37638c89e0842e781337ba651c0728ff3662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 03:03:59 +0000

    Add package lyyinit/1.4/lyyinit-1.4.tar.gz

---
+++ packages/lyyinit/lyyinit-1.4.tar.gz/lyyinit-1.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='lyyinit',
+    version='1.4',
+    author='lyy',
+    author_email='',
+    description='userfull application for  lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=[
+
+
+    ],
+)
