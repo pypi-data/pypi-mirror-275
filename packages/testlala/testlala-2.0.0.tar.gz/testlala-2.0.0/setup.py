33d90a608c4721e902e90853b763927f82289662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 03:04:06 +0000

    Add package testlala/2.0.0/testlala-2.0.0.tar.gz

---
+++ packages/testlala/testlala-2.0.0.tar.gz/testlala-2.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testlala',
+    version='2.0.0',
+    description='A simple Python package',
+    packages=find_packages(),
+    author='Marcus',
+    author_email='marcusongkiansiong@email.com',
+    url='https://github.com/yourusername/testlala',
+    license='MIT',
+)
