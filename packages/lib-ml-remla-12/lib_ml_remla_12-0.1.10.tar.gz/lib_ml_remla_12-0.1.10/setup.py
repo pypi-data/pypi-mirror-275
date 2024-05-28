168305f7a39f4dca46f142dade1563fd637cd964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 03:04:02 +0000

    Add package lib-ml-remla-12/0.1.10/lib_ml_remla_12-0.1.10.tar.gz

---
+++ packages/lib-ml-remla-12/lib_ml_remla_12-0.1.10.tar.gz/lib_ml_remla_12-0.1.10/setup.py
@@ -0,0 +1,18 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='lib-ml-remla-12',
+    version='v0.1.10',  # Use a versioning scheme
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    url='https://github.com/remla24-team12/lib_ml',
+    install_requires=[
+        "keras-preprocessing==1.1.2",
+        "keras==3.2.1",
+        "scikit-learn==1.4.2",
+        "pandas==2.2.2"
+    ],
+    extras_requires={"dvc==3.50.1": ["gdrive"]}
+)
