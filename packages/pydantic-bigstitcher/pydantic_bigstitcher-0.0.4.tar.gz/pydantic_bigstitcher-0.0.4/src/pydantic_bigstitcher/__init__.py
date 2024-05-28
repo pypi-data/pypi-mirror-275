1413544213cd55178ef9b5439afebee7965176ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 03:03:55 +0000

    Add package pydantic-bigstitcher/0.0.4/pydantic_bigstitcher-0.0.4.tar.gz

---
+++ packages/pydantic-bigstitcher/pydantic_bigstitcher-0.0.4.tar.gz/pydantic_bigstitcher-0.0.4/src/pydantic_bigstitcher/__init__.py
@@ -0,0 +1,144 @@
+from __future__ import annotations
+
+from typing import Literal
+
+from pydantic_xml import BaseXmlModel, attr, element
+
+
+class BasePath(BaseXmlModel):
+    typ: Literal["relative", "absolute"] = attr(name="type")
+    path: str | None = element(default=None)
+
+class ZGroup(BaseXmlModel, tag="zgroup"):
+    setup: str = attr()
+    timepoint: str = attr()
+    path: str | None = element(default=None)
+
+
+class ZGroups(BaseXmlModel):
+    zgroups: list[ZGroup] = element(tag="zgroup")
+
+
+class Zarr(BaseXmlModel):
+    typ: str = attr(name="type")
+    path: str
+
+class ZarrImageLoader(BaseXmlModel, tag="ImageLoader"):
+    fmt: str = attr(name="format")
+    version: str = attr()
+    s3bucket: str | None = element(default=None)
+    zarr: Zarr = element(tag="zarr")
+    zgroups: ZGroups = element(tag="zgroups")
+
+class VoxelSize(BaseXmlModel):
+    unit: str = element()
+    size: str = element()
+
+
+class Attributes(BaseXmlModel):
+    illumination: str = element()
+    channel: str = element()
+    tile: str = element()
+    angle: str = element()
+
+
+class ViewSetup(BaseXmlModel):
+    ident: str = element(tag="id")
+    name: str = element()
+    size: str = element()
+    voxel_size: VoxelSize = element(tag="voxelSize")
+    attributes: Attributes
+
+
+class ViewSetups(BaseXmlModel):
+    view_setup: list[ViewSetup] = element(tag="ViewSetup")
+
+
+class PatternTimePoints(BaseXmlModel, tag="Timepoints"):
+    typ: Literal["pattern"] = attr(name="type")
+    timepoints: list[str] = element(tag="integerpattern")
+
+
+class MissingView(BaseXmlModel):
+    setup: str = attr()
+    timepoint: str = attr()
+
+
+class MissingViews(BaseXmlModel):
+    views: list[MissingView] = element("MissingView", default=None)
+
+
+class SequenceDescription(BaseXmlModel):
+    """
+    https://github.com/bigdataviewer/spimdata/blob/46c3878baef80cc4170a33012c8281481dbbfcb2/src/main/java/mpicbg/spim/data/generic/sequence/AbstractSequenceDescription.java#L52
+    """
+
+    image_loader: ZarrImageLoader
+    view_setups: ViewSetups = element(tag="ViewSetups")
+    time_points: PatternTimePoints = element(tag="Timepoints")
+    missing_views: MissingViews = element(tag="MissingViews")
+
+
+class AffineViewTransform(BaseXmlModel):
+    typ: Literal["affine"] = attr(name="type")
+    name: str = element(tag="Name")
+    affine: str = element(tag="affine")
+
+
+class ViewRegistration(BaseXmlModel):
+    """
+    https://github.com/bigdataviewer/spimdata/blob/master/src/main/java/mpicbg/spim/data/registration/ViewRegistrations.java#L47
+    """
+
+    timepoint: str = attr()
+    setup: str = attr()
+    view_transforms: list[AffineViewTransform] = element(tag="ViewTransform")
+
+class ViewRegistrations(BaseXmlModel):
+    elements: list[ViewRegistration] = element(tag='ViewRegistration')
+
+class SpimData(BaseXmlModel):
+    """
+    https://github.com/bigdataviewer/spimdata/blob/46c3878baef80cc4170a33012c8281481dbbfcb2/src/main/java/mpicbg/spim/data/generic/AbstractSpimData.java#L36
+    """
+
+    version: Literal["0.2"] = attr(version="0.2")
+    base_path: BasePath = element(tag='BasePath')
+    sequence_description: SequenceDescription = element(tag='SequenceDescription')
+    view_registrations: ViewRegistrations = element(tag="ViewRegistrations")
+
+
+class ViewInterestPointsFile(BaseXmlModel):
+    timepoint: str = attr()
+    setup: str = attr()
+    label: str = attr()
+    params: str = attr()
+
+class ViewInterestPoints(BaseXmlModel):
+    data: list[ViewInterestPointsFile] = element(tag='ViewInterestPointsFile')
+
+class BoundingBox(BaseXmlModel):
+    """
+    https://github.com/PreibischLab/multiview-reconstruction/blob/master/src/main/java/net/preibisch/mvrecon/fiji/spimdata/boundingbox/BoundingBox.java#L30
+    """
+    title: str = attr()
+    mininum: str = attr(name='min')
+    maximum: str = attr(name='max')
+
+class BoundingBoxes(BaseXmlModel):
+    """
+    https://github.com/PreibischLab/multiview-reconstruction/blob/master/src/main/java/net/preibisch/mvrecon/fiji/spimdata/boundingbox/BoundingBoxes.java#L29
+    """
+    data: list[BoundingBox] | None = element(tag='BoundingBox', default=None)
+
+class SpimData2(SpimData, tag="SpimData"):
+    """
+    https://github.com/PreibischLab/multiview-reconstruction/blob/master/src/main/java/net/preibisch/mvrecon/fiji/spimdata/SpimData2.java#L64
+    """
+
+    view_interest_points: ViewInterestPoints = element(tag='ViewInterestPoints')
+    bounding_boxes: BoundingBoxes = element(tag='BoundingBoxes')
+    # point_spread_functions: PointSpreadFunctions
+    # stitching_results: StitchingResults
+    # intensity_adjustments: IntensityAdjustments
+
---
+++ packages/pydantic-bigstitcher/pydantic_bigstitcher-0.0.4.tar.gz/pydantic_bigstitcher-0.0.4/tests/test_models.py
@@ -0,0 +1,169 @@
+from xml.etree import ElementTree as etree
+
+import pytest
+from xmldiff import main
+
+from pydantic_bigstitcher import PatternTimePoints, SequenceDescription, SpimData2, ZarrImageLoader, ZGroup
+
+
+def test_decode_zarr_image_loader():
+    data = """
+    <ImageLoader format="bdv.multimg.zarr" version="1.0">
+      <zarr type="absolute">/data/foo_dataset/SPIM.ome.zarr</zarr>
+      <zgroups>
+        <zgroup setup="0" timepoint="0">
+          <path>tile_x_0000_y_0000_z_0000_ch_488.zarr</path>
+        </zgroup>
+      </zgroups>
+    </ImageLoader>
+    """
+    ZarrImageLoader.from_xml(data)
+
+def test_decode_zarr_image_loader_2():
+    data = """<ImageLoader format="bdv.multimg.zarr" version="1.0">
+    <s3bucket>aind-open-data</s3bucket>
+    <zarr type="absolute">
+    foo_708373_2024-04-02_19-49-38_flatfield-correction_2024-04-15_18-15-40/SPIM.ome.zarr
+    </zarr>
+    <zgroups>
+    <zgroup setup="0" timepoint="0">
+    <path>tile_x_0000_y_0000_z_0000_ch_488.zarr</path>
+    </zgroup>
+    <zgroup setup="1" timepoint="0">
+    <path>tile_x_0000_y_0001_z_0000_ch_488.zarr</path>
+    </zgroup>
+    <zgroup setup="2" timepoint="0">
+    <path>tile_x_0000_y_0002_z_0000_ch_488.zarr</path>
+    </zgroup>
+    <zgroup setup="3" timepoint="0">
+    <path>tile_x_0001_y_0000_z_0000_ch_488.zarr</path>
+    </zgroup>
+    <zgroup setup="4" timepoint="0">
+    <path>tile_x_0001_y_0001_z_0000_ch_488.zarr</path>
+    </zgroup>
+    <zgroup setup="5" timepoint="0">
+    <path>tile_x_0001_y_0002_z_0000_ch_488.zarr</path>
+    </zgroup>
+    <zgroup setup="6" timepoint="0">
+    <path>tile_x_0002_y_0000_z_0000_ch_488.zarr</path>
+    </zgroup>
+    <zgroup setup="7" timepoint="0">
+    <path>tile_x_0002_y_0001_z_0000_ch_488.zarr</path>
+    </zgroup>
+    <zgroup setup="8" timepoint="0">
+    <path>tile_x_0002_y_0002_z_0000_ch_488.zarr</path>
+    </zgroup>
+    <zgroup setup="9" timepoint="0">
+    <path>tile_x_0003_y_0000_z_0000_ch_488.zarr</path>
+    </zgroup>
+    <zgroup setup="10" timepoint="0">
+    <path>tile_x_0003_y_0001_z_0000_ch_488.zarr</path>
+    </zgroup>
+    <zgroup setup="11" timepoint="0">
+    <path>tile_x_0003_y_0002_z_0000_ch_488.zarr</path>
+    </zgroup>
+    <zgroup setup="12" timepoint="0">
+    <path>tile_x_0004_y_0000_z_0000_ch_488.zarr</path>
+    </zgroup>
+    <zgroup setup="13" timepoint="0">
+    <path>tile_x_0004_y_0001_z_0000_ch_488.zarr</path>
+    </zgroup>
+    <zgroup setup="14" timepoint="0">
+    </zgroup>
+    </zgroups>
+    </ImageLoader>"""
+    ZarrImageLoader.from_xml(data)
+
+
+def test_decode_zgroup():
+    data = """
+        <zgroup setup="0" timepoint="0">
+          <path>tile_x_0000_y_0000_z_0000_ch_488.zarr</path>
+        </zgroup>"""
+    ZGroup.from_xml(data)
+
+
+def test_decode_timepoints():
+    data = """
+    <Timepoints type="pattern">
+      <integerpattern>0</integerpattern>
+    </Timepoints>
+        """
+    PatternTimePoints.from_xml(data)
+
+
+def test_decode_sequence_description():
+    data = """
+  <SequenceDescription>
+    <ImageLoader format="bdv.multimg.zarr" version="1.0">
+      <zarr type="absolute">/data/foo/bar.ome.zarr</zarr>
+      <zgroups>
+        <zgroup setup="0" timepoint="0">
+          <path>tile_x_0000_y_0000_z_0000_ch_488.zarr</path>
+        </zgroup>
+      </zgroups>
+    </ImageLoader>
+    <ViewSetups>
+      <ViewSetup>
+        <id>0</id>
+        <name>tile_x_0000_y_0000_z_0000_ch_488</name>
+        <size>14192 10640 28672</size>
+        <voxelSize>
+          <unit>µm</unit>
+          <size>0.7480000148631624 0.7479999743009398 1.0</size>
+        </voxelSize>
+        <attributes>
+          <illumination>0</illumination>
+          <channel>0</channel>
+          <tile>0</tile>
+          <angle>0</angle>
+        </attributes>
+      </ViewSetup>
+      <Attributes name="illumination">
+        <Illumination>
+          <id>0</id>
+          <name>0</name>
+        </Illumination>
+      </Attributes>
+      <Attributes name="channel">
+        <Channel>
+          <id>0</id>
+          <name>0</name>
+        </Channel>
+      </Attributes>
+      <Attributes name="tile">
+        <Tile>
+          <id>0</id>
+          <name>tile_x_0000_y_0000_z_0000_ch_488</name>
+        </Tile>
+      </Attributes>
+      <Attributes name="angle">
+        <Angle>
+          <id>0</id>
+          <name>0</name>
+        </Angle>
+      </Attributes>
+    </ViewSetups>
+    <Timepoints type="pattern">
+      <integerpattern>0</integerpattern>
+    </Timepoints>
+    <MissingViews />
+  </SequenceDescription>
+    """
+    observed = SequenceDescription.from_xml(data)
+    observed_xml_str = etree.tostring(etree.fromstring(observed.to_xml()))
+    data_xml_str = etree.tostring(etree.fromstring(data))
+
+    diff_result = main.diff_texts(data_xml_str, observed_xml_str)
+    assert diff_result == []
+
+@pytest.mark.xfail
+def test_encode_decode(demo_xml_0: str) -> None:
+    model = SpimData2.from_xml(demo_xml_0)
+    assert model.to_xml() == demo_xml_0
+
+def test_decode_2(demo_xml_0) -> None:
+    model = SpimData2.from_xml(demo_xml_0)
+    model_xml = model.to_xml()
+
+    assert model_xml == demo_xml_0
