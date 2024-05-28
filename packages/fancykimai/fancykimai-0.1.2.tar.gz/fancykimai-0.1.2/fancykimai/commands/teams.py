89f5ee433825ca09d993362f38f87c61e78a1904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 04:04:29 +0000

    Add package fancykimai/0.1.2/fancykimai-0.1.2.tar.gz

---
+++ packages/fancykimai/fancykimai-0.1.2.tar.gz/fancykimai-0.1.2/fancykimai/commands/teams.py
@@ -0,0 +1,17 @@
+import click
+from fancykimai.functions.kimai import kimai_request
+
+@click.group(name='teams')
+def teams_group():
+    pass
+
+@teams_group.command(name='list')
+@click.option('-o', '--output', type=click.Choice(['table', 'json']), default='table', help='Output format')
+def list_teams(output: str):
+    r = kimai_request('api/teams')
+    if output == 'table':
+        click.echo('ID\tName')
+        for team in r:
+            click.echo(f'{team["id"]}\t{team["name"]}')
+    else:
+        click.echo(r)
