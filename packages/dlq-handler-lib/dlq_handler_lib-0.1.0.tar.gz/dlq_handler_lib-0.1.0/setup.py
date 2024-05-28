f9c7d1eacbb013bf3cafaf6f130d3a604c16cd7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 05:05:03 +0000

    Add package dlq-handler-lib/0.1.0/dlq_handler_lib-0.1.0.tar.gz

---
+++ packages/dlq-handler-lib/dlq_handler_lib-0.1.0.tar.gz/dlq_handler_lib-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dlq_handler_lib",
+    version="0.1.0",
+    description="A library for handling DLQ messages in AWS SQS",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/MarceloJay/lambda-dlq-reprocess-message.git",
+    author='Marcelo Ferreira',
+    author_email='jaytilangus@gmail.com',
+    license="MIT",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "boto3>=1.16.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
