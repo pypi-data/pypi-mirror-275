7502acbd726fc0350dc12f8703d54617ca57c54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 03:04:29 +0000

    Add package titli/0.0.5/titli-0.0.5.tar.gz

---
+++ packages/titli/titli-0.0.5.tar.gz/titli-0.0.5/titli/ids/__init__.py
@@ -0,0 +1,9 @@
+from .kitsune import KitNET
+from .torch_kitnet import TorchKitNET
+from .pytorch_kitsune import PyTorchKitsune
+
+__all__ = [
+    'KitNET',
+    'TorchKitNET',
+    'PyTorchKitsune'
+]
