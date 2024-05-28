5e0f0fef7c9ae8856d6ee7e091699c876e3f45ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 03:03:56 +0000

    Add package bx-django-utils/77/bx_django_utils-77.tar.gz

---
+++ packages/bx-django-utils/bx_django_utils-77.tar.gz/bx_django_utils-77/bx_django_utils_tests/test_project/publish.py
@@ -0,0 +1,28 @@
+"""
+    Helper to publish this Project to PyPi
+"""
+
+from pathlib import Path
+
+from cli_base.cli_tools.subprocess_utils import verbose_check_call
+from manageprojects.utilities.publish import publish_package
+
+import bx_django_utils
+
+
+PACKAGE_ROOT = Path(bx_django_utils.__file__).parent.parent
+
+
+def publish():
+    """
+    Publish to PyPi
+    Call this via:
+        $ poetry run publish
+    """
+    # verbose_check_call('make', 'test')  # don't publish if tests fail
+    verbose_check_call('make', 'fix-code-style')  # don't publish if code style wrong
+
+    publish_package(
+        module=bx_django_utils,
+        package_path=PACKAGE_ROOT,
+    )
