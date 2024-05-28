5fd0f1a204b891b02cd6eeb846c8f51767ea41e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 15:04:19 +0000

    Add package milvus-model/0.2.3/milvus_model-0.2.3-py3-none-any.whl

---
+++ packages/milvus-model/milvus_model-0.2.3-py3-none-any.whl/milvus_model/dense/__init__.py
@@ -0,0 +1,35 @@
+__all__ = [
+    "OpenAIEmbeddingFunction",
+    "SentenceTransformerEmbeddingFunction",
+    "VoyageEmbeddingFunction",
+    "JinaEmbeddingFunction",
+    "OnnxEmbeddingFunction",
+    "CohereEmbeddingFunction"
+]
+
+from milvus_model.utils.lazy_import import LazyImport
+
+jinaai = LazyImport("jinaai", globals(), "milvus_model.dense.jinaai")
+openai = LazyImport("openai", globals(), "milvus_model.dense.openai")
+sentence_transformer = LazyImport("sentence_transformer", globals(), "milvus_model.dense.sentence_transformer")
+voyageai = LazyImport("voyageai", globals(), "milvus_model.dense.voyageai")
+onnx = LazyImport("onnx", globals(), "milvus_model.dense.onnx")
+cohere = LazyImport("cohere", globals(), "milvus_model.dense.cohere")
+
+def JinaEmbeddingFunction(*args, **kwargs):
+    return jinaai.JinaEmbeddingFunction(*args, **kwargs)
+
+def OpenAIEmbeddingFunction(*args, **kwargs):
+    return openai.OpenAIEmbeddingFunction(*args, **kwargs)
+
+def SentenceTransformerEmbeddingFunction(*args, **kwargs):
+    return sentence_transformer.SentenceTransformerEmbeddingFunction(*args, **kwargs)
+
+def VoyageEmbeddingFunction(*args, **kwargs):
+    return voyageai.VoyageEmbeddingFunction(*args, **kwargs)
+
+def OnnxEmbeddingFunction(*args, **kwargs):
+    return onnx.OnnxEmbeddingFunction(*args, **kwargs)
+
+def CohereEmbeddingFunction(*args, **kwargs):
+    return cohere.CohereEmbeddingFunction(*args, **kwargs)
