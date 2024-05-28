80a7c9ee32590cd3669bb91330b92c20dff838fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 05:04:36 +0000

    Add package gpt-computer-assistant/0.2.0/gpt_computer_assistant-0.2.0.tar.gz

---
+++ packages/gpt-computer-assistant/gpt_computer_assistant-0.2.0.tar.gz/gpt_computer_assistant-0.2.0/gpt_computer_assistant/agent/proccess.py
@@ -0,0 +1,112 @@
+from ..llm import *
+from .assistant import *
+
+from .chat_history import *
+
+from ..audio.tts import text_to_speech
+
+from ..audio.record import audio_data
+
+
+from ..gui.signal import signal_handler
+
+import threading
+
+
+from pygame import mixer
+
+
+import time
+import random
+
+
+
+from ..utils.db import system_sound_location, mic_record_location, just_screenshot_path, screenshot_path
+
+
+def process_audio(take_screenshot=True, take_system_audio=False):
+    global audio_data
+
+    
+
+    audio_file = open(mic_record_location, "rb")
+    transcription = get_client().audio.transcriptions.create(
+        model="whisper-1",
+        file=audio_file
+    )
+
+    if take_system_audio:
+        audio_file2 = open(system_sound_location, "rb")
+        transcription2 = get_client().audio.transcriptions.create(
+            model="whisper-1",
+            file=audio_file2
+        )
+
+    
+    llm_input = "USER: "+transcription.text
+
+    if take_system_audio:
+        llm_input += " \n Other of USER: "+transcription2.text
+
+    llm_output = assistant(llm_input, chat_message_history.messages, get_client(), screenshot_path=screenshot_path if take_screenshot else None)
+
+
+
+
+
+    chat_message_history.add_message(llm_output[-1])
+    llm_output = llm_output[-1].content
+
+
+    response_path = text_to_speech(llm_output)
+
+
+    signal_handler.assistant_response_ready.emit()
+
+    def play_audio():
+        mixer.init()
+        mixer.music.load(response_path)
+        mixer.music.play()
+        while mixer.music.get_busy():
+            time.sleep(0.1)
+        signal_handler.assistant_response_stopped.emit()
+    
+    playback_thread = threading.Thread(target=play_audio)
+    playback_thread.start()
+
+
+
+def process_screenshot():
+
+
+    
+    llm_input = "USER: "+"I just take a screenshot. for you to remember. Just say ok."
+    print("LLM INPUT (just screenshot)", llm_input)
+
+    llm_output = assistant(llm_input, chat_message_history.messages, get_client(), screenshot_path=just_screenshot_path)
+
+
+
+
+
+
+    chat_message_history.add_message(llm_output[-1])
+    llm_output = llm_output[-1].content
+
+
+
+    response_path = text_to_speech(llm_output)
+
+
+    signal_handler.assistant_response_ready.emit()
+
+    def play_audio():
+        mixer.init()
+        mixer.music.load(response_path)
+        mixer.music.play()
+        while mixer.music.get_busy():
+            time.sleep(0.1)
+        signal_handler.assistant_response_stopped.emit()
+    
+    playback_thread = threading.Thread(target=play_audio)
+    playback_thread.start()
