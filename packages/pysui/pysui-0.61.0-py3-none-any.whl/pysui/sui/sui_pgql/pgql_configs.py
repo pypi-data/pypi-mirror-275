f92d0e1867311174be1b86a52a53e83e5e3544d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 15:04:30 +0000

    Add package pysui/0.61.0/pysui-0.61.0-py3-none-any.whl

---
+++ packages/pysui/pysui-0.61.0-py3-none-any.whl/pysui/sui/sui_pgql/pgql_configs.py
@@ -0,0 +1,194 @@
+#    Copyright Frank V. Castellucci
+#    SPDX-License-Identifier: Apache-2.0
+
+# -*- coding: utf-8 -*-
+
+"""Sui GraphQL configuration and constraings."""
+
+
+import dataclasses
+from typing import Any, Optional, Union, Callable
+import dataclasses_json
+
+import pysui.sui.sui_pgql.pgql_types as pgql_type
+
+# TODO: Temporary Protocol Config hack, remove when ready
+_QUERY = """
+
+    query {
+        chainIdentifier
+        checkpoints (last: 1) {
+            nodes {
+                sequenceNumber
+                timestamp
+                epoch {
+                        referenceGasPrice
+                    }
+            }
+        }
+        serviceConfig {
+            enabledFeatures
+            maxQueryDepth
+            maxQueryNodes
+            maxOutputNodes
+            maxDbQueryCost
+            defaultPageSize
+            maxPageSize
+            requestTimeoutMs
+            maxQueryPayloadSize
+        }
+      protocolConfig(protocolVersion:44) {
+          protocolVersion
+          configs {
+            key
+            value
+          }
+          featureFlags {
+            key
+            value
+          }
+        }
+    }
+"""
+
+# TODO: Make primary when changes moved through mainnet
+_QUERYE = """
+
+    query {
+        chainIdentifier
+        checkpoints (last: 1) {
+            nodes {
+                sequenceNumber
+                timestamp
+                epoch {
+                        referenceGasPrice
+                    }
+            }
+        }
+        serviceConfig {
+            availableVersions
+            enabledFeatures
+            maxQueryDepth
+            maxQueryNodes
+            maxOutputNodes
+            maxDbQueryCost
+            defaultPageSize
+            maxPageSize
+            mutationTimeoutMs
+            requestTimeoutMs
+            maxQueryPayloadSize
+            maxTypeArgumentDepth
+            maxTypeNodes
+            maxMoveValueDepth
+        }
+      protocolConfig(protocolVersion:44) {
+          protocolVersion
+          configs {
+            key
+            value
+          }
+          featureFlags {
+            key
+            value
+          }
+        }
+    }
+"""
+
+
+@dataclasses_json.dataclass_json(letter_case=dataclasses_json.LetterCase.CAMEL)
+@dataclasses.dataclass
+class ServiceConfigGQL:
+    """Sui GraphQL service controls."""
+
+    enabledFeatures: list[str]
+    maxQueryDepth: int
+    maxQueryNodes: int
+    maxOutputNodes: int
+    defaultPageSize: int
+    maxDbQueryCost: int
+    maxPageSize: int
+    requestTimeoutMs: int
+    maxQueryPayloadSize: int
+
+
+# TODO: Make primary when changes moved through mainnet
+@dataclasses_json.dataclass_json(letter_case=dataclasses_json.LetterCase.CAMEL)
+@dataclasses.dataclass
+class ServiceConfigGQLE:
+    """Sui GraphQL service controls."""
+
+    availableVersions: list[str]
+    mutationTimeoutMs: int
+    maxTypeArgumentDepth: int
+    maxTypeNodes: int
+    maxMoveValueDepth: int
+
+    enabledFeatures: list[str]
+    maxQueryDepth: int
+    maxQueryNodes: int
+    maxOutputNodes: int
+    defaultPageSize: int
+    maxDbQueryCost: int
+    maxPageSize: int
+    requestTimeoutMs: int
+    maxQueryPayloadSize: int
+
+
+@dataclasses_json.dataclass_json(letter_case=dataclasses_json.LetterCase.CAMEL)
+@dataclasses.dataclass
+class CheckpointNodeGQL:
+    sequenceNumber: int
+    timestamp: str
+    epoch: Any
+    reference_gas_price: Optional[str] = None
+
+    def __post_init__(self):
+        """."""
+        if "referenceGasPrice" in self.epoch:
+            self.reference_gas_price = self.epoch["referenceGasPrice"]
+
+
+@dataclasses_json.dataclass_json(letter_case=dataclasses_json.LetterCase.CAMEL)
+@dataclasses.dataclass
+class CheckpointConnectionGQL:
+    nodes: list[CheckpointNodeGQL]
+
+
+@dataclasses_json.dataclass_json(letter_case=dataclasses_json.LetterCase.CAMEL)
+@dataclasses.dataclass
+class SuiConfigGQL:
+    chainIdentifier: str
+    serviceConfig: ServiceConfigGQL
+    protocolConfig: pgql_type.ProtocolConfigGQL
+    checkpoints: CheckpointConnectionGQL
+    gqlEnvironment: Optional[str] = None
+
+    @classmethod
+    def from_query(clz, in_data: dict) -> "SuiConfigGQL":
+        """."""
+        return SuiConfigGQL.from_dict(in_data)
+
+
+# TODO: Make primary when changes moved through mainnet
+@dataclasses_json.dataclass_json(letter_case=dataclasses_json.LetterCase.CAMEL)
+@dataclasses.dataclass
+class SuiConfigGQLE:
+    chainIdentifier: str
+    serviceConfig: ServiceConfigGQLE
+    protocolConfig: pgql_type.ProtocolConfigGQL
+    checkpoints: CheckpointConnectionGQL
+    gqlEnvironment: Optional[str] = None
+
+    @classmethod
+    def from_query(clz, in_data: dict) -> "SuiConfigGQLE":
+        """."""
+        return SuiConfigGQLE.from_dict(in_data)
+
+
+def pgql_config(env: str) -> tuple[str, Callable]:
+    """."""
+    match env:
+        case "devnet":
+            return _QUERYE, SuiConfigGQLE.from_query
+    return _QUERY, SuiConfigGQL.from_query
