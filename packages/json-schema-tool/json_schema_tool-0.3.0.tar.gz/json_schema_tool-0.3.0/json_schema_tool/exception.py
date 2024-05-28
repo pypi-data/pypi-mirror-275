4b40c0470dfef8c35d6590c6ba7903c6ed5bede8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 05:04:31 +0000

    Add package json-schema-tool/0.3.0/json_schema_tool-0.3.0.tar.gz

---
+++ packages/json-schema-tool/json_schema_tool-0.3.0.tar.gz/json_schema_tool-0.3.0/json_schema_tool/exception.py
@@ -0,0 +1,22 @@
+class JsonSchemaToolException(Exception):
+    pass
+
+
+class TypeException(JsonSchemaToolException):
+    pass
+
+
+class InvalidSchemaException(JsonSchemaToolException):
+    pass
+
+
+class JsonPointerException(JsonSchemaToolException):
+    pass
+
+
+class CoverageException(JsonSchemaToolException):
+    pass
+
+
+class PreprocessorException(JsonSchemaToolException):
+    pass
