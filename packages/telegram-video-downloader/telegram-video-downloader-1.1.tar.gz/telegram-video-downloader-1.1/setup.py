b0677a37fa2f320ce58798a74ff7fb1d335fc542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 03:03:59 +0000

    Add package telegram-video-downloader/1.1/telegram-video-downloader-1.1.tar.gz

---
+++ packages/telegram-video-downloader/telegram-video-downloader-1.1.tar.gz/telegram-video-downloader-1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='telegram-video-downloader',
+    version='1.01',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'quart',
+        'telethon',
+    ],
+    author='David Grau Martínez',
+    author_email='fe80grau@gmail.com',
+    description='Command to download or serve HTTP video published on Telegram',
+    keywords='telegram video download',
+    entry_points={
+        'console_scripts': [
+            'telegram-video-downloader=telegram_video_downloader.app:main',
+        ],
+    },
+)
