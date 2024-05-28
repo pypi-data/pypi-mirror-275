03629dd0fe1651cde45d0aae7b6603f38bafc27e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 03:04:09 +0000

    Add package pypdf2htmlex/1.3/pypdf2htmlex-1.3.tar.gz

---
+++ packages/pypdf2htmlex/pypdf2htmlex-1.3.tar.gz/pypdf2htmlex-1.3/pypdf2htmlEX/core.py
@@ -0,0 +1,32 @@
+import subprocess
+
+class PDF():
+    def __init__(self, pdf_file_path:str, drm=None):
+        self.file_path = pdf_file_path    
+        self.drm = drm
+
+    def __make_html(self, args=None):
+
+        def add_extra_options(args, options):
+            if args:
+                options.extend(args)
+                return options
+            else:
+                return options
+
+        if self.drm is True:
+            options = ["pdf2htmlEX", f"{self.file_path}", "--no-drm", "1"]
+            options = add_extra_options(args, options)
+            subprocess.call(options)
+        else:
+            options = ["pdf2htmlEX", f"{self.file_path}"]
+            options = add_extra_options(args, options)
+            subprocess.call(options)
+
+    def to_html(self, file_path=None):
+        if file_path:
+            args = ["-o", file_path]
+            self.__make_html(args)
+        else:
+            self.__make_html()
+        
