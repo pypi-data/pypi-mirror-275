a622c7dc37ba4c28c783dcf6ecb8c8eee4073ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 16:04:28 +0000

    Add package litpy/0.1.0/litpy-0.1.0.tar.gz

---
+++ packages/litpy/litpy-0.1.0.tar.gz/litpy-0.1.0/LitPy/__init__.py
@@ -0,0 +1,11 @@
+# __init__.py
+# from .module import hello_world  # Remove this line
+
+__version__ = '0.1.0'
+__all__ = [
+    'matrix', 'ghost', 'squad', 'swipe', 'drip', 'remaster', 'flip', 'connect', 'add',
+    'subtract', 'mult', 'slice', 'stack', 'average', 'peak', 'low', 'scatter', 'vibes',
+    'merge', 'break_array', 'random_uniform', 'random_normal', 'drop', 'pick', 'rank',
+    'lowkey', 'highkey', 'anchor', 'flex', 'spot', 'studio', 'prime', 'boost', 'ignite',
+    'smooth', 'curve', 'vibe', 'scan', 'extract', 'chill'
+]
