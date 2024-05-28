06009f264751ec233a00dbdcf6abe87894a30e77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 03:03:51 +0000

    Add package PROPRINT/0.1/PROPRINT-0.1.tar.gz

---
+++ packages/PROPRINT/PROPRINT-0.1.tar.gz/PROPRINT-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PROPRINT',
+    version='0.1',
+    packages=find_packages(),
+    author='Rishabh Modi',
+    author_email='Rishabhmodi7631@gmail.com',
+)
