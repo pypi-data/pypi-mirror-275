1a0555fdeee68049741f7e7c2a4217cc5b7b8d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 06:04:04 +0000

    Add package cuminai/0.0.1.dev275/cuminai-0.0.1.dev275.tar.gz

---
+++ packages/cuminai/cuminai-0.0.1.dev275.tar.gz/cuminai-0.0.1.dev275/cuminai/constants.py
@@ -0,0 +1,25 @@
+# Creator constants
+_CUMINAI_CONFIG_DIR = ".cuminai"
+_CUMINAI_CONFIG_FILE = "config.yaml"
+_CUMIN_FILE_NAME = "CUMINFILE.yaml"
+_CUMINAI_CREATOR_HOST = "https://api.creator.cuminai.com"
+
+# Client constants
+_CUMINAI_DUMMY_COLLECTION_NAME = "dummy"
+_CUMINAI_API_KEY_ENV = "CUMINAI_API_KEY"
+_CUMINAI_HOST = "https://api.cuminai.com"
+
+# Knowledge Kinds
+_LINK_KIND = 'LINK'
+
+# Knowledge Visibility
+_PUBLIC_VISIBILITY = 'PUBLIC'
+_PRIVATE_VISIBILITY = 'PRIVATE'
+
+# Tagging
+_GLOBAL_TAGGING = 'global'
+_LOCAL_TAGGING = 'local'
+
+# Default Values
+_DEFAULT_CHUNK_SIZE = 2048
+_DEFAULT_CHUNK_OVERLAP = 100
