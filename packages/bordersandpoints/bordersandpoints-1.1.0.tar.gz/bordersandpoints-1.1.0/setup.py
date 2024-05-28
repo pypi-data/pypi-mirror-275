df7f83305c8b4ddc5a81fce39f7594a25f256247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 05:04:30 +0000

    Add package bordersandpoints/1.1.0/bordersandpoints-1.1.0.tar.gz

---
+++ packages/bordersandpoints/bordersandpoints-1.1.0.tar.gz/bordersandpoints-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='bordersandpoints',
+    packages=find_packages(include=['bordersandpoints']),
+    version='1.1.0',
+    install_requires=['numpy','opencv-python','matplotlib','Pillow','torch','torchvision','scikit-learn'],
+    description='Library for finding border lines with point in 2D video',
+    author='Malla Dhanraj',
+)
