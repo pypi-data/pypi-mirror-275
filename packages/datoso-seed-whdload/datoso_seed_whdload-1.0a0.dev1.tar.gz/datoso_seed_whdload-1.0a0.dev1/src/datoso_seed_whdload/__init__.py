19eb616843830d0c5e45a5faaf9f5af87ce6c699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 14:04:23 +0000

    Add package datoso-seed-whdload/1.0a0.dev1/datoso_seed_whdload-1.0a0.dev1.tar.gz

---
+++ packages/datoso-seed-whdload/datoso_seed_whdload-1.0a0.dev1.tar.gz/datoso_seed_whdload-1.0a0.dev1/src/datoso_seed_whdload/__init__.py
@@ -0,0 +1,6 @@
+"""__init__.py"""
+__all__ = ['__version__', '__author__', '__description__']
+__version__ = '1.0a0.dev1'
+__author__ = 'Lacides Miranda'
+__description__ = 'WHDLoad is a collection of Amiga Roms'
+__prefix__ = 'whdload'
