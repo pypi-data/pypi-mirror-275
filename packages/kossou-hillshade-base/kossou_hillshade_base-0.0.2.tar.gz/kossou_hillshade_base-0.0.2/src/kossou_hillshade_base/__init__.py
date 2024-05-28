7e58089f811b9fb758e1b5bead5d02c93f14a861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 14:04:29 +0000

    Add package kossou-hillshade-base/0.0.2/kossou_hillshade_base-0.0.2.tar.gz

---
+++ packages/kossou-hillshade-base/kossou_hillshade_base-0.0.2.tar.gz/kossou_hillshade_base-0.0.2/src/kossou_hillshade_base/__init__.py
@@ -0,0 +1,13 @@
+#===============================================================================
+# __init__.py
+#===============================================================================
+
+"""Plot hillshade from geospatial elevation TIFF file
+"""
+
+from kossou_hillshade_base.kossou_hillshade_base import (
+    AZIMUTH,
+    ALTITUDE,
+    REPORT,
+    plot_hillshade
+)
