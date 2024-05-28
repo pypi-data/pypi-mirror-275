76f8767b48994d2e1314958be65a57492bdf0aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 14:05:13 +0000

    Add package reb-probability/0.1/reb_probability-0.1.tar.gz

---
+++ packages/reb-probability/reb_probability-0.1.tar.gz/reb_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='reb_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['reb_probability'],
+      author = 'Roy Berg',
+      zip_safe=False)
