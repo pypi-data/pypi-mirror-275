d8e26e8f7a1c39fd2d74974cbc5612ade691c262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 04:04:15 +0000

    Add package librds/1.3.7/librds-1.3.7-py3-none-any.whl

---
+++ packages/librds/librds-1.3.7-py3-none-any.whl/librds/__init__.py
@@ -0,0 +1,7 @@
+from .interface import GroupInterface
+from .comfort import Groups, GroupSequencer
+from .af import AF_Bands, AlternativeFrequencyEntry
+from .generator import GroupGenerator, Group
+from .decoder import GroupDecoder
+librds_version = 1.37
+__version__ = librds_version
