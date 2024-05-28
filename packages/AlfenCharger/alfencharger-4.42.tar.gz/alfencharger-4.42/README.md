971de2a36e5e2a68b78809d7eb1db4a6aebfc55f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 16:04:28 +0000

    Add package AlfenCharger/4.42/alfencharger-4.42.tar.gz

---
+++ packages/AlfenCharger/alfencharger-4.42.tar.gz/alfencharger-4.42/README.md
@@ -0,0 +1,48 @@
+# Alfen Charge station Reader And Controller
+A charge station reader and controller for **Alfen NG9xx** chargers 
+
+This code was used in a home built Home Energy Management System (HEMS), to include an Alfen NG9xx charge station into a smart home. You can read more about it in my blog post: [How I built a HEMS with solar, a battery and a charge station (in Python)](https://medium.com/@harmvandenbrink/how-i-built-a-hems-with-solar-a-battery-and-a-charge-station-in-python-d5b51e60fd1c?source=friends_link&sk=f5e9302a02ea29065c3f677ecf1b8ed8)
+
+# How to use the code
+## Import the class and connect to an Alfen Chargestation
+
+```python
+from alfenChargestation import alfenCharger
+chargestation = alfenCharger('ICU-00001', '192.168.1.X', 0, 20)
+
+```
+
+## Show a single value
+
+```python
+chargestation.readMeasurements()
+print(chargestation.voltage_l1n)
+```
+
+## Control the charge station (single)
+
+```python
+# Sets the current to 10 amps
+chargestation.changeCurrent(10)
+```
+
+## Control a set of chargers in a Smart Charging Network (SCN)
+
+```python
+# Set the current of the entire charging plaza to 42 amps
+chargestation.changeCurrentSCN(42,42,42)
+```
+
+## Change charge using 1 or 3 phases
+
+```python
+# Set the number of phases to charge on
+# !! Please verify that your car is capable of phase switching during charging !!
+changeChargeNumberOfPhases(3)
+```
+
+# Disclaimer
+
+The code within this repository comes with no guarantee, the use of this code is your responsibility.
+
+I take NO responsibility and/or liability for how you choose to use any of the source code available here. By using any of the files available in this repository, you understand that you are AGREEING TO USE AT YOUR OWN RISK.
---
+++ packages/AlfenCharger/alfencharger-4.42.tar.gz/alfencharger-4.42/readAndControlAlfenCharger.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+"""
+A-ChargeStationReaderAndController: A charge station reader for Alfen NG9xx chargers.
+
+MIT License
+
+Copyright (c) 2022 Harm van den Brink
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+__author__  = 'Harm van den Brink'
+__email__   = 'harmvandenbrink@gmail.com'
+__license__ = 'MIT License'
+
+__version__ = '4.42'
+__status__  = 'Production'
+
+####### Imports #######
+
+from alfenchargestation import AlfenCharger
+import time
+
+               #AlfenCharger('ID, 'IP', minCurrent, maxCurrent)
+chargestation = AlfenCharger('Alfen_001', '192.168.1.5', 0, 25)
+
+# Optional: set unit_id
+chargestation.readMeasurements()
+
+print(chargestation.platformtype)
+print(chargestation.meterstate)
+print(chargestation.meterlastvaluetimestamp)
+print(chargestation.metertype)
+print(chargestation.voltage_l1n)
+print(chargestation.voltage_l2n)
+print(chargestation.voltage_l3n)
+print(chargestation.voltage_l1l2)
+print(chargestation.voltage_l2l3)
+print(chargestation.voltage_l3l1)
+print(chargestation.current_n)
+print(chargestation.current_l1)
+print(chargestation.current_l2)
+print(chargestation.current_l3)
+print(chargestation.current_sum)
+print(chargestation.powerfactor_l1)
+print(chargestation.powerfactor_l2)
+print(chargestation.powerfactor_l3)
+print(chargestation.powerfactor_sum)
+print(chargestation.frequency)
+print(chargestation.realpower_l1)
+print(chargestation.realpower_l2)
+print(chargestation.realpower_l3)
+print(chargestation.realpower_sum)
+print(chargestation.apparentpower_l1)
+print(chargestation.apparentpower_l2)
+print(chargestation.apparentpower_l3)
+print(chargestation.apparentpower_sum)
+print(chargestation.reactivepower_l1)
+print(chargestation.reactivepower_l2)
+print(chargestation.reactivepower_l3)
+print(chargestation.reactivepower_sum)
+print(chargestation.realenergydelivered_l1)
+print(chargestation.realenergydelivered_l2)
+print(chargestation.realenergydelivered_l3)
+print(chargestation.realenergydelivered_sum)
+print(chargestation.realenergyconsumed_l1)
+print(chargestation.realenergyconsumed_l2)
+print(chargestation.realenergyconsumed_l3)
+print(chargestation.realenergyconsumed_sum)
+
+chargestation.changeCurrent(16)
+
+### ONLY WHEN USING SCN (Smart Charging Network)
+
+# chargestation.readMeasurementsSCN()
+
+# chargestation.changeCurrentSCN(16,16,16)
+
+# print(f"chargestation.scnname: {chargestation.scnname}")
+# print(f"chargestation.scnsockets: {chargestation.scnsockets}")
+# print(f"chargestation.scntotalconsumptionphasel1: {chargestation.scntotalconsumptionphasel1}")
+# print(f"chargestation.scntotalconsumptionphasel2: {chargestation.scntotalconsumptionphasel2}")
+# print(f"chargestation.scntotalconsumptionphasel3: {chargestation.scntotalconsumptionphasel3}")
+# print(f"chargestation.scnactualmaxcurrentphasel1: {chargestation.scnactualmaxcurrentphasel1}")
+# print(f"chargestation.scnactualmaxcurrentphasel2: {chargestation.scnactualmaxcurrentphasel2}")
+# print(f"chargestation.scnactualmaxcurrentphasel3: {chargestation.scnactualmaxcurrentphasel3}")
+# print(f"chargestation.scnsafemaxcurrent: {chargestation.scnsafemaxcurrent}")
+# print(f"chargestation.scnmodbusslavemaxcurrentenabled: {chargestation.scnmodbusslavemaxcurrentenabled}")
