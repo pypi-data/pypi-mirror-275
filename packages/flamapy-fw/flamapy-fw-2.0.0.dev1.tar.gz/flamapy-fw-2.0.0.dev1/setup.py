3aee2bc06cfe556ed6058a9fa623734898fc49e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 06:04:04 +0000

    Add package flamapy-fw/2.0.0.dev1/flamapy-fw-2.0.0.dev1.tar.gz

---
+++ packages/flamapy-fw/flamapy-fw-2.0.0.dev1.tar.gz/flamapy-fw-2.0.0.dev1/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="flamapy-fw",
+    version="2.0.0.dev1",
+    author="Flamapy",
+    author_email="flamapy@us.es",
+    description="Flamapy is a Python-based AAFM framework that takes into consideration previous AAFM tool designs and enables multi-solver and multi-metamodel support for the integration of AAFM tooling on the Python ecosystem.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/flamapy/core",
+    packages=setuptools.find_namespace_packages(include=['flamapy.*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-mock',
+            'prospector',
+            'mypy',
+            'coverage',
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'flamapy-admin = flamapy.commands:flamapy_admin',
+        ],
+    },
+)
