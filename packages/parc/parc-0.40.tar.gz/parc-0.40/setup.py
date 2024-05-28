e7476a31454308983d03f6a46fb9a295518ef77b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 14:04:53 +0000

    Add package parc/0.40/parc-0.40.tar.gz

---
+++ packages/parc/parc-0.40.tar.gz/parc-0.40/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='parc',
+    version='0.40',  #May 28,2024
+    packages=['parc',],
+    license='MIT',
+    author_email='shobana.venkat88@gmail.com',
+    url='https://github.com/ShobiStassen/PARC',
+    setup_requires=['numpy', 'pybind11'],
+    install_requires=[
+        'pybind11', 'numpy', 'scipy', 'pandas', 'hnswlib', 'igraph',
+        'leidenalg>=0.7.0', 'umap-learn'
+    ],
+    extras_require={
+        "dev": ["pytest", "scikit-learn"]
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
