facabaec8a36d1d512e310b385cc8089a6d500ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 03:03:50 +0000

    Add package txdpy/8.9.1/txdpy-8.9.1.tar.gz

---
+++ packages/txdpy/txdpy-8.9.1.tar.gz/txdpy-8.9.1/txdpy/文本错别字检查.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @File  : 文本错别字检查.py
+# @Time  : 2024/1/18 10:19
+# @Author: 唐旭东
+
+# from pycorrector import Corrector
+# import re
+# from time import sleep
+# import requests
+# import json
+# from .read_data import ReadData
+
+
