8f39dbeb3d44555f2254b884eb311b3fca53b3f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 03:04:29 +0000

    Add package disai-agents/0.3839/disai_agents-0.3839-py3-none-any.whl

---
+++ packages/disai-agents/disai_agents-0.3839-py3-none-any.whl/disai/__init__.py
@@ -0,0 +1,3 @@
+# disai-agents/__init__.py
+from disai.disai_jazz import agent, task, openai_model, arch, utils
+from disai.disai_jazza import arch as jazza_arch
---
+++ packages/disai-agents/disai_agents-0.3839-py3-none-any.whl/disai/disai_jazz/__init__.py
@@ -0,0 +1,6 @@
+# disai/disai_jazz/__init__.py
+from .agent import Agent
+from .task import Task
+from .openai_model import OpenAIModel
+from .arch import SequentialFlow
+from .utils import InputType, OutputType
