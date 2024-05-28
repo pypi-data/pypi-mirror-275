ccda42ffa0d7f81936d9c564660eb696d15d7727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 04:04:27 +0000

    Add package aplr/10.2.1/aplr-10.2.1.tar.gz

---
+++ packages/aplr/aplr-10.2.1.tar.gz/aplr-10.2.1/setup.py
@@ -0,0 +1,42 @@
+import setuptools, sys
+from pathlib import Path
+
+extra_compile_args = []
+extra_link_args = []
+if "win" not in sys.platform:
+    extra_compile_args.append("-fopenmp")
+    extra_compile_args.append("-std=c++17")
+    extra_compile_args.append("-pthread")
+    extra_link_args.append("-fopenmp")
+else:
+    extra_compile_args.append("/openmp:llvm")
+
+sfc_module = setuptools.Extension(
+    name="aplr_cpp",
+    sources=["cpp/pythonbinding.cpp"],
+    include_dirs=["cpp", "dependencies/eigen-3.4.0", "dependencies/pybind11/include"],
+    language="c++",
+    extra_compile_args=extra_compile_args,
+    extra_link_args=extra_link_args,
+)
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="aplr",
+    version="10.2.1",
+    description="Automatic Piecewise Linear Regression",
+    ext_modules=[sfc_module],
+    author="Mathias von Ottenbreit",
+    author_email="ottenbreitdatascience@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["aplr"],
+    install_requires=["numpy>=1.20"],
+    python_requires=">=3.8",
+    classifiers=["License :: OSI Approved :: MIT License"],
+    license="MIT",
+    platforms=["Windows", "Linux"],
+    url="https://github.com/ottenbreit-data-science/aplr",
+)
