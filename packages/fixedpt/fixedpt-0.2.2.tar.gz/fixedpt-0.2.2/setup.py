c83f6f61e3e88c9a76101047fe9c49d6561f6cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 05:04:28 +0000

    Add package fixedpt/0.2.2/fixedpt-0.2.2.tar.gz

---
+++ packages/fixedpt/fixedpt-0.2.2.tar.gz/fixedpt-0.2.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='fixedpt',
+	version='0.2.2',
+
+	url='https://github.com/UnsignedByte/fixedpt',
+	author='Edmund Lam',
+	
+	package_dir={'':'src'},
+	# py_modules=['fixedpt'],
+	packages=find_packages(where='src'),
+)
