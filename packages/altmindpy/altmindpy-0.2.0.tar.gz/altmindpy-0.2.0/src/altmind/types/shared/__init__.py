9e6bcec3490cc967bfc50b5a4df60465ea7c7798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 05:04:52 +0000

    Add package altmindpy/0.2.0/altmindpy-0.2.0.tar.gz

---
+++ packages/altmindpy/altmindpy-0.2.0.tar.gz/altmindpy-0.2.0/src/altmind/types/shared/__init__.py
@@ -0,0 +1,5 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .user_out import UserOut as UserOut
+from .response_message import ResponseMessage as ResponseMessage
+from .messages_response import MessagesResponse as MessagesResponse
