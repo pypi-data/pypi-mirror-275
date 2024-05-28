669fc3ce31a998959d6d47fd7427453f0f7051bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 05:04:58 +0000

    Add package django-sql-explorer/4.3/django_sql_explorer-4.3-py3-none-any.whl

---
+++ packages/django-sql-explorer/django_sql_explorer-4.3-py3-none-any.whl/explorer/views/mixins.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+from django.shortcuts import render
+
+from explorer import app_settings
+
+
+class ExplorerContextMixin:
+
+    def gen_ctx(self):
+        return {
+            "can_view": app_settings.EXPLORER_PERMISSION_VIEW(
+                self.request
+            ),
+            "can_change": app_settings.EXPLORER_PERMISSION_CHANGE(
+                self.request
+            ),
+            "assistant_enabled": app_settings.EXPLORER_AI_API_KEY is not None,
+            "csrf_cookie_name": settings.CSRF_COOKIE_NAME,
+            "csrf_token_in_dom": settings.CSRF_COOKIE_HTTPONLY or settings.CSRF_USE_SESSIONS,
+            "view_name": self.request.resolver_match.view_name,
+        }
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx.update(self.gen_ctx())
+        return ctx
+
+    def render_template(self, template, ctx):
+        ctx.update(self.gen_ctx())
+        return render(self.request, template, ctx)
