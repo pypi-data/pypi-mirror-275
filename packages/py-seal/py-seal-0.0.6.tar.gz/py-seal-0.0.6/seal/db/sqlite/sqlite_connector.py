16503007fffe89da3ef5168af9c9ee47e23863d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 16:05:11 +0000

    Add package py-seal/0.0.6/py-seal-0.0.6.tar.gz

---
+++ packages/py-seal/py-seal-0.0.6.tar.gz/py-seal-0.0.6/seal/db/sqlite/sqlite_connector.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+from seal.config.configuration import Configuration
+from seal.db.connector import DBConnector
+from seal.wrapper.singleton import singleton
+
+
+@singleton
+class SqliteConnector(DBConnector):
+    def __init__(self):
+        self.config = Configuration().config['mysql']
+
+    def get_connection(self):
+        return sqlite3.connect(self.config['database'])
