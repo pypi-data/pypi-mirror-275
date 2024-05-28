a39854378a53186c8dde0c7653cc8022f097eaf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 05:05:13 +0000

    Add package tb1-parser/1.0.5/tb1_parser-1.0.5.tar.gz

---
+++ packages/tb1-parser/tb1_parser-1.0.5.tar.gz/tb1_parser-1.0.5/tb1_parser/signal.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+from .plc_module import PLCModule
+
+
+@dataclass
+class Signal(object):
+    
+    def __init__(self) -> None:
+        '''
+        Дата-класс. Шаблон для наследования.
+        '''
+        self.variable: None | str
+        self.name: None | str
+        # self.formated_name: None | str
+        self.plc_module: None | PLCModule
+        self.plc_channel: None | int
+
+
+    def __repr__(self) -> str:
+        params: str = '(' + '; '.join(f'{key}: {value}' for key, value in self.__dict__.items()) + ')'
+        return self.__class__.__name__ + params
+    
+
+    def isused(self) -> bool:
+        return self.name.lower() != 'резерв'
+
+
+    def isprotected(self) -> bool:
+        pass  # FIXME Заглушка метода для родителя
