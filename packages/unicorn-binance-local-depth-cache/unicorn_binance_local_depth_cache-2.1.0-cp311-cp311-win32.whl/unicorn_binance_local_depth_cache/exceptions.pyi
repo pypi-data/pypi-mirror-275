5a6f6ea7e67975fb5e55c2814d6b1c376beaeea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 16:04:54 +0000

    Add package unicorn-binance-local-depth-cache/2.1.0/unicorn_binance_local_depth_cache-2.1.0-cp311-cp311-win32.whl

---
+++ packages/unicorn-binance-local-depth-cache/unicorn_binance_local_depth_cache-2.1.0-cp311-cp311-win32.whl/unicorn_binance_local_depth_cache/exceptions.pyi
@@ -0,0 +1,13 @@
+from _typeshed import Incomplete
+
+class DepthCacheOutOfSync(Exception):
+    message: str
+    def __init__(self, market: Incomplete | None = ...) -> None: ...
+
+class DepthCacheAlreadyStopped(Exception):
+    message: str
+    def __init__(self, market: Incomplete | None = ...) -> None: ...
+
+class DepthCacheNotFound(Exception):
+    message: str
+    def __init__(self, market: Incomplete | None = ...) -> None: ...
