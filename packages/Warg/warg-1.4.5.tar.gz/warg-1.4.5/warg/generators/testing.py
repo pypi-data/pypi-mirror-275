0755afbe65ae6612ad589543e29581832f8e7d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 03:04:29 +0000

    Add package Warg/1.4.5/warg-1.4.5.tar.gz

---
+++ packages/Warg/warg-1.4.5.tar.gz/warg-1.4.5/warg/generators/testing.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import itertools
+from typing import Iterator, Optional
+
+__all__ = ["peek"]
+
+
+def peek(generator: Iterator) -> Optional[itertools.chain]:
+    try:
+        return itertools.chain((next(generator),), generator)
+    except StopIteration:
+        return None
+
+
+if __name__ == "__main__":
+    print(peek(iter(range(0))))
+    print(peek(iter(range(1))))
