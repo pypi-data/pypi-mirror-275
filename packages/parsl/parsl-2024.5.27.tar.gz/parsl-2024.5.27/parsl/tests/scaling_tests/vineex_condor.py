c7378a2a44ce5c09e85828ffcb18343f9d8e7419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 14:04:48 +0000

    Add package parsl/2024.5.27/parsl-2024.5.27.tar.gz

---
+++ packages/parsl/parsl-2024.5.27.tar.gz/parsl-2024.5.27/parsl/tests/scaling_tests/vineex_condor.py
@@ -0,0 +1,10 @@
+from parsl.config import Config
+from parsl.executors.taskvine import TaskVineExecutor
+from parsl.executors.taskvine import TaskVineManagerConfig
+from parsl.providers import CondorProvider
+
+config = Config(
+    executors=[TaskVineExecutor(manager_config=TaskVineManagerConfig(port=50055),
+                                provider=CondorProvider(),
+                                )]
+)
