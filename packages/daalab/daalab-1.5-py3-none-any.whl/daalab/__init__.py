df8379b5b9254e317fa4efbfc9d897f81619bd7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 14:05:42 +0000

    Add package daalab/1.5/daalab-1.5-py3-none-any.whl

---
+++ packages/daalab/daalab-1.5-py3-none-any.whl/daalab/__init__.py
@@ -0,0 +1,41 @@
+from .codes import print_react_form, print_react_timetable, print_react_func, print_react_sp, print_react_routes, print_react_es5_es6, print_form_validation, print_bootstrap_table, print_routing , print_routingserver, print_es5vses6 , print_funclass , print_mongo , print_formval
+
+
+form = print_react_form
+table = print_react_timetable
+func_class = print_react_func
+states_props = print_react_sp
+routes = print_react_routes
+es5_es6 = print_react_es5_es6
+form_valid = print_form_validation
+bootstrap_table = print_bootstrap_table
+routing = print_routing
+routingserver = print_routingserver
+es5vses6 = print_es5vses6 
+funclass =  print_funclass 
+mongo =  print_mongo
+formval = print_formval
+
+def show():
+    functions = {
+        "form": "Print React Form",
+        "table": "Print React Timetable",
+        "func_class": "Print React Function and Class",
+        "states_props": "Print React States and Props",
+        "routes": "Print React Routes",
+        "es5_es6": "Print React ES5 and ES6",
+        "form_valid": "Print Form Validation",
+        "bootstrap_table": "Print Bootstrap Table",
+        "routing": "print routing using react",
+        "routingserver" : "print the node server for routing",
+        "es5vses6":"prints es5 and es6",
+        "funclass":"prints functional and class comps",
+        "mongo": "prints mongo CRUD operations",
+        "formval" : "form validation using normal html and script"
+    }
+
+    print("Available functions in the daalab library:")
+    for name, description in functions.items():
+        print(f"{name}: {description}")
+
+
