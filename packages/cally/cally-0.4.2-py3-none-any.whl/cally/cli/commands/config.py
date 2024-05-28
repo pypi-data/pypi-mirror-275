e33b11db7b3d117ed97d690d85710870f0a94469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 16:04:29 +0000

    Add package cally/0.4.2/cally-0.4.2-py3-none-any.whl

---
+++ packages/cally/cally-0.4.2-py3-none-any.whl/cally/cli/commands/config.py
@@ -0,0 +1,20 @@
+import click
+import yaml
+
+from ..config import CallyConfig, service_options
+
+
+@click.group()
+def config() -> None:
+    pass
+
+
+@click.command()
+@service_options
+@click.pass_obj
+def print_service(config: CallyConfig):
+    """Prints the service config as YAML"""
+    click.secho(yaml.dump(config.settings.to_dict()))
+
+
+config.add_command(print_service)
