11c63dc828dbdab4b985381226b8c6b30c8cffb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 14:05:25 +0000

    Add package pyregrws/0.1.5/pyregrws-0.1.5.tar.gz

---
+++ packages/pyregrws/pyregrws-0.1.5.tar.gz/pyregrws-0.1.5/README.md
@@ -0,0 +1,17 @@
+# pyregrws
+
+[![GitHub Workflow Status](https://img.shields.io/github/actions/workflow/status/jsenecal/pyregrws/ci.yml?label=CI&style=for-the-badge)](https://github.com/jsenecal/pyregrws/actions/workflows/ci.yml)[![PyPI](https://img.shields.io/pypi/v/pyregrws?style=for-the-badge)](https://pypi.org/project/pyregrws/)[![Codecov](https://img.shields.io/codecov/c/github/jsenecal/pyregrws?style=for-the-badge)](https://codecov.io/github/jsenecal/pyregrws)
+
+## Currently Supported Payloads
+
+- [POC](https://www.arin.net/resources/manage/regrws/payloads/#poc-payload)
+- [Customer](https://www.arin.net/resources/manage/regrws/payloads/#customer-payload)
+- [ORG](https://www.arin.net/resources/manage/regrws/payloads/#org-payload)
+- [NET Block](https://www.arin.net/resources/manage/regrws/payloads/#net-block-payload)
+- [NET](https://www.arin.net/resources/manage/regrws/payloads/#net-payload)
+- [POC Link](https://www.arin.net/resources/manage/regrws/payloads/#poc-link-payload)
+
+- [Ticketed Request Payload](https://www.arin.net/resources/manage/regrws/payloads/#ticketed-request-payload)
+- [Ticket Payload](https://www.arin.net/resources/manage/regrws/payloads/#ticket-payload)
+
+- [Error](https://www.arin.net/resources/manage/regrws/payloads/#error-payload)
---
+++ packages/pyregrws/pyregrws-0.1.5.tar.gz/pyregrws-0.1.5/regrws/api/manager.py
@@ -0,0 +1,91 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Literal
+
+from regrws.api.core import Response
+from regrws.arin_xml_encoder import ARINXmlEncoder
+
+if TYPE_CHECKING:
+    from regrws.api.core import Api, Session
+    from regrws.models.base import BaseModel
+
+
+class BaseManager:
+    def __init__(self, api: Api, model: type[BaseModel]) -> None:
+
+        self.model = model
+        self.api = api
+        self.url_params = {"apikey": self.api.apikey.get_secret_value()}
+
+    @property
+    def endpoint_url(self):
+        if self.api and self.model._endpoint:
+            return f"{self.api.base_url}{self.model._endpoint}"
+
+    def _do(
+        self,
+        verb: Literal["get", "post", "put", "delete"],
+        url: str,
+        data: bytes | None = None,
+        return_type: type[BaseModel] | None = None,
+    ):
+        # prevent circular import
+        from regrws.api.core import Session
+        from regrws.models import Error
+
+        handlers = {200: return_type or self.model}
+        handlers.update({i: Error for i in [400, 401, 403, 404, 405, 406, 409]})
+        with Session(handlers) as session: # type: ignore
+            session_method = getattr(session, verb)
+            res: Response = session_method(
+                url, params=self.url_params, data=data
+            )  # type: ignore
+            res.raise_for_unknown_status()
+
+            if res.instance:
+                related_model = res.instance.__class__
+                res.instance.manager = related_model._manager_class(api=self.api, model=related_model)  # type: ignore
+            return res.instance
+
+    def create(self, return_type: type[BaseModel] | None = None, *args, **kwargs):
+        instance = self.model(*args, **kwargs)
+        instance.manager = self
+        url = instance.absolute_url
+        if url:
+            return self._do(
+                "post",
+                url,
+                instance.to_xml(
+                    encoder=ARINXmlEncoder(), encoding="UTF-8", skip_empty=True
+                ),  # type: ignore
+                return_type,
+            )
+
+    # retrieve
+    def from_handle(self, handle: str):
+        if self.endpoint_url:
+            handle = handle.upper()
+            url = self.endpoint_url + f"/{handle}"
+            return self._do("get", url)
+
+    # update
+    def save(self, instance: BaseModel):
+        url = instance.absolute_url
+        if url:
+            return self._do(
+                "put",
+                url,
+                instance.to_xml(
+                    encoder=ARINXmlEncoder(), encoding="UTF-8", skip_empty=True
+                ),  # type: ignore
+            )
+
+    # delete
+    def delete(
+        self,
+        instance: type[BaseModel],
+        return_type: type[BaseModel] | None = None,
+    ):
+        url = str(instance.absolute_url)
+        if url:
+            return self._do("delete", url, return_type=return_type)
