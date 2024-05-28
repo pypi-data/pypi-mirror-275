7820c983b3c8f004284a4fbf0eb4680b3ffa4650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-28 15:04:45 +0000

    Add package cerebrumscanner-hello/0.0.1b9/cerebrumscanner_hello-0.0.1b9.tar.gz

---
+++ packages/cerebrumscanner-hello/cerebrumscanner_hello-0.0.1b9.tar.gz/cerebrumscanner_hello-0.0.1b9/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_namespace_packages, setup
+
+setup(
+    name="cerebrumscanner_hello",
+    version="0.0.1.b9",
+    package_dir={"": "src"},  # Specify source code directory
+    packages=find_namespace_packages(where="src"),
+    install_requires=[],
+)
