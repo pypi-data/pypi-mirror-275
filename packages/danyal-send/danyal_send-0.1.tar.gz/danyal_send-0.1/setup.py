b6fbe742f7fc3af9047ee86b204691d1d436584c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 04:04:30 +0000

    Add package danyal-send/0.1/danyal_send-0.1.tar.gz

---
+++ packages/danyal-send/danyal_send-0.1.tar.gz/danyal_send-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='danyal_send',  # نام جدید پروژه
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    author='Danyal',
+    author_email='danyalsoltani87@gmail.com',
+    description='until testing',
+    url='https://github.com/Danyalss/my_library',
+)
