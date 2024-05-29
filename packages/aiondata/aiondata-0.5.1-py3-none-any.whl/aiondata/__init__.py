70c5c9e193b8c477e60cca64ee26004f61ced1c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 03:05:56 +0000

    Add package aiondata/0.5.1/aiondata-0.5.1-py3-none-any.whl

---
+++ packages/aiondata/aiondata-0.5.1-py3-none-any.whl/aiondata/__init__.py
@@ -0,0 +1,26 @@
+from .bindingdb import BindingDB, BindingAffinity
+from .protein_structure import (
+    FoldswitchProteinsTableS1A,
+    FoldswitchProteinsTableS1B,
+    FoldswitchProteinsTableS1C,
+    CodNas91,
+    PDBHandler,
+)
+from .moleculenet import (
+    Tox21,
+    ToxCast,
+    ESOL,
+    FreeSolv,
+    Lipophilicity,
+    QM7,
+    QM8,
+    QM9,
+    MUV,
+    HIV,
+    BACE,
+    BBBP,
+    SIDER,
+    ClinTox,
+)
+
+from .weizmann_ccca import Weizmann3CA
