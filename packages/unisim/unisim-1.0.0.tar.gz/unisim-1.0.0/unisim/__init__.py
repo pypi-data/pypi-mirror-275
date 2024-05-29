a5bd9232d9bf66f3376484d3d46133b118439ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 06:05:32 +0000

    Add package unisim/1.0.0/unisim-1.0.0.tar.gz

---
+++ packages/unisim/unisim-1.0.0.tar.gz/unisim-1.0.0/unisim/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2023 Google LLC
+#
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
+__version__ = "1.0.0"
+from .textsim import TextSim  # noqa: F401
