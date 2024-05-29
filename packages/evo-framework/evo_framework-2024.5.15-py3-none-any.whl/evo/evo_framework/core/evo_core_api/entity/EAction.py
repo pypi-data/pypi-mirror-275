dab416e6cd8d4439091ce9e80a54f9fd956d9b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 03:06:18 +0000

    Add package evo-framework/2024.5.15/evo_framework-2024.5.15-py3-none-any.whl

---
+++ packages/evo-framework/evo_framework-2024.5.15-py3-none-any.whl/evo/evo_framework/core/evo_core_api/entity/EAction.py
@@ -0,0 +1,39 @@
+from evo.evo_framework.entity.EObject import EObject
+from evo.evo_framework.core.evo_core_type.entity.EvoMap import EvoMap
+from evo.evo_framework.core.evo_core_api.entity.EnumApiAction import EnumApiAction
+from evo.evo_framework.core.evo_core_api.entity.EActionItem import EActionItem
+from evo.evo_framework.core.evo_core_api.entity.EnumApiType import EnumApiType
+
+class EAction(EObject):
+    #annotation
+  
+    def __init__(self):
+        super().__init__()
+        self.enumApiAction:EnumApiAction = EnumApiAction.NONE
+        self.action:str = None       
+        self.input:bytes = None
+        self.output:bytes = None
+ 
+    def toStream(self, stream):
+        super().toStream(stream)
+        self._doWriteInt(self.enumApiAction.value, stream)
+        self._doWriteStr(self.action, stream)
+        self._doWriteBytes(self.input, stream)
+        self._doWriteBytes(self.output, stream)
+ 
+    def fromStream(self, stream):
+        super().fromStream(stream)
+        self.enumApiAction = EnumApiAction(self._doReadInt(stream))
+        self.action = self._doReadStr(stream)
+        self.input = self._doReadBytes( stream)
+        self.output = self._doReadBytes(stream)
+
+    def __str__(self) -> str:
+        strReturn = "\n".join([
+                            super().__str__(),
+                            f"\tenumApiAction:{self.enumApiAction}",
+                            f"\taction:{self.action}",
+                            f"\tinput: {len(self.input) if self.input is not None else 'None'}",
+                            f"\toutput: {len(self.output) if self.output is not None else 'None'}",
+                            ]) 
+        return strReturn
