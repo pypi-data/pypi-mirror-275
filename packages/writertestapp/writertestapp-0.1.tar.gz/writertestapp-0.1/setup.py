ecd9e7a7f17881825435c0bc4bbf21686ef5e91c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 04:04:47 +0000

    Add package writertestapp/0.1/writertestapp-0.1.tar.gz

---
+++ packages/writertestapp/writertestapp-0.1.tar.gz/writertestapp-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='writertestapp',
+    version='0.1',
+    description='A simple writer app thats printing to the console',
+    package_dir={'': 'simple_writer_app'},
+    packages=find_packages(where='simple_writer_app'),
+    long_description_content_type='text/markdown',
+    author='Aleksander Likus',
+    author_email='test@email.com',
+    license='MIT',
+    )
