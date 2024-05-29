9f0c2262934fde3836dd2d3b5d1533052a2a0994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 03:05:48 +0000

    Add package gpe-lib/0.0.1/gpe_lib-0.0.1.tar.gz

---
+++ packages/gpe-lib/gpe_lib-0.0.1.tar.gz/gpe_lib-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name = "gpe_lib",
+	version = "0.0.1",
+	description = """
+	Funcionalidades bem comumente usadas por mim
+	""",
+	author = "Gustavo Exel",
+	packages = ["gpe_lib","gpe_lib/notebook"],
+	zip_safe = False,
+)
