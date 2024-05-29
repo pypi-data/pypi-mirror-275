6c018c8427f5f7126157917cf71d1503715d3c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 04:04:39 +0000

    Add package jupyterlab-unfold/0.3.1/jupyterlab_unfold-0.3.1.tar.gz

---
+++ packages/jupyterlab-unfold/jupyterlab_unfold-0.3.1.tar.gz/jupyterlab_unfold-0.3.1/src/index.ts
@@ -0,0 +1,94 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IFileBrowserFactory } from '@jupyterlab/filebrowser';
+
+import { IDocumentManager } from '@jupyterlab/docmanager';
+
+import { WidgetTracker } from '@jupyterlab/apputils';
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
+import { ITranslator } from '@jupyterlab/translation';
+
+import { IStateDB } from '@jupyterlab/statedb';
+
+import { FileTreeBrowser, FilterFileTreeBrowserModel } from './unfold';
+
+const SETTINGS_ID = 'jupyterlab-unfold:jupyterlab-unfold-settings';
+
+/**
+ * The file browser namespace token.
+ */
+const namespace = 'filebrowser';
+
+const fileBrowserFactory: JupyterFrontEndPlugin<IFileBrowserFactory> = {
+  id: 'jupyterlab-unfold:FileBrowserFactory',
+  provides: IFileBrowserFactory,
+  requires: [IDocumentManager, ITranslator, ISettingRegistry],
+  optional: [IStateDB],
+  activate: async (
+    app: JupyterFrontEnd,
+    docManager: IDocumentManager,
+    translator: ITranslator,
+    settings: ISettingRegistry,
+    state: IStateDB | null
+  ): Promise<IFileBrowserFactory> => {
+    const setting = await settings.load(SETTINGS_ID);
+
+    const tracker = new WidgetTracker<FileTreeBrowser>({ namespace });
+    const createFileBrowser = (
+      id: string,
+      options: IFileBrowserFactory.IOptions = {}
+    ) => {
+      const model = new FilterFileTreeBrowserModel({
+        translator: translator,
+        auto: options.auto ?? true,
+        manager: docManager,
+        driveName: options.driveName || '',
+        refreshInterval: options.refreshInterval,
+        state:
+          options.state === null
+            ? undefined
+            : options.state || state || undefined
+      });
+      const widget = new FileTreeBrowser({
+        id,
+        model,
+        restore: true,
+        translator,
+        app
+      });
+
+      widget.listing.singleClickToUnfold = setting.get('singleClickToUnfold')
+        .composite as boolean;
+
+      setting.changed.connect(() => {
+        widget.listing.singleClickToUnfold = setting.get('singleClickToUnfold')
+          .composite as boolean;
+      });
+   
+      // check the url in iframe and open
+      app.restored.then(() => {
+        const windowPathname = window.location.pathname;
+        let treeIndex = windowPathname.indexOf('/tree/');
+        let path = windowPathname.substring(treeIndex + '/tree/'.length);
+        path = decodeURIComponent(path);
+        docManager.open(path);
+      });
+
+      // Track the newly created file browser.
+      void tracker.add(widget);
+
+      return widget;
+    };
+
+    return { createFileBrowser, tracker };
+  }
+};
+
+export * from './unfold';
+
+export default fileBrowserFactory;
