deb79d1eae69012d68656d22340a1a7c83da0c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 04:04:39 +0000

    Add package poppy-pop/0.11.3/poppy_pop-0.11.3.tar.gz

---
+++ packages/poppy-pop/poppy_pop-0.11.3.tar.gz/poppy_pop-0.11.3/poppy/pop/migration.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__all__ = ["MigrateError"]
+
+class MigrateError(Exception):
+    """
+    Errors linked to the migration.
+    """
---
+++ packages/poppy-pop/poppy_pop-0.11.3.tar.gz/poppy_pop-0.11.3/poppy/pop/scripts/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from .scripts import *
+
+# vim: set tw=79 :
