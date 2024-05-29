c111649699e4420b3e2d36ec80ddca30dc55460b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 06:05:07 +0000

    Add package nlp-service/2.0.0b2/nlp_service-2.0.0b2.tar.gz

---
+++ packages/nlp-service/nlp_service-2.0.0b2.tar.gz/nlp_service-2.0.0b2/nlp_service/typing.py
@@ -0,0 +1,9 @@
+import nptyping as npt
+from numpy.typing import ArrayLike as ArrayLike
+from spacy.tokens import Doc, Span, Token
+from thinc.types import Floats1d
+
+SpacyObj = Doc | Token | Span
+NumpyVector = npt.NDArray[npt.Shape["*"], npt.Floating]
+NumpyMatrix = npt.NDArray[npt.Shape["*, ..."], npt.Floating]
+SpacyVector = Floats1d
