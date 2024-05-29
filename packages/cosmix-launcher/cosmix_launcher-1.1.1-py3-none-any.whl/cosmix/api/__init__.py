5e6c957e6179351e82608951c6238d8210ec4ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 06:05:47 +0000

    Add package cosmix-launcher/1.1.1/cosmix_launcher-1.1.1-py3-none-any.whl

---
+++ packages/cosmix-launcher/cosmix_launcher-1.1.1-py3-none-any.whl/cosmix/api/__init__.py
@@ -0,0 +1,10 @@
+VERSION = "1.1.1"
+
+from . import config
+from . import instance
+from . import logger
+from . import maven
+from . import mod
+from . import net
+from . import paths
+from . import versions
