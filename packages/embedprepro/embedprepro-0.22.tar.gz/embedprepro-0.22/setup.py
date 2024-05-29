78ee8384612346651eba70c6db2ba3b5c4904c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 04:04:27 +0000

    Add package embedprepro/0.22/embedprepro-0.22.tar.gz

---
+++ packages/embedprepro/embedprepro-0.22.tar.gz/embedprepro-0.22/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='embedprepro',
+    version='0.22',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author_email='elmajjodi.abdeljalil@gmail.com',
+    url='https://github.com/Elma-dev/embedprepro-lib.git/',
+    packages=find_packages(),
+    install_requires=[
+        "click==8.1.7",
+        "matplotlib==3.9.0",
+        "numpy==1.26.4",
+        "pandas==2.2.2",
+        "seaborn==0.13.2",
+        "sentence_transformers==2.7.0",
+        "setuptools==65.6.3",
+        "tqdm==4.66.4",
+        "umap_learn==0.5.6"
+    ],
+    entry_points={
+        'console_scripts': [
+            'embedprepro=preprocessing.cli:cli',
+        ],
+    },
+)
