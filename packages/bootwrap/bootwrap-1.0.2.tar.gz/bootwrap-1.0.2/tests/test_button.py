d42e1159439bbd611a3387b72a2c59c941c7522e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 06:05:18 +0000

    Add package bootwrap/1.0.2/bootwrap-1.0.2.tar.gz

---
+++ packages/bootwrap/bootwrap-1.0.2.tar.gz/bootwrap-1.0.2/tests/test_button.py
@@ -0,0 +1,253 @@
+"""
+Test for bootwrap/components/button.py
+"""
+
+import pytest
+
+from bootwrap import (
+    WebComponent,
+    Button,
+    Panel,
+    Dialog,
+    Text
+)
+from .helper import HelperHTMLParser
+
+
+@pytest.mark.button
+def test_button():
+    button = Button('Somename')
+    actual = HelperHTMLParser.parse(str(button))
+    expected = HelperHTMLParser.parse(f'''
+        <button id="{button.identifier}"
+            class="btn"
+            onclick="return false;">
+            Somename
+        </button>
+    ''')
+    assert actual == expected
+
+    button = Button('Somename').as_primary()
+    actual = HelperHTMLParser.parse(str(button))
+    expected = HelperHTMLParser.parse(f'''
+        <button id="{button.identifier}"
+            class="btn btn-primary"
+            onclick="return false;">
+            Somename
+        </button>
+    ''')
+    assert actual == expected
+
+    button = Button('Somename').as_primary().as_outline()
+    actual = HelperHTMLParser.parse(str(button))
+    expected = HelperHTMLParser.parse(f'''
+        <button id="{button.identifier}"
+            class="btn btn-outline-primary"
+            onclick="return false;">
+            Somename
+        </button>
+    ''')
+    assert actual == expected
+
+    button = Button('Somename').add_classes('someclass')
+    actual = HelperHTMLParser.parse(str(button))
+    expected = HelperHTMLParser.parse(f'''
+        <button id="{button.identifier}"
+            class="btn someclass"
+            onclick="return false;">
+            Somename
+        </button>
+    ''')
+    assert actual == expected
+
+    button = Button('Somename').as_disabled()
+    actual = HelperHTMLParser.parse(str(button))
+    expected = HelperHTMLParser.parse(f'''
+        <button id="{button.identifier}"
+            class="btn"
+            onclick="return false;"
+            disabled>
+            Somename
+        </button>
+    ''')
+    assert actual == expected
+
+
+@pytest.mark.button
+def test_link_button():
+    button = Button('Somename').link('someurl')
+    actual = HelperHTMLParser.parse(str(button))
+    expected = HelperHTMLParser.parse(f'''
+        <a id="{button.identifier}"
+            class="btn"
+            href="someurl"
+            role="button">
+            Somename
+        </a>
+    ''')
+    assert actual == expected
+
+    target = WebComponent()
+    button = Button('Somename').link(target)
+    actual = HelperHTMLParser.parse(str(button))
+    expected = HelperHTMLParser.parse(f'''
+        <a id="{button.identifier}"
+            class="btn" href="#{target.identifier}"
+            role="button">
+            Somename
+        </a>
+    ''')
+    assert actual == expected
+
+    button = Button('Somename').link('someurl').as_disabled()
+    actual = HelperHTMLParser.parse(str(button))
+    expected = HelperHTMLParser.parse(f'''
+        <a id="{button.identifier}"
+            class="btn disabled"
+            href="someurl"
+            role="button">
+            Somename
+        </a>
+    ''')
+    assert actual == expected
+
+    with pytest.raises(TypeError):
+        str(Button('somename').toggle(Text('sometext')))
+
+
+@pytest.mark.button
+def test_toggle_button():
+    target = Dialog('Somatitle', 'Someinfo')
+    button = Button('Somename').toggle(target)
+    actual = HelperHTMLParser.parse(str(button))
+    expected = HelperHTMLParser.parse(f'''
+        <button id="{button.identifier}"
+            class="btn"
+            type="button"
+            data-toggle="modal"
+            data-target="#{target.identifier}"
+            onclick="return false;">
+            Somename
+        </button>
+    ''')
+    assert actual == expected
+
+    target = Panel()
+    button = Button('Somename').toggle(target)
+    actual = HelperHTMLParser.parse(str(button))
+    expected = HelperHTMLParser.parse(f'''
+        <button id="{button.identifier}"
+            class="btn"
+            type="button"
+            data-toggle="tab"
+            data-target="#{target.identifier}"
+            onclick="return false;">
+            Somename
+        </button>
+    ''')
+    assert actual == expected
+
+    target = Panel().as_collapse()
+    button = Button('Somename').toggle(target)
+    actual = HelperHTMLParser.parse(str(button))
+    expected = HelperHTMLParser.parse(f'''
+        <button id="{button.identifier}"
+            class="btn"
+            type="button"
+            data-toggle="collapse"
+            data-target="#{target.identifier}"
+            onclick="return false;">
+            Somename
+        </button>
+    ''')
+    assert actual == expected
+
+
+@pytest.mark.button
+def test_dismiss_button():
+    button = Button('Somename').dismiss()
+    actual = HelperHTMLParser.parse(str(button))
+    expected = HelperHTMLParser.parse(f'''
+        <button id="{button.identifier}"
+            class="btn"
+            type="button"
+            data-dismiss="modal"
+            onclick="return false;">
+            Somename
+        </button>
+    ''')
+    assert actual == expected
+
+
+@pytest.mark.button
+def test_submit_button():
+    button = Button('Somename').submit()
+    actual = HelperHTMLParser.parse(str(button))
+    expected = HelperHTMLParser.parse(f'''
+        <button id="{button.identifier}"
+            class="btn"
+            type="submit">
+            Somename
+        </button>
+    ''')
+    assert actual == expected
+
+
+@pytest.mark.button
+def test_menu_button():
+    actions = [Button('A'), Button('B')]
+    button = Button('Somename').add_menu(*actions)
+    actual = HelperHTMLParser.parse(str(button))
+    expected = HelperHTMLParser.parse('''
+        <div class="btn-group">
+            <button id="..."
+                class="btn dropdown-toggle"
+                type="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+                onclick="return false;">
+                Somename
+            </button>
+            <div class="dropdown-menu dropdown-menu-right">
+                <button id="..."
+                    class="dropdown-item btn"
+                    onclick="return false;">
+                    A
+                </button>
+                <button id="..."
+                    class="dropdown-item btn"
+                    onclick="return false;">
+                    B
+                </button>
+            </div>
+        </div>
+    ''')
+    assert actual == expected
+
+    actions = [Button('A'), Button('B')]
+    button = Button('...').add_menu(*actions)
+    actual = HelperHTMLParser.parse(str(button))
+    expected = HelperHTMLParser.parse('''
+        <div class="btn-group">
+            <i id="..."
+                class="btn fas fa-ellipsis-v"
+                style="cursor: pointer"
+                data-toggle="dropdown"
+                onclick="return false;">
+            </i>
+            <div class="dropdown-menu dropdown-menu-right">
+                <button id="..."
+                    class="dropdown-item btn"
+                    onclick="return false;">
+                    A
+                </button>
+                <button id="..."
+                    class="dropdown-item btn"
+                    onclick="return false;">
+                    B
+                </button>
+            </div>
+        </div>
+    ''')
+    assert actual == expected
---
+++ packages/bootwrap/bootwrap-1.0.2.tar.gz/bootwrap-1.0.2/tests/test_page.py
@@ -0,0 +1,105 @@
+"""
+Test for bootwrap/page.py
+"""
+
+import pytest
+
+from bootwrap import Page, Link, Javascript, Menu, Text
+from .helper import HelperHTMLParser
+
+
+@pytest.mark.page
+def test_page_resources():
+    page = Page(
+        favicon='somename.ico',
+        resources=[
+            Link('https//someresource.com/some.css'),
+            Javascript('https//someresource.com/some.js')
+        ],
+        title='Some Title',
+        menu=Menu(logo='somelogo.jpg'),
+        container=Text('sometext')
+    ).background(
+        color='somevalue',
+        image='somevalue',
+        position='somevalue',
+        size='somevalue',
+        repeat='somevalue',
+        origin='somevalue',
+        clip='somevalue',
+        attachment='somevalue'
+    )
+    actual = HelperHTMLParser.parse(page.__html__())
+    expected = HelperHTMLParser.parse(f''' 
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+                
+                <link rel="stylesheet"
+                    type="text/css"
+                    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
+                <link rel="stylesheet"
+                    type="text/css"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css"/>
+                <link rel="stylesheet"
+                    type="text/css"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.5.0/styles/default.min.css"/>
+                <link rel="icon" type="image/x-icon" href="somename.ico"/>
+                <link rel="stylesheet" type="text/css" href="https//someresource.com/some.css"/>
+
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"
+                    type="application/javascript">
+                </script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"
+                    type="application/javascript">
+                </script>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+                    type="application/javascript">
+                </script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.5.0/highlight.min.js"
+                    type="application/javascript">
+                </script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.5.0/languages/python.min.js"
+                    type="application/javascript">
+                </script>
+                <script src="https//someresource.com/some.js"
+                    type="application/javascript">
+                </script>
+
+                <title>Some Title</title>
+            </head>
+            <body>
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+                    somelogo.jpg
+                    <button class="navbar-toggler"
+                        type="button" data-toggle="collapse"
+                        data-target="#menu"
+                        aria-controls="menu"
+                        aria-expanded="false"
+                        aria-label="Toggle menu">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    
+                    <div class="collapse navbar-collapse" id="menu">
+                        <ul class="navbar-nav mr-auto"></ul>
+                    </div>
+                </nav>
+                <div class="container-fluid">
+                    <span id="...">
+                        sometext
+                    </span>
+                </div>
+            </body>
+            <script>...</script>
+            <style>...</style>
+        </html>
+    ''')  # NOQA
+    assert actual == expected
+
+    with pytest.raises(TypeError):
+        Page(resources=[Text('Some Title')]).__html__()
+
+    with pytest.raises(TypeError):
+        Page(title=Text('Some Title')).__html__()
