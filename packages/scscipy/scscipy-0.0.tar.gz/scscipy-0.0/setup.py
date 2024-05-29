912f8d83465922195dd6fdd21a041be094bc3053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 06:05:07 +0000

    Add package scscipy/0.0/scscipy-0.0.tar.gz

---
+++ packages/scscipy/scscipy-0.0.tar.gz/scscipy-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='scscipy',
+      version='0.0',
+      description='scscipy',
+      packages=['scscipy'],
+      author_email='applepro285@gmail.com',
+      zip_safe=False)
