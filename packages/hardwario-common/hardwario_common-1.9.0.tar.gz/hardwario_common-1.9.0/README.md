d4d39ad6b684ba9fba1be6b8effccfe4f452779d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 04:04:45 +0000

    Add package hardwario-common/1.9.0/hardwario_common-1.9.0.tar.gz

---
+++ packages/hardwario-common/hardwario_common-1.9.0.tar.gz/hardwario_common-1.9.0/README.md
@@ -0,0 +1,20 @@
+<a href="https://www.hardwario.com/"><img src="https://www.hardwario.com/ci/assets/hw-logo.svg" width="200" alt="HARDWARIO Logo" align="right"></a>
+
+# HARDWARIO Common
+
+[![Main](https://github.com/hardwario/py-hardwario-common/actions/workflows/main.yaml/badge.svg)](https://github.com/hardwario/py-hardwario-common/actions/workflows/main.yaml)
+[![Release](https://img.shields.io/github/release/hardwario/py-hardwario-common.svg)](https://github.com/hardwario/py-hardwario-common/releases)
+[![PyPI](https://img.shields.io/pypi/v/hardwario-common.svg)](https://pypi.org/project/hardwario-common/)
+[![License](https://img.shields.io/github/license/hardwario/py-hardwario-common.svg)](https://github.com/hardwario/py-hardwario-common/blob/master/LICENSE)
+[![Twitter](https://img.shields.io/twitter/follow/hardwario_en.svg?style=social&label=Follow)](https://twitter.com/hardwario_en)
+
+This repository contains Python package [hardwario-common](https://pypi.org/project/hardwario-common/)
+
+
+## License
+
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT/) - see the [LICENSE](LICENSE) file for details.
+
+---
+
+Made with &#x2764;&nbsp; by [**HARDWARIO a.s.**](https://www.hardwario.com/) in the heart of Europe.
---
+++ packages/hardwario-common/hardwario_common-1.9.0.tar.gz/hardwario_common-1.9.0/src/hardwario/common/cli.py
@@ -0,0 +1,81 @@
+import os
+from pydoc import cli
+import sys
+import pkgutil
+import importlib
+import click
+from loguru import logger
+import hardwario
+
+DEFAULT_LOG_LEVEL = 'DEBUG'
+DEFAULT_LOG_FILE = os.path.expanduser("~/.hardwario/cli.log")
+
+os.makedirs(os.path.expanduser("~/.hardwario"), exist_ok=True)
+
+
+def version_cb(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    modules = ctx.obj['modules']
+    for name in sorted(modules):
+        click.echo(f'{name} {modules[name]}')
+    ctx.exit()
+
+
+@click.group()
+@click.option('--log-level', type=click.Choice(['debug', 'info', 'success', 'warning', 'error', 'critical']),
+              help='Log level to stderr', default="critical", show_default=True)
+@click.option('--version', is_flag=True, expose_value=False, help='Show the version and exit.', callback=version_cb)
+def cli_root(log_level):
+    '''HARDWARIO Command Line Tool.'''
+    logger.add(sys.stderr, level=log_level.upper())
+
+
+def main():
+    '''Application entry point.'''
+
+    logger.remove()
+    logger.add(DEFAULT_LOG_FILE,
+               format='{time} | {level} | {name}.{function}: {message}',
+               level='TRACE',
+               rotation='10 MB',
+               retention=3)
+
+    logger.debug('Argv: {}', sys.argv)
+
+    modules = {
+        'hardwario.common': hardwario.common.__version__
+    }
+
+    logger.debug('Module: hardwario.common Version: {}', hardwario.common.__version__)
+
+    # discovered and load hardwario cli plugins
+    for finder, name, ispkg in pkgutil.iter_modules(hardwario.__path__, hardwario.__name__ + '.'):
+        if not ispkg or name == 'hardwario.common':
+            continue
+        logger.debug('Discovered module: {}', name)
+        try:
+            module = importlib.import_module(name)
+            # modules[name] = '?'
+            v = module.__version__ if hasattr(module, '__version__') else '?'
+            logger.debug('Module: {} Version {}', name, v)
+            modules[name] = v
+            if hasattr(module, 'cli'):
+                cli_root.add_command(module.cli.cli)
+        except Exception as e:
+            click.secho(f'Warning: Unable to import module {name} reason: {str(e)}', err=True, fg='red')
+            logger.error('Module cli import: {}', e)
+            if os.getenv('DEBUG', False):
+                raise e
+            pass
+
+    try:
+        with logger.catch(reraise=True, exclude=KeyboardInterrupt):
+            cli_root(obj={'modules': modules}, auto_envvar_prefix='HARDWARIO')
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        click.secho(str(e), err=True, fg='red')
+        if os.getenv('DEBUG', False):
+            raise e
+        sys.exit(1)
