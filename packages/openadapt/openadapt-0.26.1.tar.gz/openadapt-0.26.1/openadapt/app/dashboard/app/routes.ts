de92e59334799ed94d22f868060bb8eaf8125994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 04:05:00 +0000

    Add package openadapt/0.26.1/openadapt-0.26.1.tar.gz

---
+++ packages/openadapt/openadapt-0.26.1.tar.gz/openadapt-0.26.1/openadapt/app/dashboard/app/routes.ts
@@ -0,0 +1,19 @@
+type Route = {
+    name: string
+    path: string
+}
+
+export const routes: Route[] = [
+    {
+        name: 'Recordings',
+        path: '/recordings',
+    },
+    {
+        name: 'Settings',
+        path: '/settings',
+    },
+    {
+        name: 'Scrubbing',
+        path: '/scrubbing',
+    }
+]
