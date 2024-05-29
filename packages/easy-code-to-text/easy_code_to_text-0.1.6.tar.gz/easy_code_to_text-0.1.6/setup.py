993b30bd7cd802bf89b2021cd0d22fcd3bf83f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 03:06:42 +0000

    Add package easy-code-to-text/0.1.6/easy_code_to_text-0.1.6.tar.gz

---
+++ packages/easy-code-to-text/easy_code_to_text-0.1.6.tar.gz/easy_code_to_text-0.1.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='easy_code_to_text',
+    version='0.1.6',
+    packages=find_packages(),
+    install_requires=[],
+    author='Benjamin QUINET',
+    author_email='benjamin.quinet59b@gmail.com',
+    description='A Python package to concatenate code files into a single text file.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Benalieur/EASY_CODE_TO_TEXT.git',
+)
