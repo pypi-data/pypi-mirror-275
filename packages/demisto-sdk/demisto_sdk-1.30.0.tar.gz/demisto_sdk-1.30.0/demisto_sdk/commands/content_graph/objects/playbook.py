96b044a0a869c5bacded6d6c7a9696d4958b2db8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 05:04:48 +0000

    Add package demisto-sdk/1.30.0/demisto_sdk-1.30.0.tar.gz

---
+++ packages/demisto-sdk/demisto_sdk-1.30.0.tar.gz/demisto_sdk-1.30.0/demisto_sdk/commands/content_graph/objects/playbook.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from pydantic import Field
+
+from demisto_sdk.commands.common.constants import TEST_PLAYBOOKS_DIR
+from demisto_sdk.commands.content_graph.common import ContentType
+from demisto_sdk.commands.content_graph.objects.base_playbook import BasePlaybook
+
+
+class Playbook(BasePlaybook, content_type=ContentType.PLAYBOOK):  # type: ignore[call-arg]
+    is_test: bool = False
+    tasks: dict = Field({}, exclude=True)
+
+    @staticmethod
+    def match(_dict: dict, path: Path) -> bool:
+        if "tasks" in _dict:
+            if TEST_PLAYBOOKS_DIR not in path.parts and path.suffix == ".yml":
+                return True
+        return False
