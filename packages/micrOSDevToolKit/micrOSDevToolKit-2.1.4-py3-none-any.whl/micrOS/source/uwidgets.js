a31b2b3c3d0a4b8f1fb1ab134f1386ba3accb181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 06:05:55 +0000

    Add package micrOSDevToolKit/2.1.4/micrOSDevToolKit-2.1.4-py3-none-any.whl

---
+++ packages/micrOSDevToolKit/micrOSDevToolKit-2.1.4-py3-none-any.whl/micrOS/source/uwidgets.js
@@ -0,0 +1,105 @@
+// WIDGETS ELEMENTS
+
+const widget_indent = '40px';
+
+function sliderWidget(container, command, options={}) {
+        var title_len = options.title_len || 1;
+        paragraph = document.createElement('p');
+        paragraph.style.textIndent = widget_indent;
+        paragraph.textContent = command.split('/').slice(1, title_len+1).join('-').replace(/=/g, '').replace(':range:', '');
+        // Create a slider
+        element = document.createElement('input');
+        element.style.marginLeft = widget_indent;
+        element.type = 'range';
+        element.min = options.range[0] || 0;
+        element.max = options.range[1] || 100;
+        element.step = options.range[2] || 5;
+        element.value = Math.round((element.max-element.min)/2);
+
+        // Create a span to display the slider value
+        var valueDisplay = document.createElement('span');
+        valueDisplay.textContent = element.value;
+        element.addEventListener('input', function () {
+            valueDisplay.textContent = this.value;
+        });
+        // Add an event listener for API CALL
+        element.addEventListener('change', function () {
+            var call_cmd;
+            if (options.hasOwnProperty('range')) {
+                // NEW
+                call_cmd = command.replace(':range:', this.value);
+            } else {
+                // LEGACY
+                if (command.endsWith("=")) {
+                    call_cmd = `${command}${this.value}`
+                } else {
+                    call_cmd = `${command}/${this.value}`;
+                }
+            }
+            console.log(`[API] Slider exec: ${call_cmd}`);
+            restAPI(call_cmd);
+        });
+        containerAppendChild([paragraph, element, valueDisplay], container);
+}
+
+function buttonWidget(container, command, options={}) {
+        var title_len = options.title_len || 1;
+        paragraph = document.createElement('p');
+        paragraph.style.textIndent = widget_indent;
+        // Create a button
+        element = document.createElement('button');
+        element.style.marginLeft = widget_indent;
+        element.textContent = command.split('/').slice(1, title_len+1).join('-');
+        // Add an event listener for API CALL
+        element.addEventListener('click', function () {
+            var call_cmd;
+            if (options.hasOwnProperty('range')) {
+                // NEW
+                call_cmd = command.replace(':range:', 'None');
+            } else {
+                // LEGACY
+                call_cmd = command;
+            }
+            console.log(`[API] Button clicked: ${call_cmd}`);
+            restAPI(call_cmd);
+        });
+        containerAppendChild([paragraph, element], container);
+}
+
+function textBoxWidget(container, command, options={}) {
+        var title_len = options.title_len || 1;
+        paragraph = document.createElement('p');
+        paragraph.style.textIndent = widget_indent;
+        paragraph.textContent = command.split('/').slice(1, title_len+1).join('-');
+        // Create a small box (div)
+        element = document.createElement('div');
+        element.style.marginLeft = widget_indent;
+        element.style.width = '30%';
+        element.style.height = '60px';
+        element.style.paddingTop = '10px';
+        element.style.paddingLeft = '10px';
+        element.style.border = '2px solid #e7e7e7';
+        element.style.borderRadius = '4px';
+        console.log(`Box update: ${data}`);
+        var call_cmd;
+        if (options.hasOwnProperty('range')) {
+            // NEW
+            call_cmd = command.replace(':range:', 'None');
+        } else {
+            // LEGACY
+            call_cmd = command;
+        }
+        console.log(`[API] textBox exec: ${call_cmd}`);
+        restAPI(call_cmd).then(resp => {
+            console.log(resp.result);
+            element.textContent = JSON.stringify(resp.result, null, 4);
+        containerAppendChild([paragraph, element], container);
+        });
+}
+
+function colorPaletteWidget(container, command, options) {
+    // TODO
+    console.log(`Dummy color widget: ${command}`)
+}
+
+// sliderWidget
