f770e0e3c313cdb3b22268492ba67044dee510c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 06:05:18 +0000

    Add package pyerualjetwork/1.3.0/pyerualjetwork-1.3.0.tar.gz

---
+++ packages/pyerualjetwork/pyerualjetwork-1.3.0.tar.gz/pyerualjetwork-1.3.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "pyerualjetwork",
+      version = "1.3.0",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "Advanced python deep learning library. New Visualize parameter added ('y' or 'n') for TrainPLAN and TestPLAN funcs. (Documentation in desc. Examples in GİTHUB: https://github.com/HCB06/PyerualJetwork)",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'pruning learning artficial neural networks'],
+
+      
+      )
