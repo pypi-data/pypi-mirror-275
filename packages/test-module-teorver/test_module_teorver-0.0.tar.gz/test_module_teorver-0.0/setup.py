387848c7f9dfeaaf6bb8b3c29ccdefb2a2365753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 06:05:49 +0000

    Add package test-module-teorver/0.0/test_module_teorver-0.0.tar.gz

---
+++ packages/test-module-teorver/test_module_teorver-0.0.tar.gz/test_module_teorver-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='test_module_teorver',
+      version='0.0',
+      description='test_module_teorver',
+      packages=['test_module_teorver'],
+      author_email='applepro285@gmail.com',
+      zip_safe=False)
---
+++ packages/test-module-teorver/test_module_teorver-0.0.tar.gz/test_module_teorver-0.0/test_module_teorver/test_module_teorver.py
@@ -0,0 +1,7 @@
+def test():
+    """пися попа тест тут ваш код
+
+    """
+    print(test)
+    kakashka = 5
+    return kakashka
