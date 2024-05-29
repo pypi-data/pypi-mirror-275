ab2e92c472872a250fe588eb91dde0ffa6c73353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 03:05:57 +0000

    Add package t2ebm/0.1.0/t2ebm-0.1.0-py3-none-any.whl

---
+++ packages/t2ebm/t2ebm-0.1.0-py3-none-any.whl/t2ebm/__init__.py
@@ -0,0 +1,12 @@
+"""
+TalkToEBM: A Natural Language Interface to Explainable Boosting Machines
+"""
+
+__version__ = "0.1.0"
+
+# high-level functions
+from .functions import (
+    feature_importances_to_text,
+    describe_graph,
+    describe_ebm,
+)
