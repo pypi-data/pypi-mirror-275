ababc5e1a04d3cea675b6e497d42ca11cc038560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 04:05:24 +0000

    Add package quickQrLib/0.2.28/quickQrLib-0.2.28.tar.gz

---
+++ packages/quickQrLib/quickQrLib-0.2.28.tar.gz/quickQrLib-0.2.28/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name='quickQrLib',
+    version='0.2.28',
+    packages=find_packages(),
+    install_requires = [
+        'djangorestframework-simplejwt',
+        'django',
+        'djangorestframework',
+        'cryptography',
+        'boto3',
+        'python-dateutil',
+        'pytz',
+    ],
+    include_package_data=True, 
+)
