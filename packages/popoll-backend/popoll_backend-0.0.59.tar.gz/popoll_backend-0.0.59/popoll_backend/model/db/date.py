8f40ccd2c7e5a6a59492b345fa22b50aed3ffeed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 03:06:10 +0000

    Add package popoll-backend/0.0.59/popoll_backend-0.0.59.tar.gz

---
+++ packages/popoll-backend/popoll_backend-0.0.59.tar.gz/popoll_backend-0.0.59/popoll_backend/model/db/date.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+import datetime
+import sqlite3
+from typing import Any, List, Optional
+
+from popoll_backend.model import Payload
+from popoll_backend.model.db import fetchall, fetchone
+
+
+class Date(Payload):
+    
+    def __init__(self, data: List[Any]):
+        self.id: int = data[0]
+        self.title: str = data[1]
+        self.date: datetime.date = datetime.date.fromisoformat(data[2])
+        self.time: Optional[datetime.time] = datetime.time.fromisoformat(data[3]) if data[3] else None
+        self.end_time: Optional[datetime.time] = datetime.time.fromisoformat(data[4]) if data[4] else None
+        
+    @classmethod
+    def create_table(cls):
+        return """ CREATE TABLE IF NOT EXISTS dates (
+            id integer PRIMARY KEY AUTOINCREMENT,
+            title text NOT NULL,
+            date text NOT NULL,
+            time text,
+            end_time text
+        ); """
