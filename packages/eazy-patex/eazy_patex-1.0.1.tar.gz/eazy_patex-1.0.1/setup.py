106ed036cc5ca07d3963697d2fa57afa806df79a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 04:04:40 +0000

    Add package eazy-patex/1.0.1/eazy_patex-1.0.1.tar.gz

---
+++ packages/eazy-patex/eazy_patex-1.0.1.tar.gz/eazy_patex-1.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from glob import glob
+
+setup(
+    name="eazy_patex",
+    version="1.0.1",
+    author="Oleg Zagorulko",
+    author_email="zagorulkooa@gmail.com",
+    description="test desc",
+    license="MIT",
+    url="https://github.com/brtbrr/python-course",
+    packages=find_packages(),
+    install_requires=[],
+    keywords="key word",
+)
