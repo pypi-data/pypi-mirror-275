f66fc9d7a3cb76fc62e2a695ce72f450698c7738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 06:05:37 +0000

    Add package AdnGAN/0.1.2/AdnGAN-0.1.2.tar.gz

---
+++ packages/AdnGAN/AdnGAN-0.1.2.tar.gz/AdnGAN-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AdnGAN',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'torchvision',
+        'numpy',
+        'matplotlib',
+    ],
+    author='Adnane MAJDOUB',
+    author_email = 'adnanemajdoub@gmail.com',
+    description='AdnGAN is a Generative Adversarial Network (GAN) that generates images from random noise.',
+    long_description=open("README.md").read(),
+    url = "https://github.com/AdnaneMaj/AdnGAN/tree/main/AdnGAN",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
