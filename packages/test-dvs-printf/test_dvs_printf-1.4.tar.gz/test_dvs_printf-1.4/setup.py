55b07453fa8c6978416f0bc6875f09a07e00a54e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 03:06:42 +0000

    Add package test-dvs-printf/1.4/test_dvs_printf-1.4.tar.gz

---
+++ packages/test-dvs-printf/test_dvs_printf-1.4.tar.gz/test_dvs_printf-1.4/setup.py
@@ -0,0 +1,28 @@
+import pathlib, setuptools
+
+setuptools.setup(
+    name="test_dvs_printf",
+    version="1.4",
+    description=
+"Animated Visual appearance for console-based applications, with different animation styles",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/dhruvan-vyas/dvs_printf",
+    author="dhruvan_vyas",
+    license="MIT License",
+    project_urls={
+        "Documentation":"https://github.com/dhruvan-vyas/dvs_printf/blob/main/README.md",
+        "hii":"https://pypi.org/project/dvs-printf",
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Utilities",
+        "Environment :: Console"],
+    python_requires=">=3.10",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+)
