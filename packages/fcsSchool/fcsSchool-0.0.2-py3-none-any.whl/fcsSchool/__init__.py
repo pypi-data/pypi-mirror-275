c7c16d686696d8cd9d9af251a95fd2aba237b4f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 04:05:02 +0000

    Add package fcsSchool/0.0.2/fcsSchool-0.0.2-py3-none-any.whl

---
+++ packages/fcsSchool/fcsSchool-0.0.2-py3-none-any.whl/fcsSchool/__init__.py
@@ -0,0 +1,15 @@
+def  schoolName():
+     return f"Future Campus School" 
+
+def  principalName(): 
+     return f"Partha Pratim Panja" 
+
+def rectorName(): 
+      return f"Arindam Banerjee" 
+
+def computerTeachers(): 
+      return f"Ananta kr Ghosh,DP sir,Priyanka Madam"
+
+def coordinators(): 
+      return f"Debapriya madam,aparna madam,Jyotirmoy sir"      
+      
