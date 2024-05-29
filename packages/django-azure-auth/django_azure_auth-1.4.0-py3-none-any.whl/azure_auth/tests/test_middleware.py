e04e595751f5df5645c0ed5492bf3e993790cdf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 05:04:36 +0000

    Add package django-azure-auth/1.4.0/django_azure_auth-1.4.0-py3-none-any.whl

---
+++ packages/django-azure-auth/django_azure_auth-1.4.0-py3-none-any.whl/azure_auth/tests/test_middleware.py
@@ -0,0 +1,45 @@
+from http import HTTPStatus
+from unittest.mock import patch
+
+import msal
+import pytest
+from django.test import TransactionTestCase
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+@pytest.mark.usefixtures("token")
+@patch.object(msal, "ConfidentialClientApplication")
+class TestAzureAuthMiddleware(TransactionTestCase):
+    def test_invalid_token(self, mocked_msal_app):
+        mocked_msal_app.return_value.acquire_token_silent.return_value = None
+        resp = self.client.get(reverse("middleware_protected"))
+        assert resp.status_code == HTTPStatus.FOUND
+        assert resp.url == "/azure_auth/login"  # type: ignore
+        assert self.client.session.get("next") == "/middleware_protected/"
+
+    def test_valid_token_unauthenticated_user(self, mocked_msal_app):
+        # Not sure how this situation could arise but test anyway...
+
+        mocked_msal_app.return_value.acquire_token_silent.return_value = self.token  # type: ignore
+        resp = self.client.get(reverse("middleware_protected"))
+        assert resp.status_code == HTTPStatus.FOUND
+        assert resp.url == "/azure_auth/login"  # type: ignore
+        assert self.client.session.get("next") == "/middleware_protected/"
+
+    def test_valid_token_authenticated_user(self, mocked_msal_app):
+        mocked_msal_app.return_value.acquire_token_silent.return_value = self.token  # type: ignore
+        self.client.force_login(self.user)  # type: ignore
+
+        resp = self.client.get(reverse("middleware_protected"))
+        assert resp.status_code == HTTPStatus.OK
+
+    def test_public_view(self, mocked_msal_app):
+        mocked_msal_app.return_value.acquire_token_silent.return_value = None
+        resp = self.client.get(reverse("public"))
+        assert resp.status_code == HTTPStatus.OK
+
+    def test_public_external_view(self, mocked_msal_app):
+        mocked_msal_app.return_value.acquire_token_silent.return_value = None
+        resp = self.client.get(reverse("public_external"))
+        assert resp.status_code == HTTPStatus.OK
