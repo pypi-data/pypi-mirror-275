97da94767ed1ead0d254d50ad109707c0f853cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 03:05:56 +0000

    Add package aes-si/0.2/aes_si-0.2.tar.gz

---
+++ packages/aes-si/aes_si-0.2.tar.gz/aes_si-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aes_si',
+    version='0.2',
+    description='A personalised Python library for encrypt messages with aes.',
+    author='Pasa Larisa',
+    packages=find_packages(),
+)
