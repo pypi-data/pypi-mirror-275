044164fba5057d6fe5f8775b94188a9974f6e004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 03:06:43 +0000

    Add package sigcalc/0.1.0/sigcalc-0.1.0-py3-none-any.whl

---
+++ packages/sigcalc/sigcalc-0.1.0-py3-none-any.whl/sigcalc/__init__.py
@@ -0,0 +1,18 @@
+# ******************************************************************************
+#
+# sigcalc, significant figures calculations
+#
+# Copyright 2023-2024 Jeremy A Gray <gray@flyquackswim.com>.
+#
+# All rights reserved.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# ******************************************************************************
+
+"""sigcalc module initialization."""
+
+from .constants import e
+from .constants import pi
+from .quantity import Quantity
+from .quantity import _most_significant_place
